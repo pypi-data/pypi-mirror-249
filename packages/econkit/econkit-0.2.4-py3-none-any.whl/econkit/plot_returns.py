8c688e4320b42f4bacfc31fca033d52f1d2a2218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:02:55 +0000

    Add package econkit/0.2.4/econkit-0.2.4-py3-none-any.whl

---
+++ packages/econkit/econkit-0.2.4-py3-none-any.whl/econkit/plot_returns.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_returns(portfolio_returns):
+    """
+    Plot the mean against the standard deviation of portfolio returns.
+
+    :param portfolio_returns: DataFrame containing the returns for each portfolio.
+    """
+    y_axis = portfolio_returns.mean()
+    x_axis = portfolio_returns.std()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(x_axis, y_axis)
+    plt.xlabel('Volatility')
+    plt.ylabel('Excepted Returns')
+
+    # Annotate each point with its portfolio name
+    for i, txt in enumerate(portfolio_returns.columns):
+        plt.annotate(txt, (x_axis[i], y_axis[i]))
+
+    plt.grid(True)
+    plt.show()
+
