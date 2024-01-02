8b701c38483dbb0a58b81fb362f5ebb84fbe6ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:32 +0000

    Add package SpecialPurpose/1/SpecialPurpose-1.tar.gz

---
+++ packages/SpecialPurpose/SpecialPurpose-1.tar.gz/SpecialPurpose-1/README.md
@@ -0,0 +1,4 @@
+This package is designed to support MonieCrypt (Cryptography tool).
+
+Cyptography Class / JU
+2023-2024
---
+++ packages/SpecialPurpose/SpecialPurpose-1.tar.gz/SpecialPurpose-1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SpecialPurpose",
+    version="1",
+    author="Sara , Sm6r",
+    long_description="A special package for MonieCrypt (Crypto tool) designed for a Cryptography assignment /JU",
+    install_requires=[
+   "pyfiglet",
+   "Crypto",
+   "matplotlib"]
+    )
+
