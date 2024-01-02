98ddbbae2f48251e51a80d329745126a1d7cc004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:35 +0000

    Add package hezar/0.35.1/hezar-0.35.1-py2.py3-none-any.whl

---
+++ packages/hezar/hezar-0.35.1-py2.py3-none-any.whl/hezar/data/datasets/__init__.py
@@ -0,0 +1,7 @@
+from ...registry import register_dataset  # noqa
+from .dataset import Dataset, DatasetConfig  # noqa
+from .image_captioning_dataset import ImageCaptioningDataset, ImageCaptioningDatasetConfig
+from .ocr_dataset import OCRDataset, OCRDatasetConfig
+from .sequence_labeling_dataset import SequenceLabelingDataset, SequenceLabelingDatasetConfig
+from .text_classification_dataset import TextClassificationDataset, TextClassificationDatasetConfig
+from .text_summarization_dataset import TextSummarizationDataset, TextSummarizationDatasetConfig
