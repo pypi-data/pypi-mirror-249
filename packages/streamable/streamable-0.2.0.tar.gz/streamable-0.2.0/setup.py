7b21ff4ce64ef4648c2d822b11ef32a34d1ee954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:55 +0000

    Add package streamable/0.2.0/streamable-0.2.0.tar.gz

---
+++ packages/streamable/streamable-0.2.0.tar.gz/streamable-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name='streamable',
+    version='0.2.0',
+    packages=find_packages(),
+    package_data={"streamable": ["py.typed"]},
+    url='http://github.com/bonnal-enzo/streamable',
+    license='Apache 2.',
+    author='bonnal-enzo',
+    author_email='bonnal.enzo.dev@gmail.com',
+    description='Ease the manipulation of iterables.'
+)
