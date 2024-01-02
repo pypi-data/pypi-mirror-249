b85f2d1ca8a9200115c3f89e5211ae852e360cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:17 +0000

    Add package korbyte-api-client/0.0.1/korbyte_api_client-0.0.1-py3-none-any.whl

---
+++ packages/korbyte-api-client/korbyte_api_client-0.0.1-py3-none-any.whl/korbyte-api-client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from korbyte-api-client.api.daily_api import DailyApi
+from korbyte-api-client.api.details_api import DetailsApi
+from korbyte-api-client.api.exchanges_api import ExchangesApi
+from korbyte-api-client.api.prepare_api import PrepareApi
+from korbyte-api-client.api.tickers_api import TickersApi
+from korbyte-api-client.api.tracking_api import TrackingApi
+
