d438b5943df80544e8b8e0fb7a87457fa7d813f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:05:08 +0000

    Add package solders/0.19.0/solders-0.19.0.tar.gz

---
+++ packages/solders/solders-0.19.0.tar.gz/solders-0.19.0/src/address_lookup_table_account.rs
@@ -0,0 +1,8 @@
+use pyo3::prelude::*;
+use solders_address_lookup_table_account::AddressLookupTableAccount;
+
+pub(crate) fn create_address_lookup_table_account_mod(py: Python<'_>) -> PyResult<&PyModule> {
+    let m = PyModule::new(py, "address_lookup_table_account")?;
+    m.add_class::<AddressLookupTableAccount>()?;
+    Ok(m)
+}
