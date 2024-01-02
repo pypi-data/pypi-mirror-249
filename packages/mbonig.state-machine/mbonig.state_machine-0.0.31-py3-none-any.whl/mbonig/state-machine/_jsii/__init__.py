52e552704e2e001894ca1060db7b51946f3fd11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:20 +0000

    Add package mbonig.state-machine/0.0.31/mbonig.state_machine-0.0.31-py3-none-any.whl

---
+++ packages/mbonig.state-machine/mbonig.state_machine-0.0.31-py3-none-any.whl/mbonig/state-machine/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@matthewbonig/state-machine",
+    "0.0.31",
+    __name__[0:-6],
+    "state-machine@0.0.31.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
