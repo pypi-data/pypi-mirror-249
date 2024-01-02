a34fed9d9802bd659f0ffa108cb4e1f32b745a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:04:55 +0000

    Add package waypaper/2.0.4/waypaper-2.0.4.tar.gz

---
+++ packages/waypaper/waypaper-2.0.4.tar.gz/waypaper-2.0.4/waypaper/__main__.py
@@ -0,0 +1,71 @@
+"""Main module that runs the program and either runs GUI or just changer wallpaper"""
+
+import time
+import argparse
+
+
+from waypaper.config import Config
+from waypaper.app import App
+from waypaper.changer import change_wallpaper
+from waypaper.common import get_random_file
+from waypaper.aboutdata import AboutData
+from waypaper.options import FILL_OPTIONS, BACKEND_OPTIONS
+from waypaper.translations import English, German, French, Russian, Polish, Chinese
+
+about = AboutData()
+cf = Config()
+
+if cf.lang == "de":
+    txt = German()
+elif cf.lang == "fr":
+    txt = French()
+elif cf.lang == "ru":
+    txt = Russian()
+elif cf.lang == "pl":
+    txt = Polish()
+elif cf.lang == "zh":
+    txt = Chinese()
+else:
+    txt = English()
+
+
+parser = argparse.ArgumentParser(prog = about.applicationName(), description = txt.msg_desc,
+                                 epilog = txt.msg_info)
+parser.add_argument("-v", "--version", help=txt.msg_arg_help, action="store_true")
+parser.add_argument("--restore", help=txt.msg_arg_rest, action="store_true")
+parser.add_argument("--random", help=txt.msg_arg_rand, action="store_true")
+parser.add_argument("--fill", help=txt.msg_arg_fill, action="store_true")
+parser.add_argument("--backend", help=txt.msg_arg_back, choices=BACKEND_OPTIONS)
+args = parser.parse_args()
+
+
+def run():
+    """Read user arguments and either run GUI app or just reset the wallpaper"""
+
+    cf.read_parameters_from_user_arguments(args)
+
+    # Set the wallpaper and quit:
+    if args.restore:
+        for wallpaper, monitor in zip(cf.wallpaper, cf.monitors):
+
+            if args.random:
+                wallpaper = get_random_file(cf.image_folder, cf.include_subfolders)
+
+            if wallpaper is None:
+                continue
+            change_wallpaper(wallpaper, cf, monitor, txt)
+            time.sleep(0.1)
+        exit(0)
+
+    # Print the version and quit:
+    if args.version:
+        print(f"{about.applicationName()} v.{about.applicationVersion()}")
+        exit(0)
+
+    # Start GUI:
+    app = App(txt)
+    app.run()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/waypaper/waypaper-2.0.4.tar.gz/waypaper-2.0.4/waypaper/changer.py
@@ -0,0 +1,95 @@
+"""Module that runs the system processes to change the wallpaper"""
+
+import subprocess
+import time
+
+from waypaper.options import BACKEND_OPTIONS
+
+
+def change_wallpaper(image_path, cf, monitor, txt):
+    """Run system commands to change the wallpaper depending on the backend"""
+
+    fill_option = cf.fill_option
+    color = cf.color
+    backend = cf.backend
+    swww_transition = cf.swww_transition
+    installed_backends = cf.installed_backends
+
+    try:
+        # swaybg backend:
+        if backend == "swaybg":
+            fill = fill_option.lower()
+            try:
+                subprocess.Popen(["killall", "swaybg"])
+                time.sleep(0.005)
+            except Exception as e:
+                print(f"{ERR_KILL} {e}")
+            command = ["swaybg"]
+            # if monitor != "All":
+                # command.extend(["-o", monitor])
+            command.extend(["-i", image_path])
+            command.extend(["-m", fill, "-c", color])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # swww backend:
+        elif backend == "swww":
+            fill_types = {
+                    "fill": "crop",
+                    "fit": "fit",
+                    "center": "no",
+                    "stretch": "crop",
+                    "tile": "no",
+                    }
+            fill = fill_types[fill_option.lower()]
+            if "swaybg" in installed_backends:
+                try:
+                    subprocess.Popen(["killall", "swaybg"])
+                    time.sleep(0.005)
+                except Exception as e:
+                    print(f"{ERR_KILL} {e}")
+            subprocess.Popen(["swww", "init"])
+            command = ["swww", "img", image_path]
+            command.extend(["--resize", fill])
+            command.extend(["--fill-color", color])
+            command.extend(["--transition-type", swww_transition])
+            # command.extend(["--transition-step", str(30)])
+            if monitor != "All":
+                command.extend(["--outputs", monitor])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # feh backend:
+        elif backend == "feh":
+            fill_types = {
+                    "fill": "--bg-fill",
+                    "fit": "--bg-max",
+                    "center": "--bg-center",
+                    "stretch": "--bg-scale",
+                    "tile": "--bg-tile",
+                    }
+            fill = fill_types[fill_option.lower()]
+            command = ["feh", fill, "--image-bg", color]
+            command.extend([image_path])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # wallutils backend:
+        elif backend == "wallutils":
+            fill_types = {
+                    "fill": "scale",
+                    "fit": "scale",
+                    "center": "center",
+                    "stretch": "stretch",
+                    "tile": "tile",
+                    }
+            fill = fill_types[fill_option.lower()]
+
+            subprocess.Popen(["setwallpaper", "--mode", fill, image_path])
+            print(f"{txt.msg_setwith} {backend}")
+
+        else:
+            print(f"{txt.err_notsup} {backend}")
+
+    except Exception as e:
+        print(f"{txt.err_wall} {e}")
