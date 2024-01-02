d1d457e1ebea1b9f7b8ad2da925ab409d96cc2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:02:56 +0000

    Add package nomad-media-pip/0.0.17/nomad_media_pip-0.0.17.tar.gz

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.17.tar.gz/nomad_media_pip-0.0.17/nomad_media_pip/admin/schedule/update_schedule_items/update_schedule_item_asset.py
@@ -0,0 +1,37 @@
+from nomad_media_pip.exceptions.api_exception_handler import _api_exception_handler
+from nomad_media_pip.admin.schedule.get_schedule_item import _get_schedule_item
+
+import requests, json
+
+def _update_schedule_item_asset(AUTH_TOKEN, URL, ID, ITEM_ID, ASSET, DAYS, DURATION_TIME_CODE, 
+                                END_TIME_CODE, TIME_CODE, DEBUG):
+
+    SCHEDULE_ITEM = _get_schedule_item(AUTH_TOKEN, URL, ID, ITEM_ID, DEBUG)
+    
+    API_URL = f"{URL}/admin/schedule/{ID}/item/{ITEM_ID}"
+    
+    HEADERS = {
+        "Authorization": "Bearer " + AUTH_TOKEN,
+        "Content-Type": "application/json"
+    }
+    
+    BODY = {
+        "asset": ASSET or SCHEDULE_ITEM["asset"],
+        "days": DAYS or SCHEDULE_ITEM["days"],
+        "durationTimeCode": DURATION_TIME_CODE or SCHEDULE_ITEM["durationTimeCode"],
+        "endTimeCode": END_TIME_CODE or SCHEDULE_ITEM["endTimeCode"],
+        "scheduleItemType": "1",
+        "sourceType": "3",
+        "timeCode": TIME_CODE or SCHEDULE_ITEM["timeCode"]
+    }
+    
+    if DEBUG:
+        print(f"URL: {API_URL},\nMETHOD: PUT,\nBODY: {json.dumps(BODY, indent=4)}")
+    
+    try:
+        RESPONSE = requests.put(API_URL, headers= HEADERS, data= json.dumps(BODY))
+        
+        if not RESPONSE.ok:
+            raise Exception()
+    except:
+        _api_exception_handler(RESPONSE, "Update Schedule Item Asset Failed")
