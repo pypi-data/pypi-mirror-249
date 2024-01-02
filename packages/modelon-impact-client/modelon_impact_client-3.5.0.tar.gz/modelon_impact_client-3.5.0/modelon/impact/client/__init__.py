036989bf146d0d483bd1e9686a26b11071a393c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:42 +0000

    Add package modelon-impact-client/3.5.0/modelon_impact_client-3.5.0.tar.gz

---
+++ packages/modelon-impact-client/modelon_impact_client-3.5.0.tar.gz/modelon_impact_client-3.5.0/modelon/impact/client/__init__.py
@@ -0,0 +1,29 @@
+from modelon.impact.client.client import Client
+from modelon.impact.client.entities.project import ContentType
+from modelon.impact.client.entities.workspace import WorkspaceDefinition
+from modelon.impact.client.experiment_definition.expansion import (
+    FullFactorial,
+    LatinHypercube,
+    Sobol,
+)
+from modelon.impact.client.experiment_definition.extension import (
+    SimpleExperimentExtension,
+)
+from modelon.impact.client.experiment_definition.fmu_based import (
+    SimpleFMUExperimentDefinition,
+)
+from modelon.impact.client.experiment_definition.model_based import (
+    SimpleModelicaExperimentDefinition,
+)
+from modelon.impact.client.experiment_definition.operators import (
+    Beta,
+    Choices,
+    Normal,
+    Range,
+    Uniform,
+)
+from modelon.impact.client.published_workspace_client import (
+    PublishedWorkspaceAccessKind,
+)
+
+SimpleExperimentDefinition = SimpleFMUExperimentDefinition  # For compatibility
