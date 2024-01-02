a77937232d5eec3dd7b19b32323823df9bf73890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:08 +0000

    Add package zrb-ollama/0.0.14/zrb_ollama-0.0.14.tar.gz

---
+++ packages/zrb-ollama/zrb_ollama-0.0.14.tar.gz/zrb_ollama-0.0.14/_cmd/prepare-playground.sh
@@ -0,0 +1,10 @@
+rm -Rf playground
+cp -r playground-template playground
+
+cd playground
+deactivate 2>/dev/null
+
+python -m venv .venv
+source .venv/bin/activate
+pip install --use-feature=in-tree-build "$(pwd)/.."
+pip install -r requirements.txt
