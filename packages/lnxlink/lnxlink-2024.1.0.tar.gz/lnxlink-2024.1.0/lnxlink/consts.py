622d82542bcac7a40700a63cc5873fe4bc597de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:04:49 +0000

    Add package lnxlink/2024.1.0/lnxlink-2024.1.0.tar.gz

---
+++ packages/lnxlink/lnxlink-2024.1.0.tar.gz/lnxlink-2024.1.0/lnxlink/consts.py
@@ -0,0 +1,56 @@
+"""Constant variables to use"""
+
+SERVICEHEADLESS = """[Unit]
+Description=LNXlink
+After=network-online.target
+
+[Service]
+Type=simple
+Restart=always
+User=root
+
+ExecStart={exec_cmd}
+
+[Install]
+WantedBy=default.target
+"""
+
+SERVICEUSER = """[Unit]
+Description=LNXlink
+After=network-online.target multi-user.target graphical.target
+PartOf=graphical-session.target
+
+[Service]
+Type=simple
+Restart=always
+
+ExecStart={exec_cmd}
+
+[Install]
+WantedBy=default.target
+"""
+
+CONFIGTEMP = """
+mqtt:
+  prefix: 'lnxlink'
+  clientId: 'DESKTOP-Linux'
+  server: '192.168.1.1'
+  port: 1883
+  auth:
+    user: 'user'
+    pass: 'pass'
+    tls: false
+  discovery:
+    enabled: true
+  lwt:
+    enabled: true
+    qos: 1
+    retain: false
+    connectMsg: 'ON'
+    disconnectMsg: 'OFF'
+update_interval: 5
+modules:
+custom_modules:
+exclude:
+settings:
+"""
---
+++ packages/lnxlink/lnxlink-2024.1.0.tar.gz/lnxlink-2024.1.0/lnxlink/system_monitor.py
@@ -0,0 +1,59 @@
+"""Monitors for shutdown/sleep events"""
+
+import threading
+import signal
+import logging
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+logger = logging.getLogger("lnxlink")
+
+
+class MonitorSuspend:
+    """Monitor DBUS for Suspend and Shutdown events"""
+
+    def __init__(self, callback):
+        dbus_loop = DBusGMainLoop(set_as_default=True)  # integrate into main loob
+        bus = dbus.SystemBus(mainloop=dbus_loop)  # connect to dbus system wide
+        bus.add_signal_receiver(  # defince the signal to listen to
+            callback,  # name of callback function
+            "PrepareForSleep",  # singal name
+            "org.freedesktop.login1.Manager",  # interface
+            "org.freedesktop.login1",  # bus name
+        )
+        bus.add_signal_receiver(  # defince the signal to listen to
+            callback,  # name of callback function
+            "PrepareForShutdown",  # singal name
+            "org.freedesktop.login1.Manager",  # interface
+            "org.freedesktop.login1",  # bus name
+        )
+        self.loop = GLib.MainLoop()
+        self.timer1 = threading.Thread(target=self.loop.run)
+
+    def start(self):
+        """Start the timer of the thread"""
+        self.timer1.start()
+
+    def stop(self):
+        """Stop the timer of the thread"""
+        self.loop.quit()
+        self.timer1.join()
+
+
+class GracefulKiller:
+    """Monitor Signal Termination"""
+
+    def __init__(self, callback):
+        """Checks for termination signals"""
+        self.kill_now = False
+        self.callback = callback
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        """Stop the app gracefully"""
+        logger.info("stopped gracefully")
+        self.callback(1)
+        self.kill_now = True
