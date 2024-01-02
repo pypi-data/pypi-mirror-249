92b43a5c1f36e0d28414a2dfb662f3b78ffee974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:23 +0000

    Add package problem-bank-helpers/0.2.6/problem_bank_helpers-0.2.6-py3-none-any.whl

---
+++ packages/problem-bank-helpers/problem_bank_helpers-0.2.6-py3-none-any.whl/problem_bank_helpers/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.2.6"
+
+from .problem_bank_helpers import *
+from . import stats # Keep stats as a separate namespace, but also make it accessible from the top level without an explicit import
