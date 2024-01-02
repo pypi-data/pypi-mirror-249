4982e1bc47b066a439f12c6c0a01682bd601ec82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:04:53 +0000

    Add package qgate-sln-mlrun/0.1.3/qgate_sln_mlrun-0.1.3-py3-none-any.whl

---
+++ packages/qgate-sln-mlrun/qgate_sln_mlrun-0.1.3-py3-none-any.whl/qgate_sln_mlrun/setup.py
@@ -0,0 +1,59 @@
+
+import mlrun
+import os
+
+class Setup:
+    """
+    Setup for solution
+    """
+
+    def __init__(self, data_size, mlrun_env_file: list[str]):
+        # set variables based on environment files
+        for env_file in mlrun_env_file:
+            if os.path.isfile(env_file):
+                self._variables=mlrun.set_env_from_file(env_file, return_dict=True)
+                break
+
+        self._variables["DIR"]=os.getcwd()
+
+        # set model dirs
+        #self._model_definition=self._variables['QGATE_DEFINITION']
+        #self._model_output=self._variables['QGATE_OUTPUT']
+
+        # set data set size
+        self._data_size=data_size
+
+    def __str__(self):
+        ret=""
+        for key in self._variables.keys():
+            ret+=key+ ": "+ "'" + self._variables[key] + "'\n"
+        return ret[:-1]
+
+    @property
+    def variables(self):
+        variable_list=[]
+        for key in self._variables.keys():
+            itm = {}
+            itm['key']=key
+            itm['value']=self._variables[key]
+            variable_list.append(itm)
+        return variable_list
+
+    @property
+    def model_output(self):
+        return self._variables['QGATE_OUTPUT']
+        #return self._model_output
+
+    @property
+    def model_definition(self):
+        return self._variables['QGATE_DEFINITION']
+        #return self._model_definition
+
+    @property
+    def data_size(self):
+        return self._data_size
+
+    @property
+    def redis(self):
+        return self._variables.get('QGATE_REDIS', None)
+        #return self._variables['QGATE_REDIS']
---
+++ packages/qgate-sln-mlrun/qgate_sln_mlrun-0.1.3-py3-none-any.whl/qgate_sln_mlrun/ts/ts502.py
@@ -0,0 +1,52 @@
+"""
+  TS501: Get data from on-line feature vector(s)
+"""
+
+from qgate_sln_mlrun.ts.tsbase import TSBase
+import mlrun
+import mlrun.feature_store as fstore
+
+
+class TS502(TSBase):
+
+    def __init__(self, solution):
+        super().__init__(solution, self.__class__.__name__)
+
+    @property
+    def desc(self) -> str:
+        return "Get data from on-line feature vector(s)"
+
+    @property
+    def long_desc(self):
+        return "Get data from on-line feature vector"
+
+    def exec(self):
+        self.get_data_online()
+
+    def get_data_online(self):
+        """
+        Get data from on-line feature vector
+        """
+        self.testscenario_new()
+
+        vectors=None
+        if self.test_setting.get('vector'):
+            if self.test_setting['vector'].get('online'):
+                vectors=self.test_setting['vector']['online']
+
+        if vectors:
+            for project_name in self.projects:
+                for featurevector_name in self.get_featurevectors(self.project_specs.get(project_name)):
+                    if featurevector_name in vectors:
+                        self._get_data_online(f"{project_name}/{featurevector_name}", project_name, featurevector_name)
+
+    @TSBase.handler_testcase
+    def _get_data_online(self, testcase_name, project_name, featurevector_name):
+        self.project_switch(project_name)
+        vector = fstore.get_feature_vector(f"{project_name}/{featurevector_name}")
+
+        with fstore.get_online_feature_service(vector) as svc:
+            # TODO add valid party-id from data
+            entities = [{"party-id": "d68fe603-7cb1-44e4-9013-7330a050a6be"}]
+            resp=svc.get(entities, as_list=True)
+
