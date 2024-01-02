317d834b83759a11dc6a844ee00bb2cd16ca53eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:34 +0000

    Add package ussy/1.0.2/ussy-1.0.2.tar.gz

---
+++ packages/ussy/ussy-1.0.2.tar.gz/ussy-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='ussy',
+      version='1.0.2',
+      description='Funny little ussifier.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ellipse-liu/ussy',
+      author='Timothy-Liu',
+      author_email='timothys.new.email@gmail.com',
+      license='MIT',
+      packages=['ussy'],
+      package_data={'eng_syl': ['*.pkl', '*.h5']},
+      install_requires=[
+          'eng-syl'
+      ],
+      keywords=['Ussy'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8',
+      ],
+      zip_safe=False)
