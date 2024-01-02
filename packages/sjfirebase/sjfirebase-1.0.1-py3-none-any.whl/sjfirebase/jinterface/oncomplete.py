edede719240f4758a9810af0c38afdf73faeb024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:23 +0000

    Add package sjfirebase/1.0.1/sjfirebase-1.0.1-py3-none-any.whl

---
+++ packages/sjfirebase/sjfirebase-1.0.1-py3-none-any.whl/sjfirebase/jinterface/oncomplete.py
@@ -0,0 +1,16 @@
+from jnius import PythonJavaClass, java_method
+
+__all__ = ("OnCompleteListener", )
+
+
+class OnCompleteListener(PythonJavaClass):
+    __javainterfaces__ = ["com/google/android/gms/tasks/OnCompleteListener"]
+    __javacontext__ = "app"
+
+    def __init__(self, callback):
+        super().__init__()
+        self.callback = callback
+
+    @java_method('(Lcom/google/android/gms/tasks/Task;)V')
+    def onComplete(self, task):
+        self.callback(task)
