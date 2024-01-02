37e464dd60c6e92f7991045a2e39700cc96f1510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:43 +0000

    Add package agentk8s/0.1.6/agentk8s-0.1.6.tar.gz

---
+++ packages/agentk8s/agentk8s-0.1.6.tar.gz/agentk8s-0.1.6/agentk8s/main.py
@@ -0,0 +1,69 @@
+import json
+import os
+import typer
+from rich import print
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    print("[green]K-GPT Command Running[/green]")
+
+
+@app.command(name='local')
+def local_cluster(
+        cmd: Annotated[
+            str, typer.Argument(help="Enter \"start\" or \"stop\" for turn on/off of minikube local cluster")]):
+    """
+    Start a minikube cluster locally or stop minikube cluster locally
+    """
+    if cmd == 'start':
+        os.system('minikube start')
+    elif cmd == 'stop':
+        os.system('minikube stop')
+
+
+@app.command(name='chat')
+def chat(prompt: Annotated[str, typer.Argument(help="Enter a prompt like \"Show all deployments and services\"")]):
+    """
+    Start chat with the k8s agent, let agent do things for you
+    """
+    # Load LLM inference endpoints from an env variable or a file
+    # See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
+    # and
+    termination_notice = (
+        '\n\nDo not show appreciation in your responses, say only what is necessary. '
+        'if "Thank you" or "You\'re welcome" are said in the conversation, then say TERMINATE '
+        'to indicate the conversation is finished and this is your last message.'
+    )
+    config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+    assistant = AssistantAgent("K-GPT", llm_config={"temperature": 0, "seed": 41,
+                                                    "config_list": config_list},
+                               system_message='You are now an Kubernetes (k8s) expert.' 'You should use kubectl '
+                                              'command to complete task, you should output code blocks in shell '
+                                              'format, do not output yaml')
+
+    user_proxy = UserProxyAgent("User", max_consecutive_auto_reply=15, human_input_mode="TERMINATE",
+                                code_execution_config={"work_dir": "coding", "use_docker": False},
+                                llm_config={"temperature": 0, "seed": 41,
+                                            "config_list": config_list})
+    prompt += termination_notice
+    user_proxy.initiate_chat(assistant, message=prompt)
+
+
+@app.command(name='setup')
+def setup():
+    """
+    Run this command first after you install \"agentk8s\", it will prompt you to enter your GPT-4 API KEY
+    """
+    api_key = typer.prompt("Enter your GPT-4 API KEY")
+    config_list = [{
+        "model": "gpt-4-1106-preview",
+        "api_key": api_key
+    }]
+    config_path = "./OAI_CONFIG_LIST"
+    with open(str(config_path), 'w') as file:
+        json.dump(config_list, file)
