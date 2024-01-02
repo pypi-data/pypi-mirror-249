105d6d7fc5f43f0b1d06fa346ac1124ef0f0175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:02:55 +0000

    Add package torch-staintools/1.0.0/torch-staintools-1.0.0.tar.gz

---
+++ packages/torch-staintools/torch-staintools-1.0.0.tar.gz/torch-staintools-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+with open('requirements.txt') as root:
+    requirements = [str(req) for req in parse_requirements(root)]
+
+version_dict = {}
+with open("./torch_staintools/version.py") as fp:
+    exec(fp.read(), version_dict)
+version = version_dict["__version__"]
+
+setup(
+    name='torch-staintools',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/CielAl/torch-staintools',
+    license='MIT',
+    author='Y Z',
+    author_email='cielmercy@gmail.com',
+    description='GPU-accelerated stain normalization and augmentation',
+    install_requires=requirements,
+    python_requires='>=3.10',
+)
