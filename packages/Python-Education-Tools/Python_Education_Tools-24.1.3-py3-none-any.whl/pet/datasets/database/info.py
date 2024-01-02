91c9e23826fb5f34d093a4f50757a98785315e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:24 +0000

    Add package Python-Education-Tools/24.1.3/Python_Education_Tools-24.1.3-py3-none-any.whl

---
+++ packages/Python-Education-Tools/Python_Education_Tools-24.1.3-py3-none-any.whl/pet/datasets/database/info.py
@@ -0,0 +1,13 @@
+datafile_dict = {
+    'ip地址分类': 'ip_address.xlsx',
+    '研究生初试成绩': 'st.xlsx',
+    '上海地铁线路': 'subway.xlsx',
+    '北京公交车': 'beijing_bus.xlsx',
+    '通识课': '2022tsk.xlsx',
+    '优秀毕业论文': '2022pst.xlsx',
+    '太乙金华宗旨': 'tyjhzz.txt',
+    '微信接龙投票': 'votes.txt',
+    'cookie样例': 'sample_cookies.txt',
+    '道德经': 'ddj.txt',
+    '双色球': 'ssq_22134.xlsx'
+}
