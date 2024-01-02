89d722b59b29774bd6233cc800402f7a033af766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 15:03:17 +0000

    Add package pyttrading/0.4.5/pyttrading-0.4.5.tar.gz

---
+++ packages/pyttrading/pyttrading-0.4.5.tar.gz/pyttrading-0.4.5/pyttrading/strategies/dummy/strategy.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+class Strategy:
+
+    def __init__(self, df=None):
+        self.df = df
+
+    def eval(self, df=None):
+        signal_distribution = [0, 1, 2]
+        signal_probabilities = [0.7, 0.1, 0.2]
+        df['actions'] = np.random.choice(signal_distribution, size=len(df), p=signal_probabilities)
+        return df
+
+    def objective_function(self, df=None):
+        result_df = self.eval(df=df.copy())
+        return result_df['actions'].sum()
+
+    def optimize(self, df=None):
+        best_return = self.objective_function(df=df)
+        return best_return
+
+    def experiment(self, df=None):
+        best_return = self.optimize(df=df)
+        return best_return
