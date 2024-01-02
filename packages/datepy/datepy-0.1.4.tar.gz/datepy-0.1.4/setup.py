6b2b9aa4b7ec763f159c3a2031e5d7be4a801b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 14:03:18 +0000

    Add package datepy/0.1.4/datepy-0.1.4.tar.gz

---
+++ packages/datepy/datepy-0.1.4.tar.gz/datepy-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='datepy',
+    version='0.1.4',
+    description='A robust utility for parsing dates in various formats.',
+    author='msj121',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*      
