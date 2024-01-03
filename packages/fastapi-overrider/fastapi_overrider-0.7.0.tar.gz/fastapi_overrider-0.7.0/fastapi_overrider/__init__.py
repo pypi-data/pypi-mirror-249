ff8eb826bf6f7af1df48095e308ffd523725935b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:24 +0000

    Add package fastapi-overrider/0.7.0/fastapi_overrider-0.7.0.tar.gz

---
+++ packages/fastapi-overrider/fastapi_overrider-0.7.0.tar.gz/fastapi_overrider-0.7.0/fastapi_overrider/__init__.py
@@ -0,0 +1,209 @@
+import asyncio
+import inspect
+from collections import UserDict
+from collections.abc import Awaitable, Callable, Iterator
+from contextlib import suppress
+from functools import wraps
+from typing import Any, ParamSpec, Self, TypeVar, overload
+from unittest.mock import MagicMock, create_autospec, seal
+
+from fastapi import FastAPI
+from fastapi.dependencies.utils import is_coroutine_callable
+
+_T = TypeVar("_T")
+_P = ParamSpec("_P")
+
+_DepType = Callable[_P, _T]
+
+
+class Overrider(UserDict):
+    """Set dependency overrides and clean the up after using.
+    To be used as a pytest fixture."""
+
+    def __init__(
+        self,
+        app: FastAPI,
+    ) -> None:
+        self._app = app
+
+    @overload
+    def __call__(self, key: _DepType, override: _DepType) -> _DepType:
+        """Override a dependency with the given function.
+        Returns the function"""
+        ...
+
+    @overload
+    def __call__(self, key: _DepType, override: _T) -> _T:
+        """Override a dependeny with a function returning the given value.
+        Returns the value"""
+        ...
+
+    @overload
+    def __call__(self, key: _DepType) -> MagicMock:
+        """Override a dependnecy with a mock value.
+        Returns the mock value"""
+        ...
+
+    def __call__(self, *args, **kwargs) -> _DepType | MagicMock | object:
+        """Override a dependency either with a function, a value or a mock."""
+        match args:
+            case [key] if isinstance(key, Callable) and (
+                list(kwargs.keys()) == ["strict"] or len(kwargs) == 0
+            ):
+                return self.mock(key)
+            case [key, override] if isinstance(key, Callable) and isinstance(
+                override, Callable
+            ):
+                return self.function(key, override)
+            case [key, override] if isinstance(key, Callable):
+                return self.value(key, override)
+            case _:
+                raise NotImplementedError
+
+    def function(self, key: _DepType, override: _DepType) -> _DepType:
+        """Override a dependency with the given function.
+        Returns the function"""
+        self[key] = override
+        return override
+
+    def value(self, key: _DepType, override: _T) -> _T:
+        """Override a dependeny with a function returning the given value.
+        Returns the value"""
+
+        def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:  # noqa: ARG001
+            return override
+
+        self[key] = wraps(key)(wrapper)
+        return override
+
+    def mock(self, key: _DepType) -> MagicMock:
+        """Override a dependnecy with a mock value.
+        Returns a mock function that returns a mock value"""
+        value_name = f"mock value for {key.__name__}"
+        function_name = f"mock function for {key.__name__}"
+        return_type = inspect.get_annotations(key)["return"]
+        return_value = create_autospec(
+            return_type,
+            instance=True,
+            unsafe=False,
+            name=value_name,
+        )
+
+        mock_func = MagicMock(unsafe=False, name=function_name)
+        mock_func.__signature__ = inspect.signature(key)
+        mock_func.return_value = return_value
+        seal(mock_func)
+        self[key] = mock_func
+        return mock_func
+
+    def spy(self, key: _DepType) -> MagicMock:
+        """Replace a dependency with a spy wrapper.
+        Returns the spy"""
+        spy_name = f"Spy for {key.__name__}"
+
+        def wrapper(*args, **kwargs) -> Any:  # noqa: ANN002,ANN003,ANN401
+            if is_coroutine_callable(key):
+                return asyncio.run(key(*args, **kwargs))
+            return key(*args, **kwargs)
+
+        spy = MagicMock(wraps=key, unsafe=False, name=spy_name)
+        spy.__signature__ = inspect.signature(key)
+        spy.side_effect = wrapper
+        seal(spy)
+        self[key] = spy
+        return spy
+
+    with suppress(ModuleNotFoundError):
+        from polyfactory.exceptions import MissingDependencyException
+        from polyfactory.factories import (
+            BaseFactory,
+            DataclassFactory,
+            TypedDictFactory,
+        )
+
+        # A list of factories used to generate a value.
+        # These will be tried in order until a matching factory has been found
+        factories: list[type[BaseFactory]] = []
+
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.beanie_odm_factory import BeanieDocumentFactory
+
+            factories.append(BeanieDocumentFactory)
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.odmantic_odm_factory import OdmanticModelFactory
+
+            factories.append(OdmanticModelFactory)
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.msgspec_factory import MsgspecFactory
+
+            factories.append(MsgspecFactory)
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
+
+            factories.append(SQLAlchemyFactory)
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.pydantic_factory import ModelFactory
+
+            factories.append(ModelFactory)
+        factories.extend([DataclassFactory, TypedDictFactory])
+        with suppress(MissingDependencyException):
+            from polyfactory.factories.attrs_factory import AttrsFactory
+
+            factories.append(AttrsFactory)
+
+        def _get_factory(self, key: _DepType) -> BaseFactory:
+            return_type = inspect.get_annotations(key)["return"]
+            for factory in self.factories:
+                if factory.is_supported_type(return_type):
+                    return factory.create_factory(model=return_type)()
+            message = f"Did not find a factory for type {return_type}"
+            raise (ValueError(message))
+
+        def some(
+            self,
+            key: Callable[_P, _T] | Callable[_P, Awaitable[_T]],
+            **kwargs: Any,  # noqa: ANN401
+        ) -> _T:
+            """Override a dependency with a value generated by Polyfactory.
+            Additional keyword args are forwarded to the factory's build() method.
+            Returns the generated value"""
+            factory = self._get_factory(key)
+            override = factory.build(**kwargs)
+            return self.value(key, override)
+
+        def batch(
+            self,
+            key: Callable[_P, _T] | Callable[_P, Awaitable[_T]],
+            size: int,
+            **kwargs: Any,  # noqa: ANN401
+        ) -> Iterator[_T]:
+            """Batch override a dependency with a given number of values generated by
+            Polyfactory.
+            Additional keyword args are forwarded to the factory's build() method.
+            Returns an Iterator of generated values"""
+            factory = self._get_factory(key)
+            overrides = factory.batch(size, **kwargs)
+            for override in overrides:
+                yield self.value(key, override)
+
+        def cover(
+            self,
+            key: Callable[_P, _T] | Callable[_P, Awaitable[_T]],
+            **kwargs: Any,  # noqa: ANN401
+        ) -> Iterator[_T]:
+            """Override a dependency with values until full coverage has been reached.
+            Additional keyword args are forwarded to the factory's coverage() method.
+            Returns an Iterator of generated values"""
+            factory = self._get_factory(key)
+            overrides = factory.coverage(**kwargs)
+            for override in overrides:
+                yield self.value(key, override)
+
+    def __enter__(self: Self) -> Self:
+        self._restore_overrides = self._app.dependency_overrides
+        self._app.dependency_overrides = self._restore_overrides.copy()
+        self.data = self._app.dependency_overrides
+        return self
+
+    def __exit__(self, *_: object) -> None:
+        self._app.dependency_overrides = self._restore_overrides
