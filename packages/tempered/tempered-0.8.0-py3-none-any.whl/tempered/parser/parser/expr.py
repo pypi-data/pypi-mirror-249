ca391c42bc49d914982e176778e3330bd5dd2270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:13 +0000

    Add package tempered/0.8.0/tempered-0.8.0-py3-none-any.whl

---
+++ packages/tempered/tempered-0.8.0-py3-none-any.whl/tempered/parser/parser/expr.py
@@ -0,0 +1,57 @@
+from ... import ast_utils
+from ..template_ast import TemplateParameter
+import ast
+import keyword
+
+
+def parse_parameter(parameter: str) -> TemplateParameter:
+    expr = parse_stmt(parameter)
+
+    if isinstance(expr, ast.AnnAssign) and isinstance(expr.target, ast.Name):
+        # name: type | name: type = default
+        return TemplateParameter(
+            name=expr.target.id,
+            type=expr.annotation,
+            default=expr.value,
+        )
+    elif (
+        isinstance(expr, ast.Assign)
+        and isinstance(expr.targets, list)
+        and isinstance(expr.targets[0], ast.Name)
+    ):
+        # name = default
+        return TemplateParameter(
+            name=expr.targets[0].id,
+            default=expr.value,
+        )
+    elif isinstance(expr, ast.Expr) and isinstance(expr.value, ast.Name):
+        # name
+        return TemplateParameter(name=expr.value.id)
+    else:
+        raise ValueError(f"Invalid Parameter: {parameter}")
+
+
+def parse_expr(expression: str) -> ast.expr:
+    expr = parse_stmt(expression)
+    if isinstance(expr, ast.Expr):
+        return expr.value
+    elif isinstance(expr, ast.expr):
+        return expr
+    else:
+        raise ValueError(f"Invalid Expr: {expression}")
+
+
+def parse_stmt(expression: str) -> ast.stmt:
+    module = ast.parse(expression)
+    if not isinstance(module, ast.Module):
+        raise ValueError(f"Invalid Expr: {expression}")
+
+    stmt = module.body[0]
+    return stmt
+
+
+def parse_ident(expr: str) -> ast.Name:
+    if keyword.iskeyword(expr):
+        raise ValueError(f"Invalid identifier: {expr}")
+
+    return ast_utils.Name(expr)
---
+++ packages/tempered/tempered-0.8.0-py3-none-any.whl/tempered/parser/tokens.py
@@ -0,0 +1,134 @@
+from . import template_ast
+import typing_extensions as t
+from dataclasses import dataclass
+
+
+@t.runtime_checkable
+class EasyParseToken(t.Protocol):
+    def into_tag(self) -> template_ast.Tag:
+        ...
+
+
+@dataclass
+class LiteralToken(EasyParseToken):
+    body: str
+
+    def into_tag(self) -> template_ast.LiteralTag:
+        return template_ast.LiteralTag(self.body)
+
+
+@dataclass
+class StylesToken(EasyParseToken):
+    def into_tag(self) -> template_ast.StyleTag:
+        return template_ast.StyleTag()
+
+
+@dataclass
+class StylesIncludeToken:
+    template: str
+
+
+@dataclass
+class LayoutToken:
+    layout: str
+
+
+@dataclass
+class SlotToken:
+    name: t.Union[str, None]
+    is_required: bool
+
+
+@dataclass
+class SlotEndToken:
+    pass
+
+
+@dataclass
+class BlockToken:
+    name: str
+    is_required: bool
+
+
+@dataclass
+class BlockEndToken:
+    pass
+
+
+@dataclass
+class EscapedExprToken:
+    expr: str
+
+
+@dataclass
+class HtmlExprToken:
+    expr: str
+
+
+@dataclass
+class ComponentToken:
+    call: str
+
+
+@dataclass
+class ParameterToken:
+    parameter: str
+
+
+@dataclass
+class IfStartToken:
+    condition: str
+
+
+@dataclass
+class ElIfToken:
+    condition: str
+
+
+@dataclass
+class ElseToken:
+    pass
+
+
+@dataclass
+class IfEndToken:
+    pass
+
+
+@dataclass
+class ForStartToken:
+    variable: str
+    iterable: str
+
+
+@dataclass
+class ForEndToken:
+    pass
+
+
+@dataclass
+class SetToken:
+    assignment: str
+
+
+Token: t.TypeAlias = t.Union[
+    LiteralToken,
+    ParameterToken,
+    StylesToken,
+    StylesIncludeToken,
+    SetToken,
+    ComponentToken,
+    HtmlExprToken,
+    EscapedExprToken,
+    ForStartToken,
+    ForEndToken,
+    IfStartToken,
+    ElIfToken,
+    ElseToken,
+    IfEndToken,
+    LayoutToken,
+    SlotToken,
+    SlotEndToken,
+    BlockToken,
+    BlockEndToken,
+]
