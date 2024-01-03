f0df6aabc5592ebeeb62128f6867e4bd930220e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:05:56 +0000

    Add package pkrtablegui/1.0.0/pkrtablegui-1.0.0.tar.gz

---
+++ packages/pkrtablegui/pkrtablegui-1.0.0.tar.gz/pkrtablegui-1.0.0/setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+import json
+
+install_requires = [
+    "python-dotenv"
+    ]
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Natural Language :: French",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Games/Entertainment",
+    "Topic :: Games/Entertainment :: Board Games"
+]
+
+
+def get_version():
+    with open("version.json", "r") as f:
+        version = json.load(f)
+        return f"{version['major']}.{version['minor']}.{version['patch']}"
+
+
+setup(
+    name="pkrtablegui",
+    version=get_version(),
+    description="A Poker Package for Graphic User Interface",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    classifiers=classifiers,
+    keywords="poker pktable table pkr pokertablegui PokerTableGui PokerTable GUI",
+    author="Alexandre MANGWA",
+    author_email="alex.mangwa@gmail.com",
+    url="https://github.com/manggy94/PokerGui",
+    license_file='LICENSE.txt',
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=["pytest", "pytest-cov", "coverage", "coveralls"],
+)
