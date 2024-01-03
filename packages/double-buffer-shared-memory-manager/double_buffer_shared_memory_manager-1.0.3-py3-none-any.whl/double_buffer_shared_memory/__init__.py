5549a5987bc9c43db092959745c0d4ce4e4d3d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:04:17 +0000

    Add package double-buffer-shared-memory-manager/1.0.3/double_buffer_shared_memory_manager-1.0.3-py3-none-any.whl

---
+++ packages/double-buffer-shared-memory-manager/double_buffer_shared_memory_manager-1.0.3-py3-none-any.whl/double_buffer_shared_memory/__init__.py
@@ -0,0 +1,15 @@
+"""
+This package provides a DoubleBufferedSharedMemoryManager class for managing
+double-buffered shared memory, suitable for concurrent read/write operations video frames data stored as a NumPy ndarray.
+"""
+
+from .double_buffered_shared_memory_manager import (
+    DoubleBufferedSharedMemoryManager,
+    LogMessages,
+)
+
+__version__ = "1.0.2"
+__author__ = "Massimo Ghiani <m.ghiani@gmail.com>"
+__status__ = "Production"
+
+__all__ = ["DoubleBufferedSharedMemoryManager", "LogMessages"]
---
+++ packages/double-buffer-shared-memory-manager/double_buffer_shared_memory_manager-1.0.3-py3-none-any.whl/double_buffer_shared_memory/infos.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+AUTHOR_NO_EMAIL = "Massimo Ghiani"
+DATE = date.today().strftime("%d %b %Y")
+EMAIL = "m.ghiani@gmail.com"
+AUTHOR = f"{AUTHOR_NO_EMAIL} <{EMAIL}>"
+DESCRIPTION = "Package for communication with shared memory using double buffer"
+KEYWORDS = (
+    "double_buffer_shared_memory_manager_package",
+    "double_buffer_shared_memory",
+    "double_buffer",
+    "shared_memory",
+    "shared",
+    "memory",
+    "manager",
+    "package",
+    "python",
+)
+ISSUES_URL = "https://github.com/m-ghiani/DOUBLE_BUFFER_SHARED_MEMORY_MANAGER/issues"
+LICENSE = "MIT"
+MAINTAINER = f"{AUTHOR_NO_EMAIL} <{EMAIL}>"
+NAME = "double-buffer-shared-memory-manager"
+STATUS = "Production"
+VERSION = "1.0.3"
+URL = "https://github.com/m-ghiani/DOUBLE_BUFFER_SHARED_MEMORY_MANAGER"
