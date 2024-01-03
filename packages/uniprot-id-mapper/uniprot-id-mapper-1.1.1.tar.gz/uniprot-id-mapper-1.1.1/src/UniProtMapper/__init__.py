406ef1ad836f569bd18886830bcbb08783661fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:29 +0000

    Add package uniprot-id-mapper/1.1.1/uniprot-id-mapper-1.1.1.tar.gz

---
+++ packages/uniprot-id-mapper/uniprot-id-mapper-1.1.1.tar.gz/uniprot-id-mapper-1.1.1/src/UniProtMapper/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""A Python wrapper for the UniProt id-mapping RESTful API: https://www.uniprot.org/id-mapping"""
+
+from .api import ProtMapper  # noqa: F401
+
+__version__ = "1.1.1"
