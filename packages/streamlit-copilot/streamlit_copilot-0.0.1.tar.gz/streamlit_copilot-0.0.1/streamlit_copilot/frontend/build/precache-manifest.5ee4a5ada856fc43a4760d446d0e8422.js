fbe0bc7ff36240d72df7a923648c33530160a066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:48 +0000

    Add package streamlit-copilot/0.0.1/streamlit_copilot-0.0.1.tar.gz

---
+++ packages/streamlit-copilot/streamlit_copilot-0.0.1.tar.gz/streamlit_copilot-0.0.1/streamlit_copilot/frontend/build/precache-manifest.5ee4a5ada856fc43a4760d446d0e8422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf029bdc941178cfe882ad2d43acd581",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b217967d89b78baa4149",
+    "url": "./static/js/2.232b3114.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.232b3114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c048e33f58ca55d4a4",
+    "url": "./static/js/main.9652d827.chunk.js"
+  },
+  {
+    "revision": "686f82ce1d73c5937ab0",
+    "url": "./static/js/runtime-main.3b2d39e5.js"
+  }
+]);
