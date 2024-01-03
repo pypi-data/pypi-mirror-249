55ee6cf3af9149d65c86dfe190ed10e33973a57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:45 +0000

    Add package jtcuhavyubhva/1.10000.10173/jtcuhavyubhva-1.10000.10173.tar.gz

---
+++ packages/jtcuhavyubhva/jtcuhavyubhva-1.10000.10173.tar.gz/jtcuhavyubhva-1.10000.10173/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jtcuhavyubhva',
+    version='1.10000.10173',
+    packages=['jtcuhavyubhva'],
+    python_requires='>=3',
+    platforms=["all"]
+)
+
