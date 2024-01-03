384d18daa8678c3c92fc31710d31e849685d9dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:05:55 +0000

    Add package autobisect/7.5.0/autobisect-7.5.0-py3-none-any.whl

---
+++ packages/autobisect/autobisect-7.5.0-py3-none-any.whl/autobisect/__main__.py
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import sys
+
+from .main import main, parse_args
+
+sys.exit(main(parse_args()))
