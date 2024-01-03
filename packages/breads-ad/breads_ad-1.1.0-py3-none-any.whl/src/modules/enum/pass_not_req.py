bcec5693b46ccee056f317c92f0102cbcb88c40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:34 +0000

    Add package breads-ad/1.1.0/breads_ad-1.1.0-py3-none-any.whl

---
+++ packages/breads-ad/breads_ad-1.1.0-py3-none-any.whl/src/modules/enum/pass_not_req.py
@@ -0,0 +1,17 @@
+from rich import print
+from src.handlers.ldap_connect import connect_and_fetch
+
+def get_pass_not_req() -> None:
+    ''' Get users from domain that does not require a password to authenticate '''
+
+    search_filter = f'(&(objectClass=user)(userAccountControl:1.2.840.113556.1.4.803:=32))'
+    query = connect_and_fetch(search_filter)
+
+    if query:
+        print(f"[yellow][!] Getting password not required users[/]\n")
+
+        for dn, attrs in query:
+            for attr_name in attrs:
+                if attr_name == 'sAMAccountName' and dn is not None:
+                    for user_name in attrs[attr_name]:
+                        print(f"[green]{user_name.decode('utf-8')}")
---
+++ packages/breads-ad/breads_ad-1.1.0-py3-none-any.whl/src/modules/enum/pass_pol.py
@@ -0,0 +1,25 @@
+from rich import print
+from src.handlers.ldap_connect import connect_and_fetch
+
+def get_pass_policy() -> None:
+    ''' Get the current password policy from the domain '''
+    
+    search_filter = f'(objectClass=domainDNS)'
+    query = connect_and_fetch(search_filter)
+
+    if query:
+        print(f"[yellow][!] Getting domain password policy[/]\n")
+
+        for dn, attributes in query:
+            for attr_name in attributes:
+
+                attributes_list = {
+                    'minPwdLength': 'Minimum Password Length', 
+                    'lockoutThreshold': 'Lockout Threshold', 
+                    'lockoutDuration': 'Lockout Duration (Milliseconds)',
+                }
+
+                for attribute, name in attributes_list.items():
+                        if(attr_name == attribute):
+                            for attribute_name in attributes[attr_name]:
+                                print(f"[cyan]* {name}: {attribute_name.decode('utf-8')} [/]")
