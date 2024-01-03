942fcf37951dc7e44fd0276925fb93fba2ac013d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:03:15 +0000

    Add package fetchrepo/2.0.1/fetchrepo-2.0.1.tar.gz

---
+++ packages/fetchrepo/fetchrepo-2.0.1.tar.gz/fetchrepo-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="fetchrepo",
+    version="2.0.1",
+    packages=find_packages(),
+    author="Al-Fareed",
+    description="Fetches and returns required content from github repository",
+    long_description=open("README.md").read(),
+    long_description_content_type = "text/markdown"
+    )
