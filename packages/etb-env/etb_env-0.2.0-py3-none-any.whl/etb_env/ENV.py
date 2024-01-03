ec27bad82feaf1a01ebf86f566e36f8d0b7b1d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:11 +0000

    Add package etb-env/0.2.0/etb_env-0.2.0-py3-none-any.whl

---
+++ packages/etb-env/etb_env-0.2.0-py3-none-any.whl/etb_env/ENV.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+class Env:
+    '''provides nice central access to grab ENV vals'''
+
+    def __init__(self, env_path: str = None):
+        load_dotenv(dotenv_path = env_path)
+
+    def get(self, vars: list[str]) -> dict:
+        '''pass list of env values to get, returns key-value mapping of env val name : value'''
+        var_dict = {}
+
+        for entry in vars:
+            value = os.environ.get(entry, False)
+            assert value, f'ENV value {entry} not found'
+
+            var_dict[entry] = value
+
+        return var_dict
+    
+    def get_db_auth(self) -> dict:
+        '''just for ease/standardization of accessing database auth'''
+        
+        db_params = ["USER", "PASSWORD", "HOST", "PORT", "DATABASE"]
+
+        return self.get(db_params)
