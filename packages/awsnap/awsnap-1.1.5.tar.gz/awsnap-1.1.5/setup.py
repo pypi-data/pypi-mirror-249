89fabbe9d9ddeb8eb622f9d7261830510459ff43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:18 +0000

    Add package awsnap/1.1.5/awsnap-1.1.5.tar.gz

---
+++ packages/awsnap/awsnap-1.1.5.tar.gz/awsnap-1.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="awsnap",
+    version="1.1.5",
+    description="AWS SSO Utility",
+    author="Aaron West",
+    author_email="aphexlog@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={
+        "console_scripts": [
+            "awsnap=awsnap.awsnap:main",
+        ],
+    },
+    install_requires=[
+        "boto3",
+    ],
+)
