2abd10f051c692df5b91d48d49ea0b76e4636abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:34 +0000

    Add package starfyre/0.20.1/starfyre-0.20.1-py3-none-any.whl

---
+++ packages/starfyre/starfyre-0.20.1-py3-none-any.whl/starfyre/component.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Any, Optional
+
+
+@dataclass
+class Component:
+    tag: str
+    props: dict
+    children: list
+    event_listeners: dict
+    state: dict
+    uuid: Any
+    signal: str = ""
+    original_data: str = ""
+    data: str = ""
+    parentComponent: Optional[Any] = None
+    html: str = ""
+    css: str = ""
+    js: str = ""
+    client_side_python: str = ""
+    original_name: str = ""
+    # on any property change, rebuild the tree
+
+    @property
+    def is_text_component(self):
+        return self.tag == "TEXT_NODE"
+
+    @property
+    def is_slot_component(self):
+        return self.tag == "slot"
+
+    def __repr__(self):
+        return f"<{self.tag}> {self.data} {self.children} </{self.tag}>"
