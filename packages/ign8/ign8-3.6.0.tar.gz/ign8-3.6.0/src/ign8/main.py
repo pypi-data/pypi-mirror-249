73fafc681fa5d3db016a56466c5768bf6deec7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:38 +0000

    Add package ign8/3.6.0/ign8-3.6.0.tar.gz

---
+++ packages/ign8/ign8-3.6.0.tar.gz/ign8-3.6.0/src/ign8/main.py
@@ -0,0 +1,17 @@
+from .common import prettyllog
+import os
+
+def main():
+    prettyllog("main", "check", "main", "all", "200", "Success")
+    return True
+
+def serve():
+    prettyllog("main", "check", "main", "all", "200", "Success")
+    os.command("pip install --uprade ign8")
+    os.command("cd /"
+    # change working directory to the root of the project
+
+
+
+
+    return True
