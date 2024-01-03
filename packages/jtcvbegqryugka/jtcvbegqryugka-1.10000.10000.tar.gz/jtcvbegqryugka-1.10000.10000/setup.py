86f94fb7a4af42cb3bd7e867a29cb792f3d8d51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:48 +0000

    Add package jtcvbegqryugka/1.10000.10000/jtcvbegqryugka-1.10000.10000.tar.gz

---
+++ packages/jtcvbegqryugka/jtcvbegqryugka-1.10000.10000.tar.gz/jtcvbegqryugka-1.10000.10000/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jtcvbegqryugka',
+    version='1.10000.10000',
+    packages=['jtcuhavyubhva'],
+    python_requires='>=3',
+    platforms=["all"]
+)
+
