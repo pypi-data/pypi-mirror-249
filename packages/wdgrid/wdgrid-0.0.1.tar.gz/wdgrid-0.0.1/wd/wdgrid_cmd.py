7b99f5d66ef3ea1a6293b231fde42fc157817def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:54 +0000

    Add package wdgrid/0.0.1/wdgrid-0.0.1.tar.gz

---
+++ packages/wdgrid/wdgrid-0.0.1.tar.gz/wdgrid-0.0.1/wd/wdgrid_cmd.py
@@ -0,0 +1,38 @@
+"""
+Created on 2024-01-03
+
+@author: wf
+"""
+from ngwidgets.cmd import WebserverCmd
+from wd.webserver import WdgridWebServer
+import sys
+from argparse import ArgumentParser
+
+class ScanCmd(WebserverCmd):
+    """
+    Command line for scan2wiki web server
+    """
+    
+    def getArgParser(self,description:str,version_msg)->ArgumentParser:
+        """
+        override the default argparser call
+        """        
+        parser=super().getArgParser(description, version_msg)
+        #parser.add_argument("-v", "--verbose", action="store_true", help="show verbose output [default: %(default)s]")
+        #parser.add_argument("-rp", "--root_path",default=ScanWebServer.examples_path(),help="path to example pdf files [default: %(default)s]")
+        #parser.add_argument("-wc", "--webcam",help="url of webcam for scans [default: %(default)s]")
+        return parser
+    
+def main(argv:list=None):
+    """
+    main call
+    """
+    cmd=ScanCmd(config=WdgridWebServer.get_config(),webserver_cls=WdgridWebServer)
+    exit_code=cmd.cmd_main(argv)
+    return exit_code
+        
+DEBUG = 0
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-d")
+    sys.exit(main())
