4cd9ea9470a4608a6ba438fca534695d8db9c356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:36 +0000

    Add package aio.core/0.10.1/aio.core-0.10.1.tar.gz

---
+++ packages/aio.core/aio.core-0.10.1.tar.gz/aio.core-0.10.1/aio/core/directory/directory.py
@@ -0,0 +1,32 @@
+
+from typing import Type
+
+import abstracts
+
+from aio.core import directory
+
+
+@abstracts.implementer(directory.ADirectory)
+class Directory:
+
+    @property
+    def finder_class(self) -> Type[directory.ADirectoryFileFinder]:
+        return DirectoryFileFinder
+
+
+@abstracts.implementer(directory.ADirectoryFileFinder)
+class DirectoryFileFinder:
+    pass
+
+
+@abstracts.implementer(directory.AGitDirectoryFileFinder)
+class GitDirectoryFileFinder:
+    pass
+
+
+@abstracts.implementer(directory.AGitDirectory)
+class GitDirectory:
+
+    @property
+    def finder_class(self) -> Type[directory.AGitDirectoryFileFinder]:
+        return GitDirectoryFileFinder
