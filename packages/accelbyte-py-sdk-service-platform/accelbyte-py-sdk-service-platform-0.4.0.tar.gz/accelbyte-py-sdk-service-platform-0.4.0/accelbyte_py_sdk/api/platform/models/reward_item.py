ddb080c1dae99f1c6c43d1a49c90825379166fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:04:17 +0000

    Add package accelbyte-py-sdk-service-platform/0.4.0/accelbyte-py-sdk-service-platform-0.4.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-platform/accelbyte-py-sdk-service-platform-0.4.0.tar.gz/accelbyte-py-sdk-service-platform-0.4.0/accelbyte_py_sdk/api/platform/models/reward_item.py
@@ -0,0 +1,194 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: model.j2
+
+# AccelByte Gaming Services Platform Service (4.43.0)
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import Model
+
+
+class RewardItem(Model):
+    """Reward item (RewardItem)
+
+    Properties:
+        duration: (duration) OPTIONAL int
+
+        end_date: (endDate) OPTIONAL str
+
+        item_id: (itemId) OPTIONAL str
+
+        quantity: (quantity) OPTIONAL int
+    """
+
+    # region fields
+
+    duration: int  # OPTIONAL
+    end_date: str  # OPTIONAL
+    item_id: str  # OPTIONAL
+    quantity: int  # OPTIONAL
+
+    # endregion fields
+
+    # region with_x methods
+
+    def with_duration(self, value: int) -> RewardItem:
+        self.duration = value
+        return self
+
+    def with_end_date(self, value: str) -> RewardItem:
+        self.end_date = value
+        return self
+
+    def with_item_id(self, value: str) -> RewardItem:
+        self.item_id = value
+        return self
+
+    def with_quantity(self, value: int) -> RewardItem:
+        self.quantity = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "duration"):
+            result["duration"] = int(self.duration)
+        elif include_empty:
+            result["duration"] = 0
+        if hasattr(self, "end_date"):
+            result["endDate"] = str(self.end_date)
+        elif include_empty:
+            result["endDate"] = ""
+        if hasattr(self, "item_id"):
+            result["itemId"] = str(self.item_id)
+        elif include_empty:
+            result["itemId"] = ""
+        if hasattr(self, "quantity"):
+            result["quantity"] = int(self.quantity)
+        elif include_empty:
+            result["quantity"] = 0
+        return result
+
+    # endregion to methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls,
+        duration: Optional[int] = None,
+        end_date: Optional[str] = None,
+        item_id: Optional[str] = None,
+        quantity: Optional[int] = None,
+        **kwargs,
+    ) -> RewardItem:
+        instance = cls()
+        if duration is not None:
+            instance.duration = duration
+        if end_date is not None:
+            instance.end_date = end_date
+        if item_id is not None:
+            instance.item_id = item_id
+        if quantity is not None:
+            instance.quantity = quantity
+        return instance
+
+    @classmethod
+    def create_from_dict(cls, dict_: dict, include_empty: bool = False) -> RewardItem:
+        instance = cls()
+        if not dict_:
+            return instance
+        if "duration" in dict_ and dict_["duration"] is not None:
+            instance.duration = int(dict_["duration"])
+        elif include_empty:
+            instance.duration = 0
+        if "endDate" in dict_ and dict_["endDate"] is not None:
+            instance.end_date = str(dict_["endDate"])
+        elif include_empty:
+            instance.end_date = ""
+        if "itemId" in dict_ and dict_["itemId"] is not None:
+            instance.item_id = str(dict_["itemId"])
+        elif include_empty:
+            instance.item_id = ""
+        if "quantity" in dict_ and dict_["quantity"] is not None:
+            instance.quantity = int(dict_["quantity"])
+        elif include_empty:
+            instance.quantity = 0
+        return instance
+
+    @classmethod
+    def create_many_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> Dict[str, RewardItem]:
+        return (
+            {k: cls.create_from_dict(v, include_empty=include_empty) for k, v in dict_}
+            if dict_
+            else {}
+        )
+
+    @classmethod
+    def create_many_from_list(
+        cls, list_: list, include_empty: bool = False
+    ) -> List[RewardItem]:
+        return (
+            [cls.create_from_dict(i, include_empty=include_empty) for i in list_]
+            if list_
+            else []
+        )
+
+    @classmethod
+    def create_from_any(
+        cls, any_: any, include_empty: bool = False, many: bool = False
+    ) -> Union[RewardItem, List[RewardItem], Dict[Any, RewardItem]]:
+        if many:
+            if isinstance(any_, dict):
+                return cls.create_many_from_dict(any_, include_empty=include_empty)
+            elif isinstance(any_, list):
+                return cls.create_many_from_list(any_, include_empty=include_empty)
+            else:
+                raise ValueError()
+        else:
+            return cls.create_from_dict(any_, include_empty=include_empty)
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "duration": "duration",
+            "endDate": "end_date",
+            "itemId": "item_id",
+            "quantity": "quantity",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "duration": False,
+            "endDate": False,
+            "itemId": False,
+            "quantity": False,
+        }
+
+    # endregion static methods
