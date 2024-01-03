1fa0ac9a550131a757c7ee185a9942fa49ae4d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:06:39 +0000

    Add package bigtree/0.15.0/bigtree-0.15.0.tar.gz

---
+++ packages/bigtree/bigtree-0.15.0.tar.gz/bigtree-0.15.0/docs/source/conf.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
+import bigtree
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "bigtree"
+copyright = "2022, Kay Jan WONG"
+author = "Kay Jan WONG"
+release = bigtree.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "autodocsumm",
+    "myst_parser",
+    "sphinxemoji.sphinxemoji",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.doctest",
+]
+autodoc_default_options = {"autosummary": True}
+sphinxemoji_style = "twemoji"
+myst_enable_extensions = [
+    "attrs_block",
+]
+base_dir = "/".join(os.getcwd().split("/")[:-2])
+doctest_global_setup = f"""
+import os
+os.chdir("{base_dir}")
+if not os.path.exists("assets/docstr"):
+    os.mkdir("assets/docstr")
+"""
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "Python"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "karma_sphinx_theme"
+html_static_path = ["_static"]
+html_favicon = "_static/favicon.ico"
+html_logo = "_static/favicon.ico"
+
+
+def setup(app):
+    app.add_css_file("custom.css")
