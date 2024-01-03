69aa20055099582471d6e27ef219d6acee6a9942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:43 +0000

    Add package vismatrix/0.1/vismatrix-0.1.tar.gz

---
+++ packages/vismatrix/vismatrix-0.1.tar.gz/vismatrix-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='vismatrix',
+      version='0.1',
+      description='Output matrix or table in beautiful form',
+      packages=['vismatrix'],
+      author_email='i4masyrov@gmail.com',
+      zip_safe=False)
