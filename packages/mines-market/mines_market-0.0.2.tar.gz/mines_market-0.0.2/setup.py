617e7f7d0ff8cd55727d78ee3b9931e5ca080fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:04:21 +0000

    Add package mines-market/0.0.2/mines_market-0.0.2.tar.gz

---
+++ packages/mines-market/mines_market-0.0.2.tar.gz/mines_market-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mines_market',
+    version='0.0.2',
+    author='Yamato Matsumura, Keenan Buckley',
+    author_email='matsumura.yamato@gmail.com, keenandbuckley@protonmail.com',
+    description='Access sodexo api to retrieve menu for desired location',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
