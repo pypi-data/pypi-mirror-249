ee7187f36c3bbd94270d9ae742df014f57fe3c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:12 +0000

    Add package poodle/1.2.1/poodle-1.2.1-py3-none-any.whl

---
+++ packages/poodle/poodle-1.2.1-py3-none-any.whl/poodle/__init__.py
@@ -0,0 +1,31 @@
+"""Poodle Mutation Test Tool."""
+
+from __future__ import annotations
+
+import importlib
+import sys
+from contextlib import suppress
+from pathlib import Path
+from typing import Any
+
+__version__ = "1.2.1"
+
+
+class PoodleInputError(ValueError):
+    """An input value from Command Line, poodle_config.py, or a config file was invalid."""
+
+
+class PoodleTrialRunError(Exception):
+    """An unexpected error occurred when running a mutation trial or clean run."""
+
+
+try:
+    import tomllib  # type: ignore [import-not-found]
+except ModuleNotFoundError:  # < py3.11
+    import tomli as tomllib  # type: ignore [no-redef]
+
+
+poodle_config: Any = None
+with suppress(ImportError):
+    sys.path.append(str(Path.cwd()))
+    poodle_config = importlib.import_module("poodle_config")
