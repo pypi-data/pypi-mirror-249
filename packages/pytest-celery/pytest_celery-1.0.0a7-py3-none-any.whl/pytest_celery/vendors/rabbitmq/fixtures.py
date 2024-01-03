68005adecd34f0afa8658653cd6e8e98dcdcccb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:37 +0000

    Add package pytest-celery/1.0.0a7/pytest_celery-1.0.0a7-py3-none-any.whl

---
+++ packages/pytest-celery/pytest_celery-1.0.0a7-py3-none-any.whl/pytest_celery/vendors/rabbitmq/fixtures.py
@@ -0,0 +1,48 @@
+# mypy: disable-error-code="misc"
+
+from __future__ import annotations
+
+import pytest
+from pytest_docker_tools import container
+from pytest_docker_tools import fxtr
+
+from pytest_celery.vendors.rabbitmq.api import RabbitMQTestBroker
+from pytest_celery.vendors.rabbitmq.container import RabbitMQContainer
+from pytest_celery.vendors.rabbitmq.defaults import RABBITMQ_CONTAINER_TIMEOUT
+
+
+@pytest.fixture
+def celery_rabbitmq_broker(default_rabbitmq_broker: RabbitMQContainer) -> RabbitMQTestBroker:
+    broker = RabbitMQTestBroker(default_rabbitmq_broker)
+    yield broker
+    broker.teardown()
+
+
+@pytest.fixture
+def default_rabbitmq_broker_cls() -> type[RabbitMQContainer]:
+    return RabbitMQContainer
+
+
+default_rabbitmq_broker = container(
+    image="{default_rabbitmq_broker_image}",
+    ports=fxtr("default_rabbitmq_broker_ports"),
+    environment=fxtr("default_rabbitmq_broker_env"),
+    network="{default_pytest_celery_network.name}",
+    wrapper_class=RabbitMQContainer,
+    timeout=RABBITMQ_CONTAINER_TIMEOUT,
+)
+
+
+@pytest.fixture
+def default_rabbitmq_broker_env(default_rabbitmq_broker_cls: type[RabbitMQContainer]) -> dict:
+    yield default_rabbitmq_broker_cls.env()
+
+
+@pytest.fixture
+def default_rabbitmq_broker_image(default_rabbitmq_broker_cls: type[RabbitMQContainer]) -> str:
+    yield default_rabbitmq_broker_cls.image()
+
+
+@pytest.fixture
+def default_rabbitmq_broker_ports(default_rabbitmq_broker_cls: type[RabbitMQContainer]) -> dict:
+    yield default_rabbitmq_broker_cls.ports()
