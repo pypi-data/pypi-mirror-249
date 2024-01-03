52a1ade136ee7cda9e387d0b58637e09ce98c184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:30 +0000

    Add package django-pydantic-field/0.3.0a5/django_pydantic_field-0.3.0a5-py3-none-any.whl

---
+++ packages/django-pydantic-field/django_pydantic_field-0.3.0a5-py3-none-any.whl/django_pydantic_field/v2/utils.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import sys
+import typing as ty
+from collections import ChainMap
+
+if ty.TYPE_CHECKING:
+    from collections.abc import Mapping
+
+
+def get_annotated_type(obj, field, default=None) -> ty.Any:
+    try:
+        if isinstance(obj, type):
+            annotations = obj.__dict__["__annotations__"]
+        else:
+            annotations = obj.__annotations__
+
+        return annotations[field]
+    except (AttributeError, KeyError):
+        return default
+
+
+def get_namespace(cls) -> ChainMap[str, ty.Any]:
+    return ChainMap(get_local_namespace(cls), get_global_namespace(cls))
+
+
+def get_global_namespace(cls) -> dict[str, ty.Any]:
+    try:
+        module = cls.__module__
+        return vars(sys.modules[module])
+    except (KeyError, AttributeError):
+        return {}
+
+
+def get_local_namespace(cls) -> dict[str, ty.Any]:
+    try:
+        return vars(cls)
+    except TypeError:
+        return {}
+
+
+if sys.version_info >= (3, 9):
+
+    def evaluate_forward_ref(ref: ty.ForwardRef, ns: Mapping[str, ty.Any]) -> ty.Any:
+        return ref._evaluate(dict(ns), {}, frozenset())
+
+else:
+
+    def evaluate_forward_ref(ref: ty.ForwardRef, ns: Mapping[str, ty.Any]) -> ty.Any:
+        return ref._evaluate(dict(ns), {})
