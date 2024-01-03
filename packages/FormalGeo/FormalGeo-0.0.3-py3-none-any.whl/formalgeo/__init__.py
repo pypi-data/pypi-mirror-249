19a61a31139c8c5dc56fdbc92f8f4798bc7cbbf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:18 +0000

    Add package FormalGeo/0.0.3/FormalGeo-0.0.3-py3-none-any.whl

---
+++ packages/FormalGeo/FormalGeo-0.0.3-py3-none-any.whl/formalgeo/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2022-2023 FormalGeo Development Team
+# Author: Xiaokai Zhang
+# Contact: formalgeo@gmail.com
+
+"""Formal representation and solving for Euclidean plane geometry problems."""
+
+__all__ = [
+    "core", "data", "parse", "problem", "solver", "tools"
+]
