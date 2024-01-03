aea75141c7aaf6e1e48222ff161bd4a9ea237924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:56 +0000

    Add package dokker/0.1.14/dokker-0.1.14.tar.gz

---
+++ packages/dokker/dokker-0.1.14.tar.gz/dokker-0.1.14/dokker/project.py
@@ -0,0 +1,61 @@
+from typing import Protocol, runtime_checkable
+from .cli import CLI
+
+
+@runtime_checkable
+class Project(Protocol):
+    """A Project
+
+    Projects are the core representation of the organization of a
+    project that can be run with the docker CLI. When a project is
+    setup by the Setup class, it can decide to setup the project asy
+    nchronousl, e.g by cloning a git repository, or copiying a
+    directory into the .dokker directory. The project can also
+    implement methods that will be run before and after certain
+    docker-compose commands are run. For example, a project can
+    implement a method that will be run before the docker-compose
+    up command is run.
+    """
+
+    async def ainititialize(self) -> CLI:
+        ...
+
+    async def abefore_pull(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_up(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_enter(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_down(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_stop(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
