faf9d8db306875ae5c25f6ed59dc5860fef3bf68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:17 +0000

    Add package oogeso/1.1.1/oogeso-1.1.1-py3-none-any.whl

---
+++ packages/oogeso/oogeso-1.1.1-py3-none-any.whl/oogeso/core/devices/__init__.py
@@ -0,0 +1,12 @@
+from .compressor import CompressorEl, CompressorGas
+from .electrolyser import Electrolyser
+from .fuelcell import FuelCell
+from .gasheater import GasHeater
+from .gasturbine import GasTurbine
+from .heatpump import HeatPump
+from .pump import PumpOil, PumpWater, PumpWellStream
+from .separator import Separator, Separator2
+from .sink import PowerSink, SinkCarbon, SinkEl, SinkGas, SinkHeat, SinkOil, SinkWater
+from .source import PowerSource, SourceEl, SourceGas, SourceOil, SourceWater
+from .storage import StorageEl, StorageHydrogen
+from .well import WellGasLift, WellProduction
---
+++ packages/oogeso/oogeso-1.1.1-py3-none-any.whl/oogeso/core/devices/gasturbine.py
@@ -0,0 +1,68 @@
+from typing import Dict, Union
+
+import pyomo.environ as pyo
+
+from oogeso import dto
+from oogeso.core.devices.base import Device
+
+
+class GasTurbine(Device):
+    """Gas turbine generator"""
+
+    carrier_in = ["gas"]
+    carrier_out = ["el", "heat", "carbon"]
+    serial = list()
+
+    def __init__(
+        self,
+        dev_data: dto.DeviceGasTurbineData,
+        carrier_data_dict: Dict[str, dto.CarrierGasData],
+    ):
+        super().__init__(dev_data=dev_data, carrier_data_dict=carrier_data_dict)
+        self.dev_data = dev_data
+        self.id = dev_data.id
+        self.carrier_data = carrier_data_dict
+
+    def _rules_misc(self, model: pyo.Model, t: int, i: int) -> Union[pyo.Expression, pyo.Constraint.Skip]:
+        dev = self.id
+        gas_data = self.carrier_data["gas"]
+        # el_power = model.varDeviceFlow[dev, "el", "out", t]
+        gas_energy_content = gas_data.energy_value  # MJ/Sm3
+        if i == 1:
+            """turbine el power out vs gas fuel in"""
+            # fuel consumption (gas in) is a linear function of el power output
+            # fuel = B + A*power
+            # => efficiency = power/(A+B*power)
+            A = self.dev_data.fuel_A
+            B = self.dev_data.fuel_B
+            P_max = self.dev_data.flow_max
+            lhs = model.varDeviceFlow[dev, "gas", "in", t] * gas_energy_content / P_max
+            rhs = (
+                B * (model.varDeviceIsOn[dev, t] + model.varDeviceIsPrep[dev, t])
+                + A * model.varDeviceFlow[dev, "el", "out", t] / P_max
+            )
+            return lhs == rhs
+        elif i == 2:
+            """heat output = (gas energy in - el power out)* heat efficiency"""
+            lhs = model.varDeviceFlow[dev, "heat", "out", t]
+            rhs = (
+                model.varDeviceFlow[dev, "gas", "in", t] * gas_energy_content - model.varDeviceFlow[dev, "el", "out", t]
+            ) * self.dev_data.eta_heat
+            return lhs == rhs
+        elif i == 3:
+            """carbon emitted given by gas consumption"""
+            gasflow_co2 = gas_data.co2_content  # kg/m3
+            lhs = model.varDeviceFlow[dev, "carbon", "out", t]
+            rhs = gasflow_co2 * (model.varDeviceFlow[dev, "gas", "in", t])
+            return lhs == rhs
+
+    def define_constraints(self, pyomo_model: pyo.Model):
+        """Specifies the list of constraints for the device"""
+        list_to_reconstruct = super().define_constraints(pyomo_model)
+
+        constr = pyo.Constraint(pyomo_model.setHorizon, pyo.RangeSet(1, 3), rule=self._rules_misc)
+        setattr(pyomo_model, "constr_{}_{}".format(self.id, "misc"), constr)
+        return list_to_reconstruct
+
+    def get_flow_var(self, pyomo_model: pyo.Model, t: int):
+        return pyomo_model.varDeviceFlow[self.id, "el", "out", t]
