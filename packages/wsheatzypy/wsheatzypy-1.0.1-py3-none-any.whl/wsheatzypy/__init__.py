dae7cb3f9aedfe27e3ed6802ddf8e93ed2dc977d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:22 +0000

    Add package wsheatzypy/1.0.1/wsheatzypy-1.0.1-py3-none-any.whl

---
+++ packages/wsheatzypy/wsheatzypy-1.0.1-py3-none-any.whl/wsheatzypy/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+"""heatzypy package."""
+from .exception import (
+    AuthenticationFailed,
+    CommandFailed,
+    HeatzyException,
+    RetrieveFailed,
+    TimeoutExceededError,
+)
+from .heatzy import HeatzyClient
+
+__all__ = [
+    "AuthenticationFailed",
+    "CommandFailed",
+    "HeatzyClient",
+    "HeatzyException",
+    "RetrieveFailed",
+    "TimeoutExceededError",
+]
