3c34d1ced9f10d1a8709400438e33fb80f9416c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:08 +0000

    Add package pyrfuniverse/0.10.7/pyrfuniverse-0.10.7.tar.gz

---
+++ packages/pyrfuniverse/pyrfuniverse-0.10.7.tar.gz/pyrfuniverse-0.10.7/pyrfuniverse/attributes/__init__.py
@@ -0,0 +1,42 @@
+from pyrfuniverse.attributes.base_attr import BaseAttr
+from pyrfuniverse.attributes.camera_attr import CameraAttr
+from pyrfuniverse.attributes.gameobject_attr import GameObjectAttr
+from pyrfuniverse.attributes.light_attr import LightAttr
+from pyrfuniverse.attributes.activelightsensor_attr import ActiveLightSensorAttr
+from pyrfuniverse.attributes.collider_attr import ColliderAttr
+from pyrfuniverse.attributes.controller_attr import ControllerAttr
+from pyrfuniverse.attributes.rigidbody_attr import RigidbodyAttr
+from pyrfuniverse.attributes.cloth_attr import ClothAttr
+from pyrfuniverse.attributes.softbody_attr import SoftbodyAttr
+from pyrfuniverse.attributes.humanbody_attr import HumanbodyAttr
+from pyrfuniverse.attributes.graspsim_attr import GraspSimAttr
+from pyrfuniverse.attributes.digit_attr import DigitAttr
+from pyrfuniverse.attributes.gelslim_attr import GelSlimAttr
+from pyrfuniverse.attributes.pointcloud_attr import PointCloudAttr
+from pyrfuniverse.attributes.fallingcloth_attr import FallingClothAttr
+from pyrfuniverse.attributes.custom_attr import CustomAttr
+
+try:
+    from pyrfuniverse.attributes.omplmanager_attr import OmplManagerAttr
+except:
+    pass
+
+__all__ = [
+    "base_attr",
+    "camera_attr",
+    "activelightsensor_attr",
+    "collider_attr",
+    "controller_attr",
+    "gameobject_attr",
+    "rigidbody_attr",
+    "cloth_attr",
+    "softbody_attr",
+    "humanbody_attr",
+    "graspsim_attr",
+    "digit_attr",
+    "gelslim_attr",
+    "pointcloud_attr",
+    "fallingcloth_attr",
+    "custom_attr",
+    "omplmanager_attr",
+]
