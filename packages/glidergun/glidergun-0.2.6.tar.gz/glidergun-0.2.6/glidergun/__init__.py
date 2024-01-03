94341ac23df71ac9b4554e7d5c4c92a3dcf01c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:04:08 +0000

    Add package glidergun/0.2.6/glidergun-0.2.6.tar.gz

---
+++ packages/glidergun/glidergun-0.2.6.tar.gz/glidergun-0.2.6/glidergun/__init__.py
@@ -0,0 +1,16 @@
+import glidergun.ipython
+import glidergun.stats as stats
+from glidergun.core import (
+    grid,
+    stack,
+    Grid,
+    Stack,
+    Extent,
+    Resampling,
+    con,
+    mean,
+    std,
+    minimum,
+    maximum,
+    mosaic,
+)
