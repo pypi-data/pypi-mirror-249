03beddcc114783fcf71bff9204f358aaa44da252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:12 +0000

    Add package forecat/0.0.3/forecat-0.0.3.tar.gz

---
+++ packages/forecat/forecat-0.0.3.tar.gz/forecat-0.0.3/forecat/__init__.py
@@ -0,0 +1,17 @@
+from forecat.archs import (
+    CNNArch,
+    DenseArch,
+    EncoderDecoder,
+    LSTMArch,
+    Transformer,
+    UNETArch,
+)
+
+__all__ = [
+    "CNNArch",
+    "DenseArch",
+    "LSTMArch",
+    "UNETArch",
+    "EncoderDecoder",
+    "Transformer",
+]
