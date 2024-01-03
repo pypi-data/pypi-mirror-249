123670355da00dfb4649af12e8c78023dd85f7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:09 +0000

    Add package mymodules/5.0/mymodules-5.0.tar.gz

---
+++ packages/mymodules/mymodules-5.0.tar.gz/mymodules-5.0/setup.py
@@ -0,0 +1,19 @@
+# from setuptools import setup
+#
+# setup(
+#     name='mymodules',
+#     version='4.0',
+#     packages=['''Xbyte_Common_Scrape'''],
+# )
+
+from setuptools import setup
+
+setup(
+    name='mymodules',
+    version='5.0',
+    packages=['Xbyte_Common_Scrape'],
+    author='Xbyte',
+    author_email='',
+    description='Works on Linux, Windows, Mac OSX, BSD',
+    long_description='''''',
+)
