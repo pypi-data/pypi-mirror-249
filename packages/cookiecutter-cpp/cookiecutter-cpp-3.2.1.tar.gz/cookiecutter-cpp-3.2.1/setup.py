bce6ecc557cbcd4f47f4c4bc96a3c2155aea4130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:29 +0000

    Add package cookiecutter-cpp/3.2.1/cookiecutter-cpp-3.2.1.tar.gz

---
+++ packages/cookiecutter-cpp/cookiecutter-cpp-3.2.1.tar.gz/cookiecutter-cpp-3.2.1/setup.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+from pathlib import Path
+this_folder = Path(__file__).parent
+
+setup(
+    name='cookiecutter-cpp',
+    packages=[],
+    version='3.2.1',
+    description='Cookiecutter template for C++ with several libs ready to be used.',
+    long_description=(this_folder / 'README.rst').read_text(),
+    long_description_content_type='text/x-rst',
+    author='Riccardo Petraglia (forked from https://github.com/audreyr/cookiecutter-pypackage)',
+    license='BSD',
+    author_email='riccardo.petraglia@gmail.com',
+    url='https://github.com/grhawk/cookiecutter-cpp.git',
+    keywords=['cookiecutter', 'template', 'package', 'c++', 'cpp', 'gtest', 'spdlog'],
+    python_requires='>=3.8',
+    setup_requires=['wheel'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ]
+)
