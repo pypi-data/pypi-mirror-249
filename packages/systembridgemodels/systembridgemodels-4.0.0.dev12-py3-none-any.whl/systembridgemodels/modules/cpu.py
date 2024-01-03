92e801677a27e7bbbe9c1084e1e388831126847b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:02:47 +0000

    Add package systembridgemodels/4.0.0.dev12/systembridgemodels-4.0.0.dev12-py3-none-any.whl

---
+++ packages/systembridgemodels/systembridgemodels-4.0.0.dev12-py3-none-any.whl/systembridgemodels/modules/cpu.py
@@ -0,0 +1,63 @@
+"""CPU."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class CPUFrequency:
+    """CPU Frequency."""
+
+    current: float | None = None
+    min: float | None = None
+    max: float | None = None
+
+
+@dataclass
+class CPUStats:
+    """CPU Stats."""
+
+    ctx_switches: int | None = None
+    interrupts: int | None = None
+    soft_interrupts: int | None = None
+    syscalls: int | None = None
+
+
+@dataclass
+class CPUTimes:
+    """CPU Times."""
+
+    user: float | None = None
+    system: float | None = None
+    idle: float | None = None
+    interrupt: float | None = None
+    dpc: float | None = None
+
+
+@dataclass
+class PerCPU:
+    """Per CPU."""
+
+    frequency: CPUFrequency | None = None
+    power: float | None = None
+    times: CPUTimes | None = None
+    times_percent: CPUTimes | None = None
+    usage: float | None = None
+    voltage: float | None = None
+
+
+@dataclass
+class CPU:
+    """CPU."""
+
+    count: int | None = None
+    frequency: CPUFrequency | None = None
+    load_average: float | None = None
+    per_cpu: list[PerCPU] | None = None
+    power: float | None = None
+    stats: CPUStats | None = None
+    temperature: float | None = None
+    times: CPUTimes | None = None
+    times_percent: CPUTimes | None = None
+    usage: float | None = None
+    voltage: float | None = None
