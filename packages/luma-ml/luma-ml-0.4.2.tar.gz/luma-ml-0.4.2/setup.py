1f3dc3fbcc0aae6f40bb8118cea7c49dc9ae9bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:19 +0000

    Add package luma-ml/0.4.2/luma-ml-0.4.2.tar.gz

---
+++ packages/luma-ml/luma-ml-0.4.2.tar.gz/luma-ml-0.4.2/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="luma-ml",
+    version='0.4.2',
+    author="ChanLumerico",
+    author_email="greensox284@gmail.com",
+    description="Custom-made powerful and light Python machine learning module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChanLumerico/LUMA",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'seaborn'
+    ]
+)
+
+"""
+TODO: update README.md version
+
+>>> python setup.py sdist bdist_wheel
+>>> python -m twine upload dist/*
+"""
+
+# pypi-AgEIcHlwaS5vcmcCJDYyOTk0Zjc5LTVlNzctNDg4NS05YTRhLTQxM2FkNTdlMTcyNAACKlszLCJkZDJkOTMyZi0wOGE1LTRlYTYtOGU5My1hNjM5OGJmMGJiOTIiXQAABiB5XpuH173wM--7xuIBV38NgCtf6Hjedyi6RSNKHIXZkA
