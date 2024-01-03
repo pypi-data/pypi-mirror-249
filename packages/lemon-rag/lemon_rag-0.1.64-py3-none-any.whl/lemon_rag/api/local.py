49e08783adfd09ffbba7251b164ac749844bc893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:04:45 +0000

    Add package lemon-rag/0.1.64/lemon_rag-0.1.64-py3-none-any.whl

---
+++ packages/lemon-rag/lemon_rag-0.1.64-py3-none-any.whl/lemon_rag/api/local.py
@@ -0,0 +1,36 @@
+import threading
+import uuid
+from contextlib import contextmanager
+
+request_context = threading.local()
+
+
+def get_rid() -> str:
+    if hasattr(request_context, "rid"):
+        return request_context.rid
+    return "not-set"
+
+
+def get_user():
+    if hasattr(request_context, "user"):
+        return request_context.user
+    return None
+
+
+@contextmanager
+def with_rid():
+    try:
+        rid = uuid.uuid4().hex
+        request_context.rid = rid
+        yield rid
+    finally:
+        del request_context.rid
+
+
+@contextmanager
+def with_auth(auth_user):
+    try:
+        request_context.user = auth_user
+        yield
+    finally:
+        del request_context.user
