86b809a2cd6a675d9cceb0f405f67315d664bd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:02:58 +0000

    Add package crispr-millipede/0.1.5/crispr_millipede-0.1.5.tar.gz

---
+++ packages/crispr-millipede/crispr_millipede-0.1.5.tar.gz/crispr_millipede-0.1.5/crispr_millipede/modelling/CrisprMillipedeModelling.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+from millipede import NormalLikelihoodVariableSelector
+from millipede import BinomialLikelihoodVariableSelector
+from millipede import NegativeBinomialLikelihoodVariableSelector
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
