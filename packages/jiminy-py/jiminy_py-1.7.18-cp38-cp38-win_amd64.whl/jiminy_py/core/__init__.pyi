e61b2ba81f152c777cba2cf16693441be109b1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:23 +0000

    Add package jiminy-py/1.7.18/jiminy_py-1.7.18-cp38-cp38-win_amd64.whl

---
+++ packages/jiminy-py/jiminy_py-1.7.18-cp38-cp38-win_amd64.whl/jiminy_py/core/__init__.pyi
@@ -0,0 +1,80 @@
+from .core import *
+from .core import __raw_version__ as __raw_version__, __version__ as __version__
+
+__all__ = ['AbstractConstraint', 'AbstractController', 'AbstractControllerFunctor', 'AbstractMotor', 'AbstractPerlinProcess', 'AbstractSensor', 'BaseConstraint', 'BaseController', 'ConstraintsHolder', 'ContactSensor', 'ControllerFunctor', 'DistanceConstraint', 'EffortSensor', 'EncoderSensor', 'Engine', 'EngineMultiRobot', 'ForceCoupling', 'ForceCouplingVector', 'ForceImpulse', 'ForceImpulseVector', 'ForceProfile', 'ForceProfileVector', 'ForceSensor', 'FrameConstraint', 'HeightmapFunctor', 'ImuSensor', 'InitializationFailed', 'JiminyException', 'JointConstraint', 'JointModelType', 'Model', 'NotInitialized', 'PeriodicFourierProcess', 'PeriodicGaussianProcess', 'PeriodicPerlinProcess', 'RandomPerlinProcess', 'Robot', 'SimpleMotor', 'SphereConstraint', 'StepperState', 'SystemState', 'TimeStateFunctorBool', 'TimeStateFunctorPinocchioForce', 'WheelConstraint', 'aba', 'array_copyto', 'build_geom_from_urdf', 'build_models_from_urdf', 'computeJMinvJt', 'computeKineticEnergy', 'crba', 'discretize_heightmap', 'get_joint_position_idx', 'get_joint_type', 'get_random_seed', 'heightmapType_t', 'hresult_t', 'interpolate', 'is_position_valid', 'merge_heightmap', 'random_tile_ground', 'reset_random_generator', 'rnea', 'seed', 'sensorsData', 'sharedMemory', 'solveJMinvJtv', 'sum_heightmap', 'system', 'systemVector', 'get_cmake_module_path', 'get_include', 'get_libraries', '__version__', '__raw_version__']
+
+def get_cmake_module_path() -> str: ...
+def get_include() -> str: ...
+def get_libraries() -> str: ...
+
+# Names in __all__ with no definition:
+#   AbstractConstraint
+#   AbstractController
+#   AbstractControllerFunctor
+#   AbstractMotor
+#   AbstractPerlinProcess
+#   AbstractSensor
+#   BaseConstraint
+#   BaseController
+#   ConstraintsHolder
+#   ContactSensor
+#   ControllerFunctor
+#   DistanceConstraint
+#   EffortSensor
+#   EncoderSensor
+#   Engine
+#   EngineMultiRobot
+#   ForceCoupling
+#   ForceCouplingVector
+#   ForceImpulse
+#   ForceImpulseVector
+#   ForceProfile
+#   ForceProfileVector
+#   ForceSensor
+#   FrameConstraint
+#   HeightmapFunctor
+#   ImuSensor
+#   InitializationFailed
+#   JiminyException
+#   JointConstraint
+#   JointModelType
+#   Model
+#   NotInitialized
+#   PeriodicFourierProcess
+#   PeriodicGaussianProcess
+#   PeriodicPerlinProcess
+#   RandomPerlinProcess
+#   Robot
+#   SimpleMotor
+#   SphereConstraint
+#   StepperState
+#   SystemState
+#   TimeStateFunctorBool
+#   TimeStateFunctorPinocchioForce
+#   WheelConstraint
+#   aba
+#   array_copyto
+#   build_geom_from_urdf
+#   build_models_from_urdf
+#   computeJMinvJt
+#   computeKineticEnergy
+#   crba
+#   discretize_heightmap
+#   get_joint_position_idx
+#   get_joint_type
+#   get_random_seed
+#   heightmapType_t
+#   hresult_t
+#   interpolate
+#   is_position_valid
+#   merge_heightmap
+#   random_tile_ground
+#   reset_random_generator
+#   rnea
+#   seed
+#   sensorsData
+#   sharedMemory
+#   solveJMinvJtv
+#   sum_heightmap
+#   system
+#   systemVector
