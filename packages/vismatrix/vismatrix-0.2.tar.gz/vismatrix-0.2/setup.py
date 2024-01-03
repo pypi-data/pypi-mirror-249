3d9c34790e6cda84c8b27436818f8a226bb12116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:16 +0000

    Add package vismatrix/0.2/vismatrix-0.2.tar.gz

---
+++ packages/vismatrix/vismatrix-0.2.tar.gz/vismatrix-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='vismatrix',
+      author='VolodyaHoi',
+      version='0.2',
+      description='Output matrix or table in beautiful form',
+      long_description="README.md",
+      packages=['vismatrix'],
+      install_requires=['multipledispatch'],
+      url="https://github.com/VolodyaHoi/vismatrix",
+      author_email='i4masyrov@gmail.com',
+      zip_safe=False)
