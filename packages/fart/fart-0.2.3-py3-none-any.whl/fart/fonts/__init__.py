c9cd8615bce432890066e2d859abb35894a17e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:10 +0000

    Add package fart/0.2.3/fart-0.2.3-py3-none-any.whl

---
+++ packages/fart/fart-0.2.3-py3-none-any.whl/fart/fonts/__init__.py
@@ -0,0 +1,11 @@
+"""fart - Figlet ART for documentation """
+import os
+import sys
+
+__version__ = '0.2.3'
+__author__ = "Evan Cushing <evdcush@protonmail.com>"
+
+#=== Pathing hack
+project_path = os.path.abspath(os.path.dirname(__file__))
+if project_path not in sys.path:
+    sys.path.append(project_path)
