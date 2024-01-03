d49d70de6fec3ffba40a58ad8ec59a8d267ece76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:26 +0000

    Add package lemon-rag/0.1.93/lemon_rag-0.1.93.tar.gz

---
+++ packages/lemon-rag/lemon_rag-0.1.93.tar.gz/lemon_rag-0.1.93/lemon_rag/scheduler/base_scheduler.py
@@ -0,0 +1,14 @@
+import threading
+
+from lemon_rag.scheduler.vectorize_sentences import vectorize_sentences
+from lemon_rag.utils import log
+
+lock = threading.Lock()
+
+
+def check_and_do_schedule():
+    if lock.locked():
+        log.info("another thread is already running")
+        return
+    with lock:
+        vectorize_sentences()
