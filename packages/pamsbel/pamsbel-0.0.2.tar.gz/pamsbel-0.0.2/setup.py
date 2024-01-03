dad940f507f10d7cd0613d9dbc0025c06a931921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:02:52 +0000

    Add package pamsbel/0.0.2/pamsbel-0.0.2.tar.gz

---
+++ packages/pamsbel/pamsbel-0.0.2.tar.gz/pamsbel-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+requirements = ["numpy == 1.26.2", "sklearn == 1.2.1", "genser == 1.0.1"]
+
+setuptools.setup(name="pamsbel",
+	version="0.0.2",
+	author="Sergei Zuev",
+	author_email="shoukhov@mail.ru",
+	description="A module for the multiple signals predictive analysis",
+	packages=setuptools.find_packages(),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Programming Language :: Python :: 3.10",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.7',
+)
+
+#"License :: OSI Approved :: GNU", (in classifiers)
+install_requires=requirements
