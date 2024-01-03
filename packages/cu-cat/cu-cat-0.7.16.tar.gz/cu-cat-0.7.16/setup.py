088250921ff7b373e17072e32788e8a8b036a4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:58 +0000

    Add package cu-cat/0.7.16/cu-cat-0.7.16.tar.gz

---
+++ packages/cu-cat/cu-cat-0.7.16.tar.gz/cu-cat-0.7.16/setup.py
@@ -0,0 +1,50 @@
+##github
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+# import versioneer
+
+def unique_flatten_dict(d):
+  return list(set(sum( d.values(), [] )))
+
+core_requires = [
+  'numpy',
+  'pandas',
+  'setuptools',
+  'logging',
+  'typing',
+  'pyarrow>=0.15.0',
+  'scikit-learn',
+  'flake8>=5.0',
+  'mypy',
+  'pytest',
+  'psutil',
+  'build',
+]
+
+gpu_requires = [
+  'cuml', ## cannot test on github actions yet
+  'cudf',
+  'cupy',
+]
+
+# if __name__ == "__main__":
+setup(
+    name='cu-cat',
+    version='v0.07.16',  # versioneer.get_version(),
+    packages = find_packages(),
+    platforms='any',
+    description = 'An end-to-end gpu Python library that encodes categorical variables into machine-learnable numerics',
+    long_description=open("./README.md").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/graphistry/cu-cat',
+    download_url= 'https://github.com/graphistry/cu-cat',
+    python_requires='>3.7',
+    author='The Graphistry Team',
+    author_email='pygraphistry@graphistry.com',
+    install_requires=core_requires,
+    gpu_requires=gpu_requires,
+    license='BSD',
+    # dependency_links=['https://pypi.nvidia.com'],
+    keywords=['cudf', 'cuml', 'GPU', 'Rapids']
+)
