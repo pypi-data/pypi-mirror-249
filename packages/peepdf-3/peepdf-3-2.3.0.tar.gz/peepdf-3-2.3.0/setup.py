7d48696f58767f455e0c94c18484cd4e9b1511f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:38 +0000

    Add package peepdf-3/2.3.0/peepdf-3-2.3.0.tar.gz

---
+++ packages/peepdf-3/peepdf-3-2.3.0.tar.gz/peepdf-3-2.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="peepdf-3",
+    version="2.3.0",
+    author="Jose Miguel Esparza, Corey Forman",
+    license="GNU GPLv3",
+    url="https://github.com/digitalsleuth/peepdf-3",
+    description=("The original peepdf, ported to Python 3, and packaged in a setup"),
+    install_requires=[
+        "requests",
+        "pypdf",
+        "jsbeautifier",
+        "colorama",
+        "Pillow",
+        "pylibemu",
+        "pythonaes",
+        "lxml",
+    ],
+    entry_points={
+        "console_scripts": [
+            "peepdf = peepdf.main:main",
+        ],
+    },
+    packages=[
+        "peepdf",
+    ],
+    package_data={"": ["README.md, COPYING"]},
+)
