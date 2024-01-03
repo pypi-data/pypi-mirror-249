d9ac62e45824047b1b743060e643e0b33ff8bbda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:03:49 +0000

    Add package lemon-rag/0.1.87/lemon_rag-0.1.87.tar.gz

---
+++ packages/lemon-rag/lemon_rag-0.1.87.tar.gz/lemon_rag-0.1.87/lemon_rag/llm/agents/rag/vectorization/runner.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
+
+from lemon_rag.dependencies.data_access import data_access
+from lemon_rag.dependencies.vector_access import vector_access, embed_sentence
+from lemon_rag.lemon_runtime import models
+from lemon_rag.llm.agents.rag.vectorization.content_extractor import Paragraph
+
+vectorization_pool = ThreadPoolExecutor(max_workers=8)
+
+
+def parse_and_vectorize_document(file: models.KnowledgeBaseFileTab, paragraphs: List[Paragraph]):
+    paragraphs_records = data_access.create_paragraphs(file, paragraphs)
+
+
+def start_parse_document(file: models.KnowledgeBaseFileTab, paragraphs: List[Paragraph]):
+    parse_and_vectorize_document(file, paragraphs)
---
+++ packages/lemon-rag/lemon_rag-0.1.87.tar.gz/lemon_rag-0.1.87/lemon_rag/scheduler/base_scheduler.py
@@ -0,0 +1,5 @@
+from lemon_rag.scheduler.vectorize_sentences import vectorize_sentences
+
+
+def check_and_do_schedule():
+    vectorize_sentences()
