5c322002df1faee9648394ebf6e756add4883543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:51 +0000

    Add package pytest-celery/1.0.0a8/pytest_celery-1.0.0a8-py3-none-any.whl

---
+++ packages/pytest-celery/pytest_celery-1.0.0a8-py3-none-any.whl/pytest_celery/__init__.py
@@ -0,0 +1,60 @@
+"""
+pytest-celery a shim pytest plugin to enable celery.contrib.pytest
+"""
+
+# flake8: noqa
+
+
+__version__ = "v1.0.0a8"
+
+
+import re
+from collections import namedtuple
+
+from pytest_celery.api.backend import *
+from pytest_celery.api.base import *
+from pytest_celery.api.broker import *
+from pytest_celery.api.container import *
+from pytest_celery.api.setup import *
+from pytest_celery.api.worker import *
+from pytest_celery.defaults import *
+from pytest_celery.fixtures.backend import *
+from pytest_celery.fixtures.broker import *
+from pytest_celery.fixtures.setup import *
+from pytest_celery.fixtures.worker import *
+from pytest_celery.vendors.memcached.api import *
+from pytest_celery.vendors.memcached.container import *
+from pytest_celery.vendors.memcached.fixtures import *
+from pytest_celery.vendors.rabbitmq.api import *
+from pytest_celery.vendors.rabbitmq.container import *
+from pytest_celery.vendors.rabbitmq.fixtures import *
+from pytest_celery.vendors.redis.backend.api import *
+from pytest_celery.vendors.redis.backend.fixtures import *
+from pytest_celery.vendors.redis.broker.api import *
+from pytest_celery.vendors.redis.broker.fixtures import *
+from pytest_celery.vendors.redis.container import *
+from pytest_celery.vendors.worker.container import *
+from pytest_celery.vendors.worker.fixtures import *
+
+version_info_t = namedtuple(
+    "version_info_t",
+    (
+        "major",
+        "minor",
+        "micro",
+        "releaselevel",
+        "serial",
+    ),
+)
+
+# bumpversion can only search for {current_version}
+# so we have to parse the version here.
+match = re.match(r"(\d+)\.(\d+)\.(\d+)(.+)?", __version__)
+if match:
+    _temp = match.groups()
+else:
+    # Comments are not allowed in the same line as the version string.
+    raise ValueError(f"The version string '{__version__}' does not match the expected pattern.")
+VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or "", "")
+del _temp
+del re
---
+++ packages/pytest-celery/pytest_celery-1.0.0a8-py3-none-any.whl/pytest_celery/api/backend.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from pytest_celery.api.base import CeleryTestCluster
+from pytest_celery.api.base import CeleryTestNode
+from pytest_celery.api.container import CeleryTestContainer
+from pytest_celery.defaults import DEFAULT_WORKER_ENV
+
+
+class CeleryTestBackend(CeleryTestNode):
+    @classmethod
+    def default_config(cls) -> dict:
+        return {
+            "url": DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"],
+            "local_url": DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"],
+        }
+
+    def restart(self, reload_container: bool = True, force: bool = False) -> None:
+        super().restart(reload_container, force)
+        if self.app:
+            self.app.conf.update(
+                result_backend=self.config()["local_url"],
+            )
+
+
+class CeleryBackendCluster(CeleryTestCluster):
+    def __init__(self, *backends: tuple[CeleryTestBackend | CeleryTestContainer]) -> None:
+        super().__init__(*backends)
+
+    def _set_nodes(
+        self,
+        *nodes: tuple[CeleryTestNode | CeleryTestContainer],
+        node_cls: type[CeleryTestNode] = CeleryTestBackend,
+    ) -> tuple[CeleryTestNode]:
+        return super()._set_nodes(*nodes, node_cls=node_cls)
+
+    @classmethod
+    def default_config(cls) -> dict:
+        return {
+            "urls": [DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"]],
+            "local_urls": [DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"]],
+        }
