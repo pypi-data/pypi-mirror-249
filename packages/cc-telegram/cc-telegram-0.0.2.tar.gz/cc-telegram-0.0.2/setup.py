630c8c053da861545e8c898ae67c1e63f02df0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:11 +0000

    Add package cc-telegram/0.0.2/cc-telegram-0.0.2.tar.gz

---
+++ packages/cc-telegram/cc-telegram-0.0.2.tar.gz/cc-telegram-0.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.0.2'
+
+setup(
+    name='cc-telegram',  # package name
+    version=VERSION,  # package version
+    description='my utils package',  # package description
+    packages=find_packages(),
+    url="https://github.com/houm01/cc-telegram",
+    zip_safe=False,
+)
+
