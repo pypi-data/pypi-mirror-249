7e8972770f2bb304ed9118bfa4be5a354ffd3851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:27 +0000

    Add package citros/0.2.10/citros-0.2.10.tar.gz

---
+++ packages/citros/citros-0.2.10.tar.gz/citros-0.2.10/citros_meta.py
@@ -0,0 +1,8 @@
+__title__ = "citros"
+__description__ = "citros"
+__url__ = "https://citros.io"
+__version__ = "0.2.10"
+__author__ = "lulav"
+__author_email__ = "contact@lulav.space"
+__license__ = "LICENSE"
+__copyright__ = "Copyright 2023 Lulav Space"
