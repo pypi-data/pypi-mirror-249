93fb69324972912fdefa7b8fc3f702736d46b65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:02:50 +0000

    Add package fjformer/0.0.21/fjformer-0.0.21.tar.gz

---
+++ packages/fjformer/fjformer-0.0.21.tar.gz/fjformer-0.0.21/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fjformer",
+    version="0.0.21",
+    author="Erfan Zare Chavoshi",
+    author_email="erfanzare82@yahoo.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erfanzar/fjformer",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "chex",
+        "typing",
+        "jax>=0.4.17",
+        "transformers>=4.34.0",
+        "flax>=0.7.1",
+        "optax>=0.1.7",
+        "einops",
+        "msgpack",
+        "ml_collections",
+        "einops",
+        "datasets",
+        "IPython"
+    ],
+    python_requires=">=3.7",
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
