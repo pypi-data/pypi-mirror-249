7428c5a64614880ff74ba6a1ba1260a00ba2540a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:02:58 +0000

    Add package batch-options-price/1.11.0/batch_options_price-1.11.0.tar.gz

---
+++ packages/batch-options-price/batch_options_price-1.11.0.tar.gz/batch_options_price-1.11.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='batch_options_price',
+      version='1.1',
+      description='Batch Black-Scholes pricing European options on stocks without dividends',
+      packages=['batch_options_price'],
+      readme="README.md",
+      author_email='filimoncevvv.anton@gmail.com',
+      zip_safe=False)
