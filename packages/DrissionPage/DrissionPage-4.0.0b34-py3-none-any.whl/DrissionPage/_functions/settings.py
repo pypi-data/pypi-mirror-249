3d237693c688125317bdbde696105ebaab5b35d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:57 +0000

    Add package DrissionPage/4.0.0b34/DrissionPage-4.0.0b34-py3-none-any.whl

---
+++ packages/DrissionPage/DrissionPage-4.0.0b34-py3-none-any.whl/DrissionPage/_functions/settings.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@Author  :   g1879
+@Contact :   g1879@qq.com
+"""
+
+
+class Settings(object):
+    raise_when_ele_not_found = False
+    raise_when_click_failed = False
+    raise_when_wait_failed = False
