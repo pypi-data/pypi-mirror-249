98d2772600c710ccfe3e350102ec30a8df3dc547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:04:43 +0000

    Add package ign8/3.5.41/ign8-3.5.41.tar.gz

---
+++ packages/ign8/ign8-3.5.41.tar.gz/ign8-3.5.41/src/ign8/terraform/terraform.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+
+from ign8 import runme
+
+
+
+
+
+def init():
+    """Initialize terraform"""
+    pass
+
+def plan():
+    """Run terraform plan"""
+    pass
+
+def pluging_install():
+    # we need to have go installed
+    # execute go version to check if go is installed
+    # if go is not installed, install go
+    gotest = runme("go version")
+    print(gotest)
+    
+
+  
+
+
+    # we need to have terraform installed
+
+
+def apply():
+    """Run terraform apply"""
+    pass
+
+
+
