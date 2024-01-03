76480689aa4b4b9f4b0f733595d39914074035b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:03:15 +0000

    Add package slickpy/0.4.6/slickpy-0.4.6.tar.gz

---
+++ packages/slickpy/slickpy-0.4.6.tar.gz/slickpy-0.4.6/src/slickpy/comp.py
@@ -0,0 +1,39 @@
+import sys
+import typing
+
+if sys.version_info[:2] < (3, 7):
+    from asyncio import (  # pragma: nocover
+        _get_running_loop as get_running_loop,
+    )
+else:
+    from asyncio import get_running_loop  # pragma: nocover
+
+try:
+    from ujson import dumps as ujson_dumps, loads as ujson_loads
+except ImportError:  # pragma: nocover
+
+    def ujson_dumps(  # noqa: CFQ002
+        obj: typing.Any,
+        ensure_ascii: bool = True,
+        double_precision: int = 0,
+        encode_html_chars: bool = False,
+        escape_forward_slashes: bool = True,
+        sort_keys: bool = False,
+        indent: int = 0,
+        allow_nan: bool = False,
+        reject_bytes: bool = False,
+        default: typing.Optional[
+            typing.Callable[[typing.Any], typing.Any]
+        ] = None,
+        separators: typing.Optional[typing.Tuple[str, str]] = None,
+    ) -> str:
+        raise AssertionError("The 'ujson' package must be installed.")
+
+    def ujson_loads(
+        s: typing.Union[str, bytes, bytearray],
+        precise_float: bool = True,
+    ) -> typing.Any:
+        raise AssertionError("The 'ujson' package must be installed.")
+
+
+__all__ = ("get_running_loop", "ujson_dumps", "ujson_loads")
