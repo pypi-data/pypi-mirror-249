70036460e93c12da0cfbeabc505d788e842fa9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:54 +0000

    Add package convert-tif-to-png/1.0.6/convert_tif_to_png-1.0.6-py3-none-any.whl

---
+++ packages/convert-tif-to-png/convert_tif_to_png-1.0.6-py3-none-any.whl/convert_tif_to_png/__init__.py
@@ -0,0 +1,5 @@
+"""A utility to convert TIF images to PNG with optional resizing and renaming. Preserve quality of photos from apps like Adobe Lightroom while formatting files for social media sharing."""
+from .main import main
+
+if __name__ == '__main__':
+    main()
