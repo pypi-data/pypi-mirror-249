5f2087e1bac4e91a9e7dd26c70e6571c798a7359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:38 +0000

    Add package lazyfpl/0.0.12/lazyfpl-0.0.12.tar.gz

---
+++ packages/lazyfpl/lazyfpl-0.0.12.tar.gz/lazyfpl-0.0.12/lazyfpl/show.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import argparse
+import itertools
+
+from lazyfpl import fetch, helpers
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--top",
+        "-t",
+        default=None,
+        help="Top N players per position. (default: %(default)s)",
+        type=int,
+    )
+    parser.add_argument(
+        "--no-news",
+        action="store_true",
+        help="Drop players with news attched to them. (default: %(default)s)",
+    )
+    args = parser.parse_args()
+
+    players = sorted(
+        fetch.players(),
+        key=lambda x: (x.position, x.xP or 0),
+        reverse=True,
+    )
+
+    if args.no_news:
+        players = [p for p in players if not p.news]
+
+    players = list(
+        itertools.chain.from_iterable(
+            [
+                list(p)[: args.top]
+                for _, p in itertools.groupby(
+                    players,
+                    key=lambda x: x.position,
+                )
+            ]
+        )
+    )
+    bis = helpers.best_lineup(players)
+
+    print(
+        helpers.tabulater(
+            [p.display() | {"BIS": "X" if p in bis else ""} for p in players],
+        ),
+    )
