3ef1aacb668c6f0d129c5156f3f3ad3fe31efb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:04:48 +0000

    Add package ai21/2.0.0rc6/ai21-2.0.0rc6-py3-none-any.whl

---
+++ packages/ai21/ai21-2.0.0rc6-py3-none-any.whl/ai21/clients/studio/resources/studio_answer.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from ai21.resources.bases.answer_base import Answer, AnswerLength, Mode
+from ai21.resources.responses.answer_response import AnswerResponse
+from ai21.resources.studio_resource import StudioResource
+
+
+class StudioAnswer(StudioResource, Answer):
+    def create(
+        self,
+        context: str,
+        question: str,
+        *,
+        answer_length: Optional[AnswerLength] = None,
+        mode: Optional[Mode] = None,
+        **kwargs,
+    ) -> AnswerResponse:
+        url = f"{self._client.get_base_url()}/{self._module_name}"
+
+        body = self._create_body(context=context, question=question, answer_length=answer_length, mode=mode)
+
+        response = self._post(url=url, body=body)
+
+        return self._json_to_response(response)
