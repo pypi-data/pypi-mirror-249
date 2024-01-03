0884137eb4f4818d3ef97908d639e3e06f076ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:50 +0000

    Add package cosmic-pipeline/0.0.6/cosmic_pipeline-0.0.6-py3-none-any.whl

---
+++ packages/cosmic-pipeline/cosmic_pipeline-0.0.6-py3-none-any.whl/cosmic_pipeline/constants.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class OperandChoices(models.TextChoices):
+    NONE = "none", "Equals"
+    EXACT = "exact", "Exact"
+    IEXACT = "iexact", "Exact (case-insensitive)"
+    GT = "gt", "Greater than"
+    GTE = "gte", "Greater than or equal to"
+    LT = "lt", "Less than"
+    LTE = "lte", "Less than or equal to"
+    IN = "in", "In"
+    ISNULL = "isnull", "Is null"
+
+
+class AnnotateChoices(models.TextChoices):
+    SUM = "sum", "Sum"
+    AVG = "avg", "Average"
+    MAX = "max", "Maximum"
+    MIN = "min", "Minimum"
+    COUNT = "count", "Count"
+
+class QueryOperatorChoices(models.TextChoices):
+    AND = "and", "And"
+    OR = "or", "Or"
+    NOT = "not", "Not"
