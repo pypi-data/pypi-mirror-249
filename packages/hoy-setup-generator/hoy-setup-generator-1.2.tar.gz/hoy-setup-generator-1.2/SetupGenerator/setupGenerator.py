24e7463dca615e73e38ef9de395413fcbffc69ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:33 +0000

    Add package hoy-setup-generator/1.2/hoy-setup-generator-1.2.tar.gz

---
+++ packages/hoy-setup-generator/hoy-setup-generator-1.2.tar.gz/hoy-setup-generator-1.2/SetupGenerator/setupGenerator.py
@@ -0,0 +1,83 @@
+import os, subprocess
+from setuptools import find_packages, setup
+from TypedInput import typedInput
+from KeyboardManager import listenKeyboard
+from slugify import slugify
+
+def setupGenerator():
+
+    clear = lambda: os.system('cls') if os.name == 'nt' else os.system('clear')
+
+    clear()
+    print("Creating setup.py file...")
+
+    name = typedInput("1. Project name: ", str)
+
+    if name != None:
+        name = slugify(name)
+
+    version = typedInput("2. Project version: (1.0) ", float, 1.0)
+
+    description = typedInput("3. Project description: ", str)
+
+    long_description = ""
+    if os.path.isfile("README.md"):
+        with open("README.md", "r", encoding="utf-8") as readme:
+            long_description = readme.read()
+
+    author = typedInput("4. Project author: ", str)
+
+    author_email = typedInput("5. Project author email: ", str)
+
+    url = typedInput("6. Project url: ", str)
+
+    license = typedInput("7. Project license: ", str)
+    
+    clear()
+
+    packages = find_packages()
+
+    # Get Virtual Enviroment name
+    venv = os.environ.get("VIRTUAL_ENV")
+
+    if venv is not None:
+        venv = venv.split("\\")[-1]
+
+        install_requires = None
+        
+        if os.name == "nt":
+            install_requires = subprocess.run([f".\\{ venv }\\Scripts\\python", "-m", "pip", "freeze"], capture_output=True)
+        else:
+            install_requires = subprocess.run(["source", f"./{ venv }/bin/python", "-m", "pip", "freeze"], capture_output=True)
+
+        install_requires = install_requires.stdout.decode("utf-8").split("\n")
+        install_requires = [ package[:-1] for package in install_requires if package != "" ]
+    
+    else:
+        if os.name == "nt":
+            install_requires = subprocess.run(["python", "-m", "pip", "freeze"], capture_output=True)
+        else:
+            install_requires = subprocess.run(["python3", "-m", "pip", "freeze"], capture_output=True)
+
+    entry_points = { 'console_scripts': [ ] }
+    
+    with open("setup.py", "w", encoding="utf-8") as setupFile:
+
+        setupFile.write(
+f"""from setuptools import setup
+
+setup(
+    name="{name}",
+    version={version},
+    description="{description}",
+    long_description=\"\"\"{long_description}\"\"\",
+    long_description_content_type="text/markdown",
+    author="{author}",
+    author_email="{author_email}",
+    url="{url}",
+    packages={packages},
+    install_requires={install_requires},
+    license="{license}",
+    entry_points={entry_points}
+)
+""")
---
+++ packages/hoy-setup-generator/hoy-setup-generator-1.2.tar.gz/hoy-setup-generator-1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="hoy-setup-generator",
+    version=1.2,
+    description="Pequena biblioteca para criar um setup.py rápido.",
+    long_description="""## [SetupGenerator](https://pypi.org/project/hoy-setup-generator/)
+- Pequena biblioteca para criar um setup.py rápido.
+- Clique [**aqui**](https://github.com/Hoyasumii/SetupGenerator) para acessar o repositório.
+---
+## Instalação
+- Você pode baixar pelo pip:
+```
+pip install hoy-setup-generator
+```
+---
+## Como usar?
+- Abra o terminal e digite:
+```
+setup-generator 
+```
+- Depois disso, você será solicitado a digitar o nome do seu projeto, o nome do autor, a versão do projeto, a descrição do projeto, o nome do arquivo principal e o nome do arquivo de instalação. Após isso, o arquivo setup.py será criado.""",
+    long_description_content_type="text/markdown",
+    author="Alan Reis Anjos",
+    author_email="alanreisanjo@gmail.com",
+    url="https://github.com/Hoyasumii/SetupGenerator.git",
+    packages=['SetupGenerator'],
+    install_requires=['hoy-typed-input==1.1', 'hoyl-keyboard-manager==1.0.1', 'keyboard==0.13.5', 'python-slugify==8.0.1', 'setuptools==69.0.3', 'text-unidecode==1.3'],
+    license="GPL-3.0 license",
+    entry_points={'console_scripts': [
+        "setup-generator = SetupGenerator.setupGenerator:setupGenerator"
+    ]}
+)
