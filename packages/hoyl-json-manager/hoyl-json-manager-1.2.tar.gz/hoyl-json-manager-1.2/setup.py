d6f8ec4f38c851f7ea2258d23e75bff908817f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:52 +0000

    Add package hoyl-json-manager/1.2/hoyl-json-manager-1.2.tar.gz

---
+++ packages/hoyl-json-manager/hoyl-json-manager-1.2.tar.gz/hoyl-json-manager-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("./README.md", "r", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(name='hoyl-json-manager',
+    version='1.2',
+    license='MIT License',
+    author='Alan Reis Anjos',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='alanreisanjo@gmail.com',
+    keywords='Hoyl JSON Manager',
+    description='Pequena biblioteca para validação e manipulação de JSONs',
+    packages=['HoylJsonManager'])
