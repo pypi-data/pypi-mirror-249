44fb2a42db9408193f358b3a57ad00d8d0a632e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:57 +0000

    Add package DrissionPage/4.0.0b33/DrissionPage-4.0.0b33.tar.gz

---
+++ packages/DrissionPage/DrissionPage-4.0.0b33.tar.gz/DrissionPage-4.0.0b33/DrissionPage/_pages/chromium_page.pyi
@@ -0,0 +1,79 @@
+# -*- coding:utf-8 -*-
+"""
+@Author  :   g1879
+@Contact :   g1879@qq.com
+"""
+from pathlib import Path
+from typing import Union, Tuple, List, Optional
+
+from .._base.browser import Browser
+from .._configs.chromium_options import ChromiumOptions
+from .._pages.chromium_base import ChromiumBase
+from .._pages.chromium_tab import ChromiumTab
+from .._units.rect import TabRect
+from .._units.setter import ChromiumPageSetter
+from .._units.waiter import PageWaiter
+
+
+class ChromiumPage(ChromiumBase):
+
+    def __init__(self,
+                 addr_or_opts: Union[str, int, ChromiumOptions] = None,
+                 tab_id: str = None,
+                 timeout: float = None):
+        self._chromium_options: ChromiumOptions = ...
+        self._browser: Browser = ...
+        self._rect: Optional[TabRect] = ...
+
+    def _handle_options(self, addr_or_opts: Union[str, ChromiumOptions]) -> str: ...
+
+    def _run_browser(self) -> None: ...
+
+    def _page_init(self) -> None: ...
+
+    @property
+    def browser(self) -> Browser: ...
+
+    @property
+    def tabs_count(self) -> int: ...
+
+    @property
+    def tabs(self) -> List[str]: ...
+
+    @property
+    def wait(self) -> PageWaiter: ...
+
+    @property
+    def main_tab(self) -> str: ...
+
+    @property
+    def latest_tab(self) -> str: ...
+
+    @property
+    def process_id(self) -> Optional[int]: ...
+
+    @property
+    def set(self) -> ChromiumPageSetter: ...
+
+    def save(self, path: Union[str, Path] = None, name: str = None) -> str: ...
+
+    def get_tab(self, tab_id: Union[str, ChromiumTab, int] = None) -> ChromiumTab: ...
+
+    def find_tabs(self, title: str = None, url: str = None,
+                  tab_type: Union[str, list, tuple] = None, single: bool = True) -> Union[str, List[str]]: ...
+
+    def new_tab(self, url: str = None, new_window: bool = False, background: bool = False,
+                new_context: bool = False) -> ChromiumTab: ...
+
+    def _new_tab(self, new_window: bool = False, background: bool = False, new_context: bool = False) -> str: ...
+
+    def close_tabs(self, tabs_or_ids: Union[str, ChromiumTab, List[Union[str, ChromiumTab]],
+    Tuple[Union[str, ChromiumTab]]] = None, others: bool = False) -> None: ...
+
+    def close_other_tabs(self, tabs_or_ids: Union[
+        str, ChromiumTab, List[Union[str, ChromiumTab]], Tuple[Union[str, ChromiumTab]]] = None) -> None: ...
+
+    def quit(self, timeout: float = 5, force: bool = True) -> None: ...
+
+
+def get_rename(original: str, rename: str) -> str: ...
---
+++ packages/DrissionPage/DrissionPage-4.0.0b33.tar.gz/DrissionPage-4.0.0b33/pjstealth/pjstealth.py
@@ -0,0 +1,21 @@
+from playwright.async_api import Page as AsyncPage
+from playwright.sync_api import Page as SyncPage
+from .stealth import StealthConfig
+
+
+def stealth_sync(page: SyncPage, config: StealthConfig = None):
+    """teaches synchronous playwright Page to be stealthy like a ninja!"""
+    navigator_user_agent: str = page.evaluate("navigator.userAgent")
+    navigator_platform = page.evaluate("navigator.platform")
+    c = 0
+    for script in (config or StealthConfig(navigator_user_agent, navigator_platform)).enabled_scripts:
+        page.add_init_script(script)
+
+
+async def stealth_async(page: AsyncPage, config: StealthConfig = None):
+    """teaches asynchronous playwright Page to be stealthy like a ninja!"""
+    navigator_user_agent: str = await page.evaluate("navigator.userAgent")
+    navigator_platform = await page.evaluate("navigator.platform")
+    
+    for script in (config or StealthConfig(navigator_user_agent, navigator_platform)).enabled_scripts:
+        await page.add_init_script(script)
