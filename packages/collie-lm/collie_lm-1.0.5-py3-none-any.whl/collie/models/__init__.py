eebb76b9c0af397a94a778d4f0e171a7bd3ceb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:04:08 +0000

    Add package collie-lm/1.0.5/collie_lm-1.0.5-py3-none-any.whl

---
+++ packages/collie-lm/collie_lm-1.0.5-py3-none-any.whl/collie/models/__init__.py
@@ -0,0 +1,7 @@
+from .llama.model import LlamaForCausalLM
+from .moss.model import MossForCausalLM
+from .chatglm import ChatGLMForCausalLM
+from .base import CollieModelForCausalLM
+from .internlm import InternLMForCausalLM
+from .chatglm2 import ChatGLM2ForCausalLM
+from .moss_moon import Moss003MoonForCausalLM
