b167b57776e76001edd9dcda9a5d356335dfa4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 03:04:53 +0000

    Add package torchrl-nightly/2024.1.2/torchrl_nightly-2024.1.2-cp38-cp38-win_amd64.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2024.1.2-cp38-cp38-win_amd64.whl/torchrl/data/replay_buffers/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .replay_buffers import (
+    PrioritizedReplayBuffer,
+    RemoteTensorDictReplayBuffer,
+    ReplayBuffer,
+    TensorDictPrioritizedReplayBuffer,
+    TensorDictReplayBuffer,
+)
+from .samplers import (
+    PrioritizedSampler,
+    RandomSampler,
+    Sampler,
+    SamplerWithoutReplacement,
+    SliceSampler,
+    SliceSamplerWithoutReplacement,
+)
+from .storages import (
+    LazyMemmapStorage,
+    LazyTensorStorage,
+    ListStorage,
+    Storage,
+    TensorStorage,
+)
+from .writers import (
+    RoundRobinWriter,
+    TensorDictMaxValueWriter,
+    TensorDictRoundRobinWriter,
+    Writer,
+)
