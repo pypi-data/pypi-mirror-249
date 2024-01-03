698214be55532d8ff7c86f3265f2f3200d77354f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 15:04:53 +0000

    Add package quant-wheel/0.0.0/quant_wheel-0.0.0-py2.py3-none-any.whl

---
+++ packages/quant-wheel/quant_wheel-0.0.0-py2.py3-none-any.whl/quant-wheel/__init__.py
@@ -0,0 +1,21 @@
+"""
+# quant-wheel
+A template repository for building python packages. Please replace `$package` with the package's
+name.
+
+## See Also
+### Github repository
+* https://github.com/Chitaoji/quant-wheel/
+
+### PyPI project
+* https://pypi.org/project/quant-wheel/
+
+## License
+This project falls under the BSD 3-Clause License.
+
+"""
+
+from .__version__ import __version__
+from .core import *
+
+__all__ = []
