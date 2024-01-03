3bb5fe0b274e1e3740f66a6f57e242d31772f0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 14:03:13 +0000

    Add package pythonray/0.1.5/pythonray-0.1.5.tar.gz

---
+++ packages/pythonray/pythonray-0.1.5.tar.gz/pythonray-0.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+VERSION="0.1.5"
+
+setup(
+    name='pythonray',
+    version=VERSION,
+    author='Harry Lipnick',
+    author_email='harry.lipnick@gmail.com',
+    description='A package to send messages to Spatie\'s Ray from Python',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python','ray','spatie'],
+)
