1ef61ad814e719b1fd1cd3b87450c80112f8aded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:57 +0000

    Add package xbyte-test-module/3.0/xbyte_test_module-3.0.tar.gz

---
+++ packages/xbyte-test-module/xbyte_test_module-3.0.tar.gz/xbyte_test_module-3.0/Common_Module_of_Xbyte/xbyte_test_module.py
@@ -0,0 +1,5 @@
+def Test():
+    print("This is a test function.")
+
+def Test2():
+    print("This is a second test function.")
---
+++ packages/xbyte-test-module/xbyte_test_module-3.0.tar.gz/xbyte_test_module-3.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='xbyte_test_module',
+    version='3.0',
+    packages=['''Common_Module_of_Xbyte'''],
+)
