4d61817eb4f860ab96a242244d8ab369ec000dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:58 +0000

    Add package ws2webhook/0.1.2/ws2webhook-0.1.2.tar.gz

---
+++ packages/ws2webhook/ws2webhook-0.1.2.tar.gz/ws2webhook-0.1.2/ws2webhook/cli.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+import argparse
+
+from .main import Ws2webhook
+from .version import get_version
+from .const import PACKAGE_NAME
+
+def main():
+    """CLI entrypoint for the ws2webhook package
+    """
+    parser = argparse.ArgumentParser(prog='ws2webhook', usage='ws2webhook -ws ws://localhost/ws -wh http://localhost/webhook', description='ws2webhook')
+    parser.add_argument('-ws', '--websocket_endpoint', help='Websocket endpoint', type=str, required=True)
+    parser.add_argument('-wh', '--webhook_endpoint', help='Webhook endpoint', type=str, required=True)
+    parser.add_argument('-v', '--version', help='Get version', action='version', version=PACKAGE_NAME + ": v" + get_version())
+    parser.add_argument('--verbose', help='Show all logs', action='store_true')
+    args = parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if args.verbose else logging.WARN)
+
+    client = Ws2webhook(args.websocket_endpoint, args.webhook_endpoint)
+    client.run()
+
+if __name__ == "__main__":
+    main()
