4175495012f5666b3ca17776da26a083e25dbce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 04:03:43 +0000

    Add package raphtory/0.7.0/raphtory-0.7.0-cp310-none-win_amd64.whl

---
+++ packages/raphtory/raphtory-0.7.0-cp310-none-win_amd64.whl/raphtory/__init__.py
@@ -0,0 +1,19 @@
+import sys
+from .raphtory import *
+
+sys.modules["raphtory.algorithms"] = algorithms
+sys.modules["raphtory.graph_gen"] = graph_gen
+sys.modules["raphtory.graph_loader"] = graph_loader
+sys.modules["raphtory.vectors"] = vectors
+sys.modules["raphtory.graphql"] = graphql
+
+from .nullmodels import *
+from .plottingutils import *
+
+__doc__ = raphtory.__doc__
+if hasattr(raphtory, "__all__"):
+    __all__ = raphtory.__all__
+
+algorithms.__doc__ = "Algorithmic functions that can be run on Raphtory graphs"
+graph_gen.__doc__ = "Generate Raphtory graphs from attachment models"
+graph_loader.__doc__ = "Load and save Raphtory graphs from/to file(s)"
