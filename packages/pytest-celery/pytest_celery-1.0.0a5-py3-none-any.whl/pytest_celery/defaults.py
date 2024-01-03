c5913955f0b9c64f4dac3b436c7876e0e856b3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-03 05:03:01 +0000

    Add package pytest-celery/1.0.0a5/pytest_celery-1.0.0a5-py3-none-any.whl

---
+++ packages/pytest-celery/pytest_celery-1.0.0a5-py3-none-any.whl/pytest_celery/defaults.py
@@ -0,0 +1,57 @@
+# flake8: noqa
+
+from __future__ import annotations
+
+from pytest_docker_tools import network
+
+from pytest_celery.vendors.memcached.defaults import CELERY_MEMCACHED_BACKEND
+from pytest_celery.vendors.memcached.defaults import *
+from pytest_celery.vendors.rabbitmq.defaults import CELERY_RABBITMQ_BROKER
+from pytest_celery.vendors.rabbitmq.defaults import *
+from pytest_celery.vendors.redis.backend.defaults import CELERY_REDIS_BACKEND
+from pytest_celery.vendors.redis.backend.defaults import *
+from pytest_celery.vendors.redis.broker.defaults import CELERY_REDIS_BROKER
+from pytest_celery.vendors.redis.broker.defaults import *
+from pytest_celery.vendors.redis.defaults import *
+from pytest_celery.vendors.worker.defaults import CELERY_SETUP_WORKER
+from pytest_celery.vendors.worker.defaults import *
+
+##########
+# Fixtures
+##########
+
+CELERY_SETUP = "celery_setup"
+CELERY_WORKER = "celery_worker"
+CELERY_WORKER_CLUSTER = "celery_worker_cluster"
+CELERY_BACKEND = "celery_backend"
+CELERY_BACKEND_CLUSTER = "celery_backend_cluster"
+CELERY_BROKER = "celery_broker"
+CELERY_BROKER_CLUSTER = "celery_broker_cluster"
+
+######################
+# Fixtures collections
+######################
+
+# These collections define which components are used by
+# pytest-celery by default. If not specified otherwise, the
+# user's tests will have a matrix of all possible combinations automatically
+
+ALL_CELERY_WORKERS = (CELERY_SETUP_WORKER,)
+ALL_CELERY_BACKENDS = (
+    CELERY_REDIS_BACKEND,
+    # CELERY_MEMCACHED_BACKEND,  # Beta support at the moment, to be used manually
+)
+ALL_CELERY_BROKERS = (
+    CELERY_REDIS_BROKER,
+    CELERY_RABBITMQ_BROKER,
+)
+
+##########
+# Docker
+##########
+
+CONTAINER_TIMEOUT = 60
+RESULT_TIMEOUT = 60
+
+
+default_pytest_celery_network = network()
