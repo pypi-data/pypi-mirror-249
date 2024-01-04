3943011f84e7fbc40dc32b45fcbca2981535ed6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:37 +0000

    Add package memmpy/0.1.3/memmpy-0.1.3-py3-none-any.whl

---
+++ packages/memmpy/memmpy-0.1.3-py3-none-any.whl/memmpy/_loader.py
@@ -0,0 +1,466 @@
+import math
+import os
+from typing import Any, Callable, Generator, Hashable, Self, TypeAlias
+
+import numpy as np
+import typeguard
+
+from . import _jagged, _labels, _vector
+
+
+data_indexable: TypeAlias = (
+    np.ndarray | np.memmap | _jagged.ReadJagged | _jagged.ReadShaped
+)
+
+
+@typeguard.typechecked
+def load_memmaps(
+    path: str,
+    keys: set[str] | None = None,
+) -> dict[Hashable, data_indexable]:
+    """
+    Load memmaps from a memmpy directory.
+
+    Parameters
+    ---
+    path : str
+        Path to the memmpy directory.
+
+    keys : set[str], optional
+        Keys to load. If None, all keys are loaded.
+
+    Returns
+    ---
+    dict[Hashable, data_indexable]
+        The loaded data. Has the same keys as requested.
+
+    Raises
+    ---
+    ValueError
+        If the path is not found or not a directory.
+
+    KeyError
+        If a requested key is not found in the metadata.
+    """
+    if not os.path.isdir(path):
+        raise ValueError(f"Path '{path}' is not a directory.")
+
+    meta = _labels.safe_load(path)
+    keys = keys or meta["arrays"].keys()
+    data = {}
+
+    for k in keys:
+        if _labels.get_index_name(k) in meta["arrays"]:
+            data[k] = _jagged.ReadJagged(path, k)
+            continue
+
+        if _labels.get_shape_name(k) in meta["arrays"]:
+            data[k] = _jagged.ReadShaped(path, k)
+            continue
+
+        if _labels.get_structured_name(k) in meta["arrays"]:
+            # Skipping structured data.
+            # This is because we do not know how to reconstruct it here.
+            continue
+
+        if k not in meta["arrays"]:
+            raise KeyError(f"Key '{k}' not found in metadata.")
+
+        data[k] = _vector.read_vector(path, k)
+
+    return data
+
+
+@typeguard.typechecked
+def permutation_bands(length: int) -> Callable[[np.ndarray], np.ndarray]:
+    bands_total = int(math.log2(length) ** 2)
+    band_offsets = np.random.uniform(size=bands_total)
+    band_offsets = (band_offsets * length).astype(np.int64)
+
+    @typeguard.typechecked
+    def permutation_func(indicies: np.ndarray) -> np.ndarray:
+        indicies_offsets = indicies // bands_total
+        indicies_remainders = indicies % bands_total
+
+        indicies_offsets += band_offsets[indicies_remainders]
+        indicies_offsets %= length // bands_total
+
+        return indicies_offsets * bands_total + indicies_remainders
+
+    return permutation_func
+
+
+@typeguard.typechecked
+def permutation_modulus(
+    length: int,
+    modulus: int = int(2**17 - 1),
+) -> Callable[[np.ndarray], np.ndarray]:
+    modulus = min(int(modulus), length)
+    subpermutation = np.random.uniform(size=modulus).argsort()
+
+    perm = np.random.uniform(size=modulus)
+    perm = np.argsort(perm)
+
+    @typeguard.typechecked
+    def permute(indicies: np.ndarray) -> np.ndarray:
+        indicies_offsets = indicies // modulus
+        indicies_remainders = indicies % modulus
+
+        indicies_remainders = subpermutation[indicies_remainders]
+
+        result = indicies_offsets * modulus + indicies_remainders
+        return result % length
+
+    return permute
+
+
+@typeguard.typechecked
+def permutation_combination(length: int) -> Callable[[np.ndarray], np.ndarray]:
+    perm1 = permutation_modulus(length)
+    perm2 = permutation_bands(length)
+
+    @typeguard.typechecked
+    def perm(x: np.ndarray) -> np.ndarray:
+        x = perm1(x)
+        x = perm2(x)
+        return x
+
+    return perm
+
+
+@typeguard.typechecked
+def generate_batch_indicies(
+    length: int,
+    batch_size: int,
+    drop_remainder: bool = True,
+) -> Generator[np.ndarray, None, None]:
+    for i in range(0, length - batch_size + 1, batch_size):
+        yield np.arange(i, i + batch_size, dtype=np.int64)
+
+    if not drop_remainder:
+        remainder = length % batch_size
+        yield np.arange(length - remainder, length, dtype=np.int64)
+
+
+def _safe_take_set(s) -> Any:
+    s = set(s)
+
+    if len(s) != 1:
+        raise ValueError("Set does not have size 1.")
+
+    return next(iter(s))
+
+
+@typeguard.typechecked
+class SimpleLoader:
+    """
+    Iterates over a dictionary of numpy arrays or memmaps in batches.
+
+    Attributes
+    ---
+    data : dict[Hashable, np.ndarray | np.memmap]
+        The data to iterate over.
+
+    batch_size : int
+        The batch size.
+    """
+
+    data: dict[Hashable, data_indexable]
+    batch_size: int
+
+    def __init__(
+        self: Self,
+        data: dict[Hashable, data_indexable],
+        batch_size: int = 32,
+    ) -> None:
+        """
+        Initialize an itereable class which can generate batches from a
+        dictionary of numpy arrays or memmaps.
+
+        Parameters
+        ---
+        data : dict[Hashable, data_indexable]
+            The data to iterate over.
+
+        batch_size : int, optional, default: 32
+            The batch size.
+
+        Raises
+        ---
+        ValueError
+            If the data is empty or if the length is not the same for all
+            arrays.
+
+        ValueError
+            If the batch size is not positive.
+
+        ValueError
+            If the batch size is larger than the data length.
+
+        KeyError
+            If the key '_index' is present in the data. This key is used
+            internally already.
+
+        TypeError
+            If the given arguments do not match the type annotations.
+        """
+        if len(data) == 0:
+            raise ValueError("Data is empty.")
+
+        if batch_size <= 0:
+            raise ValueError("Batch size must be positive.")
+
+        if "_index" in data:
+            raise KeyError("Key '_index' is already used internally.")
+
+        self.data = data
+        self.data_len: int = _safe_take_set(len(v) for v in self.data.values())
+
+        if batch_size > self.data_len:
+            raise ValueError("Batch size is larger than the data length.")
+
+        self.batch_size = batch_size
+
+    def __iter__(self: Self) -> Generator[dict, None, None]:
+        """
+        Iterate over the data in batches. The last batch may be smaller than
+        the batch size.
+
+        Yields
+        ---
+        dict
+            The next batch. The keys are the same as the data given to the
+            constructor. A new key '_index' is added which contains the
+            indicies of the batch.
+
+        Raises
+        ---
+        TypeError
+            If the given arguments do not match the type annotations.
+        """
+        data_len_rounded = self.data_len - self.batch_size + 1
+
+        for i in range(0, data_len_rounded, self.batch_size):
+            indicies = np.arange(i, i + self.batch_size, dtype=np.int64)
+            batch = {k: v[indicies] for k, v in self.data.items()}
+            yield batch | {"_index": indicies}
+
+        remainder = self.data_len % self.batch_size
+        if remainder != 0:
+            indicies = np.arange(
+                self.data_len - remainder,
+                self.data_len,
+                dtype=np.int64,
+            )
+            batch = {k: v[indicies] for k, v in self.data.items()}
+            yield batch | {"_index": indicies}
+
+    def __len__(self: Self) -> int:
+        """
+        Get the length of the iterator.
+
+        Returns
+        ---
+        int
+            The length of the iterator.
+        """
+        result = self.data_len // self.batch_size
+
+        if self.data_len % self.batch_size != 0:
+            result += 1
+
+        return result
+
+
+class FixedSeed:
+    def __init__(self, seed: int) -> None:
+        self.seed = seed
+
+    def __enter__(self: Self) -> None:
+        self.state = np.random.get_state()
+        np.random.seed(self.seed)
+
+    def __exit__(self: Self, *_) -> None:
+        np.random.set_state(self.state)
+
+
+@typeguard.typechecked
+class SplitLoader:
+    """
+    Loads batches from a dictionary of numpy arrays or memmaps. This loader is
+    designed to be used in machine learning training loops. It supports
+    shuffling, splitting the data and subindexing, i.e. only loading a subset
+    of the data.
+
+    Attributes
+    ---
+    data : dict[Hashable, data_indexable]
+        The data to iterate over.
+
+    batch_size : int
+        The batch size.
+
+    split : int
+        The split to use. This is an index into the fractions tuple.
+
+    fractions : tuple[float, ...]
+        The fractions of the data to use for training, validation and testing.
+        The fractions must sum to 1.
+
+    subindex : np.memmap | None
+        A memmap containing the indicies to use. If None, all indicies are
+        used.
+
+    split_seed : int
+        The seed to use for splitting the data.
+
+    shuffle : bool
+        Whether to shuffle the data before splitting.
+
+    data_len : int
+        The length of the data.
+
+    split_length : int
+        The length of the split.
+
+    split_start : int
+        The start index of the split in the split permutation
+    """
+
+    data: dict[Hashable, data_indexable]
+    batch_size: int
+    split: int
+    fractions: tuple[float, ...]
+    subindex: np.memmap | np.ndarray | None
+    split_seed: int
+    shuffle: bool
+    data_len: int
+    split_length: int
+    split_start: int
+
+    # TODO if batchsize is larger than length, repeat data
+
+    def __init__(
+        self: Self,
+        data: dict[Hashable, data_indexable],
+        batch_size: int = 32,
+        *,
+        split: int = 0,
+        fractions: tuple[float, ...] = (0.8, 0.1, 0.1),
+        subindex: np.memmap | np.ndarray | None = None,
+        split_seed: int = 0,
+        shuffle=True,
+    ) -> None:
+        """
+        Initialize an itereable class which can generate batches from a
+        dictionary of numpy arrays or memmaps for machine learning training
+        loops.
+
+        Parameters
+        ---
+        data : dict[Hashable, data_indexable]
+            The data to iterate over.
+
+        batch_size : int, optional, default: 32
+            The batch size.
+
+        split : int, optional, default: 0
+            The split to use. This is an index into the fractions tuple.
+
+        fractions : tuple[float, ...], optional, default: (0.8, 0.1, 0.1)
+            The fractions of the data to use for training, validation and
+            testing. The fractions must sum to 1.
+
+        subindex : np.memmap | np.ndarray | None, optional, default: None
+            A memmap containing the indicies to use. If None, all indicies are
+            used.
+
+        split_seed : int, optional, default: 0
+            The seed to use for splitting the data.
+
+        shuffle : bool, optional, default: True
+            Whether to shuffle the data before splitting.
+
+        Raises
+        ---
+        ValueError
+            If the fractions do not sum to 1, or the batch size is not positive.
+
+        TypeError
+            If the given arguments do not match the type annotations.
+        """
+
+        if abs(sum(fractions) - 1.0) > 1e-8:
+            raise ValueError("Fractions must sum to 1.")
+
+        if batch_size <= 0:
+            raise ValueError("Batch size must be positive.")
+
+        self.data = data
+        self.data_len: int = _safe_take_set(len(v) for v in self.data.values())
+
+        if batch_size > self.data_len and subindex is None:
+            factor = math.ceil(batch_size / self.data_len)
+            subindex = np.arange(self.data_len, dtype=np.int64)
+
+        if subindex is not None and batch_size > len(subindex):
+            factor = math.ceil(batch_size / len(subindex))
+            subindex = np.tile(subindex, factor)
+
+        self.batch_size = batch_size
+        self.split = split
+        self.fractions = fractions
+        self.subindex = subindex
+        self.shuffle = shuffle
+
+        if self.subindex is not None:
+            self.data_len = len(self.subindex)
+
+        with FixedSeed(split_seed):
+            self.split_perm = permutation_combination(self.data_len)
+
+        self.split_length = int(self.data_len * fractions[split])
+        self.split_start = int(
+            sum(self.data_len * f for f in fractions[:split]),
+        )
+
+    def __iter__(self: Self) -> Generator[dict, None, None]:
+        """
+        Iterate over the data in batches. If the last batch would be smaller
+        than the batch size, it is dropped.
+
+        Yields
+        ---
+        dict
+            The next batch. The keys are the same as the data given to the
+            constructor. A new key '_index' is added which contains the
+            indicies of the batch.
+        """
+        data_len_rounded = self.split_length - self.batch_size + 1
+
+        if self.shuffle:
+            perm_shuffle = permutation_combination(self.split_length)
+        else:
+            perm_shuffle = lambda x: x
+
+        for i in range(0, data_len_rounded, self.batch_size):
+            indicies = np.arange(i, i + self.batch_size, dtype=np.int64)
+            indicies = self.split_perm(indicies + self.split_start)
+            indicies = perm_shuffle(indicies)
+
+            if self.subindex is not None:
+                indicies = self.subindex[indicies]
+
+            batch = {k: v[indicies] for k, v in self.data.items()}
+            yield batch | {"_index": indicies}
+
+    def __len__(self) -> int:
+        """
+        Get the length of the iterator.
+
+        Returns
+        ---
+        int
+            The length of the iterator.
+        """
+        return self.split_length // self.batch_size
---
+++ packages/memmpy/memmpy-0.1.3-py3-none-any.whl/memmpy/reroot.py
@@ -0,0 +1,299 @@
+try:
+    import uproot
+except ImportError:
+    error = "'Uproot' is a requirement for 'memmpy.reroot', but was not found."
+    raise ImportError(error)
+
+import dataclasses
+import hashlib
+import os
+from typing import Any
+
+import awkward as ak
+import numpy as np
+import tqdm
+import typeguard
+
+from . import _labels, _loader, _subset, _vector
+
+__all__ = [
+    "RFileConfig",
+    "memmap_vectors_root",
+    "load_root",
+]
+
+
+@dataclasses.dataclass(frozen=True)
+class RFileConfig:
+    """
+    Stores the configuration for a single ROOT file.
+
+    Parameters
+    ---
+    path: str
+        Path to the ROOT file.
+
+    tree: str, optional, default: "nominal_Loose"
+        Name of the tree to read from.
+
+    metadata: dict[str, str], optional, default: {}
+        Metadata to store in the memmap file. This can be used to store
+        information about the what is stored in the file, i.e. from which
+        process the data originates, or which run it is from.
+    """
+
+    path: str
+    tree: str = dataclasses.field(default="nominal_Loose")
+    metadata: dict[str, str] = dataclasses.field(default_factory=dict)
+
+
+def _encode_metadata(
+    metadata: dict[str, str],
+    size: int,
+) -> dict[str, np.ndarray]:
+    metadata_encoded = {}
+
+    for k, v in metadata.items():
+        hashed = _labels.hash_64bit(v.encode())
+        metadata_encoded[k] = np.full(
+            size,
+            hashed,
+            dtype="uint64",
+        )
+
+    return metadata_encoded
+
+
+@typeguard.typechecked
+def memmap_vectors_root(
+    root_files: list[RFileConfig],
+    path_mmap: str,
+    keys: set[str],
+    keys_padded: dict[str, tuple[int, Any]] | None = None,
+    aliases: dict[str, str] | None = None,
+    recreate: bool = False,
+) -> None:
+    """
+    Store the data from the given ROOT files in a memmap files.
+
+    Parameters
+    ---
+    root_files: list[RFileConfig]
+        List of ROOT file configurations to read from, i.e. the path to the file,
+        the name of the tree to read from, and metadata to store in memmaps.
+
+    path_mmap: str
+        Path to the memmap directory.
+
+    keys: set[str]
+        Set of keys to read from the ROOT files. The keys must be scalar valued.
+
+    keys_padded: dict[str, tuple[int, Any]], optional, default: None
+        Dictionary of 1d vector valued keys to read from the ROOT files. The
+        dictionary maps the key to a tuple of the size of the vector and the
+        value to pad with.
+
+    aliases: dict[str, str], optional, default: None
+        Dictionary of aliases to use for the keys.
+
+    recreate: bool, optional, default: False
+        If True, the memmap files are recreated even if they already exist.
+
+    Raises
+    ---
+    FileNotFoundError
+        If a ROOT file is not found.
+
+    TypeError
+        If the arguments do not match the type hints.
+    """
+    keys_padded = keys_padded or {}
+    aliases = aliases or {}
+    keys = keys | set(keys_padded.keys())
+
+    hasher = hashlib.sha256()
+    hasher.update(str(aliases).encode())
+
+    for rfile in root_files:
+        hasher.update(os.path.basename(rfile.path).encode())
+        hasher.update(str(os.path.getmtime(rfile.path)).encode())
+        hasher.update(str(os.path.getsize(rfile.path)).encode())
+        hasher.update(str(rfile.tree).encode())
+        hasher.update(str(rfile.metadata).encode())
+
+    hashed = hasher.hexdigest()
+
+    if not recreate:
+        meta = _labels.safe_load(path_mmap)
+
+        for k in keys:
+            if k not in meta["arrays"]:
+                break
+
+            if meta["arrays"][k]["check"] != hashed:
+                break
+
+            if k in keys_padded:
+                if meta["arrays"][k]["shape"][-1] != keys_padded[k][0]:
+                    break
+
+        else:
+            path_mmap = os.path.abspath(path_mmap)
+            print(f"Using cached files in '{path_mmap}' for {list(keys)}.")
+            return
+
+    with _vector.WriteVectorDict(path_mmap, check=hashed) as vectors:
+        for rfile in tqdm.tqdm(root_files):
+            tqdm.tqdm.write(f"processing '{rfile.path}'")
+
+            if not os.path.exists(rfile.path):
+                raise FileNotFoundError(f"File '{rfile.path}' not found.")
+
+            with uproot.open(rfile.path) as file:  # type: ignore
+                data_root = file[rfile.tree].arrays(  # type: ignore
+                    keys,
+                    aliases=aliases,
+                    library="ak",
+                    how=dict,
+                )
+                data_numpy = {}
+
+                for k, v in data_root.items():
+                    if k in keys_padded:
+                        assert v.ndim == 2
+                        pad_size, pad_value = keys_padded[k]
+                        array = ak.pad_none(v, pad_size, clip=True)
+                        array = ak.fill_none(array, pad_value)
+                        data_numpy[k] = array.to_numpy()  # type: ignore
+                        continue
+
+                    assert v.ndim == 1
+                    data_numpy[k] = v.to_numpy()
+
+                size = next(iter(data_numpy.values())).shape[0]
+
+                data_path = _encode_metadata(rfile.metadata, size)
+                vectors.expand(data_numpy | data_path)
+
+        print("Saving...")
+
+    # add hahses
+    metadata_values = set()
+
+    for rfile in root_files:
+        metadata_values |= set(rfile.metadata.values())
+
+    for value in metadata_values:
+        _labels.add_hash(path_mmap, value)
+
+    print("Saving done.")
+
+
+@typeguard.typechecked
+def load_root(
+    root_files: list[RFileConfig],
+    path_mmap: str,
+    keys: set[str],
+    keys_padded: dict[str, tuple[int, Any]] | None = None,
+    tcut: str | None = None,
+    aliases: dict[str, str] | None = None,
+    recreate: bool = False,
+    batch_size: int = 32,
+    split: int = 0,
+    fractions: tuple[float, ...] = (0.8, 0.1, 0.1),
+    split_seed: int = 0,
+    shuffle=True,
+    cut_constants: dict[str, Any] | None = None,
+) -> _loader.SplitLoader:
+    """
+    Save data from ROOT files in memmap files and load them into a SplitLoader.
+
+    Parameters
+    ---
+    root_files: list[RFileConfig]
+        List of ROOT file configurations to read from, i.e. the path to the file,
+        the name of the tree to read from, and metadata to store in memmaps.
+
+    path_mmap: str
+        Path to the memmap directory.
+
+    keys: set[str]
+        Set of keys to read from the ROOT files. The keys must be scalar valued.
+
+    keys_padded: dict[str, tuple[int, Any]], optional, default: None
+        Dictionary of 1d vector valued keys to read from the ROOT files. The
+        dictionary maps the key to a tuple of the size of the vector and the
+        value to pad with.
+
+    tcut: str, optional, default: None
+        Cut on which events to load.
+
+    aliases: dict[str, str], optional, default: None
+        Dictionary of aliases to use for the keys.
+
+    recreate: bool, optional, default: False
+        If True, the memmap files are recreated even if they already exist.
+
+    batch_size: int, optional, default: 32
+        Batch size to use for the SplitLoader.
+
+    split: int, optional, default: 0
+        Which split to use for the SplitLoader.
+
+    fractions: tuple[float, ...], optional, default: (0.8, 0.1, 0.1)
+        Fractions to use for the SplitLoader.
+
+    split_seed: int, optional, default: 0
+        Seed to use for the SplitLoader.
+
+    shuffle: bool, optional, default: True
+        If True, the SplitLoader shuffles the data.
+
+    cut_constants: dict[str, Any], optional, default: None
+        Constant values to use in the cut.
+
+    Returns
+    ---
+    SplitLoader
+        Loader to load the data from the memmap files.
+
+    Raises
+    ---
+    FileNotFoundError
+        If a ROOT file is not found.
+
+    TypeError
+        If the arguments do not match the type hints.
+    """
+    keys_mmemap = keys
+    if tcut is not None:
+        keys_mmemap = keys_mmemap | _subset._get_symbols(tcut)
+
+    memmap_vectors_root(
+        root_files,
+        path_mmap,
+        keys_mmemap,
+        keys_padded,
+        aliases,
+        recreate,
+    )
+    keys_metadata = set()
+    for rfile in root_files:
+        keys_metadata |= set(rfile.metadata.keys())
+
+    data = _loader.load_memmaps(path_mmap, keys | keys_metadata)
+
+    subindex = None
+    if tcut is not None:
+        _subset.compute_cut_batched(path_mmap, tcut, constants=cut_constants)
+        subindex = _vector.read_vector(path_mmap, tcut)
+
+    return _loader.SplitLoader(
+        data,
+        batch_size,
+        split=split,
+        fractions=fractions,
+        subindex=subindex,
+        split_seed=split_seed,
+        shuffle=shuffle,
+    )
