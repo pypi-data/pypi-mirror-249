2725e12ed9f35ad3ba35487ffa6c1b66c325e7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:03:45 +0000

    Add package cwhois/1.3.0/cwhois-1.3.0.tar.gz

---
+++ packages/cwhois/cwhois-1.3.0.tar.gz/cwhois-1.3.0/as32_del.h
@@ -0,0 +1,11 @@
+{ 131077u, 131086u,	"whois.nic.ad.jp" },
+{ 131092u, 131101u,	"whois.nic.or.kr" },
+{ 131152u, 131161u,	"whois.nic.ad.jp" },
+{ 131791u, 131890u,	"whois.nic.or.kr" },
+{ 131893u, 131992u,	"whois.nic.ad.jp" },
+{ 131072u, 196607u,	"whois.apnic.net" },	/* 2.0 2.65535 */
+{ 196608u, 262143u,	"whois.ripe.net" },	/* 3.0 3.65535 */
+{ 262144u, 327679u,	"whois.lacnic.net" },	/* 4.0 4.65535 */
+{ 327680u, 393215u,	"whois.afrinic.net" },	/* 5.0 5.65535 */
+{ 393216u, 458751u,	"whois.arin.net" },	/* 6.0 6.65535 */
+{ 4200000000u, 4294967294u,	"whois.ripe.net" },
