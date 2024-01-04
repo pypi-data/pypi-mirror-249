926a512cde59f35f5279a0493e3e6a9ec687b438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:04:08 +0000

    Add package dr-eval/0.0.1/dr-eval-0.0.1.tar.gz

---
+++ packages/dr-eval/dr-eval-0.0.1.tar.gz/dr-eval-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dr-eval',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Dr. Eval',
+    long_description='Upcoming LLM test framework',
+    author='lumpenspace',
+    author_email='lumpenspace@gmail.com',
+    url='https://github.com/lumpenspace/dr-eval',
+    install_requires=[],
+)
