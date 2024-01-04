a15314835a54c7091780f6200930527f3c80d1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:03:51 +0000

    Add package pyxllib/0.3.87/pyxllib-0.3.87.tar.gz

---
+++ packages/pyxllib/pyxllib-0.3.87.tar.gz/pyxllib-0.3.87/pyxllib/xl.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Date   : 2021/06/03 22:08
+
+""" pyxllib常用功能
+"""
+import fire
+
+from pyxllib.file.packlib import *
+
+from pyxllib.prog.newbie import *
+from pyxllib.prog.pupil import *
+from pyxllib.prog.specialist import *
+from pyxllib.prog.deprecatedlib import deprecated
+
+from pyxllib.algo.newbie import *
+from pyxllib.algo.pupil import *
+from pyxllib.algo.specialist import *
+
+from pyxllib.text.newbie import *
+from pyxllib.text.pupil import *
+from pyxllib.text.specialist import *
+
+from pyxllib.file.newbie import *
+from pyxllib.file.pupil import *
+from pyxllib.file.specialist import *
+
+if __name__ == '__main__':
+    # 直接运行的话，支持开放出所有函数类接口
+    fire.Fire()
