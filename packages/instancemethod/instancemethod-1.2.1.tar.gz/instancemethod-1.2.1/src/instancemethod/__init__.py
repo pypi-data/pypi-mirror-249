7cd146a77156670534b0934ba7add4dc201ac9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:39 +0000

    Add package instancemethod/1.2.1/instancemethod-1.2.1.tar.gz

---
+++ packages/instancemethod/instancemethod-1.2.1.tar.gz/instancemethod-1.2.1/src/instancemethod/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "decorator",
+]
+__version__ = "1.2.1"
+AUTHOR = {
+    "Name": "Braden Toone",
+    "Email": "braden@toonetown.com"
+}
+HOMEPAGE = "https://github.com/Braden2n/InstanceMethod"
+DOCUMENTATION = "https://github.com/Braden2n/InstanceMethod"
+ISSUES = "https://github.com/Braden2n/InstanceMethod/issues"
+REPOSITORY = "https://github.com"
+CHANGELOG = "https://github.com/Braden2n/InstanceMethod/activity"
