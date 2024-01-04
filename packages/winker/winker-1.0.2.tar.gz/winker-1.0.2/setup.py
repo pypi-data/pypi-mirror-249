b4c165172240ca6560d4aaf6db2ad946d2ab23f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:03:31 +0000

    Add package winker/1.0.2/winker-1.0.2.tar.gz

---
+++ packages/winker/winker-1.0.2.tar.gz/winker-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='winker',
+    version='1.0.2',
+    packages=find_packages(),
+    package_data={'': ['*.html']},
+    include_package_data=True,
+    author='KVI Kontent',
+    author_email='kviappsgames@gmail.com',
+    description='A library for creating web apps in Python with futuristic minimalistic design',
+    url='https://kvi-kontent.gitbook.io/winker/',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'winker = winker.main:main'
+        ]
+    }
+)
