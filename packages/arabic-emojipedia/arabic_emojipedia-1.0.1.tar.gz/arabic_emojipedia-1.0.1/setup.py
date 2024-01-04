d3ab007682843bafe76a77cb3e686aa7f197d25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:56 +0000

    Add package arabic-emojipedia/1.0.1/arabic_emojipedia-1.0.1.tar.gz

---
+++ packages/arabic-emojipedia/arabic_emojipedia-1.0.1.tar.gz/arabic_emojipedia-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arabic_emojipedia',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='A Python package to get emoji descriptions in Arabic',
+    author='Anass Ibrahimi',
+    author_email='a.ibrahimi@tum.de',
+    url='https://github.com/a-ibrahimi/Arabic-Emojipedia',
+)
