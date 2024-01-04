a7e740e79d078a27b1d82125fe278555bf7a14e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:04:17 +0000

    Add package tku/1.0.0/tku-1.0.0.tar.gz

---
+++ packages/tku/tku-1.0.0.tar.gz/tku-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(name='tku',
+      version='1.0.0',
+      description='use Tkinter',
+      author='ZhaoXiJie',
+      author_email='3208733169@qq.com',
+      requires= [],
+      packages=find_packages(),  
+      license="apache 3.0"
+      )
+
