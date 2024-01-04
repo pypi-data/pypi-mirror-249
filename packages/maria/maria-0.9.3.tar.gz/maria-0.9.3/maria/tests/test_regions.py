9b978a8dd7c2385933aaa4baa01ae93d85fa75b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:04:33 +0000

    Add package maria/0.9.3/maria-0.9.3.tar.gz

---
+++ packages/maria/maria-0.9.3.tar.gz/maria-0.9.3/maria/tests/test_regions.py
@@ -0,0 +1,12 @@
+import time
+
+import pytest
+
+import maria
+from maria.weather import Weather
+
+
+@pytest.mark.parametrize("region_name", maria.all_regions)
+def test_weather(region_name):
+    weather = Weather(t=time.time(), region=region_name)
+    print(f"pwv={weather.pwv:.03f}mm")
