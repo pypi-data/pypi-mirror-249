492d1708e3250f6a1161295b455d95c4a18bcdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:28 +0000

    Add package DRAM-bio/1.5.0/DRAM-bio-1.5.0.tar.gz

---
+++ packages/DRAM-bio/DRAM-bio-1.5.0.tar.gz/DRAM-bio-1.5.0/tests/test_config_cleen.py
@@ -0,0 +1,52 @@
+"""
+One test to stop so much pain, did you accidentally commit a test config?
+This tests that the config is clean. It will fail when you are testing a working copy but not when you push to github.
+
+"""
+
+import pytest
+import logging
+from os import path
+from mag_annotator.database_handler import DatabaseHandler
+from mag_annotator.utils import setup_logger
+
+
+
+def test_clean_config():
+    empty_config = {
+        "search_databases": {
+        "kegg": None,
+        "kofam_hmm": None,
+        "kofam_ko_list": None,
+        "uniref": None,
+        "pfam": None,
+        "dbcan": None,
+        "viral": None,
+        "peptidase": None,
+        "vogdb": None,
+        "camper_fa_db": None,
+        "camper_fa_db_cutoffs": None,
+        "camper_hmm": None,
+        "camper_hmm_cutoffs": None
+      },
+      "custom_dbs": None,
+      "database_descriptions": {
+        "pfam_hmm_dat": None,
+        "dbcan_fam_activities": None,
+        "dbcan_subfam_ec": None,
+        "vog_annotations": None
+      },
+      "dram_sheets": {
+        "camper_distillate": None,
+        "genome_summary_form": None,
+        "module_step_form": None,
+        "etc_module_database": None,
+        "function_heatmap_form": None,
+        "amg_database": None
+      },
+      "description_db": None,
+      "dram_version": None
+    }
+    logger = logging.getLogger('test_log')
+    db_handler = DatabaseHandler(logger)
+    assert db_handler.config == empty_config
