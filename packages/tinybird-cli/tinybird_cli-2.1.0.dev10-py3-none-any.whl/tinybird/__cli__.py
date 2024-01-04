3718dda451c132cb20b39337302d2fd392b750a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:32 +0000

    Add package tinybird-cli/2.1.0.dev10/tinybird_cli-2.1.0.dev10-py3-none-any.whl

---
+++ packages/tinybird-cli/tinybird_cli-2.1.0.dev10-py3-none-any.whl/tinybird/__cli__.py
@@ -0,0 +1,8 @@
+
+__name__ = 'tinybird-cli'
+__description__ = 'Tinybird Command Line Tool'
+__url__ = 'https://docs.tinybird.co/cli.html'
+__author__ = 'Tinybird'
+__author_email__ = 'support@tinybird.co'
+__version__ = '2.1.0.dev10'
+__revision__ = '1fec830'
---
+++ packages/tinybird-cli/tinybird_cli-2.1.0.dev10-py3-none-any.whl/tinybird/tb_cli.py
@@ -0,0 +1,24 @@
+import sys
+import asyncio
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+import tinybird.tb_cli_modules.common
+import tinybird.tb_cli_modules.cli
+import tinybird.tb_cli_modules.auth
+import tinybird.tb_cli_modules.workspace
+import tinybird.tb_cli_modules.workspace_members
+import tinybird.tb_cli_modules.branch
+import tinybird.tb_cli_modules.datasource
+import tinybird.tb_cli_modules.pipe
+import tinybird.tb_cli_modules.connection
+import tinybird.tb_cli_modules.job
+import tinybird.tb_cli_modules.test
+import tinybird.tb_cli_modules.token
+
+
+cli = tinybird.tb_cli_modules.cli.cli
+
+if __name__ == "__main__":
+    cli()
