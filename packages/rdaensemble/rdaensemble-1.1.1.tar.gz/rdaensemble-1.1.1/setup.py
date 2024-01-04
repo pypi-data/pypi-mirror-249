3e30d8963bd17856785df2e5c64f06f031d55eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:15 +0000

    Add package rdaensemble/1.1.1/rdaensemble-1.1.1.tar.gz

---
+++ packages/rdaensemble/rdaensemble-1.1.1.tar.gz/rdaensemble-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdaensemble",
+    version="1.1.1",
+    description="Redistricting ensembles",
+    url="https://github.com/rdatools/rdaensemble",
+    author="alecramsay",
+    author_email="a73cram5ay@gmail.com",
+    license="MIT",
+    packages=[
+        "rdaensemble",
+        "rdaensemble.mcmc",
+        "rdaensemble.rmfrsp",
+        "rdaensemble.rmfrst",
+        "rdaensemble.smc",
+        "rdaensemble.ust",
+    ],
+    install_requires=["rdabase", "rdascore"],
+    zip_safe=False,
+)
