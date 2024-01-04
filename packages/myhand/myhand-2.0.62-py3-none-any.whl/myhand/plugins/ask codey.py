1cd79f00fc58706a67c57c9216af07e52c117d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:38 +0000

    Add package myhand/2.0.62/myhand-2.0.62-py3-none-any.whl

---
+++ packages/myhand/myhand-2.0.62-py3-none-any.whl/myhand/plugins/ask codey.py
@@ -0,0 +1,37 @@
+"""
+LetMeDoIt AI Plugin - ask Codey
+
+Ask Google Codey for information about coding
+
+[FUNCTION_CALL]
+"""
+
+
+from myhand import config
+from myhand.palm2 import VertexAIModel
+
+def ask_codey(function_args):
+    query = function_args.get("query") # required
+    config.stopSpinning()
+    VertexAIModel().run(query, temperature=config.llmTemperature)
+    return ""
+
+functionSignature = {
+    "name": "ask_codey",
+    "description": "Ask Codey for information about coding",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The request in detail",
+            },
+        },
+        "required": ["query"],
+    },
+}
+
+config.pluginsWithFunctionCall.append("ask_codey")
+config.chatGPTApiFunctionSignatures.append(functionSignature)
+config.chatGPTApiAvailableFunctions["ask_codey"] = ask_codey
+config.inputSuggestions.append("Ask Codey: ")
