5071218c53dcd5f7b6a53189f32a7510fefe12df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:03:51 +0000

    Add package udak-probability/0.1/udak_probability-0.1.tar.gz

---
+++ packages/udak-probability/udak_probability-0.1.tar.gz/udak_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="udak_probability",
+      version="0.1",
+      description="Gaussian and Binomial distributions",
+      packages=['udak_probability'],
+	  author='Alan Kang',
+	  author_email = 'kangfucius@gmail.com',
+      zip_safe=False)
+
