bd2510229ab4bde8dc7df459759f0f358fe5ad81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:05:49 +0000

    Add package pdal/3.3.0/pdal-3.3.0.tar.gz

---
+++ packages/pdal/pdal-3.3.0.tar.gz/pdal-3.3.0/pdal/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "3.3.0"
+__all__ = ["Pipeline", "Stage", "Reader", "Filter", "Writer", "dimensions", "info"]
+
+from . import libpdalpython
+from .drivers import inject_pdal_drivers
+from .pipeline import Filter, Pipeline, Reader, Stage, Writer
+
+inject_pdal_drivers()
+dimensions = libpdalpython.getDimensions()
+info = libpdalpython.getInfo()
+
+del inject_pdal_drivers, libpdalpython
