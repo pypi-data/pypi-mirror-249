06e76078ed12a71a236862e38a138ec227a38a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:28 +0000

    Add package zetascale/1.4.0/zetascale-1.4.0.tar.gz

---
+++ packages/zetascale/zetascale-1.4.0.tar.gz/zetascale-1.4.0/zeta/nn/embeddings/__init__.py
@@ -0,0 +1,51 @@
+from zeta.nn.embeddings.abc_pos_emb import AbsolutePositionalEmbedding
+from zeta.nn.embeddings.embedding import (
+    BaseEmbedding,
+    Embedding,
+    TextEmbedding,
+)
+from zeta.nn.embeddings.multiway_network import (
+    MultiwayEmbedding,
+    MultiwayNetwork,
+)
+from zeta.nn.embeddings.nominal_embeddings import NominalEmbedding
+from zeta.nn.embeddings.positional import PositionalEmbedding
+from zeta.nn.embeddings.positional_interpolation import (
+    PositionInterpolationEmbeddings,
+)
+from zeta.nn.embeddings.rope import RotaryEmbedding
+from zeta.nn.embeddings.sinusoidal import SinusoidalEmbeddings
+from zeta.nn.embeddings.truncated_rope import TruncatedRotaryEmbedding
+from zeta.nn.embeddings.vis_lang_emb import VisionLanguageEmbedding
+from zeta.nn.embeddings.xpos_relative_position import (
+    XPOS,
+    apply_rotary_pos_emb,
+    rotate_every_two,
+)
+from zeta.nn.embeddings.yarn import YarnEmbedding
+from zeta.nn.embeddings.sine_positional import SinePositionalEmbedding
+from zeta.nn.embeddings.qft_embeddings import QFTSPEmbeddings
+from zeta.nn.embeddings.qfsp_embeddings import QFTSPEmbedding
+
+__all__ = [
+    "AbsolutePositionalEmbedding",
+    "BaseEmbedding",
+    "Embedding",
+    "TextEmbedding",
+    "MultiwayEmbedding",
+    "MultiwayNetwork",
+    "NominalEmbedding",
+    "PositionalEmbedding",
+    "PositionInterpolationEmbeddings",
+    "RotaryEmbedding",
+    "SinusoidalEmbeddings",
+    "TruncatedRotaryEmbedding",
+    "VisionLanguageEmbedding",
+    "XPOS",
+    "apply_rotary_pos_emb",
+    "rotate_every_two",
+    "YarnEmbedding",
+    "SinePositionalEmbedding",
+    "QFTSPEmbeddings",
+    "QFTSPEmbedding",
+]
