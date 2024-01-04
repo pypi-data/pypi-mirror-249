ee1ccff2419dc96b913838ec3d48641190eef79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:03:11 +0000

    Add package ign8/3.7.17/ign8-3.7.17-py3-none-any.whl

---
+++ packages/ign8/ign8-3.7.17-py3-none-any.whl/ign8/main.py
@@ -0,0 +1,29 @@
+from .common import prettyllog
+import os
+import subprocess
+
+def main():
+    prettyllog("main", "check", "main", "all", "200", "Success")
+    return True
+
+def serve():
+    prettyllog("main", "check", "main", "all", "200", "Success")
+    os.system("pip install --upgrade ign8 >/dev/null 2>&1")
+    os.chdir("/usr/local/lib/python3.9/site-packages/ign8/ui/project/ignite/")
+    os.system("python manage.py makemigrations")
+    os.system("python manage.py migrate")
+    #p = Popen(['espeak', '-b', '1'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    #gunicorn = subprocess.Popen(["gunicorn", "ignite.wsgi", "-c", "gunicorn.conf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    gunicorn = subprocess.Popen(["gunicorn", "ignite.wsgi", "-c", "gunicorn.conf"])
+    prettyllog("main", "check", "main", "all", "200", "Success", "info")
+    gunicorn.wait()
+
+
+
+#    os.command("gunicorn ignite.wsgi -c gunicorn.conf")
+    # change working directory to the root of the project
+
+
+
+
+    return True
