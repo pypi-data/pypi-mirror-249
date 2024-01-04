4ddc9a4cec6c8ad284daabcfbc2aa5e271ae48bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:03:42 +0000

    Add package pygoml/0.0.1/pygoml-0.0.1.tar.gz

---
+++ packages/pygoml/pygoml-0.0.1.tar.gz/pygoml-0.0.1/README.md
@@ -0,0 +1,4 @@
+# PyGOML: Decision Diagram based Branch-and-Bound in combination with Machine Learning
+
+Under constructio! Not ready for use yet! Currently experimenting and planning.
+Developed by Abdelouahed Ben Mhamed, a Ph.D. student at the Moroccan International Center for Artificial Intelligence (AI Movement).
---
+++ packages/pygoml/pygoml-0.0.1.tar.gz/pygoml-0.0.1/pygoml/__init__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+"""
+DDBnB - Distributed Database for Neural Networks in Python
+
+Disclaimer:
+===========================================================================
+This Python library, PyGOML, is developed as part of a Ph.D. thesis at the International Center
+for Artificial Intelligence in Morocco (AI-Movement). The library is a work in progress and is
+provided 'as is' without any warranties or guarantees of any kind.
+
+The author and contributors make no representations or warranties regarding the accuracy, completeness,
+or suitability of the software, either express or implied, including but not limited to the implied
+warranties of merchantability, fitness for a particular purpose, or non-infringement.
+
+In no event shall the authors or copyright holders be liable for any claim, damages, or other liability,
+whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the
+software or the use or other dealings in the software.
+
+Users are encouraged to provide feedback, report issues, and contribute to the improvement of the library.
+However, the development team reserves the right to make changes to the library without prior notice.
+
+By using this software, you agree to the terms of this disclaimer and acknowledge the inherent risks
+associated with experimental software development.
+
+For more information about PyGOML please feel free to reach out to the author at:
+- a.benmhamed@intelligentica.net or
+- abdelouahed.benmhamed@um6p.ma
+
+===========================================================================
+"""
