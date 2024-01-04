7c384f463c7dbec0cbdece0ce235bd02af6d6e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 06:03:52 +0000

    Add package livekit-agents/0.0.27/livekit-agents-0.0.27.tar.gz

---
+++ packages/livekit-agents/livekit-agents-0.0.27.tar.gz/livekit-agents-0.0.27/livekit/agents/__init__.py
@@ -0,0 +1,48 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .version import __version__
+
+from .worker import (
+    Worker,
+    JobCancelledError,
+    AssignmentTimeoutError,
+    run_app,
+    JobType,
+)
+
+from .job_request import SubscribeCallbacks, AutoDisconnectCallbacks, JobRequest
+from .job_context import JobContext
+
+from . import stt
+from . import vad
+
+from .utils import AudioBuffer, merge_frames
+
+__all__ = [
+    "__version__",
+    "Worker",
+    "JobRequest",
+    "SubscribeCallbacks",
+    "AutoDisconnectCallbacks",
+    "JobContext",
+    "JobCancelledError",
+    "AssignmentTimeoutError",
+    "run_app",
+    "JobType",
+    "AudioBuffer",
+    "merge_frames",
+    "stt",
+    "vad",
+]
