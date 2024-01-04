ceb0ccd6281b486e4de9a60407cfedf622ac4eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:04:10 +0000

    Add package network-yuanzhe/1.0.0/network_yuanzhe-1.0.0.tar.gz

---
+++ packages/network-yuanzhe/network_yuanzhe-1.0.0.tar.gz/network_yuanzhe-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+with open("README.md", "r") as f:
+    long_description = f.read()
+setup(name='network_yuanzhe',
+    version='1.0.0',
+    description='Build A Neural Network from Scratch',
+    long_description=long_description,
+    author='yuanzhe-jia',
+    author_email='jiayuanzhe1@gmail.com',
+    install_requires=["numpy"],
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
