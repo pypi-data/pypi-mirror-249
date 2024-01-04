6df96cce76d71f3078488857acf94a1b494dc1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:05:07 +0000

    Add package iftc/1.2.4/iftc-1.2.4-py3-none-any.whl

---
+++ packages/iftc/iftc-1.2.4-py3-none-any.whl/iftc/spelling_corrector.py
@@ -0,0 +1,27 @@
+from iftc.spell_checker.intentional import Intentional
+from iftc.spell_checker.unintentional import Unintentional
+from iftc.spell_checker.text_processor import remove_mask, text_normalize
+
+class SpellingCorrector:
+    def __init__(self):
+        self.it_corrector = Intentional()
+        self.uit_corrector = Unintentional()
+
+    def acronym_correction(self, text: str):
+        text = text_normalize(text)
+        corrected_acronym = self.it_corrector.spelling_correction(text)
+
+        return remove_mask(corrected_acronym)
+
+    def telex_correction(self, text: str):
+        pass
+        # text = text_normalize(text)
+        # acronym_corrected = self.it_corrector.spelling_correction(text)
+        # sent_corrected   = self.uit_corrector.select_candidate(acronym_corrected)
+        # return sent_corrected
+
+    def detect_spelling_error(self, text: str):
+        text = text_normalize(text)
+        acronym_corrected = self.it_corrector.spelling_correction(text)
+        word_candidates = self.uit_corrector.gen_candidate.word_candidate(acronym_corrected)
+        return text, word_candidates
