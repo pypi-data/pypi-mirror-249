e4aba438fd899222aada7ff77675d10b6788cee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:04:18 +0000

    Add package gradio-yolov8-det/2.0.0/gradio_yolov8_det-2.0.0-py3-none-any.whl

---
+++ packages/gradio-yolov8-det/gradio_yolov8_det-2.0.0-py3-none-any.whl/gradio_yolov8_det/__init__.py
@@ -0,0 +1,9 @@
+# Gradio YOLOv8 Det, GPL-3.0 license
+# https://gitee.com/CV_Lab/gradio-yolov8-det
+
+
+__version__ = "v2.0.0"
+
+print(
+    f"欢迎使用 Gradio YOLOv8 Det {__version__} \n具体内容参见源码地址：https://gitee.com/CV_Lab/gradio-yolov8-det"
+)
