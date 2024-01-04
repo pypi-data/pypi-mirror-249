9399f9217a0904882cd2d1c10af5fd829a718f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:35 +0000

    Add package QualysPy/0.4.3.3/qualyspy-0.4.3.3-py3-none-any.whl

---
+++ packages/QualysPy/qualyspy-0.4.3.3-py3-none-any.whl/qualyspy/exceptions.py
@@ -0,0 +1,19 @@
+"""Exceptions for QualysPy."""
+
+
+class QualysAPIError(Exception):
+    """Indicates an error with the Qualys API."""
+
+    pass
+
+
+class ConfigError(Exception):
+    """Indicates an error with the configuration file."""
+
+    pass
+
+
+class ValidationError(Exception):
+    """Wrapper for pydantic.error_wrappers.ValidationError"""
+
+    pass
