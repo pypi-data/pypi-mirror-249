4501fb0145e36e31508f55e77dd2e9321631d3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:05:02 +0000

    Add package kalm/3.11.37/kalm-3.11.37-py3-none-any.whl

---
+++ packages/kalm/kalm-3.11.37-py3-none-any.whl/kalm/netbox/redis.py
@@ -0,0 +1,71 @@
+import redis
+import json
+import pprint   
+import time
+from ..common import prettyllog
+from .netbox_server import create_virtual_server
+from .netbox import create_tag
+from .common import get_env
+import os
+
+
+
+def refresh_netbox_from_redis(myenv, netboxdata):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    knownservers = {}
+    knownlinuxservers = {}
+    orphanservers = []
+    knownserverkeys = r.keys("kalm:vmware:*:known")
+    knownlinuxserverkeys = r.keys("kalm:vmware:*:known:linux")
+    for key in knownlinuxserverkeys:
+        key = key.decode("utf-8")
+        server = key.split(":")[2]
+        value = r.get(key)
+        value = value.decode("utf-8")
+        age = time.time() - float(value)
+        ageindays = int(age / 86400)
+        ageinhours = int(age / 3600)
+        ageinminutes = int(age / 60)
+        if ageindays > 1:
+            prettyllog("netbox", "get", "server", key, ageindays , "Date is older than one day", severity="INFO")
+            orphanservers.append(server)
+        else:
+            if ageindays < 1 and ageinhours > 1:
+                prettyllog("netbox", "get", "server", key, ageinhours , "Data is aging (Hours)", severity="INFO")
+            else:
+                prettyllog("netbox", "get", "server", key, ageinminutes , "data is fresh (Minutes)", severity="INFO")
+            #
+            detailkey = "kalm:vmware:" + server + ":details"
+            if r.exists(detailkey):
+              detailvalue = r.get(detailkey)
+              decodeddetailvalue = detailvalue.decode("utf-8").replace("'", '"')
+              knownlinuxservers[server] = detailvalue.decode("utf-8")
+              detailjson = json.loads(decodeddetailvalue)   
+              try:
+                toolstatus = detailjson['toolstatus']
+              except:
+                toolstatus = "unknown"
+                
+              try: 
+                create_tag(toolstatus, myenv, netboxdata)
+              except:
+                pass
+              
+              if toolstatus == "toolsOk":
+                prettyllog("netbox", "get", "server", key, "000" , "Server is OK", severity="INFO")
+              else:
+                prettyllog("netbox", "get", "server", key, "000" , "Server is not OK %s " % toolstatus, severity="ERROR")
+
+              print("-------------------------------------------------------")
+              pprint.pprint(detailjson)
+              print("-------------------------------------------------------")
+
+              create_virtual_server(detailjson, myenv, netboxdata)
+            else:
+                prettyllog("netbox", "get", "server", key, "000" , "No details found", severity="ERROR")
+                orphanservers.append(server)
+    print("orphanservers: %s" % len(orphanservers))
+    print("knownservers:  %s" % len(knownservers))
+    return True     
+
+
