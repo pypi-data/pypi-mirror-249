0c4cd317afa51f12d42c6ee739c828946a3894d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:05:06 +0000

    Add package flash-server/0.1.1/flash-server-0.1.1.tar.gz

---
+++ packages/flash-server/flash-server-0.1.1.tar.gz/flash-server-0.1.1/README.md
@@ -0,0 +1,20 @@
+# py-http-server
+
+一个基于 Python 开发的简单 HTTP 服务器，支持文件上传功能。
+
+它提供了轻量级的 Web 服务，让您能够快速搭建一个本地 Web Server。
+
+无需复杂的配置，只需一行代码，即可启动服务器并允许用户上传文件。
+
+![](./demo.jpg)
+
+```python
+python http-server.py
+```
+
+```shell
+Starting up http-server, serving /Users/dev/Desktop/py-http-server
+Available on:
+  http://127.0.0.1:8000
+  http://169.254.53.221:8000
+```
---
+++ packages/flash-server/flash-server-0.1.1.tar.gz/flash-server-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flash-server',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A simple http server written in python that supports file uploads.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='fxfy',
+    url='https://github.com/fxfyio/py-http-server',
+    install_requires=[
+    ],
+)
