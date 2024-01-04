6dfbeee07491240c606ec78bdf5de2f73993a46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:05:52 +0000

    Add package nnsight/0.1.2/nnsight-0.1.2-py3-none-any.whl

---
+++ packages/nnsight/nnsight-0.1.2-py3-none-any.whl/nnsight/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+logging_handler = logging.FileHandler(os.path.join(PATH, f"nnsight.log"), "a")
+logging_handler.setFormatter(
+    logging.Formatter(
+        "%(asctime)s %(processName)-10s %(name)s %(levelname)-8s %(message)s"
+    )
+)
+logging_handler.setLevel(logging.INFO)
+
+logger = logging.getLogger("nnsight")
+logger.addHandler(logging_handler)
+logger.setLevel(logging.INFO)
