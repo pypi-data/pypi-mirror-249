f6ea367bda2ce77ff00ba1be4f03f425011937be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:30 +0000

    Add package Record/4.1/Record-4.1.tar.gz

---
+++ packages/Record/Record-4.1.tar.gz/Record-4.1/CHANGES.rst
@@ -0,0 +1,72 @@
+Changelog
+=========
+
+4.1 (2024-01-04)
+----------------
+
+- Add support for Python 3.12.
+
+
+4.0 (2023-03-24)
+----------------
+
+- Add support for Python 3.11.
+
+- Drop support for Python 2.7, 3.5, 3.6.
+
+
+3.6 (2022-10-11)
+----------------
+
+- Add support for Python 3.8, 3.9, 3.10.
+
+- Drop support for Python 3.4.
+
+
+3.5 (2018-10-05)
+----------------
+
+- Add support for Python 3.7.
+
+3.4 (2017-05-15)
+----------------
+
+- Add `__hash__` method to Record.
+
+3.3 (2017-05-06)
+----------------
+
+- Set `__allow_access_to_unprotected_subobjects__` on the Record class.
+
+- Remove the C extension.
+
+3.2 (2017-04-26)
+----------------
+
+- Use `ExtensionClass.Base.__new__`.
+
+- Add support for Python 3.6, drop support for Python 3.3.
+
+3.1 (2016-04-03)
+----------------
+
+- Add support for Python 3.4 and 3.5.
+
+- Drop support for Python 2.6 and 3.2.
+
+3.0 (2013-05-04)
+----------------
+
+- Add support for Python 3.2, 3.3 and PyPy using the Python reference
+  implementation.
+
+- Add support for `__contains__`.
+
+- Provide an Python reference implementation using `__slots__`.
+
+- Rewrite tests as unit tests.
+
+2.13.0 (2010-03-30)
+-------------------
+
+- Released as separate package.
---
+++ packages/Record/Record-4.1.tar.gz/Record-4.1/setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '4.1'
+
+setup(
+    name='Record',
+    version=version,
+    url='https://github.com/zopefoundation/Record',
+    license='ZPL 2.1',
+    description="Special Record objects used in Zope.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.rst').read() + '\n' +
+        open('CHANGES.rst').read(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope :: 4",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    python_requires='>=3.7',
+    install_requires=['ExtensionClass >= 4.1a1'],
+    include_package_data=True,
+    zip_safe=False,
+)
