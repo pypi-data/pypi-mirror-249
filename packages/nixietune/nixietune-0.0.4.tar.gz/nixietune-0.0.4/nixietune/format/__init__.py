a10aaeba3818b8283d15b6805b53c3a71956075a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:04:39 +0000

    Add package nixietune/0.0.4/nixietune-0.0.4.tar.gz

---
+++ packages/nixietune/nixietune-0.0.4.tar.gz/nixietune-0.0.4/nixietune/format/__init__.py
@@ -0,0 +1,4 @@
+from nixietune.format.format import Format  # noqa: F401
+from nixietune.format.query_doc_label import QueryDocLabelFormat  # noqa: F401
+from nixietune.format.query_pos_negs import QueryPosNegsFormat  # noqa: F401
+from nixietune.format.triplet import TripletFormat  # noqa: F401
