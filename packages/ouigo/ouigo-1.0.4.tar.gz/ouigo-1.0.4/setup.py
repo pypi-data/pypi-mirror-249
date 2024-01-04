3ae333754692e9925513a1e4ae2ef59985a979b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:04:19 +0000

    Add package ouigo/1.0.4/ouigo-1.0.4.tar.gz

---
+++ packages/ouigo/ouigo-1.0.4.tar.gz/ouigo-1.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ouigo",
+    version="1.0.4",
+    description="A module which allows you to retrieve data about the cheapest one-way travels of Ouigo "
+    "in a date range.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ricardo Alegre",
+    author_email="ricardomanuel.alegre@gmail.com",
+    url="https://github.com/RicardoAlegreMiranda/ouigo",
+    packages=["ouigo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"]
+)
