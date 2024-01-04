2bbd8c8d4f2f240ac1d4e934e90874faf32559c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:53 +0000

    Add package tjaf/1.0.0/tjaf-1.0.0.tar.gz

---
+++ packages/tjaf/tjaf-1.0.0.tar.gz/tjaf-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='tjaf',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    author='Yuuki',
+    author_email='skifree@skiff.com',
+    description='TJA converter',
+    long_description='Easy to use .tja files in Python for everyone.',
+    url='https://github.com/yuukialpha/tjaf',
+)
