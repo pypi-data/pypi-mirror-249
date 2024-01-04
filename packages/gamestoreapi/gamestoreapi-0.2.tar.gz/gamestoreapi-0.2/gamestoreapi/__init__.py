74197d42f95dc828eb21d65ec41adba9ab6abc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:05:19 +0000

    Add package gamestoreapi/0.2/gamestoreapi-0.2.tar.gz

---
+++ packages/gamestoreapi/gamestoreapi-0.2.tar.gz/gamestoreapi-0.2/gamestoreapi/__init__.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_achievements(token):
+    url = 'http://game-store.7m.pl/api.php?action=get_achievements&token=' + token
+    response = requests.get(url)
+    return response.json()
+
+def unlock_achievement(token, game_id, achievement_name):
+    url = f'http://game-store.7m.pl/api.php?action=unlock_achievement&token={token}&game_id={game_id}&achievement_name={achievement_name}'
+    response = requests.get(url)
+    return response.json()
+
+def add_to_inventory(token, itemName, quantity):
+    url = f'http://game-store.7m.pl/api.php?action=get_inventory$token={token}&itemName={itemName}&quantity={quantity}'
