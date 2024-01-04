a8eda2c0d8e683561e46e68ab6a6c63c5e02002a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:15 +0000

    Add package Pyhoot/1.0.1/Pyhoot-1.0.1.tar.gz

---
+++ packages/Pyhoot/Pyhoot-1.0.1.tar.gz/Pyhoot-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A Python library used for the Kahoot! Api'
+LONG_DESCRIPTION = 'A package that allows users to create Kahoot! clients and use the Kahoot! Api to their will \n check out https://github.com/maxgamertyper/Pyhoot for more'
+
+setup(
+    name="Pyhoot",
+    version=VERSION,
+    author="Maxgamertyper1 (Max Allen)",
+    author_email="maxa5302@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'beautifulsoup4', 'websocket-client', 'fake_useragent'],
+    keywords=['python', 'Kahoot', 'pyhoot', 'bot', 'flooder', 'sockets'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    license="MIT",
+    python_requires=">=3.6"
+)
