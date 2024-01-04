3ff2796fa942298325fb517937713df26cf89cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:14 +0000

    Add package promplate/0.3.2/promplate-0.3.2-py3-none-any.whl

---
+++ packages/promplate/promplate-0.3.2-py3-none-any.whl/promplate/chain/utils.py
@@ -0,0 +1,37 @@
+from inspect import Parameter, isasyncgen, isawaitable, signature
+from typing import AsyncIterable, Awaitable, Callable, Iterable, TypeVar
+
+T = TypeVar("T")
+
+
+def appender(to_append: list[T]) -> Callable[[T], T]:
+    def append_processer(func: T) -> T:
+        to_append.append(func)
+
+        return func
+
+    return append_processer
+
+
+def is_positional_parameter(p: Parameter):
+    return p.kind is Parameter.POSITIONAL_OR_KEYWORD or p.kind is Parameter.KEYWORD_ONLY
+
+
+def count_position_parameters(func):
+    return sum(map(is_positional_parameter, signature(func).parameters.values()))
+
+
+async def resolve(maybe_awaitable: T | Awaitable[T], /) -> T:
+    while isawaitable(maybe_awaitable):
+        maybe_awaitable = await maybe_awaitable
+
+    return maybe_awaitable  # type: ignore
+
+
+async def iterate(maybe_asyncgen: Iterable | AsyncIterable):
+    if isasyncgen(maybe_asyncgen):
+        async for i in maybe_asyncgen:
+            yield i
+    else:
+        for i in maybe_asyncgen:  # type: ignore
+            yield i
