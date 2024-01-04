645c7f2b7985e915d713d9191ec68a993b5590e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:39 +0000

    Add package sverilogpy/0.0.0a2/sverilogpy-0.0.0a2-cp38-cp38-win32.whl

---
+++ packages/sverilogpy/sverilogpy-0.0.0a2-cp38-cp38-win32.whl/sverilogpy/sverilog.cpp
@@ -0,0 +1,61 @@
+/***********************************************************************************
+ * A python System Verilog Parser and AST
+ * Copyright (C) 2024  RISCY-Lib Contributors
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; If not, see <https://www.gnu.org/licenses/>.
+ ***********************************************************************************/
+
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(sverilogpy_bind, m) {
+    m.doc() = R"pbdoc(
+        A python System Verilog Parser and AST
+        -----------------------
+
+        .. currentmodule:: sverilogpy
+
+        .. autosummary::
+           :toctree: _generate
+
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("sub", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
