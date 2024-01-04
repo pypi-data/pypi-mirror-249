cc59545c877a23652096e4ed1f4bc7417275f5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:14 +0000

    Add package Diskspace-Report/0.1.6/Diskspace-Report-0.1.6.tar.gz

---
+++ packages/Diskspace-Report/Diskspace-Report-0.1.6.tar.gz/Diskspace-Report-0.1.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+setup(
+    name='Diskspace-Report',
+    version='0.1.6',
+    author='Andreas Paeffgen',
+    author_email='your.email@example.com',
+    description='Check the available disk space and write it to a csv file. Eventually email the csv file.',
+    long_description='Check the available disk space and write it to a csv file. Eventually email the csv file.',
+    long_description_content_type="text/markdown",
+    url='https://github.com/apaeffgen/diskspace_report',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    console_scripts=[
+        'diskspace-report= diskspace-report:main'],
+    python_requires='>=3.6',
+)
