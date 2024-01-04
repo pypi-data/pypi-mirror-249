a065434d53172b011a05603636ca3b87a330da09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:04:44 +0000

    Add package kalm/3.11.31/kalm-3.11.31.tar.gz

---
+++ packages/kalm/kalm-3.11.31.tar.gz/kalm-3.11.31/src/kalm/netbox/terraform/cmdb/clusters.tf
@@ -0,0 +1,17 @@
+resource "netbox_cluster" "openstack" {
+  cluster_type_id  = netbox_cluster_type.openstack.id
+  name             = "openstackcluster01"
+  cluster_group_id = netbox_cluster_group.openstackclusters.id
+}
+
+resource "netbox_cluster" "vmwareclusters" {
+  cluster_type_id  = netbox_cluster_type.vmware.id
+  name             = "vmwarecluster01"
+  cluster_group_id = netbox_cluster_group.vmwareclusters.id
+}
+
+resource "netbox_cluster" "linuxclusters" {
+  cluster_type_id  = netbox_cluster_type.vmware.id
+  name             = "linuxcluster01"
+  cluster_group_id = netbox_cluster_group.linuxclusters.id
+}
