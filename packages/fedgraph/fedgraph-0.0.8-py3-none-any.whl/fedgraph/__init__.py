26e725991f1df9ae82db8677fc79ab6302acbe81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:35 +0000

    Add package fedgraph/0.0.8/fedgraph-0.0.8-py3-none-any.whl

---
+++ packages/fedgraph/fedgraph-0.0.8-py3-none-any.whl/fedgraph/__init__.py
@@ -0,0 +1,8 @@
+import fedgraph.data_process
+import fedgraph.gnn_models
+import fedgraph.server_class
+import fedgraph.train_func
+import fedgraph.trainer_class
+import fedgraph.utils
+
+from .version import __version__
