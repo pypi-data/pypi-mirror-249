45ca20f53867a34c70a3fc49760023cb66632812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 05:04:31 +0000

    Add package onnx-extended/0.2.4/onnx_extended-0.2.4-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/onnx-extended/onnx_extended-0.2.4-cp310-cp310-macosx_10_15_x86_64.whl/onnx_extended/_config.py
@@ -0,0 +1,7 @@
+HAS_CUDA = 0
+ORT_VERSION = '1.16.1'
+ORT_VERSION_INT = 1161
+CXX_FLAGS = ' -Wall -Wno-unknown-pragmas -Wextra -mavx2 -mf16c -fPIC -Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include'
+CMAKE_CXX_STANDARD_REQUIRED = ''
+CMAKE_CXX_EXTENSIONS = ''
+CMAKE_CXX_STANDARD = 17
---
+++ packages/onnx-extended/onnx_extended-0.2.4-cp310-cp310-macosx_10_15_x86_64.whl/onnx_extended/ortops/optim/cpu/__init__.py
@@ -0,0 +1,81 @@
+import os
+import textwrap
+from typing import List
+from ... import _get_ort_ext_libs
+
+
+def get_ort_ext_libs() -> List[str]:
+    """
+    Returns the list of libraries implementing new simple
+    :epkg:`onnxruntime` kernels implemented for the
+    :epkg:`CPUExecutionProvider`.
+    """
+    return _get_ort_ext_libs(os.path.dirname(__file__))
+
+
+def documentation() -> List[str]:
+    """
+    Returns a list of rst string documenting every implemented kernels
+    in this subfolder.
+    """
+    return list(
+        map(
+            textwrap.dedent,
+            [
+                """
+    onnx_extented.ortops.option.cpu.TreeEnsembleClassifier
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    It does the sum of two tensors.
+
+    **Provider**
+    
+    CPUExecutionProvider
+    
+    **Inputs**
+    
+    * X (T1): tensor of type T1
+
+    **Outputs**
+
+    * label (T3): labels of type T3
+    * Y (T2): probabilities of type T2
+
+    **Constraints**
+
+    * T1: float, double
+    * T2: float, double
+    * T3: int64
+
+    **Attributes**
+
+    """,
+                """
+    onnx_extented.ortops.option.cpu.TreeEnsembleRegressor
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    It does the sum of two tensors.
+
+    **Provider**
+    
+    CPUExecutionProvider
+    
+    **Inputs**
+    
+    * X (T1): tensor of type T1
+
+    **Outputs**
+
+    * Y (T2): prediction of type T2
+
+    **Constraints**
+
+    * T1: float, double
+    * T2: float, double
+
+    **Attributes**
+
+    """,
+            ],
+        )
+    )
