eeb26329411471353a97d3f66aba2b18f2d32080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:51 +0000

    Add package streaminghub-datamux/0.1.0/streaminghub_datamux-0.1.0-py3-none-any.whl

---
+++ packages/streaminghub-datamux/streaminghub_datamux-0.1.0-py3-none-any.whl/datamux/remote/rpc/server/__init__.py
@@ -0,0 +1,29 @@
+import asyncio
+from abc import ABC, abstractmethod
+from ..codec import Codec, create_codec
+
+
+class RpcServer(ABC):
+    @abstractmethod
+    async def start(self, host: str, port: int) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def stop(self) -> None:
+        raise NotImplementedError()
+
+
+def create_rpc_server(
+    name: str,
+    codec_name: str,
+    incoming: asyncio.Queue,
+    outgoing: asyncio.Queue,
+) -> RpcServer:
+    assert name == "websocket"
+    from .websocket import WebsocketRPC
+
+    return WebsocketRPC(
+        codec_name=codec_name,
+        incoming=incoming,
+        outgoing=outgoing,
+    )
