26883607fa75c835de9ef4f04e7529d169864fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:52 +0000

    Add package dfbar/0.4.1/dfbar-0.4.1.tar.gz

---
+++ packages/dfbar/dfbar-0.4.1.tar.gz/dfbar-0.4.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = {
+    'name': 'dfbar',
+    'version': os.environ['BUILD_VERSION'],
+    'description': 'Dockerfile Build and Run',
+    'long_description_content_type': 'text/markdown',
+    'long_description': README,
+    'license': 'MIT',
+    'packages': find_packages(
+        where='src',
+        include=[
+            'dfbar',
+            'dfbar.*'
+        ]
+    ),
+    'author': 'Jesse Reichman',
+    'keywords': [ 'Dockerfile', 'Build', 'Run' ],
+    'url': 'https://github.com/archmachina/dfbar',
+    'download_url': 'https://pypi.org/project/dfbar/',
+    'entry_points': {
+        'console_scripts': [
+            'dfbar = dfbar:main'
+        ]
+    },
+    'package_dir': {
+        '': 'src'
+    }
+}
+
+install_requires = []
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
