c5279d36a6d096363b055f1943439cc6174ef709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:42 +0000

    Add package hamonsoft-ai-database/0.0.1/hamonsoft-ai-database-0.0.1.tar.gz

---
+++ packages/hamonsoft-ai-database/hamonsoft-ai-database-0.0.1.tar.gz/hamonsoft-ai-database-0.0.1/database/db_pool.py
@@ -0,0 +1,38 @@
+import threading
+import queue
+
+from abc import ABCMeta, abstractmethod
+
+
+class DBPool(metaclass=ABCMeta):
+    def __init__(self):
+        self.connectLock = threading.Lock()
+        self.releaseLock = threading.Lock()
+
+        self.queue = queue.Queue()
+        self.allList = []
+
+    @abstractmethod
+    def init(self, *args, **kwargs):
+        pass
+
+    def get_session(self):
+        try:
+            self.connectLock.acquire()
+            dbImpl = self.queue.get()
+            return dbImpl
+
+        except:
+            return None
+        
+        finally:
+            self.connectLock.release()
+    
+    def release(self, dbImpl):
+        if dbImpl is not None:
+            self.releaseLock.acquire()
+            self.queue.put(dbImpl)
+            self.releaseLock.release()
+    
+    def check_queue_size(self):
+        return self.queue.qsize()
