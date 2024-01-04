16980ff62b3385306b9a5c0ee5dad9d88d4d28e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:33 +0000

    Add package tjaf/1.0.6/tjaf-1.0.6.tar.gz

---
+++ packages/tjaf/tjaf-1.0.6.tar.gz/tjaf-1.0.6/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='tjaf',
+    version='1.0.6',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    author='Yuuki',
+    author_email='skifree@skiff.com',
+    description='TJA decoder in Python',
+    long_description='Easy to use TJA decoder in Python for everyone.',
+    url='https://github.com/yuukialpha/tjaf',
+)
