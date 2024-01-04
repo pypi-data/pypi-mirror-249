274c4ae0722f76146dd546e09dd8076950f56416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:37 +0000

    Add package uptrain/0.4.9/uptrain-0.4.9-py3-none-any.whl

---
+++ packages/uptrain/uptrain-0.4.9-py3-none-any.whl/uptrain/framework/evals.py
@@ -0,0 +1,36 @@
+import enum
+import pydantic
+import typing as t
+
+class Evals(enum.Enum):
+    CONTEXT_RELEVANCE = "context_relevance"
+    FACTUAL_ACCURACY = "factual_accuracy"
+    RESPONSE_RELEVANCE = "response_relevance"
+    CRITIQUE_LANGUAGE = "critique_language"
+    RESPONSE_COMPLETENESS = "response_completeness"
+    RESPONSE_COMPLETENESS_WRT_CONTEXT = "response_completeness_wrt_context"
+    RESPONSE_CONSISTENCY = "response_consistency"
+    RESPONSE_CONCISENESS = "response_conciseness"
+    VALID_RESPONSE = "valid_response"
+
+
+class ParametricEval(pydantic.BaseModel):
+    ...
+
+
+class CritiqueTone(ParametricEval):
+    llm_persona: str = "helpful-chatbot"
+
+
+class GuidelineAdherence(ParametricEval):
+    guideline: str
+    guideline_name: str = "guideline"  # User-assigned name of the guideline to distinguish between multiple checks
+    response_schema: t.Union[str, None] = None  # Schema of the response in case it is of type JSON, XML, etc.
+
+class ConversationSatisfaction(ParametricEval):
+    user_persona: str = "user"
+    llm_persona: t.Union[str, None] = None
+    
+
+class ResponseMatching(ParametricEval):
+    method: str = "llm"
