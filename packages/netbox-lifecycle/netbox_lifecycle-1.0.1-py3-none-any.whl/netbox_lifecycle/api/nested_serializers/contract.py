7e4bcc6d2a344ee35231ef62ec6968ac487950a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:34 +0000

    Add package netbox-lifecycle/1.0.1/netbox_lifecycle-1.0.1-py3-none-any.whl

---
+++ packages/netbox-lifecycle/netbox_lifecycle-1.0.1-py3-none-any.whl/netbox_lifecycle/api/nested_serializers/contract.py
@@ -0,0 +1,64 @@
+from django.contrib.contenttypes.models import ContentType
+from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
+from dcim.api.nested_serializers import NestedManufacturerSerializer, NestedDeviceSerializer
+from netbox.api.fields import ContentTypeField
+from netbox.api.serializers import WritableNestedSerializer
+from netbox.constants import NESTED_SERIALIZER_PREFIX
+from netbox_lifecycle.models import Vendor, SupportContract, SupportContractAssignment, SupportSKU
+
+__all__ = (
+    'NestedVendorSerializer',
+    'NestedSupportSKUSerializer',
+    'NestedSupportContractSerializer',
+    'NestedSupportContractAssignmentSerializer',
+)
+
+from utilities.api import get_serializer_for_model
+
+
+class NestedVendorSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_lifecycle-api:hardwarelifecycle-detail')
+
+    class Meta:
+        model = Vendor
+        fields = ('url', 'id', 'display', 'name')
+
+
+class NestedSupportSKUSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_lifecycle-api:hardwarelifecycle-detail')
+    manufacturer = NestedManufacturerSerializer()
+
+    class Meta:
+        model = SupportSKU
+        fields = ('url', 'id', 'display', 'manufacturer', 'sku')
+
+
+class NestedSupportContractSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_lifecycle-api:hardwarelifecycle-detail')
+    vendor = NestedVendorSerializer()
+
+    class Meta:
+        model = SupportContract
+        fields = ('url', 'id', 'display', 'contract_id', 'vendor')
+
+
+class NestedSupportContractAssignmentSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_lifecycle-api:licenseassignment-detail')
+    contract = NestedSupportContractSerializer()
+
+    assigned_object_type = ContentTypeField(
+        queryset=ContentType.objects.all()
+    )
+    assigned_object = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = SupportContractAssignment
+        fields = ('url', 'id', 'display', 'contract', 'assigned_object_type', 'assigned_object_id', 'assigned_object')
+
+    @extend_schema_field(serializers.JSONField(allow_null=True))
+    def get_assigned_object(self, instance):
+        serializer = get_serializer_for_model(instance.assigned_object, prefix=NESTED_SERIALIZER_PREFIX)
+        context = {'request': self.context['request']}
+        return serializer(instance.assigned_object, context=context).data
