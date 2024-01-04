582b437923f7c3d4a1676ae8d27585d8b441140c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 06:03:54 +0000

    Add package py-gen-dist/0.1/py_gen_dist-0.1.tar.gz

---
+++ packages/py-gen-dist/py_gen_dist-0.1.tar.gz/py_gen_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='py_gen_dist',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['py_gen_dist'],
+      zip_safe=False)
