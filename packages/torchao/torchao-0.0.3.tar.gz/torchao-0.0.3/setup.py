cdf1cf6a07329191d1531f2ea18e359a91afd2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:58 +0000

    Add package torchao/0.0.3/torchao-0.0.3.tar.gz

---
+++ packages/torchao/torchao-0.0.3.tar.gz/torchao-0.0.3/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='torchao',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ],
+    description='Package for applying ao techniques to GPU models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pytorch-labs/ao',
+)
