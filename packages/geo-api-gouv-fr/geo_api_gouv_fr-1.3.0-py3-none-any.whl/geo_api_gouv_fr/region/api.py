4f793a0b830a1516a0a1465a4fa5fa7403337734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 16:04:01 +0000

    Add package geo-api-gouv-fr/1.3.0/geo_api_gouv_fr-1.3.0-py3-none-any.whl

---
+++ packages/geo-api-gouv-fr/geo_api_gouv_fr-1.3.0-py3-none-any.whl/geo_api_gouv_fr/region/api.py
@@ -0,0 +1,38 @@
+import requests
+
+from .schemas import RegionsParams, RegionCodeParams
+
+
+class Api:
+    """This is the api to interact with the regions
+
+    Documentation : https://geo.api.gouv.fr/decoupage-administratif/regions
+
+    """
+
+    def __init__(self, **kwargs):
+        self.url = kwargs.pop("url", "https://geo.api.gouv.fr")
+        self.timeout = kwargs.get("timeout", 10)
+
+    def regions(self, **kwargs) -> requests.Response:
+        """
+        Parameters:
+            **kwargs (RegionsParams):
+        """
+        params = RegionsParams(**kwargs)
+        return requests.get(
+            self.url + "/regions", params=params.model_dump(), timeout=self.timeout
+        )
+
+    def regions_by_code(self, **kwargs) -> requests.Response:
+        """
+        Parameters:
+            **kwargs (RegionCodeParams):
+        """
+        params = RegionCodeParams(**kwargs)
+
+        return requests.get(
+            self.url + "/regions/" + params.code,
+            params=params.model_dump(),
+            timeout=self.timeout,
+        )
