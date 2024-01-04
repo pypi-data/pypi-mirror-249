254a658a0ef1039fcea85c6c69059a4918bea22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:04:00 +0000

    Add package fastapi-responseschema/2.1.0/fastapi_responseschema-2.1.0-py3-none-any.whl

---
+++ packages/fastapi-responseschema/fastapi_responseschema-2.1.0-py3-none-any.whl/fastapi_responseschema/__init__.py
@@ -0,0 +1,9 @@
+from .interfaces import AbstractResponseSchema
+from .routing import respond, SchemaAPIRoute
+from .helpers import wrap_app_responses, wrap_error_responses
+
+
+__version__ = "2.1.0"
+
+
+__all__ = ["AbstractResponseSchema", "respond", "SchemaAPIRoute", "wrap_app_responses", "wrap_error_responses"]
