97afe0cbb093fd75827f89dab58ee09d988987c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:37 +0000

    Add package fab-react-toolkit/0.0.5/fab-react-toolkit-0.0.5.tar.gz

---
+++ packages/fab-react-toolkit/fab-react-toolkit-0.0.5.tar.gz/fab-react-toolkit-0.0.5/fab_react_toolkit/apis/__init__.py
@@ -0,0 +1,7 @@
+from .security.auth import AuthApi
+from .security.permission_view import PermissionViewApi
+from .security.permissions import PermissionsApi
+from .security.roles import RolesApi
+from .security.users import UsersApi
+from .security.views_menus import ViewsMenusApi
+from .info import InfoApi
---
+++ packages/fab-react-toolkit/fab-react-toolkit-0.0.5.tar.gz/fab-react-toolkit-0.0.5/fab_react_toolkit/apis/security/permissions.py
@@ -0,0 +1,17 @@
+from flask_appbuilder.security.sqla.models import Permission
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+from ...api import ModelRestApi
+
+
+class PermissionsApi(ModelRestApi):
+    # Will allow flask-login cookie authorization on the API
+    allow_browser_login = True
+    datamodel = SQLAInterface(Permission)
+
+    # this also effects schema of related models
+    page_size = 200
+    max_page_size = 200
+
+    resource_name = "permissions"
+    base_permissions = ['can_get', 'can_info']
