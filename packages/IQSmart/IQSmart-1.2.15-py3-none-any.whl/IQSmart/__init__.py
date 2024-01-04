afe5840ad3fc6263753d32994bc4d89ce6672376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:44 +0000

    Add package IQSmart/1.2.15/IQSmart-1.2.15-py3-none-any.whl

---
+++ packages/IQSmart/IQSmart-1.2.15-py3-none-any.whl/IQSmart/__init__.py
@@ -0,0 +1,9 @@
+# IQ Smart module for Theia's MCR600 motor control board
+#
+# Theia Technologies
+# https://www.theiatech.com
+# (c) 2023 Theia Technologies
+__author__ = 'Mark Peterson'
+__credits__ = 'Theia Technologies'
+
+from IQSmart.IQSmart import *
