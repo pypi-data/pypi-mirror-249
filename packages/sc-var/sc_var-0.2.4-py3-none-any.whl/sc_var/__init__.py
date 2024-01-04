f0fcea3ce407812c4a2c17813cc14883b984c5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 03:03:28 +0000

    Add package sc-var/0.2.4/sc_var-0.2.4-py3-none-any.whl

---
+++ packages/sc-var/sc_var-0.2.4-py3-none-any.whl/sc_var/__init__.py
@@ -0,0 +1,7 @@
+from . import method
+
+from .method import snp_peak,dis_peak,gene_corr,annotate,dmagma,get_peak_weights,save_gs
+from .method import scads,stat_analysis,plot_heatmap,plot_stat
+#import all functions in method.py
+form .method import *
+__all__ = ["method"]
