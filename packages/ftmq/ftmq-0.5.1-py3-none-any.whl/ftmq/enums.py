9bfd91be37f80c1b954e2d341275392142175e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:48 +0000

    Add package ftmq/0.5.1/ftmq-0.5.1-py3-none-any.whl

---
+++ packages/ftmq/ftmq-0.5.1-py3-none-any.whl/ftmq/enums.py
@@ -0,0 +1,99 @@
+from collections.abc import Iterable
+from enum import Enum, EnumMeta
+from typing import Any
+
+from followthemoney import model
+from nomenklatura.dataset.coverage import DataCoverage
+
+
+class EMeta(EnumMeta):
+    def __contains__(self, member: Any) -> bool:
+        try:
+            self(member)
+            return True
+        except ValueError:
+            return False
+
+
+def StrEnum(name: str, values: Iterable[Any]) -> Enum:
+    # mimic py3.11 enum.StrEnum
+    # and fix default enum implementation:
+    # https://gist.github.com/simonwoerpel/bdb9959de75e550349961677549624fb
+    class _StrEnum(str, Enum, metaclass=EMeta):
+        def __str__(self):
+            return self.value
+
+        @property
+        def value(self) -> Any:
+            return super().value
+
+        @property
+        def name(self) -> Any:
+            return super().name
+
+        @value.setter
+        def value(self, value: str):
+            self.value = value
+
+        @name.setter
+        def name(self, name: str):
+            self.name = name
+
+    return _StrEnum(name, {str(v): str(v) for v in values})
+
+
+Schemata = StrEnum("Schemata", [k for k in model.schemata.keys()])
+Properties = StrEnum("Properties", {p.name for p in model.properties})
+PropertyTypes = StrEnum("PropertyTypes", {p.type for p in model.properties})
+PropertyTypesMap = Enum("PropertyTypesMap", {p.name: p.type for p in model.properties})
+Comparators = StrEnum(
+    "Comparators",
+    [
+        "eq",
+        "not",
+        "in",
+        "not_in",
+        "null",
+        "gt",
+        "gte",
+        "lt",
+        "lte",
+        "like",
+        "ilike",
+        "notlike",
+        "notilike",
+        "between",
+        "startswith",
+        "endswith",
+    ],
+)
+Frequencies = StrEnum("Frequencies", [f for f in DataCoverage.FREQUENCIES])
+Aggregations = StrEnum("Aggregations", ("min", "max", "sum", "avg", "count"))
+Fields = StrEnum("Fields", ["id", "dataset", "schema", "year"])
+
+# aleph
+Categories = StrEnum(
+    "Categories",
+    (
+        "news",
+        "leak",
+        "land",
+        "gazette",
+        "court",
+        "company",
+        "sanctions",
+        "procurement",
+        "finance",
+        "grey",
+        "library",
+        "license",
+        "regulatory",
+        "poi",
+        "customs",
+        "census",
+        "transport",
+        "casefile",
+        "other",
+        "casefile",
+    ),
+)
---
+++ packages/ftmq/ftmq-0.5.1-py3-none-any.whl/ftmq/model/dataset.py
@@ -0,0 +1,193 @@
+from collections.abc import Generator
+from datetime import datetime
+from typing import Any, ForwardRef, Iterable, Literal, Optional, TypeVar
+
+from nomenklatura.dataset.catalog import DataCatalog as NKCatalog
+from nomenklatura.dataset.dataset import Dataset as NKDataset
+from nomenklatura.dataset.publisher import DataPublisher as NKPublisher
+from nomenklatura.dataset.resource import DataResource as NKResource
+from normality import slugify
+from pantomime.types import FTM
+from pydantic import AnyUrl, HttpUrl
+
+from ftmq.enums import Categories, Frequencies
+from ftmq.model.coverage import Coverage
+from ftmq.model.mixins import BaseModel, NKModel, RemoteMixin, YamlMixin
+from ftmq.types import CEGenerator
+
+Frequencies = Literal[tuple(Frequencies)]
+Categories = Literal[tuple(Categories)]
+
+C = TypeVar("C", bound="Catalog")
+DS = TypeVar("DS", bound="Dataset")
+
+
+class Publisher(NKModel):
+    _nk_model = NKPublisher
+
+    name: str
+    url: HttpUrl
+    description: str | None = None
+    country: str | None = None
+    country_label: str | None = None
+    official: bool = False
+    logo_url: HttpUrl | None = None
+
+
+class Resource(NKModel):
+    _nk_model = NKResource
+
+    name: str
+    url: AnyUrl
+    title: str | None = None
+    checksum: str | None = None
+    timestamp: datetime | None = None
+    mime_type: str | None = None
+    mime_type_label: str | None = None
+    size: int | None = 0
+
+
+class Maintainer(BaseModel, RemoteMixin, YamlMixin):
+    """
+    this is our own addition
+    """
+
+    name: str
+    description: str | None = None
+    url: HttpUrl | None = None
+    logo_url: HttpUrl | None = None
+
+
+Catalog = ForwardRef("Catalog")
+
+
+class Dataset(NKModel):
+    _nk_model = NKDataset
+
+    name: str
+    prefix: str | None = None
+    title: str | None = None
+    license: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    url: HttpUrl | None = None
+    updated_at: datetime | None = None
+    version: str | None = None
+    category: Categories | None = None
+    publisher: Publisher | None = None
+    coverage: Coverage | None = None
+    resources: list[Resource] | None = []
+
+    # own addition / aleph
+    countries: list[str] | None = []
+    info_url: HttpUrl | None = None
+    data_url: HttpUrl | None = None
+
+    git_repo: AnyUrl | None = None
+    uri: str | None = None
+    maintainer: Maintainer | None = None
+    catalog: Optional[Catalog] = None
+
+    def __init__(self, **data):
+        # FIXME
+        Catalog.model_rebuild()
+        Dataset.model_rebuild()
+        if "include" in data:  # legacy behaviour
+            data["uri"] = data.pop("include", None)
+        data["updated_at"] = data.get("updated_at") or datetime.utcnow().replace(
+            microsecond=0
+        )
+        data["catalog"] = data.get("catalog") or Catalog().model_dump()
+        super().__init__(**data)
+        self.prefix = self.prefix or data.get("prefix") or slugify(self.name)
+        self.coverage = self.coverage or Coverage()
+        self.title = self.title or self.name.title()
+
+    def to_nk(self):
+        return self._nk_model(self.catalog.to_nk(), self.model_dump())
+
+    def iterate(self) -> CEGenerator:
+        from ftmq.io import smart_read_proxies  # FIXME
+
+        for resource in self.resources:
+            if resource.mime_type == FTM:
+                yield from smart_read_proxies(resource.url)
+
+
+class Catalog(NKModel):
+    _nk_model = NKCatalog
+
+    datasets: list[Dataset] | None = []
+    updated_at: datetime | None = None
+
+    # own additions
+    name: str | None = "default"
+    title: str | None = "Catalog"
+    description: str | None = None
+    maintainer: Maintainer | None = None
+    publisher: Publisher | None = None
+    url: HttpUrl | None = None
+    uri: str | None = None
+    logo_url: HttpUrl | None = None
+    git_repo: AnyUrl | None = None
+    catalogs: list[Catalog] | None = []
+
+    def __init__(self, **data):
+        if "name" not in data:
+            data["name"] = "Catalog"
+        super().__init__(**data)
+
+    def to_nk(self):
+        return self._nk_model(NKDataset, self.model_dump())
+
+    def get(self, name: str) -> Dataset | None:
+        for dataset in self.datasets:
+            if dataset.name == name:
+                return dataset
+
+    def get_datasets(self) -> Generator[Dataset, None, None]:
+        yield from self.datasets
+        for catalog in self.catalogs:
+            yield from catalog.datasets
+
+    def get_scope(self) -> NKDataset:
+        # FIXME
+        catalog = self.model_copy()
+        for ds in catalog.datasets:
+            ds.coverage = None
+        catalog = catalog.to_nk()
+        return NKDataset(
+            catalog,
+            {
+                "name": slugify(self.name),
+                "title": self.name.title(),
+                "children": catalog.names,
+            },
+        )
+
+    def metadata(self) -> dict[str, Any]:
+        catalog = self.model_copy()
+        catalog.datasets = []
+        catalog.catalogs = [c.metadata() for c in self.catalogs]
+        return catalog.model_dump()
+
+    @property
+    def names(self) -> set:
+        names = set()
+        for dataset in self.datasets:
+            names.add(dataset.name)
+        for catalog in self.catalogs:
+            names.update(catalog.names)
+        return names
+
+    @classmethod
+    def from_names(cls, names: Iterable[set]) -> Catalog:
+        return cls(datasets=[Dataset(name=n) for n in names])
+
+    def iterate(self) -> CEGenerator:
+        for dataset in self.datasets:
+            yield from dataset.iterate()
+
+
+Dataset.model_rebuild()
+Catalog.model_rebuild()
