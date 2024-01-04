a2b91383799e225f697c30ff5e1d031c99e739f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:35 +0000

    Add package holamundoplayertobbycr/0.0.1/holamundoplayertobbycr-0.0.1.tar.gz

---
+++ packages/holamundoplayertobbycr/holamundoplayertobbycr-0.0.1.tar.gz/holamundoplayertobbycr-0.0.1/holamundoplayer/__init__.py
@@ -0,0 +1,7 @@
+"""
+Esta es la documentacion del paquete
+"""
+
+# El paquete se llama "holamundoplayer" y es todo lo que tenemos en la carpeta que tiene ese mismo nombre. Todo
+# lo que colocamos dentro de los """""" en el archivo __init__.py será tomado por python como la documentacion
+# del paquete.
