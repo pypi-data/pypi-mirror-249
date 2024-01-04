2f0346b7587158067135e575117a920ea29f2871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:34 +0000

    Add package instancemethod/1.2.0/instancemethod-1.2.0-py3-none-any.whl

---
+++ packages/instancemethod/instancemethod-1.2.0-py3-none-any.whl/instancemethod/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "decorator",
+]
+AUTHOR = {
+    "Name": "Braden Toone",
+    "Email": "braden@toonetown.com"
+}
+HOMEPAGE = "https://github.com/Braden2n/InstanceMethod"
+DOCUMENTATION = "https://github.com/Braden2n/InstanceMethod"
+ISSUES = "https://github.com/Braden2n/InstanceMethod/issues"
+REPOSITORY = "https://github.com"
+CHANGELOG = "https://github.com/Braden2n/InstanceMethod/activity"
