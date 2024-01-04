e6cd8db3320502142d28a3cab71f1bd05b88cdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 06:03:50 +0000

    Add package tap-instagram/0.4.6/tap_instagram-0.4.6.tar.gz

---
+++ packages/tap-instagram/tap_instagram-0.4.6.tar.gz/tap_instagram-0.4.6/tap_instagram/client.py
@@ -0,0 +1,120 @@
+"""REST client handling, including InstagramStream base class."""
+
+import urllib.parse
+from pathlib import Path
+from typing import Any, Dict, Iterable, Optional
+
+import requests
+from singer_sdk.exceptions import FatalAPIError, RetriableAPIError
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+BASE_URL = "https://graph.facebook.com"
+
+
+class InstagramStream(RESTStream):
+    # TODO: Return the actual error from the Facebook API when fails
+    """Instagram stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return BASE_URL
+
+    records_jsonpath = "$[*]"  # Or override `parse_response`.
+    next_page_token_jsonpath = "$.paging.next"  # Or override `get_next_page_token`.
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        return headers
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        if self.next_page_token_jsonpath:
+            all_matches = extract_jsonpath(
+                self.next_page_token_jsonpath, response.json()
+            )
+            first_match = next(iter(all_matches), None)
+            next_page_token = first_match
+        else:
+            next_page_token = response.headers.get("X-Next-Page", None)
+
+        return next_page_token
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        if next_page_token:  # TODO: understand what this does & why it works!
+            return urllib.parse.parse_qs(urllib.parse.urlparse(next_page_token).query)
+        params: dict = {"access_token": self.config["access_token"]}
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
+    def validate_response(self, response: requests.Response) -> None:
+        if response.status_code == 400 and "Unsupported get request" in str(
+            response.json().get("error", {}).get("message")
+        ):
+            msg = (
+                f"{response.status_code} Client Error: "
+                f"{response.reason} - {response.json()['error']['message']}"
+                f" for path: {self.path}"
+            )
+            raise UnsupportedGetRequestError(msg)
+
+        elif 400 <= response.status_code < 500:
+            msg = (
+                f"{response.status_code} Client Error: "
+                f"{response.reason} - {response.json()['error']['message']}"
+                f" for path: {self.path}"
+            )
+            raise FatalAPIError(msg)
+
+        elif 500 <= response.status_code < 600:
+            msg = (
+                f"{response.status_code} Server Error: "
+                f"{response.reason} for path: {self.path}"
+            )
+            raise RetriableAPIError(msg)
+
+    def get_records(self, context: Optional[dict]) -> Iterable[Dict[str, Any]]:
+        """Return a generator of row-type dictionary objects.
+
+        Each row emitted should be a dictionary of property names to their values.
+
+        Args:
+            context: Stream partition or context dictionary.
+
+        Yields:
+            One item per (possibly processed) record in the API.
+        """
+        try:
+            for record in self.request_records(context):
+                transformed_record = self.post_process(record, context)
+                if transformed_record is None:
+                    # Record filtered out during post_process()
+                    continue
+                yield transformed_record
+        except UnsupportedGetRequestError as e:
+            self.logger.warning(e)
+
+
+class UnsupportedGetRequestError(Exception):
+    """
+    Error object to facilitate skipping IDs that cause trouble
+    with the API but aren't themselves grounds for ending the
+    entire ingestion process.
+    """
