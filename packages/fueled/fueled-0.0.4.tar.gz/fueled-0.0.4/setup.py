08bf974883957db3891bec6e695e3a7952cf4ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:31 +0000

    Add package fueled/0.0.4/fueled-0.0.4.tar.gz

---
+++ packages/fueled/fueled-0.0.4.tar.gz/fueled-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fueled",
+    version="0.0.4",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'fueled': ['data/vehicles.json']},
+    install_requires=[
+
+    ],
+    author='shoshone-s',
+    description="A Python package for vehicle mileage calculations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shoshone-s/fueled",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11.0',
+)
