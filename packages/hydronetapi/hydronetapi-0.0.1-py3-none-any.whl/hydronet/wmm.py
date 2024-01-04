a98818728046e907ee836d407ff14c66f402bf36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 04:03:23 +0000

    Add package hydronetapi/0.0.1/hydronetapi-0.0.1-py3-none-any.whl

---
+++ packages/hydronetapi/hydronetapi-0.0.1-py3-none-any.whl/hydronet/wmm.py
@@ -0,0 +1,16 @@
+import socket
+
+class wmm:
+    def __init__(self, modemid, bufsize=256, port=52001):
+        self.ipaddr = "192.168.2.%d" % (modemid)
+        self.port = port
+        self.bufsize = bufsize
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", self.port))
+
+    def send(self, data):
+        self.sock.sendto(bytes(data), (self.ipaddr, self.port))
+
+    def recv(self):
+        data = self.sock.recv(self.bufsize)
+        return data
