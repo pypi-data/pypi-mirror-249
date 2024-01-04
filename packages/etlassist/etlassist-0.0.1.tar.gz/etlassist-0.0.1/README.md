b1edba21b45be637ec4361cb61ace39014efe83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 15:04:44 +0000

    Add package etlassist/0.0.1/etlassist-0.0.1.tar.gz

---
+++ packages/etlassist/etlassist-0.0.1.tar.gz/etlassist-0.0.1/README.md
@@ -0,0 +1,14 @@
+# etlsassist
+
+etlassist is a python package supporting ETL operations
+
+
+## How to install etlassist
+
+```shell
+pip install etlassist
+```
+
+## How to use etlassist
+
+follow this article for better understanding and usage of etlassist
---
+++ packages/etlassist/etlassist-0.0.1.tar.gz/etlassist-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'etlassist is a python package supporting ETL operations'
+LONG_DESCRIPTION = 'etlassist can be used with general python and the functions used can also be extended as UDF for the databricks opertaions.'
+
+# Setting up
+setup(
+    name="etlassist",
+    version=VERSION,
+    author="Gokul Kumar Jayaram",
+    author_email="<gokulkumar.jayaram@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['lxml'],
+    keywords=['python', 'etlassist', 'packaging' , 'etl operations' , 'databricks'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
