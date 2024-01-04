7d149dada012b570cf5f53abce88a60dedf1006e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 06:03:53 +0000

    Add package BulkFileOperationsManager/0.0.2/BulkFileOperationsManager-0.0.2.tar.gz

---
+++ packages/BulkFileOperationsManager/BulkFileOperationsManager-0.0.2.tar.gz/BulkFileOperationsManager-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='BulkFileOperationsManager',
+    version='0.0.2',
+    description='Python package for managing bulk file CRUD operations.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Eugene Evstafev',
+    author_email='chigwel@gmail.com',
+    url='https://github.com/chigwell/BulkFileOperationsManager',
+    packages=find_packages(),
+    install_requires=[
+        'filechunkcrud'
+    ],
+)
