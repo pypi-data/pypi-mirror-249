8d061a3379871355248735fc9b060f2f243d2814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-04 14:03:57 +0000

    Add package best-toolbox/0.1.0a0/best_toolbox-0.1.0a0.tar.gz

---
+++ packages/best-toolbox/best_toolbox-0.1.0a0.tar.gz/best_toolbox-0.1.0a0/best/test/test_best_deep_learning.py
@@ -0,0 +1,18 @@
+# Copyright 2020-present, Mayo Clinic Department of Neurology
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import unittest
+from unittest import TestCase
+
+class Test_deep_learning(TestCase):
+    def test_import(self):
+        print("Testing import 'from best.deep_learning'" )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
