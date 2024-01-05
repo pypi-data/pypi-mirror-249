280fb95a58a86077a85e3f29cd2acd3e4bd91a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:04:24 +0000

    Add package tesla-fleet-api/0.0.3/tesla_fleet_api-0.0.3-py3-none-any.whl

---
+++ packages/tesla-fleet-api/tesla_fleet_api-0.0.3-py3-none-any.whl/tesla_fleet_api/Teslemetry.py
@@ -0,0 +1,27 @@
+import aiohttp
+from .TeslaFleetApi import TeslaFleetApi
+
+
+class Teslemetry(TeslaFleetApi):
+    def __init__(
+        self,
+        session: aiohttp.ClientSession,
+        access_token: str,
+        raise_for_status: bool = False,
+    ):
+        """Initialize the Teslemetry API."""
+        super().__init__(
+            session,
+            access_token,
+            use_command_protocol=False,
+            server="https://teslemetry.com",
+            raise_for_status=raise_for_status,
+        )
+
+    async def find_server(self):
+        """Find the server URL for the Tesla Fleet API."""
+        raise NotImplementedError("Do not use this function for Teslemetry.")
+
+    def stream(self, vin: str, fields, alerts, expire: int):
+        """Stream data from the Tesla Fleet API."""
+        raise NotImplementedError("Not implemented yet")
