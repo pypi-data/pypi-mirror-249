43fcf8260ca94844574e600d586077d62781d52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:03 +0000

    Add package leverage/1.12.0rc3/leverage-1.12.0rc3.tar.gz

---
+++ packages/leverage/leverage-1.12.0rc3.tar.gz/leverage-1.12.0rc3/leverage/modules/aws.py
@@ -0,0 +1,169 @@
+import click
+from click.exceptions import Exit
+
+from leverage import logger
+from leverage._internals import pass_state
+from leverage._internals import pass_container
+from leverage.container import get_docker_client
+from leverage.container import AWSCLIContainer
+from leverage.modules.utils import _handle_subcommand
+
+CONTEXT_SETTINGS = {"ignore_unknown_options": True}
+
+
+@click.group(invoke_without_command=True, add_help_option=False, context_settings=CONTEXT_SETTINGS)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+@pass_state
+@click.pass_context
+def aws(context, state, args):
+    """Run AWS CLI commands in a custom containerized environment."""
+    cli = AWSCLIContainer(get_docker_client())
+    state.container = cli
+    state.container.ensure_image()
+
+    _handle_subcommand(context=context, cli_container=cli, args=args)
+
+
+@aws.group(invoke_without_command=True, add_help_option=False, context_settings=CONTEXT_SETTINGS)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+@pass_container
+@click.pass_context
+def configure(context, cli, args):
+    """configure"""
+    _handle_subcommand(context=context, cli_container=cli, args=args, caller_name="configure")
+
+
+@configure.command("sso")
+@pass_container
+@click.pass_context
+def _sso(context, cli):
+    """configure sso"""
+    if (
+        cli.cwd in (cli.root_dir, cli.account_dir)
+        or cli.account_dir.parent != cli.root_dir
+        or not list(cli.cwd.glob("*.tf"))
+    ):
+        logger.error("SSO configuration can only be performed at [bold]layer[/bold] level.")
+        raise Exit(1)
+
+    # region_primary was added in refarch v1
+    # for v2 it was replaced by region at project level
+    region_primary = "region_primary"
+    if not "region_primary" in cli.common_conf:
+        region_primary = "region"
+    default_region = cli.common_conf.get(region_primary, cli.common_conf.get("sso_region"))
+    if default_region is None:
+        logger.error("No primary region configured in global config file.")
+        raise Exit(1)
+
+    logger.info("Configuring default profile.")
+    default_profile = {"region": default_region, "output": "json"}
+    for key, value in default_profile.items():
+        cli.exec(f"configure set {key} {value}", profile="default")
+
+    if not all(sso_key in cli.common_conf for sso_key in ("sso_start_url", "sso_region")):
+        logger.error("Missing configuration values for SSO in global config file.")
+        raise Exit(1)
+
+    sso_role = cli.account_conf.get("sso_role")
+    if not sso_role:
+        logger.error("Missing SSO role in account config file.")
+        raise Exit(1)
+
+    current_account = cli.account_conf.get("environment")
+    try:
+        # this is for refarch v1
+        account_id = cli.common_conf.get("accounts").get(current_account).get("id")
+    except AttributeError:
+        # this is for refarch v2
+        try:
+            # this is for accounts with no org unit on top of it
+            account_id = cli.common_conf.get("organization").get("accounts").get(current_account).get("id")
+        except AttributeError:
+            try:
+                # this is for accounts with no org unit on top of it
+                found = False
+                for ou in cli.common_conf.get("organization").get("organizational_units"):
+                    if current_account in cli.common_conf.get("organization").get("organizational_units").get(ou).get(
+                        "accounts"
+                    ):
+                        account_id = (
+                            cli.common_conf.get("organization")
+                            .get("organizational_units")
+                            .get(ou)
+                            .get("accounts")
+                            .get(current_account)
+                            .get("id")
+                        )
+                        found = True
+                        break
+                if not found:
+                    raise AttributeError
+            except AttributeError:
+                logger.error(f"Missing account configuration for [bold]{current_account}[/bold] in global config file.")
+                raise Exit(1)
+    if not account_id:
+        logger.error(f"Missing id for account [bold]{current_account}[/bold].")
+        raise Exit(1)
+
+    logger.info(f"Configuring [bold]{cli.project}-sso[/bold] profile.")
+    sso_profile = {
+        "sso_start_url": cli.common_conf.get("sso_start_url"),
+        "sso_region": cli.common_conf.get("sso_region", cli.common_conf.get(region_primary)),
+        "sso_account_id": account_id,
+        "sso_role_name": sso_role,
+    }
+    for key, value in sso_profile.items():
+        cli.exec(f"configure set {key} {value}", profile=f"{cli.project}-sso")
+
+    context.invoke(login)
+
+    logger.info("Storing account information.")
+    exit_code = cli.system_start(cli.AWS_SSO_CONFIGURE_SCRIPT)
+    if exit_code:
+        raise Exit(exit_code)
+
+
+@aws.group(invoke_without_command=True, add_help_option=False, context_settings=CONTEXT_SETTINGS)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+@pass_container
+@click.pass_context
+def sso(context, cli, args):
+    """sso"""
+    _handle_subcommand(context=context, cli_container=cli, args=args, caller_name="sso")
+
+
+@sso.command()
+@pass_container
+def login(cli):
+    """Login"""
+    # only from account or layer directories
+    # when to fail:
+    # - when this cond meets:
+    #   - no account dir
+    #   - no layer dir
+    if not cli.get_location_type() in ["account", "layer", "layers-group"]:
+        logger.error("SSO configuration can only be performed at [bold]layer[/bold] or [bold]account[/bold] level.")
+        raise Exit(1)
+
+    exit_code, region = cli.exec(f"configure get sso_region --profile {cli.project}-sso")
+    if exit_code:
+        logger.error(f"Region configuration for [bold]{cli.project}-sso[/bold] profile not found.")
+        raise Exit(1)
+
+    if exit_code := cli.sso_login():
+        raise Exit(exit_code)
+
+
+@sso.command()
+@pass_container
+def logout(cli):
+    """Logout"""
+    exit_code = cli.system_start(cli.AWS_SSO_LOGOUT_SCRIPT)
+    if exit_code:
+        raise Exit(exit_code)
+
+    logger.info(
+        f"Don't forget to log out of your [bold]AWS SSO[/bold] start page {cli.common_conf.get('sso_start_url')}"
+        " and your external identity provider portal."
+    )
---
+++ packages/leverage/leverage-1.12.0rc3.tar.gz/leverage-1.12.0rc3/leverage/path.py
@@ -0,0 +1,122 @@
+"""
+    Utilities to obtain relevant files' and directories' locations
+"""
+from pathlib import Path
+from subprocess import run
+from subprocess import PIPE
+from subprocess import CalledProcessError
+
+
+class NotARepositoryError(RuntimeError):
+    pass
+
+
+def get_working_path():
+    """Get the interpreters current directory.
+
+    Returns:
+        str: Current working directory.
+    """
+    return Path.cwd().as_posix()
+
+
+def get_home_path():
+    """Get the current user's home directory.
+
+    Returns:
+        str: User's home directory.
+    """
+    return Path.home().as_posix()
+
+
+def get_root_path():
+    """Get the path to the root of the Git repository.
+
+    Raises:
+        NotARepositoryError: If the current directory is not within a git repository.
+
+    Returns:
+        str: Root of the repository.
+    """
+    try:
+        root = run(
+            ["git", "rev-parse", "--show-toplevel"], stdout=PIPE, stderr=PIPE, check=True, encoding="utf-8"
+        ).stdout
+
+    except CalledProcessError as exc:
+        if "fatal: not a git repository" in exc.stderr:
+            raise NotARepositoryError("Not running in a git repository.")
+
+    return root.strip()
+
+
+def get_account_path():
+    """Get the path to the current account directory.
+
+    Returns:
+        str: Path to the current account directory.
+    """
+    root_path = Path(get_root_path())
+    cur_path = Path(get_working_path())
+    prev_path = cur_path
+
+    # NOTE: currently we only support up to 5 subdir levels. Normally we use
+    #       only 2 subdirectories so this should be enough for most cases.
+    for _ in range(5):
+        if cur_path.resolve() == root_path:
+            break
+
+        prev_path = cur_path
+        cur_path = cur_path.parent
+
+    return prev_path.as_posix()
+
+
+def get_global_config_path():
+    """Get the path to the config that is common to all accounts.
+
+    Returns:
+        str: Global config file path.
+    """
+    return f"{get_root_path()}/config"
+
+
+def get_account_config_path():
+    """Get the path to the config of the current account.
+
+    Returns:
+        str: Current config file path.
+    """
+    return f"{get_account_path()}/config"
+
+
+def get_build_script_path(filename="build.py"):
+    """Get path to the build script containing all tasks to be run.
+    Search through the current directory up to the repository's root directory.
+
+    Args:
+        filename (str, optional): The name of the build script containing the tasks.
+            Defaults to "build.py".
+
+    Returns:
+        str: Build script file path. None if no file with the given name is found or
+            the current directory is not a git repository.
+    """
+    try:
+        root_path = Path(get_root_path())
+    except NotARepositoryError:
+        script = Path(filename)
+        return script.absolute().as_posix() if script.exists() else None
+
+    cur_path = Path(get_working_path())
+
+    while True:
+        build_script = list(cur_path.glob(filename))
+
+        if build_script:
+            return build_script[0].as_posix()
+
+        if cur_path == root_path:
+            break
+
+        cur_path = cur_path.parent
