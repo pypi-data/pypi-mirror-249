f8b00910934026ed40c2fb3db889eb7ecdfb63b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:18 +0000

    Add package hyko-sdk/0.4.22/hyko_sdk-0.4.22-py3-none-any.whl

---
+++ packages/hyko-sdk/hyko_sdk-0.4.22-py3-none-any.whl/hyko_sdk/function.py
@@ -0,0 +1,293 @@
+import asyncio
+import inspect
+import json
+from typing import Any, Callable, Coroutine, Optional, Type, TypeVar
+
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+from .io import HykoBaseType
+from .metadata import HykoJsonSchemaExt, IOPortType, MetaDataBase
+from .types import PyObjectId
+from .utils import model_to_friendly_property_types
+
+InputsType = TypeVar("InputsType", bound="BaseModel")
+ParamsType = TypeVar("ParamsType", bound="BaseModel")
+OutputsType = TypeVar("OutputsType", bound="BaseModel")
+
+OnStartupFuncType = Callable[[], Coroutine[Any, Any, None]]
+OnShutdownFuncType = Callable[[], Coroutine[Any, Any, None]]
+OnExecuteFuncType = Callable[[InputsType, ParamsType], Coroutine[Any, Any, OutputsType]]
+
+
+class ExecStorageParams(BaseModel):
+    host: str
+    project_id: PyObjectId
+    blueprint_id: PyObjectId
+
+
+class SDKFunction(FastAPI):
+    class InvalidExecSignature(BaseException):
+        f_args: list[tuple[str, Type[Any]]]
+        f_ret_type: Optional[Type[Any]]
+
+        def __init__(
+            self, f_args: list[tuple[str, Type[Any]]], f_ret_type: Optional[Type[Any]]
+        ) -> None:
+            self.f_args = f_args
+            self.f_ret_type = f_ret_type
+
+        def __str__(self) -> str:
+            return f"args: {self.f_args}, ret_type: {self.f_ret_type}"
+
+    class InvalidExecParamsCount(InvalidExecSignature):
+        pass
+
+    class InvalidExecInputsType(InvalidExecSignature):
+        name: str
+        type: Type[Any]
+
+        def __init__(
+            self,
+            f_args: list[tuple[str, Type[Any]]],
+            f_ret_type: Optional[Type[Any]],
+            name: str,
+            type: Type[Any],
+        ) -> None:
+            super().__init__(f_args, f_ret_type)
+            self.name = name
+            self.type = type
+
+    class InvalidExecParamsType(InvalidExecInputsType):
+        pass
+
+    class InvalidExecRetType(InvalidExecSignature):
+        pass
+
+    __metadata__: MetaDataBase
+    startup_tasks: list[asyncio.Task[None]] = []
+
+    def __init__(
+        self,
+        description: str,
+        requires_gpu: bool,
+        **kwargs: Any,
+    ):
+        super().__init__(**kwargs)
+        self._status = asyncio.Future[bool]()
+        self.description = description
+        self.requires_gpu = requires_gpu
+
+    def on_startup(self, f: OnStartupFuncType):
+        def blocking_exec():
+            asyncio.run(f())
+
+        try:
+            asyncio.get_running_loop()
+            self.startup_tasks.append(
+                asyncio.create_task(asyncio.to_thread(blocking_exec))
+            )
+        except RuntimeError:
+            pass
+
+    async def _wait_startup_tasks(self):
+        if not len(self.startup_tasks):
+            return
+
+        done, _ = await asyncio.wait(
+            self.startup_tasks, return_when=asyncio.FIRST_EXCEPTION
+        )
+        for task in done:
+            exc = task.exception()
+            if exc is not None:
+                raise exc
+
+    def on_shutdown(self, f: OnShutdownFuncType) -> OnShutdownFuncType:
+        async def wrapper() -> None:
+            await f()
+
+        return self.on_event("shutdown")(wrapper)
+
+    def on_execute(self, f: OnExecuteFuncType[InputsType, ParamsType, OutputsType]):
+        async def wait_startup_handler():
+            try:
+                await self._wait_startup_tasks()
+            except HTTPException as e:
+                raise e
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Exception occured during startup:\n{e.__repr__()}",
+                ) from e
+
+        self.get("/wait_startup")(wait_startup_handler)
+        f_args = [
+            (param.name, param.annotation)
+            for param in inspect.signature(f).parameters.values()
+        ]
+
+        f_ret_type = inspect.signature(f).return_annotation
+        if len(f_args) < 2:
+            raise SDKFunction.InvalidExecParamsCount(
+                f_args=f_args,
+                f_ret_type=f_ret_type,
+            )
+
+        f_inputs_name, f_inputs_type = f_args[0]
+
+        f_params_name, f_params_type = f_args[1]
+
+        if not issubclass(f_inputs_type, BaseModel):
+            raise SDKFunction.InvalidExecInputsType(
+                f_args=f_args,
+                f_ret_type=f_ret_type,
+                name=f_inputs_name,
+                type=f_inputs_type,
+            )
+
+        if not issubclass(f_params_type, BaseModel):
+            raise SDKFunction.InvalidExecInputsType(
+                f_args=f_args,
+                f_ret_type=f_ret_type,
+                name=f_params_name,
+                type=f_params_type,
+            )
+
+        if not issubclass(f_ret_type, BaseModel) or isinstance(
+            f_ret_type, inspect.Parameter.empty
+        ):
+            raise SDKFunction.InvalidExecRetType(
+                f_args=f_args,
+                f_ret_type=f_ret_type,
+            )
+
+        inputs_json_schema = f_inputs_type.model_json_schema()
+
+        if inputs_json_schema.get("$defs"):
+            for k, v in inputs_json_schema["$defs"].items():
+                if v.get("type") and v["type"] == "numeric":
+                    inputs_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+        if inputs_json_schema.get("properties"):
+            for k, v in inputs_json_schema["properties"].items():
+                if v.get("allOf") and len(v["allOf"]) == 1:
+                    all_of = inputs_json_schema["properties"][k].pop("allOf")
+                    inputs_json_schema["properties"][k]["$ref"] = all_of[0]["$ref"]
+
+        params_json_schema = f_params_type.model_json_schema()
+        if params_json_schema.get("$defs"):
+            for k, v in params_json_schema["$defs"].items():
+                if v.get("type") and v["type"] == "numeric":
+                    params_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+        if params_json_schema.get("properties"):
+            for k, v in params_json_schema["properties"].items():
+                if v.get("allOf") and len(v["allOf"]) == 1:
+                    all_of = params_json_schema["properties"][k].pop("allOf")
+                    params_json_schema["properties"][k]["$ref"] = all_of[0]["$ref"]
+
+        outputs_json_schema = f_ret_type.model_json_schema()
+        if outputs_json_schema.get("$defs"):
+            for k, v in outputs_json_schema["$defs"].items():
+                if v.get("type") and v["type"] == "numeric":
+                    outputs_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+        if outputs_json_schema.get("properties"):
+            for k, v in outputs_json_schema["properties"].items():
+                if v.get("allOf") and len(v["allOf"]) == 1:
+                    all_of = outputs_json_schema["properties"][k].pop("allOf")
+                    outputs_json_schema["properties"][k]["$ref"] = all_of[0]["$ref"]
+
+        self.__metadata__ = MetaDataBase(
+            description=self.description,
+            inputs=HykoJsonSchemaExt(
+                **inputs_json_schema,
+                friendly_property_types=model_to_friendly_property_types(f_inputs_type),
+            ),
+            params=HykoJsonSchemaExt(
+                **params_json_schema,
+                friendly_property_types=model_to_friendly_property_types(f_params_type),
+            ),
+            outputs=HykoJsonSchemaExt(
+                **outputs_json_schema,
+                friendly_property_types=model_to_friendly_property_types(f_ret_type),
+            ),
+            requires_gpu=self.requires_gpu,
+        )
+
+        async def wrapper(
+            storage_params: ExecStorageParams, inputs: InputsType, params: ParamsType
+        ) -> JSONResponse:
+            for task in self.startup_tasks:
+                if not task.done():
+                    raise HTTPException(
+                        status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                        detail="SDK Function did not finish startup",
+                    )
+
+            pending_download_tasks: list[asyncio.Task[None]] = []
+            HykoBaseType.set_sync(
+                storage_params.host,
+                storage_params.project_id,
+                storage_params.blueprint_id,
+                pending_download_tasks,
+            )
+
+            inputs = inputs.model_validate_json(inputs.model_dump_json())
+            params = params.model_validate_json(params.model_dump_json())
+            HykoBaseType.clear_sync()
+
+            if len(pending_download_tasks):
+                done, _ = await asyncio.wait(pending_download_tasks)
+                for task in done:
+                    exc = task.exception()
+                    if exc is not None:
+                        raise exc
+
+            try:
+                outputs = await f(inputs, params)
+            except HTTPException as e:
+                raise e
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Exception occured during execution:\n{e.__repr__()}",
+                ) from e
+
+            pending_upload_tasks: list[asyncio.Task[None]] = []
+            HykoBaseType.set_sync(
+                storage_params.host,
+                storage_params.project_id,
+                storage_params.blueprint_id,
+                pending_upload_tasks,
+            )
+
+            outputs = outputs.model_validate(outputs.model_dump())
+            HykoBaseType.clear_sync()
+
+            if len(pending_upload_tasks):
+                done, _ = await asyncio.wait(pending_upload_tasks)
+                for task in done:
+                    exc = task.exception()
+                    if exc is not None:
+                        raise exc
+
+            return JSONResponse(content=json.loads(outputs.model_dump_json()))
+
+        storage_params_annotations = wrapper.__annotations__["storage_params"]
+        wrapper.__annotations__ = f.__annotations__
+        wrapper.__annotations__["storage_params"] = storage_params_annotations
+
+        return self.post("/execute")(wrapper)
+
+    def get_metadata(self) -> MetaDataBase:
+        return self.__metadata__
+
+    def dump_metadata(self, indent: Optional[int] = None) -> str:
+        return self.get_metadata().model_dump_json(indent=indent)
+
+
+__all__ = [
+    "SDKFunction",
+]
---
+++ packages/hyko-sdk/hyko_sdk-0.4.22-py3-none-any.whl/hyko_sdk/utils.py
@@ -0,0 +1,163 @@
+import base64
+import json
+from io import BytesIO
+from typing import Type
+from uuid import UUID
+
+import httpx
+from fastapi import HTTPException, status
+from httpx import Timeout
+from pydantic import BaseModel
+
+from .metadata import HykoJsonSchemaExt, IOPortType, MetaData, MetaDataBase
+from .types import PyObjectId, StorageObjectType
+
+
+class ObjectStorageConn:
+    class DownloadError(HTTPException):
+        """Raised when an error occurs on file download"""
+
+        pass
+
+    class UploadError(HTTPException):
+        """Raised when an error occurs on file upload"""
+
+        pass
+
+    def __init__(
+        self,
+        host: str,
+        project_id: PyObjectId,
+        blueprint_id: PyObjectId,
+    ) -> None:
+        self.project_id = project_id
+        self.blueprint_id = blueprint_id
+        self._conn = httpx.AsyncClient(
+            base_url=f"https://{host}/projects/{project_id}/blueprints/{blueprint_id}",
+            http2=True,
+            verify=False,
+            timeout=Timeout(timeout=120),
+        )
+        pass
+
+    async def download_object(
+        self,
+        id: UUID,
+        expected_object_types: list[StorageObjectType],
+    ):
+        res = await self._conn.get(f"/storage/{id}")
+        if not res.is_success:
+            raise ObjectStorageConn.DownloadError(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Could not get object content, status: {res.status_code}, res: {res.text}",
+            )
+
+        headers = res.headers
+        object_name = headers.get("X-Hyko-Storage-Name")
+        object_type = headers.get("X-Hyko-Storage-Type")
+        object_data = bytearray(res.content)
+
+        if object_type not in expected_object_types:
+            raise ObjectStorageConn.DownloadError(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Expected object types {expected_object_types}, but got {object_type}",
+            )
+
+        return (object_name, object_type, object_data)
+
+    async def upload_object(self, filename: str, content_type: str, data: bytearray):
+        res = await self._conn.post(
+            url="/storage",
+            files={"file": (filename, BytesIO(data), content_type)},
+        )
+        if not res.is_success:
+            raise ObjectStorageConn.UploadError(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Upload error, status: {res.status_code}, res: {res.text}",
+            )
+        obj_id = UUID(res.text[1:-1])
+
+        return obj_id
+
+
+def metadata_to_docker_label(metadata: MetaData) -> str:
+    return base64.b64encode(
+        metadata.model_dump_json(exclude_unset=True, exclude_none=True).encode()
+    ).decode()
+
+
+def docker_label_to_metadata(label: str) -> MetaData:
+    return MetaData(**json.loads(base64.b64decode(label.encode()).decode()))
+
+
+def model_to_friendly_property_types(pydantic_model: Type[BaseModel]):
+    out: dict[str, str] = {}
+    for field_name, field in pydantic_model.model_fields.items():
+        annotation = str(field.annotation).lower()
+        if "enum" in annotation:
+            try:
+                out[
+                    field_name
+                ] = f"enum[{str(pydantic_model.model_json_schema()['$defs'][str(field.annotation)[7:-2]]['type'])}]"
+                if "numeric" in out[field_name]:
+                    out[field_name] = out[field_name].replace("numeric", "number")
+            except KeyError as e:
+                raise RuntimeError(
+                    f'Could not find {str(pydantic_model.model_json_schema()["$defs"][str(field.annotation)[7:-2]])} the enums defined in the json schema. Usually happens when you use class(Video, Enum) or similar type'
+                ) from e
+            continue
+        if "<class" in annotation:
+            annotation = annotation[8:-2]
+        annotation = annotation.replace("hyko_sdk.io.", "")
+        annotation = annotation.replace("typing.", "")
+        annotation = annotation.replace("str", "string")
+        annotation = annotation.replace("int", "integer")
+        annotation = annotation.replace("float", "number")
+
+        out[field_name] = annotation
+    return out
+
+
+def extract_metadata(
+    inputs: BaseModel,
+    params: BaseModel,
+    outputs: BaseModel,
+    description: str,
+    requires_gpu: bool,
+):
+    inputs_json_schema = inputs.model_json_schema()
+    if inputs_json_schema.get("$defs"):
+        for k, v in inputs_json_schema["$defs"].items():
+            if v.get("type") and v["type"] == "numeric":
+                inputs_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+    params_json_schema = params.model_json_schema()
+    if params_json_schema.get("$defs"):
+        for k, v in params_json_schema["$defs"].items():
+            if v.get("type") and v["type"] == "numeric":
+                params_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+    outputs_json_schema = outputs.model_json_schema()
+    if outputs_json_schema.get("$defs"):
+        for k, v in outputs_json_schema["$defs"].items():
+            if v.get("type") and v["type"] == "numeric":
+                outputs_json_schema["$defs"][k]["type"] = IOPortType.NUMBER
+
+    __meta_data__ = MetaDataBase(
+        description=description,
+        inputs=HykoJsonSchemaExt(
+            **inputs_json_schema,
+            friendly_property_types=model_to_friendly_property_types(inputs),  # type: ignore
+        ),
+        params=HykoJsonSchemaExt(
+            **params_json_schema,
+            friendly_property_types=model_to_friendly_property_types(params),  # type: ignore
+        ),
+        outputs=HykoJsonSchemaExt(
+            **outputs_json_schema,
+            friendly_property_types=model_to_friendly_property_types(outputs),  # type: ignore
+        ),
+        requires_gpu=requires_gpu,
+    )
+
+    print(__meta_data__.model_dump_json(indent=2, exclude_unset=True))  # noqa: T201  # Important: Do not delete this print statement, as it outputs the metadata captured in the stdout
