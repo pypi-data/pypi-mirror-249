cfbac34e3e60d166ea1cd7d23c263d23493b18b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:00 +0000

    Add package aiml-l-prjt/1.2.9/aiml_l_prjt-1.2.9.tar.gz

---
+++ packages/aiml-l-prjt/aiml_l_prjt-1.2.9.tar.gz/aiml_l_prjt-1.2.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiml_l_prjt',
+    version='1.2.9',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
