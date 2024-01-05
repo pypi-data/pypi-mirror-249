cfb6a817e10ab064ccabba58ecf9af168adceb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:20 +0000

    Add package linkquarto/0.2.4/linkquarto-0.2.4-py3-none-any.whl

---
+++ packages/linkquarto/linkquarto-0.2.4-py3-none-any.whl/linkquarto/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("package Linkmaths_quarto")
+
+__name__ = "linkmaths_quarto"
+
+"""
+Ce package est conçu pour générer une mise en forme des documents linkpact sous différents formats.
+Des templates de rédections sont initiés pour permettre à l'utilisateur de produire des rendus 
+au bon format sans se soucier de la mise en forme.
+"""
+
+#TODO: importer tous les modules du package ici
+
+from linkquarto import link_color
