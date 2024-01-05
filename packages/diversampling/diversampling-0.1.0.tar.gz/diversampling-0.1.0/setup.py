148309deb295b86e75c39a2ea233a6f3a1fb9e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:05:02 +0000

    Add package diversampling/0.1.0/diversampling-0.1.0.tar.gz

---
+++ packages/diversampling/diversampling-0.1.0.tar.gz/diversampling-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='diversampling',
+      version='0.1.0',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      description='An unsupervised methodology to implement a diversified under-sampling process to deal with the extremely imbalanced binary classification problem',
+      author='ReidZ',
+      author_email='reid.zhuang@icloud.com',
+      requires= ['numpy','sklearn', 'pandas', 'joblib', 'imblearn'], 
+      packages=find_packages(),
+      package_data={"diversampling": ["demo.ipynb"], "diversampling": ["README.md"], "diversampling":["LICENSE"], "diversampling":["NOTICE"]},
+      license="Apache License 2.0",
+      install_requires=['numpy','scikit-learn', 'pandas', 'joblib', 'imbalanced-learn']
+      )
