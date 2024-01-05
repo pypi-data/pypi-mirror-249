62358c7c956cb37928d987d69dd806efae9ef7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:03:49 +0000

    Add package fastask/0.2.3/fastask-0.2.3.tar.gz

---
+++ packages/fastask/fastask-0.2.3.tar.gz/fastask-0.2.3/setup.py
@@ -0,0 +1,26 @@
+# file: setup.py
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fastask',
+    packages=['fastask'],
+    version='0.2.3',
+    py_modules=['fastask.ask'],
+    include_package_data=True,
+    package_data={'fastask': ['Modelfile']},
+    install_requires=[
+        'openai',
+        'configparser',
+        'inquirer',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ask = fastask.ask:main',
+        ],
+    },
+     long_description=long_description,
+    long_description_content_type="text/markdown",
+)
