25ed5653fd629d8fdc423ef243e398839c00ab54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:12 +0000

    Add package localstack-extension-aws-replicator/0.1.6/localstack_extension_aws_replicator-0.1.6-py3-none-any.whl

---
+++ packages/localstack-extension-aws-replicator/localstack_extension_aws_replicator-0.1.6-py3-none-any.whl/aws_replicator/config.py
@@ -0,0 +1,5 @@
+from localstack.constants import INTERNAL_RESOURCE_PATH
+
+# handler path within the internal /_localstack endpoint
+HANDLER_PATH_REPLICATE = f"{INTERNAL_RESOURCE_PATH}/aws/replicate"
+HANDLER_PATH_PROXIES = f"{INTERNAL_RESOURCE_PATH}/aws/proxies"
