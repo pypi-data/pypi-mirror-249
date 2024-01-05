36cc1c3fc2ac6afe4e199cd897e0d4529bb4c26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:05:01 +0000

    Add package poptimizer/0.0.1/poptimizer-0.0.1.tar.gz

---
+++ packages/poptimizer/poptimizer-0.0.1.tar.gz/poptimizer-0.0.1/setup.py
@@ -0,0 +1,20 @@
+"""
+Setup file
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='poptimizer',
+    version='0.0.1',
+    description='Utilize Large Language Models to Automatically Optimize Prompts for LLM Application Development',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='alkhalifas',
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "langchain",
+        "openai",
+        "pylint"
+    ],
+)
