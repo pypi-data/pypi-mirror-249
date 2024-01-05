58d794e2dfb3537c75c6949defebff4363f69187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 15:05:47 +0000

    Add package RepoDynamics/0.0.0.dev207/RepoDynamics-0.0.0.dev207.tar.gz

---
+++ packages/RepoDynamics/RepoDynamics-0.0.0.dev207.tar.gz/RepoDynamics-0.0.0.dev207/src/repodynamics/meta/files/package/__init__.py
@@ -0,0 +1,15 @@
+from repodynamics.meta.files.package.python import PythonPackageFileGenerator
+from repodynamics.meta.manager import MetaManager
+from repodynamics.path import PathFinder
+from repodynamics.logger import Logger
+
+
+def generate(
+    metadata: MetaManager,
+    paths: PathFinder,
+    logger: Logger = None,
+):
+    if metadata["package"]["type"] == "python":
+        return PythonPackageFileGenerator(metadata=metadata, paths=paths, logger=logger).generate()
+    else:
+        return []
