e52448973047cd8f8ba490db8e02823174b1ac8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:21 +0000

    Add package looqbox/5.0.7/looqbox-5.0.7-py3-none-any.whl

---
+++ packages/looqbox/looqbox-5.0.7-py3-none-any.whl/looqbox/objects/response_parameters/temporal_granularity.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class TemporalGranularity(Enum):
+    DAYS = "DAYS"
+    HOURS = "HOURS"
+    WEEKS = "WEEKS"
+    MINUTES = "MINUTES"
+    SECONDS = "SECONDS"
+    MONTHS = "MONTHS"
+    YEARS = "YEARS"
+    DECADES = "DECADES"
+    CENTURIES = "CENTURIES"
+    MILLENNIA = "MILLENNIA"
