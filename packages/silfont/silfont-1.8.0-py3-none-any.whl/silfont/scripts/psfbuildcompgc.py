8e62b0666b1596efda8147bfe489cce7777e7faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:05:06 +0000

    Add package silfont/1.8.0/silfont-1.8.0-py3-none-any.whl

---
+++ packages/silfont/silfont-1.8.0-py3-none-any.whl/silfont/scripts/psfbuildcompgc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''Uses the GlyphConstruction library to build composite glyphs.'''
+__url__ = 'https://github.com/silnrsi/pysilfont'
+__copyright__ = 'Copyright (c) 2018 SIL International (https://www.sil.org)'
+__license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
+__author__ = 'Victor Gaultney'
+
+from silfont.core import execute
+from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
+
+argspec = [
+    ('ifont', {'help': 'Input font filename'}, {'type': 'infont'}),
+    ('ofont',{'help': 'Output font file','nargs': '?' }, {'type': 'outfont'}),
+    ('-i','--cdfile',{'help': 'Composite Definitions input file'}, {'type': 'infile', 'def': 'constructions.txt'}),
+    ('-l','--log',{'help': 'Set log file name'}, {'type': 'outfile', 'def': '_gc.log'})]
+
+def doit(args) :
+    font = args.ifont
+    logger = args.logger
+
+    constructions = ParseGlyphConstructionListFromString(args.cdfile)
+
+    for construction in constructions :
+        # Create a new constructed glyph object
+        try:
+            constructionGlyph = GlyphConstructionBuilder(construction, font)
+        except ValueError as e:
+            logger.log("Invalid CD line '" + construction + "' - " + str(e), "E")
+        else:
+            # Make a new glyph in target font with the new glyph name
+            glyph = font.newGlyph(constructionGlyph.name)
+            # Draw the constructed object onto the new glyph
+            # This is rather odd in how it works
+            constructionGlyph.draw(glyph.getPen())
+            # Copy glyph metadata from constructed object
+            glyph.name = constructionGlyph.name
+            glyph.unicode = constructionGlyph.unicode
+            glyph.note = constructionGlyph.note
+            #glyph.markColor = constructionGlyph.mark
+            glyph.width = constructionGlyph.width
+
+    return font
+
+def cmd() : execute("FP",doit,argspec)
+if __name__ == "__main__": cmd()
---
+++ packages/silfont/silfont-1.8.0-py3-none-any.whl/silfont/scripts/psfmakedeprecated.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+'''Creates deprecated versions of glyphs: takes the specified glyph and creates a
+duplicate with an additional box surrounding it so that it becomes reversed, 
+and assigns a new unicode encoding to it.
+Input is a csv with three fields: original,new,unicode'''
+__url__ = 'https://github.com/silnrsi/pysilfont'
+__copyright__ = 'Copyright (c) 2018 SIL International (https://www.sil.org)'
+__license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
+__author__ = 'Victor Gaultney'
+
+from silfont.core import execute
+
+argspec = [
+    ('ifont', {'help': 'Input font filename'}, {'type': 'infont'}),
+    ('ofont',{'help': 'Output font file','nargs': '?' }, {'type': 'outfont'}),
+    ('-i','--input',{'help': 'Input csv file'}, {'type': 'incsv', 'def': 'todeprecate.csv'}),
+    ('-l','--log',{'help': 'Set log file name'}, {'type': 'outfile', 'def': '_deprecated.log'})]
+
+offset = 30
+
+def doit(args) :
+    font = args.ifont
+    logger = args.logger
+
+    # Process csv list into a dictionary structure
+    args.input.numfields = 3
+    deps = {}
+    for line in args.input :
+        deps[line[0]] = {"newname": line[1], "newuni": line[2]}
+
+    # Iterate through dictionary (unsorted)
+    for source, target in deps.items() :
+        # Check if source glyph is in font
+        if source in font.keys() :
+            # Give warning if target is already in font, but overwrite anyway
+            targetname = target["newname"]
+            targetuni = int(target["newuni"], 16)
+            if targetname in font.keys() :
+                logger.log("Warning: " + targetname + " already in font and will be replaced")
+
+            # Make a copy of source into a new glyph object
+            sourceglyph = font[source]            
+            newglyph = sourceglyph.copy()
+            
+            # Draw box around it
+            xmin, ymin, xmax, ymax = sourceglyph.bounds
+            pen = newglyph.getPen()
+            pen.moveTo((xmax + offset, ymin - offset))
+            pen.lineTo((xmax + offset, ymax + offset))
+            pen.lineTo((xmin - offset, ymax + offset))
+            pen.lineTo((xmin - offset, ymin - offset))
+            pen.closePath()
+
+            # Set unicode
+            newglyph.unicodes = []
+            newglyph.unicode = targetuni
+
+            # Add the new glyph object to the font with name target
+            font.__setitem__(targetname,newglyph)
+
+            # Decompose glyph in case there may be components
+            # It seems you can't decompose a glyph has hasn't yet been added to a font
+            font[targetname].decompose()
+            # Correct path direction            
+            font[targetname].correctDirection()            
+
+            logger.log(source + " duplicated to " + targetname)
+        else :
+            logger.log("Warning: " + source + " not in font")
+
+    return font
+
+def cmd() : execute("FP",doit,argspec)
+if __name__ == "__main__": cmd()
