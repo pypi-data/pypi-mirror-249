8ee90f59d26802d50a135b860e8bf04371fedad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:04 +0000

    Add package cliconfig/1.3.6/cliconfig-1.3.6-py3-none-any.whl

---
+++ packages/cliconfig/cliconfig-1.3.6-py3-none-any.whl/cliconfig/processing/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 Valentin Goldite. All Rights Reserved.
+"""Processing module to apply functions on merge (before and after), save and load."""
+
+from cliconfig.processing.base import Processing
+
+__all__ = ["Processing"]
