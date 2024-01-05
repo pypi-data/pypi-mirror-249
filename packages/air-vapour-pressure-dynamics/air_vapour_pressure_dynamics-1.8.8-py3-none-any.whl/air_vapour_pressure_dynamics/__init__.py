aa65e5c13f50f28205b9144655f53c5faad878d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:00 +0000

    Add package air-vapour-pressure-dynamics/1.8.8/air_vapour_pressure_dynamics-1.8.8-py3-none-any.whl

---
+++ packages/air-vapour-pressure-dynamics/air_vapour_pressure_dynamics-1.8.8-py3-none-any.whl/air_vapour_pressure_dynamics/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+air_vapour_pressure_dynamics: Repository with the basic functions related with the temperature, relative humidity, absolute humidity and entalpy of the air with the different variable transformations
+"""
+__author__  = "Robert Rijnbeek"
+__email__   = "robert270384@gmail.com"
+__version__ = "1.8.8"
+
+# ======== IMPORTS ===========
+
+from .base import *
+
+__all__ = [ # SETTINGS
+            "setArgumentCheck", 
+            "setApplyUnits",
+            # CORE FUNCTIONS
+            "vapourpressure",
+            "density_air",
+            "absolutehumidity_kg_air",
+            "absolutehumidity_m3_air",
+            "entalpie_kg_air",
+            "entalpie_m3_air",
+            "moisuredeficit_kg_air",
+            "moisuredeficit_m3_air",
+            "dew_point_factor",
+            "dew_point_temperature"
+            ]
+
+if __name__ == '__main__':
+    
+    pass
