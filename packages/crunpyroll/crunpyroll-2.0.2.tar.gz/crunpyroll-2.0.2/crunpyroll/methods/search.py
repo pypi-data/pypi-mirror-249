032a531fded89acfa3e95359f29a5e8f4ffc22dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:34 +0000

    Add package crunpyroll/2.0.2/crunpyroll-2.0.2.tar.gz

---
+++ packages/crunpyroll/crunpyroll-2.0.2.tar.gz/crunpyroll-2.0.2/crunpyroll/methods/search.py
@@ -0,0 +1,37 @@
+from crunpyroll import enums
+from crunpyroll import types
+
+from typing import List
+
+import crunpyroll
+
+class Search:
+    async def search(
+        self: "crunpyroll.Client",
+        query: str,
+        *,
+        max_results: int = 6,
+        locale: str = None,
+        filters: List["enums.ContentType"] = [
+            enums.ContentType.SERIES,
+            enums.ContentType.MOVIE_LISTING,
+            enums.ContentType.EPISODE,
+            enums.ContentType.MUSIC,
+        ]
+    ) -> "types.SearchQuery":
+        await self.session.retrieve()
+        filters_string = ",".join(
+            filter.value for filter in filters
+            if isinstance(filter, enums.ContentType)
+        )
+        response = await self.api_request(
+            method="GET",
+            endpoint="content/v2/discover/search",
+            params={
+                "q": query,
+                "n": max_results,
+                "locale": locale or self.locale,
+                "type": filters_string
+            }
+        )
+        return types.SearchQuery.parse(response)
---
+++ packages/crunpyroll/crunpyroll-2.0.2.tar.gz/crunpyroll-2.0.2/crunpyroll/types/__init__.py
@@ -0,0 +1,15 @@
+from .series import Series
+from .seasons import SeasonsQuery, Season
+from .episodes import EpisodesQuery, Episode
+from .search import SearchQuery
+from .images import Image, Images
+from .cms import CMS
+from .index import Index
+from .profile import Profile
+from .content import Content
+from .streams import MediaStreams
+from .subtitles import SubtitlesStream
+from .hardsub import HardsubStream
+from .old_streams import OldMediaStreams
+from .manifest import Manifest, ManifestVideoStream, ManifestAudioStream
+from .drm import DRM, ContentProtection
