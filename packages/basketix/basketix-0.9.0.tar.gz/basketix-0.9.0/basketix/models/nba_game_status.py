6e2d638e37cf4aa4a70dbea8721922b9ed309f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:37 +0000

    Add package basketix/0.9.0/basketix-0.9.0.tar.gz

---
+++ packages/basketix/basketix-0.9.0.tar.gz/basketix-0.9.0/basketix/models/nba_game_status.py
@@ -0,0 +1,24 @@
+"""NBA game status"""
+
+from enum import Enum
+
+
+class NbaGameStatus(str, Enum):
+    UNPLAYED = 'unplayed'
+    UNSTARTED = 'unstarted'
+    INPROGRESS = 'inProgress'
+    FINISHED = 'finished'
+
+    @classmethod
+    def match(cls, status_number: int) -> 'NbaGameStatus':
+        if status_number == 0: return cls.UNPLAYED
+        if status_number == 1: return cls.UNSTARTED
+        if status_number == 2: return cls.INPROGRESS
+        if status_number == 3: return cls.FINISHED
+        return cls.UNSTARTED
+
+    @classmethod
+    def is_started(cls, status: "NbaGameStatus") -> bool:
+        return status in (cls.FINISHED, cls.INPROGRESS)
+
+NbaGameStatusType = NbaGameStatus
