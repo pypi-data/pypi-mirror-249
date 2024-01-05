2a66040dc210dac9b76daaa6a9f204a795065a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:05:12 +0000

    Add package oci-client/0.2.1/oci_client-0.2.1.tar.gz

---
+++ packages/oci-client/oci_client-0.2.1.tar.gz/oci_client-0.2.1/oci_client/__init__.py
@@ -0,0 +1,2 @@
+from oci_client.cfg import ApiKey, Config  # noqa: F401 # pyright: ignore[reportUnusedImport]
+from oci_client.client import Client  # noqa: F401 # pyright: ignore[reportUnusedImport]
