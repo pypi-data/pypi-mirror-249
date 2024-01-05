254f40e997c6f26db377a3e0fc6e016f4660d2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:05:11 +0000

    Add package ramses_rf/0.31.1/ramses_rf-0.31.1-py3-none-any.whl

---
+++ packages/ramses_rf/ramses_rf-0.31.1-py3-none-any.whl/ramses_rf/system/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser.
+
+Heating devices.
+"""
+from __future__ import annotations
+
+import logging
+
+#
+# from .schedule import (  # noqa: F401, isort: skip, pylint: disable=unused-import
+# )
+
+
+from .heat import (  # noqa: F401, isort: skip, pylint: disable=unused-import
+    Evohome,
+    System,
+    system_factory,
+)
+
+#
+# from .systems_hvac import (  # noqa: F401, isort: skip, pylint: disable=unused-import
+# )
+
+
+from .zones import (  # noqa: F401, isort: skip, pylint: disable=unused-import
+    DhwZone,
+    Zone,
+)
+
+
+_LOGGER = logging.getLogger(__name__)
