5d82b65d70f38076a206d9f6e3373116ee42ad62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:03:49 +0000

    Add package eod2pd/0.1.3/eod2pd-0.1.3-py3-none-any.whl

---
+++ packages/eod2pd/eod2pd-0.1.3-py3-none-any.whl/eod2pd/__init__.py
@@ -0,0 +1,30 @@
+# =============================================================================
+#
+#  Licensed Materials, Property of Ralph Vogl, Munich
+#
+#  Project : stocksdb
+#
+#  Copyright (c) by Ralph Vogl
+#
+#  All rights reserved.
+#
+#  Description:
+#
+#  a simple package that creates a database based on postgres to store stock values
+#
+# =============================================================================
+
+# -------------------------------------------------------------
+# IMPORTS
+# -------------------------------------------------------------
+from basefunctions import ThreadPool
+from eod2pd.downloader import Downloader
+from eod2pd.functions import (
+    getStockPrices,
+    getStockDividends,
+    getExchanges,
+    getStocksForExchange,
+)
+
+downloader = Downloader()
+threadPool = ThreadPool(numOfThreads=10, callable=downloader)
