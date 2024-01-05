e4bf6aa78ce178552d55987fe7759dc0affca240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:04:25 +0000

    Add package fakesnow/0.8.2/fakesnow-0.8.2.tar.gz

---
+++ packages/fakesnow/fakesnow-0.8.2.tar.gz/fakesnow-0.8.2/fakesnow/fixtures.py
@@ -0,0 +1,23 @@
+from collections.abc import Iterator
+
+import pytest
+
+import fakesnow
+
+
+@pytest.fixture
+def _fakesnow() -> Iterator[None]:
+    with fakesnow.patch() as fake_fns:
+        yield fake_fns
+
+
+@pytest.fixture
+def _fakesnow_no_auto_create() -> Iterator[None]:
+    with fakesnow.patch(create_database_on_connect=False, create_schema_on_connect=False) as fake_fns:
+        yield fake_fns
+
+
+@pytest.fixture(scope="session")
+def _fakesnow_session() -> Iterator[None]:
+    with fakesnow.patch() as fake_fns:
+        yield fake_fns
