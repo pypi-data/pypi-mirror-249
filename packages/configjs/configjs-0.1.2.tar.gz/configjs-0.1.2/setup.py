36d28065b2f74e8e9496f168110221f681604b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 15:05:39 +0000

    Add package configjs/0.1.2/configjs-0.1.2.tar.gz

---
+++ packages/configjs/configjs-0.1.2.tar.gz/configjs-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='configjs',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+    },
+    author='Doosik Kim',
+    author_email='doosik71@gmail.com',
+    description='A Python package to read "config.json" in the working directory and access its content using the package name itself.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/doosik71/configjs',
+    license='MIT',
+)
