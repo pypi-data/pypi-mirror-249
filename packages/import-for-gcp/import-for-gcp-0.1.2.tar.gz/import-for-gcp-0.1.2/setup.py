0b2f4c9af4bd26107a565a41df6c688da4fa64bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:04:32 +0000

    Add package import-for-gcp/0.1.2/import-for-gcp-0.1.2.tar.gz

---
+++ packages/import-for-gcp/import-for-gcp-0.1.2.tar.gz/import-for-gcp-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='import-for-gcp',
+    version='0.1.2',
+    author='Caleb Hopkins',
+    author_email='caleb.hopkins@method.com',
+    description='Use to import classes from .py files saved on google cloud storage',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'google-cloud-storage>=2.14.0'
+    ]
+)
