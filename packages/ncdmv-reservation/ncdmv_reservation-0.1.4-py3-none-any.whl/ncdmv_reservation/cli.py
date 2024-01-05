7b9ea84d4732c11c66567f010123d3211961277e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:04:59 +0000

    Add package ncdmv-reservation/0.1.4/ncdmv_reservation-0.1.4-py3-none-any.whl

---
+++ packages/ncdmv-reservation/ncdmv_reservation-0.1.4-py3-none-any.whl/ncdmv_reservation/cli.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2024-01-04
+Purpose: Why not?
+"""
+
+import argparse
+from pathlib import Path
+
+from ncdmv_reservation.utils import (
+    get_ncdmv_driver_license_office_availability_html,
+    extract_divs_to_dict,
+)
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="get ncdmv driver license office availability",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("-H", "--no-headless", help="show browser", action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    html_content = get_ncdmv_driver_license_office_availability_html(
+        headless=not args.no_headless
+    )
+    extracted_data = extract_divs_to_dict(html_content)
+    import json
+
+    print(json.dumps(extracted_data, indent=4, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()
