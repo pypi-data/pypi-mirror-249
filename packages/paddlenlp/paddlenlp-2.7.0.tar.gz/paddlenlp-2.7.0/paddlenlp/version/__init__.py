c049ca518ee7e3a90248669f4c962f5d78f44bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:03:35 +0000

    Add package paddlenlp/2.7.0/paddlenlp-2.7.0.tar.gz

---
+++ packages/paddlenlp/paddlenlp-2.7.0.tar.gz/paddlenlp-2.7.0/paddlenlp/version/__init__.py
@@ -0,0 +1,25 @@
+# THIS FILE IS GENERATED FROM PADDLENLP SETUP.PY
+commit           = '49520692fb80898fd53ab708831ae54bb53073b7'
+
+__all__ = ['show']
+
+def show():
+    """Get the corresponding commit id of paddlenlp.
+
+    Returns:
+        The commit-id of paddlenlp will be output.
+
+        full_version: version of paddlenlp
+
+
+    Examples:
+        .. code-block:: python
+
+            import paddlenlp
+
+            paddlenlp.version.show()
+            # commit: 1ef5b94a18773bb0b1bba1651526e5f5fc5b16fa
+
+    """
+    print("commit:", commit)
+
