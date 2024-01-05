7154c1607522a24204fde0c6decf96db17ec2f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:53 +0000

    Add package neuq/0.1.1/neuq-0.1.1.tar.gz

---
+++ packages/neuq/neuq-0.1.1.tar.gz/neuq-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name='neuq',
+  version='0.1.1',
+  entry_points={
+      'console_scripts': ['neuq=server:main']
+  },
+  install_requires=[
+    'Flask==3.0.0',
+    'Flask-Cors==4.0.0'
+  ],
+  classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+  ],
+  description="Supercharge your analysis with the power of AI.",
+  # long_description=open('README.md').read(), #TODO
+)
