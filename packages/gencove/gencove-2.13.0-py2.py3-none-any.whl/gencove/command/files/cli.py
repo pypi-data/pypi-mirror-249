05fce62b97a6ffd0243e9a6f5213843e4884856a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 06:04:49 +0000

    Add package gencove/2.13.0/gencove-2.13.0-py2.py3-none-any.whl

---
+++ packages/gencove/gencove-2.13.0-py2.py3-none-any.whl/gencove/command/files/cli.py
@@ -0,0 +1,19 @@
+"""Commands to be executed from command line."""
+import click
+
+from gencove.command.common_cli_options import add_options, common_options
+from gencove.constants import Credentials, Optionals
+
+from .main import ListFileTypes
+
+
+@click.command("file-types")
+@click.option("--project-id", required=False, help="Gencove project ID", type=str)
+@add_options(common_options)
+def list_file_types(project_id, host, email, password, api_key):
+    """List file types in Gencove's system."""
+    ListFileTypes(
+        project_id,
+        Credentials(email=email, password=password, api_key=api_key),
+        Optionals(host=host),
+    ).run()
