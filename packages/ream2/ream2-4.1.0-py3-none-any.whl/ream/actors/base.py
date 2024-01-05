08f16152becc9947e00ca377673ccdf1496acf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:23 +0000

    Add package ream2/4.1.0/ream2-4.1.0-py3-none-any.whl

---
+++ packages/ream2/ream2-4.1.0-py3-none-any.whl/ream/actors/base.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+import os
+from dataclasses import is_dataclass
+from typing import TYPE_CHECKING, Generic, Optional, Protocol, Sequence, Type, TypeVar
+
+from loguru import logger
+from ream.actor_state import ActorState
+from ream.actors.interface import Actor
+from ream.fs import FS
+from ream.helper import resolve_type_arguments
+from ream.params_helper import EnumParams
+from ream.workspace import ReamWorkspace
+
+if TYPE_CHECKING:
+    from loguru import Logger
+
+P = TypeVar("P", covariant=True)
+
+
+class BaseActorProtocol(Protocol[P]):
+    @property
+    def params(self) -> P:
+        ...
+
+    @property
+    def logger(self) -> Logger:
+        ...
+
+    @property
+    def dep_actors(self) -> Sequence[BaseActorProtocol]:
+        ...
+
+
+class BaseActor(Actor, Generic[P]):
+    """A based for parameterized actor that its output is always determisitic given: the input, actor's state, and actor's provenance.
+
+    The actor's provenance is an optional mechanism to provide additional guarantee to always have deterministic output if
+    the combination of input and actor's state is not sufficient. For example, training a model with a random seed and the store the model
+    for later use, the provenance can be the path to the model. Normally, we advise to avoid using provenance if possible.
+
+    The actor state is determined based on: its parameters and the dependant actors' states.
+    """
+
+    def __init__(
+        self,
+        params: P,
+        dep_actors: Optional[Sequence[BaseActor]] = None,
+    ):
+        self._working_fs: Optional[FS] = None
+        self.dep_actors: Sequence[BaseActor] = dep_actors or []
+        self.params = params
+        self.logger = logger.bind(name=self.__class__.__name__)
+
+    def get_actor_state(self) -> ActorState:
+        """Get the state of this actor"""
+        deps = [actor.get_actor_state() for actor in self.dep_actors]
+
+        if isinstance(self.params, EnumParams):
+            for field in self.params.get_method_fields():
+                deps.append(
+                    ActorState.create(
+                        self.params.get_method_class(field),
+                        self.params.get_method_params(field),
+                    )
+                )
+            params = self.params.without_method_args()
+        else:
+            params = self.params
+
+        return ActorState.create(
+            self.__class__,
+            params,
+            dependencies=deps,
+        )
+
+    def get_provenance(self):
+        """Return the provenance of this actor. The provenance is used when there are cases where even if we have the same state, the result can be different.
+
+        For example:
+            - training a model with a random seed and store the model for later use
+            - use external method that has a version itself and the version can be changed later
+
+        To use this provenance as a key to cache result of a function, use `cache_self_args=CacheArgsHelper.gen_cache_self_args(get_provenance)` in Cache decorator.
+        """
+        return ""
+
+    def get_working_fs(self) -> FS:
+        """Get a working directory for this actor that can be used to store the results of each example."""
+        if self._working_fs is None:
+            state = self.get_actor_state()
+            cache_dir = ReamWorkspace.get_instance().reserve_working_dir(state)
+            self.logger.debug(
+                "Using working directory: {}",
+                cache_dir,
+            )
+            self._working_fs = FS(cache_dir)
+        return self._working_fs
+
+    @classmethod
+    def get_param_cls(cls) -> Type[P]:
+        """Get the parameter class of this actor"""
+        args = resolve_type_arguments(BaseActor, cls)
+        assert len(args) == 1
+        assert is_dataclass(args[0])
+        return args[0]  # type: ignore
+
+    def get_verbose_level(self) -> int:
+        """Get the verbose level of this actor from the environment variable"""
+        return int(os.environ.get(self.__class__.__name__.upper() + "_VERBOSE", "0"))
+
+    def _fmt_prov(self, *prov: str) -> str:
+        """Format the provenances"""
+        return ";".join(prov)
---
+++ packages/ream2/ream2-4.1.0-py3-none-any.whl/ream/workspace.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import Union
+
+from slugify import slugify
+
+from ream.actor_state import ActorState
+from ream.actor_version import ActorVersion
+from ream.fs import FS
+
+
+class ReamWorkspace:
+    instance = None
+
+    def __init__(self, workdir: Union[str, Path]):
+        self.workdir = Path(workdir)
+        self.fs = FS(self.workdir)
+
+    @staticmethod
+    def get_instance() -> ReamWorkspace:
+        if ReamWorkspace.instance is None:
+            raise Exception("ReamWorkspace must be initialized before using")
+        return ReamWorkspace.instance
+
+    @staticmethod
+    def init(workdir: Union[Path, str]):
+        if ReamWorkspace.instance is not None:
+            # allow calling re-initialization if the workdir is the same
+            assert ReamWorkspace.instance.workdir == Path(workdir)
+        else:
+            ReamWorkspace.instance = ReamWorkspace(workdir)
+        return ReamWorkspace.instance
+
+    def reserve_working_dir(self, state: ActorState) -> Path:
+        classversion = slugify(str(state.classversion)).replace("-", "_")
+        relpath = os.path.join(state.classpath, classversion)
+        key = state.to_dict()
+        diskpath = f"{state.get_classname()}/v{classversion}"
+        path = self.fs.get(
+            relpath=relpath,
+            key=key,
+            diskpath=diskpath,
+            save_key=True,
+            subdir=True,
+            subdir_incr=True,
+        )
+
+        if path.exists():
+            return self.ensure_consistent_version(state.classversion, path.get())
+
+        # reserve the working directory for this actor, as we do not perform any
+        # action that need to track, we need to mark it as success otherwise, the
+        # next time we reserve it, it is going to delete the content of the directory
+        with path.reserve_and_track() as realpath:
+            return self.ensure_consistent_version(state.classversion, realpath)
+
+    def ensure_consistent_version(
+        self, version: Union[str, int, ActorVersion], realpath: Path
+    ):
+        """Used in reserve_working_dir to ensure the version is consistent"""
+        if isinstance(version, ActorVersion):
+            version_file = realpath.parent / "version.json"
+            if not version_file.exists():
+                version.save(version_file)
+            else:
+                version.assert_equal(ActorVersion.load(version_file))
+
+        return realpath
