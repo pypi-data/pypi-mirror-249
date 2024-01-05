ab70f9f3724d0659f8515377cda7707a65e67fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:23 +0000

    Add package air-vapour-pressure-dynamics/1.8.7/air_vapour_pressure_dynamics-1.8.7.tar.gz

---
+++ packages/air-vapour-pressure-dynamics/air_vapour_pressure_dynamics-1.8.7.tar.gz/air_vapour_pressure_dynamics-1.8.7/src/air_vapour_pressure_dynamics/extra/_numpy.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# ====== IMPORTS ===============
+
+from basic_decorators import argument_check
+
+from ..initialize import SETTINGS, np
+
+# ======= CONSTANTS ============
+
+numpyArray = np.ndarray
+numpyFloat64 = np.float64
+
+# ======= CLASSES ==============
+
+if SETTINGS.NUMPY_DETECTED :
+    class UnitNumpyArray(np.ndarray):
+
+        @argument_check(object, np.ndarray, str)
+        def __new__(cls, value, unit=None):
+            return np.asarray(value).view(cls)
+        
+        @argument_check(object , np.ndarray, str)
+        def __init__(self, value, unit=None):
+            self.unit = unit
+else:
+    class UnitNumpyArray():
+        pass
+
+class NumpyArray():
+    """numpy array value: numpy.ndarray"""
+    pass
+
+
+# ======= FUNCTIONS ============
+
+def log_numpy(value):
+    return np.log(value)
+
+def isNumpyValue(value):
+    return SETTINGS.NUMPY_DETECTED and isinstance(value, np.ndarray)
+
+if __name__ == '__main__':
+    
+    pass
---
+++ packages/air-vapour-pressure-dynamics/air_vapour_pressure_dynamics-1.8.7.tar.gz/air_vapour_pressure_dynamics-1.8.7/src/air_vapour_pressure_dynamics/extra/_sympy.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# ====== IMPORTS ===============
+
+from basic_decorators import argument_check
+
+from ..initialize import SETTINGS, sp
+
+# ======= CONSTANTS ============
+
+sympySymbol = sp.Symbol
+
+# ======= CLASSES ============
+
+if SETTINGS.SYMPY_DETECTED :
+    class UnitSympyExpression(sp.UnevaluatedExpr):
+
+        @argument_check(object, sp.Expr, str)
+        def __new__(self, value, unit=None):
+            return sp.UnevaluatedExpr.__new__(self, value)
+        
+        @argument_check(object, sp.Expr, str)
+        def __init__(self, value, unit=None):
+            self.unit = unit
+            super()
+else:
+    class UnitSympyExpression():
+        pass
+
+class SympySimbol():
+    """sympy Simbol: sympy.Simbol"""
+    pass
+class SympyExpression():
+    """sympy Expression: sympy.Expr"""
+    pass
+
+# ======= FUNCTIONS ===========
+
+def log_sympy(value):
+    return sp.log(value)
+
+def isSympyExpr(value):
+    return SETTINGS.SYMPY_DETECTED and isinstance(value, sp.Expr)
+
+if __name__ == '__main__':
+    
+    pass
