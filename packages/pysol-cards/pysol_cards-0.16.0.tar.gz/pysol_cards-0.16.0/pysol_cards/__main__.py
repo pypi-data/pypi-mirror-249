a5a38f6bf7c3ac580b21390e218cf71c0b250b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 15:06:07 +0000

    Add package pysol-cards/0.16.0/pysol_cards-0.16.0.tar.gz

---
+++ packages/pysol-cards/pysol_cards-0.16.0.tar.gz/pysol_cards-0.16.0/pysol_cards/__main__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# pysol_cards v0.16.0
+# Deal PySol FC Cards
+# Copyright © 2020, Shlomi Fish.
+# See /LICENSE for licensing information.
+
+"""
+Main routine of pysol_cards.
+
+:Copyright: © 2020, Shlomi Fish.
+:License: BSD (see /LICENSE).
+"""
+
+__all__ = ('main',)
+
+
+def main():
+    """Main routine of pysol_cards."""
+    print("Hello, world!")
+    print("This is pysol_cards.")
+    print("You should customize __main__.py to your liking (or delete it).")
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pysol-cards/pysol_cards-0.16.0.tar.gz/pysol_cards-0.16.0/pysol_cards/template.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+# pysol_cards v0.16.0
+# Deal PySol FC Cards
+# Copyright © 2020, Shlomi Fish.
+# See /LICENSE for licensing information.
+
+"""
+INSERT MODULE DESCRIPTION HERE.
+
+:Copyright: © 2020, Shlomi Fish.
+:License: BSD (see /LICENSE).
+"""
+
+__all__ = ()
