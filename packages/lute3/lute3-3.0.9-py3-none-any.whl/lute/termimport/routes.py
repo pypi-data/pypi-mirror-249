46238f45c54064f24cc13291631de7201cd46afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:17 +0000

    Add package lute3/3.0.9/lute3-3.0.9-py3-none-any.whl

---
+++ packages/lute3/lute3-3.0.9-py3-none-any.whl/lute/termimport/routes.py
@@ -0,0 +1,49 @@
+"""
+Term import.
+"""
+
+import os
+import tempfile
+from flask import Blueprint, render_template, flash, redirect
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms.validators import DataRequired
+from lute.termimport.service import import_file, BadImportFileError
+
+
+bp = Blueprint("termimport", __name__, url_prefix="/termimport")
+
+
+class TermImportForm(FlaskForm):
+    "Form for imports."
+    text_file = FileField("Text File", validators=[DataRequired()])
+
+
+@bp.route("/index", methods=["GET", "POST"])
+def term_import_index():
+    "Import posted file."
+    form = TermImportForm()
+
+    if form.validate_on_submit():
+        text_file = form.text_file.data
+        if text_file:
+            # Track the file descriptor to close it later,
+            # avoiding problems on Windows.
+            # (https://stackoverflow.com/questions/34716996/
+            #  cant-remove-a-file-which-created-by-tempfile-mkstemp-on-windows)
+            fd, temp_file_name = tempfile.mkstemp()
+            try:
+                text_file.save(temp_file_name)
+                stats = import_file(temp_file_name)
+                flash(
+                    f"Imported {stats['created']} terms (skipped {stats['skipped']})",
+                    "notice",
+                )
+                return redirect("/term/index", 302)
+            except BadImportFileError as e:
+                flash(f"Error on import: {str(e)}", "notice")
+            finally:
+                os.close(fd)
+                os.remove(temp_file_name)
+
+    return render_template("termimport/index.html", form=form)
