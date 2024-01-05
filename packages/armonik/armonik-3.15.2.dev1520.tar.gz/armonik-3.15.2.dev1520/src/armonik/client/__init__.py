71dd1ea09f090f053299155fce9c971eeabd0d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:42 +0000

    Add package armonik/3.15.2.dev1520/armonik-3.15.2.dev1520.tar.gz

---
+++ packages/armonik/armonik-3.15.2.dev1520.tar.gz/armonik-3.15.2.dev1520/src/armonik/client/__init__.py
@@ -0,0 +1,4 @@
+from .submitter import ArmoniKSubmitter
+from .tasks import ArmoniKTasks, TaskFieldFilter
+from .results import ArmoniKResults, ResultFieldFilter
+from .versions import ArmoniKVersions
