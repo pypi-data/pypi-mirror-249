bc4894b9554dbd37e6fabdffa6da612177282a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:05:13 +0000

    Add package ntv-pandas/1.1.1/ntv_pandas-1.1.1-py3-none-any.whl

---
+++ packages/ntv-pandas/ntv_pandas-1.1.1-py3-none-any.whl/ntv_pandas/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+***NTV-pandas Package***
+
+Created on Sept 2023
+
+@author: philippe@loco-labs.io
+
+This package contains the following classes and functions:
+
+- `ntv-pandas.ntv_pandas.pandas_ntv_connector` :
+
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.DataFrameConnec`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.SeriesConnec`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.PdUtil`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.to_json`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.read_json`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.analysis`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.as_def_type`
+    - `ntv-pandas.ntv_pandas.pandas_ntv_connector.equals`
+
+- `ntv-pandas.ntv_pandas.accessors` :
+    
+    - `ntv-pandas.ntv_pandas.accessors.NpdSeriesAccessor`
+    - `ntv-pandas.ntv_pandas.accessors.NpdDataFrameAccessor`
+    
+    
+"""
+#from pathlib import Path
+from ntv_pandas.pandas_ntv_connector import DataFrameConnec, SeriesConnec, read_json
+from ntv_pandas.pandas_ntv_connector import to_json, as_def_type, equals, to_analysis
+import ntv_pandas.accessors
+#import ntv_pandas.pandas_ntv_connector
+
+#path = Path(ntv_pandas.pandas_ntv_connector.__file__).parent
+
+#print('package :', __package__)
