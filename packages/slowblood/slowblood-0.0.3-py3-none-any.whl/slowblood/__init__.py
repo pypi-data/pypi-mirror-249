43ccaad4ba2dd1430a1fc2ede75349726b3539d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:03:50 +0000

    Add package slowblood/0.0.3/slowblood-0.0.3-py3-none-any.whl

---
+++ packages/slowblood/slowblood-0.0.3-py3-none-any.whl/slowblood/__init__.py
@@ -0,0 +1,15 @@
+from .lib_llm_inference import (
+    InvoiceToChatGPT3_5,
+    InvoiceToHFLlama13B,
+)
+
+from .lib_pdf_manipulation import (
+    ConvertPdfToImages,
+    ExtractTextFromPdf, 
+    ExtractTextFromImgs, 
+)
+
+from .lib_settings import (
+  BasicInferenceRequest,
+  ArtifactNames,
+)
