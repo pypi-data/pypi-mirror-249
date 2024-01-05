b2ba80295bd140d01ea7e26e6198089251197539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:03:57 +0000

    Add package lifemanapy/0.0.11/lifemanapy-0.0.11.tar.gz

---
+++ packages/lifemanapy/lifemanapy-0.0.11.tar.gz/lifemanapy-0.0.11/lifemanapy/dimensions/dimensions.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import pyautogui
+from PIL import Image
+import lifemanapy.constants.constants as const
+import lifemanapy.configs.config as config
+import numpy as np
+
+def get_bar_dimensions(location_life):
+    if location_life != None:
+        inicio_x = location_life.left + 14
+        inicio_y = location_life.top + 5
+        final_x = inicio_x + 92
+        final_y = location_life.height + location_life.top - 5
+        return (inicio_x, inicio_y, final_x, final_y), (inicio_x, inicio_y+const.RANGE_MANA_POSITION, final_x, final_y+const.RANGE_MANA_POSITION)
+    else:
+        return None
+
+def set_dimensions(img:Image):
+    ar = Image.fromarray(np.uint8(np.asarray(const.HEART_IMAGE)))
+    config.dimensions = get_bar_dimensions(location_life=pyautogui.locate(ar, img, confidence=.8))
---
+++ packages/lifemanapy/lifemanapy-0.0.11.tar.gz/lifemanapy-0.0.11/lifemanapy/teste.py
@@ -0,0 +1,11 @@
+import lifemanatibia as lmt
+import pyautogui as pg 
+import keyboard as k
+
+
+
+
+lmt.set_dimensions(pg.screenshot())
+
+while True:
+    print(lmt.main(pg.screenshot()))
