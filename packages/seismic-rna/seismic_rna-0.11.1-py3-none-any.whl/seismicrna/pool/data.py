0821ef4b913501c56270a602825850ded3da2613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 15:06:53 +0000

    Add package seismic-rna/0.11.1/seismic_rna-0.11.1-py3-none-any.whl

---
+++ packages/seismic-rna/seismic_rna-0.11.1-py3-none-any.whl/seismicrna/pool/data.py
@@ -0,0 +1,36 @@
+from .report import PoolReport
+from ..core.data import PooledMutsDataset
+from ..relate.data import RelateDataset
+
+
+class PoolDataset(PooledMutsDataset):
+    """ Load pooled batches of relation vectors. """
+
+    @classmethod
+    def get_report_type(cls):
+        return PoolReport
+
+    @classmethod
+    def get_dataset_type(cls):
+        return RelateDataset
+
+########################################################################
+#                                                                      #
+# © Copyright 2024, the Rouskin Lab.                                   #
+#                                                                      #
+# This file is part of SEISMIC-RNA.                                    #
+#                                                                      #
+# SEISMIC-RNA is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation; either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# SEISMIC-RNA is distributed in the hope that it will be useful, but   #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT- #
+# ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General     #
+# Public License for more details.                                     #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with SEISMIC-RNA; if not, see <https://www.gnu.org/licenses>.  #
+#                                                                      #
+########################################################################
