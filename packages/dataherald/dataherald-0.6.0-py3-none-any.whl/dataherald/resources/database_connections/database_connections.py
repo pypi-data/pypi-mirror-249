bb01436d747e91bb1525ea7d31241fbe42e90600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:12 +0000

    Add package dataherald/0.6.0/dataherald-0.6.0-py3-none-any.whl

---
+++ packages/dataherald/dataherald-0.6.0-py3-none-any.whl/dataherald/resources/database_connections/database_connections.py
@@ -0,0 +1,387 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Union
+
+import httpx
+
+from ...types import (
+    DBConnectionResponse,
+    DatabaseConnectionListResponse,
+    database_connection_create_params,
+    database_connection_update_params,
+)
+from .drivers import Drivers, AsyncDrivers, DriversWithRawResponse, AsyncDriversWithRawResponse
+from ..._types import (
+    NOT_GIVEN,
+    Body,
+    Query,
+    Headers,
+    NotGiven,
+)
+from ..._utils import maybe_transform
+from ..._resource import SyncAPIResource, AsyncAPIResource
+from ..._response import to_raw_response_wrapper, async_to_raw_response_wrapper
+from ..._base_client import (
+    make_request_options,
+)
+
+if TYPE_CHECKING:
+    from ..._client import Dataherald, AsyncDataherald
+
+__all__ = ["DatabaseConnections", "AsyncDatabaseConnections"]
+
+
+class DatabaseConnections(SyncAPIResource):
+    drivers: Drivers
+    with_raw_response: DatabaseConnectionsWithRawResponse
+
+    def __init__(self, client: Dataherald) -> None:
+        super().__init__(client)
+        self.drivers = Drivers(client)
+        self.with_raw_response = DatabaseConnectionsWithRawResponse(self)
+
+    def create(
+        self,
+        *,
+        alias: str | NotGiven = NOT_GIVEN,
+        connection_uri: str | NotGiven = NOT_GIVEN,
+        credential_file_content: Union[object, str] | NotGiven = NOT_GIVEN,
+        llm_api_key: str | NotGiven = NOT_GIVEN,
+        metadata: object | NotGiven = NOT_GIVEN,
+        ssh_settings: database_connection_create_params.SshSettings | NotGiven = NOT_GIVEN,
+        use_ssh: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Add Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._post(
+            "/api/database-connections",
+            body=maybe_transform(
+                {
+                    "alias": alias,
+                    "connection_uri": connection_uri,
+                    "credential_file_content": credential_file_content,
+                    "llm_api_key": llm_api_key,
+                    "metadata": metadata,
+                    "ssh_settings": ssh_settings,
+                    "use_ssh": use_ssh,
+                },
+                database_connection_create_params.DatabaseConnectionCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    def retrieve(
+        self,
+        id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Api Get Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get(
+            f"/api/database-connections/{id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    def update(
+        self,
+        id: str,
+        *,
+        alias: str | NotGiven = NOT_GIVEN,
+        connection_uri: str | NotGiven = NOT_GIVEN,
+        credential_file_content: Union[object, str] | NotGiven = NOT_GIVEN,
+        llm_api_key: str | NotGiven = NOT_GIVEN,
+        metadata: object | NotGiven = NOT_GIVEN,
+        ssh_settings: database_connection_update_params.SshSettings | NotGiven = NOT_GIVEN,
+        use_ssh: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Update Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._put(
+            f"/api/database-connections/{id}",
+            body=maybe_transform(
+                {
+                    "alias": alias,
+                    "connection_uri": connection_uri,
+                    "credential_file_content": credential_file_content,
+                    "llm_api_key": llm_api_key,
+                    "metadata": metadata,
+                    "ssh_settings": ssh_settings,
+                    "use_ssh": use_ssh,
+                },
+                database_connection_update_params.DatabaseConnectionUpdateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DatabaseConnectionListResponse:
+        """Api Get Db Connections"""
+        return self._get(
+            "/api/database-connections",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DatabaseConnectionListResponse,
+        )
+
+
+class AsyncDatabaseConnections(AsyncAPIResource):
+    drivers: AsyncDrivers
+    with_raw_response: AsyncDatabaseConnectionsWithRawResponse
+
+    def __init__(self, client: AsyncDataherald) -> None:
+        super().__init__(client)
+        self.drivers = AsyncDrivers(client)
+        self.with_raw_response = AsyncDatabaseConnectionsWithRawResponse(self)
+
+    async def create(
+        self,
+        *,
+        alias: str | NotGiven = NOT_GIVEN,
+        connection_uri: str | NotGiven = NOT_GIVEN,
+        credential_file_content: Union[object, str] | NotGiven = NOT_GIVEN,
+        llm_api_key: str | NotGiven = NOT_GIVEN,
+        metadata: object | NotGiven = NOT_GIVEN,
+        ssh_settings: database_connection_create_params.SshSettings | NotGiven = NOT_GIVEN,
+        use_ssh: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Add Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._post(
+            "/api/database-connections",
+            body=maybe_transform(
+                {
+                    "alias": alias,
+                    "connection_uri": connection_uri,
+                    "credential_file_content": credential_file_content,
+                    "llm_api_key": llm_api_key,
+                    "metadata": metadata,
+                    "ssh_settings": ssh_settings,
+                    "use_ssh": use_ssh,
+                },
+                database_connection_create_params.DatabaseConnectionCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    async def retrieve(
+        self,
+        id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Api Get Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._get(
+            f"/api/database-connections/{id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    async def update(
+        self,
+        id: str,
+        *,
+        alias: str | NotGiven = NOT_GIVEN,
+        connection_uri: str | NotGiven = NOT_GIVEN,
+        credential_file_content: Union[object, str] | NotGiven = NOT_GIVEN,
+        llm_api_key: str | NotGiven = NOT_GIVEN,
+        metadata: object | NotGiven = NOT_GIVEN,
+        ssh_settings: database_connection_update_params.SshSettings | NotGiven = NOT_GIVEN,
+        use_ssh: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DBConnectionResponse:
+        """
+        Update Db Connection
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._put(
+            f"/api/database-connections/{id}",
+            body=maybe_transform(
+                {
+                    "alias": alias,
+                    "connection_uri": connection_uri,
+                    "credential_file_content": credential_file_content,
+                    "llm_api_key": llm_api_key,
+                    "metadata": metadata,
+                    "ssh_settings": ssh_settings,
+                    "use_ssh": use_ssh,
+                },
+                database_connection_update_params.DatabaseConnectionUpdateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DBConnectionResponse,
+        )
+
+    async def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> DatabaseConnectionListResponse:
+        """Api Get Db Connections"""
+        return await self._get(
+            "/api/database-connections",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=DatabaseConnectionListResponse,
+        )
+
+
+class DatabaseConnectionsWithRawResponse:
+    def __init__(self, database_connections: DatabaseConnections) -> None:
+        self.drivers = DriversWithRawResponse(database_connections.drivers)
+
+        self.create = to_raw_response_wrapper(
+            database_connections.create,
+        )
+        self.retrieve = to_raw_response_wrapper(
+            database_connections.retrieve,
+        )
+        self.update = to_raw_response_wrapper(
+            database_connections.update,
+        )
+        self.list = to_raw_response_wrapper(
+            database_connections.list,
+        )
+
+
+class AsyncDatabaseConnectionsWithRawResponse:
+    def __init__(self, database_connections: AsyncDatabaseConnections) -> None:
+        self.drivers = AsyncDriversWithRawResponse(database_connections.drivers)
+
+        self.create = async_to_raw_response_wrapper(
+            database_connections.create,
+        )
+        self.retrieve = async_to_raw_response_wrapper(
+            database_connections.retrieve,
+        )
+        self.update = async_to_raw_response_wrapper(
+            database_connections.update,
+        )
+        self.list = async_to_raw_response_wrapper(
+            database_connections.list,
+        )
