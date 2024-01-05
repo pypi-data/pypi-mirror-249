ec308a005c50443200ed90b54a40e53a69d429fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:04:53 +0000

    Add package zwave-js-server-python/0.55.3/zwave-js-server-python-0.55.3.tar.gz

---
+++ packages/zwave-js-server-python/zwave-js-server-python-0.55.3.tar.gz/zwave-js-server-python-0.55.3/zwave_js_server/model/device_class.py
@@ -0,0 +1,67 @@
+"""
+Model for a Zwave Node's device class.
+
+https://zwave-js.github.io/node-zwave-js/#/api/node?id=deviceclass
+"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TypedDict
+
+
+class DeviceClassItemDataType(TypedDict):
+    """Represent a device class data dict type."""
+
+    key: int
+    label: str
+
+
+class DeviceClassDataType(TypedDict):
+    """Represent a device class data dict type."""
+
+    basic: DeviceClassItemDataType
+    generic: DeviceClassItemDataType
+    specific: DeviceClassItemDataType
+    mandatorySupportedCCs: list[int]
+    mandatoryControlledCCs: list[int]
+
+
+@dataclass
+class DeviceClassItem:
+    """Model for a DeviceClass item (e.g. basic or generic)."""
+
+    key: int
+    label: str
+
+
+class DeviceClass:
+    """Model for a Zwave Node's device class."""
+
+    def __init__(self, data: DeviceClassDataType) -> None:
+        """Initialize."""
+        self.data = data
+
+    @property
+    def basic(self) -> DeviceClassItem:
+        """Return basic DeviceClass."""
+        return DeviceClassItem(**self.data["basic"])
+
+    @property
+    def generic(self) -> DeviceClassItem:
+        """Return generic DeviceClass."""
+        return DeviceClassItem(**self.data["generic"])
+
+    @property
+    def specific(self) -> DeviceClassItem:
+        """Return specific DeviceClass."""
+        return DeviceClassItem(**self.data["specific"])
+
+    @property
+    def mandatory_supported_ccs(self) -> list[int]:
+        """Return list of mandatory Supported CC id's."""
+        return self.data["mandatorySupportedCCs"]
+
+    @property
+    def mandatory_controlled_ccs(self) -> list[int]:
+        """Return list of mandatory Controlled CC id's."""
+        return self.data["mandatoryControlledCCs"]
