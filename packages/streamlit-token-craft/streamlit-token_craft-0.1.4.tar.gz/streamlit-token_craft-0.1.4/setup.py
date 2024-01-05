0f217e567e59d090c4b31092d5decb89f24d7be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:19 +0000

    Add package streamlit-token-craft/0.1.4/streamlit-token_craft-0.1.4.tar.gz

---
+++ packages/streamlit-token-craft/streamlit-token_craft-0.1.4.tar.gz/streamlit-token_craft-0.1.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit-token_craft",
+    version="0.1.4",
+    author="Stavros Theocharis",
+    author_email="stavrostheocharis@yahoo.gr",
+    description="streamlit custom token craft and management component",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stavrostheocharis/streamlit-token-craft",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "Python",
+        "Streamlit",
+        "React",
+        "JavaScript",
+        "token craft",
+        "token manager",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "streamlit >= 1.29.0",
+    ],
+)
