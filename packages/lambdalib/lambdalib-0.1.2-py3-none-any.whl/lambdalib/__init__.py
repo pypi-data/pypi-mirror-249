0558b1225345c0bd0e20ddaa29efb9c8a23d16b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:11 +0000

    Add package lambdalib/0.1.2/lambdalib-0.1.2-py3-none-any.whl

---
+++ packages/lambdalib/lambdalib-0.1.2-py3-none-any.whl/lambdalib/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+__version__ = "0.1.2"
+
+
+def register_data_source(chip):
+    # check if local
+    root_path = os.path.dirname(os.path.dirname(__file__))
+    test_path = os.path.join(root_path, 'lambdalib', 'iolib', 'rtl', 'la_ioanalog.v')
+    if os.path.exists(test_path):
+        path = root_path
+        ref = None
+    else:
+        path = 'git+https://github.com/siliconcompiler/lambdalib.git'
+        ref = f'v{__version__}'
+
+    chip.register_package_source(name='lambdalib',
+                                 path=path,
+                                 ref=ref)
