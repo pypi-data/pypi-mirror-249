a4c1075f78fb12e348b09cc8936297503b214d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:00 +0000

    Add package Scripto/0.0.8/Scripto-0.0.8.tar.gz

---
+++ packages/Scripto/Scripto-0.0.8.tar.gz/Scripto-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='Scripto',
+    version='0.0.8',
+    description='A simple framework to help you build scripts faster!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ron Hasleton',
+    author_email='ronhasleton@gmail.com',
+    keywords='scripting, auto, cli, argparse',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src', include=['*']),
+    python_requires='>=3.7, <4',
+    license='GNU GPLv3'
+)
