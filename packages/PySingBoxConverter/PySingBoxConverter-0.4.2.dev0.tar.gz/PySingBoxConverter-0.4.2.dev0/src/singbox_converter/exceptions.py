ec5092c15a9dccc6160747c46ada3e89cb8e55f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:23 +0000

    Add package PySingBoxConverter/0.4.2.dev0/PySingBoxConverter-0.4.2.dev0.tar.gz

---
+++ packages/PySingBoxConverter/PySingBoxConverter-0.4.2.dev0.tar.gz/PySingBoxConverter-0.4.2.dev0/src/singbox_converter/exceptions.py
@@ -0,0 +1,22 @@
+class InvalidSubscriptionsJsonFile(Exception):
+    pass
+
+
+class InvalidSubscriptionsConfig(Exception):
+    pass
+
+
+class NoTemplateConfigured(Exception):
+    pass
+
+
+class InvalidTemplate(Exception):
+    pass
+
+
+class UnknownRuleSet(Exception):
+    pass
+
+
+class FailedToFetchSubscription(Exception):
+    pass
