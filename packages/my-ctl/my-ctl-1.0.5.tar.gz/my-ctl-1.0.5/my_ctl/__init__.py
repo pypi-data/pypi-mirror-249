0b60d74bc7bb24ddc38fe2298fa4f45fcd490190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:04:26 +0000

    Add package my-ctl/1.0.5/my-ctl-1.0.5.tar.gz

---
+++ packages/my-ctl/my-ctl-1.0.5.tar.gz/my-ctl-1.0.5/my_ctl/__init__.py
@@ -0,0 +1,12 @@
+import importlib
+import os
+import sys
+
+from .my_ctl import *
+
+# MODULE REGISTER
+globals().update(importlib.import_module('my_ctl').__dict__)
+
+# STATIC PATH
+if sys.platform.startswith('linux'):
+	os.environ['MY_CTL_STATIC'] = os.path.dirname(os.path.abspath(__file__))
---
+++ packages/my-ctl/my-ctl-1.0.5.tar.gz/my-ctl-1.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+name='my-ctl',
+version='1.0.5',
+description='myctl quick create  project',
+author_email='labelnet@foxmail.com',
+author='labelnet',
+license='labelnet',
+keywords=['my-ctl'],
+packages=find_packages(),
+include_package_data=True,
+install_requires=['nuitka', 'requests', 'pytest', 'pyyaml', 'click', 'twine'],
+python_requires='>=3.8',
+entry_points="""
+[console_scripts]
+myctl=my_ctl:cli
+"""
+)
