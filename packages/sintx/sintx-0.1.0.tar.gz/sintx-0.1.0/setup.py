e1c11efec852686d4c7460071ab178cdb63a7f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 04:04:22 +0000

    Add package sintx/0.1.0/sintx-0.1.0.tar.gz

---
+++ packages/sintx/sintx-0.1.0.tar.gz/sintx-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='sintx',
+  version='0.1.0',
+  description='Sintacs Modules',
+  long_description=open('README.md').read(),
+  url='https://facebook.com/sintxcs',  
+  author='Sintacs',
+  author_email='sintacs@isnotdev.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Sintxcs, Sintacs, Sintx', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
