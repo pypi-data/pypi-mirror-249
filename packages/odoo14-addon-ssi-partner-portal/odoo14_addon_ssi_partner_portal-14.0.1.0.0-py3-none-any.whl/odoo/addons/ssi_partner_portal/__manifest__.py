1a4aec4e75bf5d56ded5a43b9d3ef19d6c9a8ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:30 +0000

    Add package odoo14-addon-ssi-partner-portal/14.0.1.0.0/odoo14_addon_ssi_partner_portal-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-partner-portal/odoo14_addon_ssi_partner_portal-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_partner_portal/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Portal",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "portal",
+        "ssi_partner",
+    ],
+    "data": [
+        "views/portal_templates.xml",
+    ],
+    "demo": [],
+}
