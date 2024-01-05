8f304a1aafa8b6b592ec9ef0a53fa0ef7ce125fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 05:04:26 +0000

    Add package lifemanapy/0.0.16/lifemanapy-0.0.16.tar.gz

---
+++ packages/lifemanapy/lifemanapy-0.0.16.tar.gz/lifemanapy-0.0.16/lifemanapy/__init__.py
@@ -0,0 +1,1 @@
+from . import lifemanatibia, dimensions, config, constants_location, conversor
---
+++ packages/lifemanapy/lifemanapy-0.0.16.tar.gz/lifemanapy-0.0.16/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='lifemanapy',
+    version='0.0.16',
+    license='MIT License',
+    author='Ítalo Pereira Barbosa',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='tato361.portugal@gmail.com',
+    keywords='life mana',
+    description=u'Módulo criado para auxiliar na captura de vida e mana do personagem no jogo tibia.',
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'numpy', 'pillow'],)
