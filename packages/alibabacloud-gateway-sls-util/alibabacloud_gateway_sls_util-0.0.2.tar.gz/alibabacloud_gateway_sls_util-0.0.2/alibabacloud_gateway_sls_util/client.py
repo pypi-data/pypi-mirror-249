49ec0372a6f6f060a8f52c85cbec36b077a513eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:04:26 +0000

    Add package alibabacloud-gateway-sls-util/0.0.2/alibabacloud_gateway_sls_util-0.0.2.tar.gz

---
+++ packages/alibabacloud-gateway-sls-util/alibabacloud_gateway_sls_util-0.0.2.tar.gz/alibabacloud_gateway_sls_util-0.0.2/alibabacloud_gateway_sls_util/client.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from typing import BinaryIO
+import lz4.block
+
+class Client:
+    """
+    Read data from a readable stream, and parse it by JSON format
+    @param stream the readable stream
+    @return the parsed result
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def read_and_uncompress_block(
+        stream: BinaryIO,
+        compress_type: str,
+        body_raw_size: str,
+    ) -> BinaryIO:
+        if compress_type != "lz4":
+            raise Exception(f"Unsupported compress type: {compress_type}")
+        compressed_data = stream[:]
+        try:
+            decompressed_data = lz4.block.decompress(compressed_data, uncompressed_size=int(body_raw_size))
+        except RuntimeError as e:
+            raise RuntimeError(f"Failed to decompress LZ4 block: {e}")
+
+        return decompressed_data
+
+    @staticmethod
+    async def read_and_uncompress_block_async(
+        stream: BinaryIO,
+        compress_type: str,
+        body_raw_size: str,
+    ) -> BinaryIO:
+        raise Exception('Un-implemented')
