1676bc5826a3a15011920c696d16d0d2a228357f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 15:06:20 +0000

    Add package pastel-gateway-py/1.0.0/pastel_gateway_py-1.0.0-py3-none-any.whl

---
+++ packages/pastel-gateway-py/pastel_gateway_py-1.0.0-py3-none-any.whl/gateway-client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from gateway_client.api.account_api import AccountApi
+from gateway_client.api.api_keys_api import ApiKeysApi
+from gateway_client.api.cascade_api import CascadeApi
+from gateway_client.api.collection_api import CollectionApi
+from gateway_client.api.login_api import LoginApi
+from gateway_client.api.nft_api import NftApi
+from gateway_client.api.sense_api import SenseApi
+from gateway_client.api.users_api import UsersApi
+
