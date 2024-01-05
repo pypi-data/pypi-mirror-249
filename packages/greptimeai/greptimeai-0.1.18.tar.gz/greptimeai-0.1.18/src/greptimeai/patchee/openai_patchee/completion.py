685342a64ad3d835ea427c05c2b0b2680c9f53f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 03:04:29 +0000

    Add package greptimeai/0.1.18/greptimeai-0.1.18.tar.gz

---
+++ packages/greptimeai/greptimeai-0.1.18.tar.gz/greptimeai-0.1.18/src/greptimeai/patchee/openai_patchee/completion.py
@@ -0,0 +1,43 @@
+from typing import Union
+
+import openai
+from openai import AsyncOpenAI, OpenAI
+
+from greptimeai.patchee import Patchee
+
+from . import _SPAN_NAME_COMPLETION, OpenaiPatchees
+
+
+class CompletionPatchees(OpenaiPatchees):
+    def __init__(self, client: Union[OpenAI, AsyncOpenAI, None] = None):
+        completion_create = Patchee(
+            obj=client.completions if client else openai.completions,
+            method_name="create",
+            span_name=_SPAN_NAME_COMPLETION,
+            event_name="completions.create",
+        )
+
+        completion_raw_create = Patchee(
+            obj=client.completions.with_raw_response
+            if client
+            else openai.completions.with_raw_response,
+            method_name="create",
+            span_name=_SPAN_NAME_COMPLETION,
+            event_name="completions.with_raw_response.create",
+        )
+
+        patchees = [
+            completion_create,
+            completion_raw_create,
+        ]
+
+        if client:
+            raw_completion_create = Patchee(
+                obj=client.with_raw_response.completions,
+                method_name="create",
+                span_name=_SPAN_NAME_COMPLETION,
+                event_name="with_raw_response.completions.create",
+            )
+            patchees.append(raw_completion_create)
+
+        super().__init__(patchees=patchees, client=client)
