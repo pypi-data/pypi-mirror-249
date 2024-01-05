c9c8d19a456bccebbeaf9661d0aeeb25f2854d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 06:04:34 +0000

    Add package WPP-Whatsapp/0.1.5.3/WPP_Whatsapp-0.1.5.3-py3-none-any.whl

---
+++ packages/WPP-Whatsapp/WPP_Whatsapp-0.1.5.3-py3-none-any.whl/WPP_Whatsapp/api/Whatsapp.py
@@ -0,0 +1,102 @@
+import threading
+import time
+
+from WPP_Whatsapp.api.layers.HostLayer import HostLayer
+from WPP_Whatsapp.api.const import defaultOptions, Logger
+from WPP_Whatsapp.api.layers.BusinessLayer import BusinessLayer
+from WPP_Whatsapp.api.layers.ListenerLayer import ListenerLayer
+
+
+class Whatsapp(BusinessLayer):
+    interval: threading.Event
+
+    def __init__(self, session, threadsafe_browser, **kwargs):
+        self.connected = None
+        self.options = {}
+        # self.autoCloseInterval = None
+        self.autoCloseCalled = False
+        self.isInitialized = False
+        self.isInjected = False
+        self.isStarted = False
+        self.isLogged = False
+        self.isInChat = False
+        # self.checkStartInterval = None
+        self.urlCode = ''
+        self.status = ''
+        self.attempt = 0
+        self.lastPercent = ""
+        self.lastPercentMessage = ""
+        self.session = session
+        self.page = threadsafe_browser.page
+        self.browser = threadsafe_browser.browser
+        self.ThreadsafeBrowser = threadsafe_browser
+        self.options.update(defaultOptions)
+        self.logger = self.options.get("logger") or Logger
+        self.logger.info(f'{self.session}: Initializing...')
+        self.logQR = kwargs.get("logQR") or False
+        self.autoClose = kwargs.get("autoClose") or self.options.get("autoClose") or 60
+        HostLayer.__init__(self)
+        ListenerLayer.__init__(self)
+        self.handel()
+
+    def handel(self):
+        # self.interval = None
+        if self.page:
+            self.interval = self.__setInterval(self.__intervalHandel, 60)
+            self.page.on('close', lambda: self.clearInterval(self.interval))
+
+    @staticmethod
+    def __setInterval(func, interval, *args, **kwargs):
+        stopped = threading.Event()
+
+        def _loop_():
+            while not stopped.is_set():
+                func()
+                time.sleep(interval)
+
+        th = threading.Thread(target=_loop_)
+        th.start()
+        return stopped
+
+    def __intervalHandel(self):
+        # Add window, when WPP not  create yet
+        newConnected = self.ThreadsafeBrowser.sync_page_evaluate("() => window.WPP && window.WPP.conn.isRegistered()")
+
+        if newConnected is None or newConnected == self.connected:
+            return
+        self.connected = newConnected
+
+        if not newConnected:
+            self.logger.info(f'{self.session}: Session Unpaired')
+            self.statusFind('disconnectedMobile', self.session)
+
+    async def afterPageScriptInjected(self):
+        await self._afterPageScriptInjectedHost()
+        await self._afterPageScriptInjectedListener()
+        is_authenticated = await self.ThreadsafeBrowser.page_evaluate("() => WPP.conn.isRegistered()")
+        self.connected = is_authenticated
+
+    def useHere(self):
+        return self.ThreadsafeBrowser.sync_page_evaluate("() => WAPI.takeOver()")
+
+    async def logout(self):
+        return self.ThreadsafeBrowser.page_evaluate("() => WPP.conn.logout()")
+
+    def getMessageById(self, messageId):
+        return self.ThreadsafeBrowser.sync_page_evaluate("(messageId) => WAPI.getMessageById(messageId)", messageId)
+
+    def getMessages(self, chatId, params=None):
+        """
+        :param chatId:
+        :param params: (count, id, direction)
+        :return:
+        """
+        if not params:
+            params = {}
+        chatId = self.valid_chatId(chatId)
+        return self.ThreadsafeBrowser.sync_page_evaluate("({ chatId, params }) => WAPI.getMessages(chatId, params)",
+                                                    {"chatId": chatId, "params": params})
+
+    def rejectCall(self, callId):
+        return self.ThreadsafeBrowser.sync_page_evaluate(
+            "({callId}) => WPP.call.rejectCall(callId)", callId)
---
+++ packages/WPP-Whatsapp/WPP_Whatsapp-0.1.5.3-py3-none-any.whl/WPP_Whatsapp/api/layers/SenderLayer.py
@@ -0,0 +1,293 @@
+import base64
+import mimetypes
+import os
+from WPP_Whatsapp.api.layers.ListenerLayer import ListenerLayer
+
+
+class SenderLayer(ListenerLayer):
+
+    def sendLinkPreview(self, chatId, url, text=''):
+        """
+        * Automatically sends a link with the auto generated link preview. You can also add a custom message to be added.
+        * Deprecated: please use {@link sendText}
+        """
+        message = text if url in text else f"{text}\n{url}"
+        chatId = self.valid_chatId(chatId)
+        result = self.ThreadsafeBrowser.sync_page_evaluate("""({ chatId, message }) => {
+                    return WPP.chat.sendTextMessage(chatId, message, { linkPreview: true });
+                    }""", {"chatId": chatId, "message": message})
+        return result
+
+    async def sendText_(self, to, content, options=None):
+        if not options:
+            options = {}
+        to = self.valid_chatId(to)
+        send_result = await self.ThreadsafeBrowser.page_evaluate("""({ to, content, options }) =>
+                            WPP.chat.sendTextMessage(to, content, {
+                              ...options,
+                              waitForAck: true,
+                            })""", {"to": to, "content": content, "options": options})
+        self.logger.debug(f'{self.session}: Send Message {send_result=}')
+
+        return send_result
+
+    def sendText(self, to, content, options=None):
+        """
+           Sends a text message to given chat
+           @category Chat
+           @param to chat to: xxxxx@us.c
+           @param content text message
+           @param options dict
+           @example
+           // Simple message
+           client.sendText('<number>@c.us', 'A simple message')
+           :return: dict
+        """
+        if not options:
+            options = {}
+        to = self.valid_chatId(to)
+        send_result = self.ThreadsafeBrowser.sync_page_evaluate("""({ to, content, options }) =>
+                            WPP.chat.sendTextMessage(to, content, {
+                              ...options,
+                              waitForAck: true,
+                            })""", {"to": to, "content": content, "options": options})
+        self.logger.debug(f'{self.session}: Send Message {send_result=}')
+
+        return send_result
+
+    def sendMessageOptions(self, chat, content, options=None):
+        if not options:
+            options = {}
+        message_id = self.ThreadsafeBrowser.sync_page_evaluate("""({ chat, content, options }) => {
+        return WAPI.sendMessageOptions(chat, content, options);
+      }""", {"chat": chat, "content": content, "options": options})
+        result = self.ThreadsafeBrowser.sync_page_evaluate("""(messageId) => WAPI.getMessageById(messageId)""",
+                                                           message_id)
+        return result
+
+    def sendImage(self, to, filePath, filename="", caption="", quotedMessageId=None, isViewOnce=None):
+        to = self.valid_chatId(to)
+        if filePath and os.path.exists(filePath):
+            _base64 = self.convert_to_base64(filePath)
+            filename = os.path.basename(filePath) if not filename else filename
+            return self.sendImageFromBase64(to, _base64, filename, caption, quotedMessageId, isViewOnce)
+        else:
+            raise Exception("Path Not Found")
+
+    def sendImageFromBase64(self, to, _base64, filename, caption, quotedMessageId, isViewOnce):
+        mime_type = self.base64MimeType(_base64)
+        if not mime_type:
+            raise Exception("Not valid mimeType")
+
+        if 'image' not in mime_type:
+            raise Exception('Not an image, allowed formats png, jpeg and webp')
+
+        # filename = filenameFromMimeType(filename, mimeType)
+        result = self.ThreadsafeBrowser.sync_page_evaluate("""async ({
+        to,
+        base64,
+        filename,
+        caption,
+        quotedMessageId,
+        isViewOnce,
+      }) => {
+        const result = await WPP.chat.sendFileMessage(to, base64, {
+          type: 'image',
+          isViewOnce,
+          filename,
+          caption,
+          quotedMsg: quotedMessageId,
+          waitForAck: true,
+        }).catch((e) => {return e});
+        
+        return {
+          ack: result.ack,
+          id: result.id,
+          sendMsgResult: await result.sendMsgResult,
+          error: result.message,
+        };
+      }""", {"to": to, "base64": _base64, "filename": filename, "caption": caption, "quotedMessageId": quotedMessageId,
+             "isViewOnce": isViewOnce})
+        return result
+
+    def reply(self, to, content, quotedMsg):
+        """
+            @param to chat to: xxxxx@us.c
+            @param content text message
+            @param quotedMsg: @param quotedMsg Message id to reply to.
+            @example
+           // Simple message
+           client.reply('<number>@c.us', 'A simple message', '<message-id>')
+        """
+        to = self.valid_chatId(to)
+        result = self.ThreadsafeBrowser.sync_page_evaluate("""({ to, content, quotedMsg }) => {
+                                    return WPP.chat.sendTextMessage(to, content, { quotedMsg });
+                                  }""", {"to": to, "content": content, "quotedMsg": quotedMsg})
+        # message = self.ThreadsafeBrowser.sync_page_evaluate()("(messageId) => WAPI.getMessageById(messageId)", result.get("id"))
+        return result
+
+    def sendFile(self, to, pathOrBase64, nameOrOptions, caption, timeout_=60):
+        to = self.valid_chatId(to)
+        options = {"type": 'auto-detect'}
+        if type(nameOrOptions) is str:
+            options["filename"] = nameOrOptions
+            options["caption"] = caption
+
+        elif type(nameOrOptions) is dict:
+            options = nameOrOptions
+
+        _base64 = ''
+        if pathOrBase64.startswith('data:'):
+            _base64 = pathOrBase64
+        else:
+            if pathOrBase64 and os.path.exists(pathOrBase64):
+                _base64 = self.convert_to_base64(pathOrBase64)
+
+            if not options.get("filename"):
+                options["filename"] = os.path.basename(pathOrBase64)
+        if not _base64:
+            raise Exception("Empty or invalid file or base64")
+
+        return self.ThreadsafeBrowser.sync_page_evaluate("""async ({ to, base64, options }) => {
+        const result = await WPP.chat.sendFileMessage(to, base64, options);
+        return {
+          ack: result.ack,
+          id: result.id,
+          sendMsgResult: await result.sendMsgResult,
+        };
+      }""", {"to": to, "base64": _base64, "options": options}, timeout_=timeout_)
+
+    def sendContactVcard(self, to, contactsId, name):
+        """
+          /**
+           * Sends contact card to iven chat id
+           * @category Chat
+           * @param to Chat id
+           * @param contactsId Example: 0000@c.us | [000@c.us, 1111@c.us]
+           */
+        """
+        to = self.valid_chatId(to)
+        return self.ThreadsafeBrowser.sync_page_evaluate("""({ to, contactsId, name }) => {
+        return WPP.chat.sendVCardContactMessage(to, {
+          id: contactsId,
+          name: name,
+        });
+      }""", {"to": to, "contactsId": contactsId, "name": name})
+
+    def forwardMessages(self, to, messages, skipMyMessages):
+        to = self.valid_chatId(to)
+        return self.ThreadsafeBrowser.sync_page_evaluate("""({ to, messages, skipMyMessages }) =>
+        WAPI.forwardMessages(to, messages, skipMyMessages)""",
+                                                         {"to": to, "messages": messages,
+                                                          "skipMyMessages": skipMyMessages})
+
+    def sendLocation(self, to, options):
+        to = self.valid_chatId(to)
+        options = {
+            "lat": options.get("latitude"),
+            "lng": options.get("longitude"),
+            "title": options.get("title"),
+        }
+        return self.ThreadsafeBrowser.sync_page_evaluate("""async ({ to, options }) => {
+        const result = await WPP.chat.sendLocationMessage(to, options);
+
+        return {
+          ack: result.ack,
+          id: result.id,
+          sendMsgResult: await result.sendMsgResult,
+        };
+      }""", {"to": to, "options": options})
+
+    def sendSeen(self, chatId):
+        chatId = self.valid_chatId(chatId)
+        return self.ThreadsafeBrowser.sync_page_evaluate("(chatId) => WPP.chat.markIsRead(chatId)", chatId)
+
+    def startTyping(self, to, duration=None):
+        to = self.valid_chatId(to)
+        return self.ThreadsafeBrowser.sync_page_evaluate("({ to, duration }) => WPP.chat.markIsComposing(to, duration)",
+                                                         {"to": to, "duration": duration})
+
+    def stopTyping(self, to):
+        to = self.valid_chatId(to)
+        return self.ThreadsafeBrowser.sync_page_evaluate("(to) => WPP.chat.markIsPaused(to)", to)
+
+    def setOnlinePresence(self, online=True):
+        return self.ThreadsafeBrowser.sync_page_evaluate("(online) => WPP.conn.markAvailable(online)", online)
+
+    def sendListMessage(self, to, options):
+        to = self.valid_chatId(to)
+        """
+          /**
+           * Sends a list message
+           *
+           * ```typescript
+           *   // Example
+           *   client.sendListMessage('<number>@c.us', {
+           *     buttonText: 'Click here',
+           *     description: 'Choose one option',
+           *     sections: [
+           *       {
+           *         title: 'Section 1',
+           *         rows: [
+           *           {
+           *             rowId: 'my_custom_id',
+           *             title: 'Test 1',
+           *             description: 'Description 1',
+           *           },
+           *           {
+           *             rowId: '2',
+           *             title: 'Test 2',
+           *             description: 'Description 2',
+           *           },
+           *         ],
+           *       },
+           *     ],
+           *   });
+           * ```
+           *
+           * @category Chat
+           */
+        """
+        sendResult = self.ThreadsafeBrowser.sync_page_evaluate(
+            "({ to, options }) => WPP.chat.sendListMessage(to, options)",
+            {"to": to, "options": options})
+        result = self.ThreadsafeBrowser.sync_page_evaluate("""async ({ messageId }) => {
+                        return JSON.parse(JSON.stringify(await WAPI.getMessageById(messageId)));
+                      }""", sendResult.get("id"))
+        return result
+
+    def setChatState(self, chatId, chatState):
+        """
+          /**
+           * Sets the chat state
+           * Deprecated in favor of Use startTyping or startRecording functions
+           * @category Chat
+           * @param chatState   Typing = 0, Recording = 1, Paused = 2
+           * @param chatId
+           * @deprecated Deprecated in favor of Use startTyping or startRecording functions
+           */
+        """
+        chatId = self.valid_chatId(chatId)
+        return self.ThreadsafeBrowser.sync_page_evaluate("""({ chatState, chatId }) => {
+                WAPI.sendChatstate(chatState, chatId);
+              }""", {"chatState": chatState, "chatId": chatId})
+
+    @staticmethod
+    def convert_to_base64(path):
+        mimetypes_add = {"webp": "image/webp"}
+        # mime = magic.Magic(mime=True)
+        # content_type = mime.from_file(path)
+        content_type = mimetypes.guess_type(path)[0]
+        if not content_type:
+            content_type = mimetypes_add.get(path.split(".")[-1], None)
+        # filename = os.path.basename(path)
+        with open(path, "rb") as image_file:
+            archive = base64.b64encode(image_file.read())
+            archive = archive.decode("utf-8")
+
+        return "data:" + content_type + ";base64," + archive
+
+    @staticmethod
+    def base64MimeType(encoded):
+        result = encoded.split(";base64")[0].split(":")[-1]
+        return result
