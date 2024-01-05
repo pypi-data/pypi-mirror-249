03efc93fc17fffa5ab7ae788ba7b61b8fcd3fbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 06:04:56 +0000

    Add package bettermdptools/0.5.5/bettermdptools-0.5.5.tar.gz

---
+++ packages/bettermdptools/bettermdptools-0.5.5.tar.gz/bettermdptools-0.5.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+"""
+"pip install pygame" error warning:
+if you're facing trouble installing pygame with Python 3.11, you need to install with following command:
+'pip install pygame --pre'
+
+More discussion:
+https://stackoverflow.com/questions/74188013/python-pygame-not-installing
+"""
+
+setup(
+    name='bettermdptools',
+    url='https://github.com/jlm429/bettermdptools',
+    version='0.5.5',
+    platforms=['Any'],
+    license='New BSD',
+    author='John Mansfield',
+    author_email='jlm429@gmail.com',
+    packages=["algorithms", "utils"],
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=['gymnasium>=0.26, <=0.26.3', 'pygame', 'numpy', 'tqdm', 'pandas', 'seaborn', 'matplotlib'],
+)
