2e3e89e460ec6500d4ad92e327de6e502aa42909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 06:04:38 +0000

    Add package kbrainsdk/0.1.65/kbrainsdk-0.1.65-py3-none-any.whl

---
+++ packages/kbrainsdk/kbrainsdk-0.1.65-py3-none-any.whl/kbrainsdk/datasets.py
@@ -0,0 +1,40 @@
+from kbrainsdk.validation.datasets import validate_list_datasets, validate_search_datasets
+from kbrainsdk.apibase import APIBase
+
+class Datasets(APIBase):
+
+    def list_datasets(self, email, token, client_id, oauth_secret, tenant_id):
+        
+        payload = {
+            "email": email,
+            "token": token,
+            "client_id": client_id,
+            "oauth_secret": oauth_secret,
+            "tenant_id": tenant_id
+        }
+
+        validate_list_datasets(payload)
+
+        path = f"/datasets/list/v1"
+        response = self.apiobject.call_endpoint(path, payload, "post")
+        return response
+
+    def search_datasets(self, query, topic, citations, email, token, client_id, oauth_secret, tenant_id):
+        
+        payload = {
+            "email": email,
+            "token": token,
+            "client_id": client_id,
+            "oauth_secret": oauth_secret,
+            "tenant_id": tenant_id,
+            "query": query,
+            "topic": topic,
+            "citations": citations
+        }
+
+        validate_list_datasets(payload)
+        validate_search_datasets(payload)
+
+        path = f"/datasets/search/v1"
+        response = self.apiobject.call_endpoint(path, payload, "post")
+        return response
