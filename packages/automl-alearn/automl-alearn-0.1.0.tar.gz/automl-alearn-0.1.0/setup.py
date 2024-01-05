3f815007118ebfd1c1a969b634ad5014fd8246d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:05:12 +0000

    Add package automl-alearn/0.1.0/automl-alearn-0.1.0.tar.gz

---
+++ packages/automl-alearn/automl-alearn-0.1.0.tar.gz/automl-alearn-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='automl-alearn',
+    packages=find_packages(),
+    version='0.1.0',
+    long_description='Auto ML Library',
+    long_description_content_type='',
+    author='Patale Akhil',
+)
