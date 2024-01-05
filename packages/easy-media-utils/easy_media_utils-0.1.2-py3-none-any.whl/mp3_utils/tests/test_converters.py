96b5d559f4e0c6696d99dd16301ad1dfc65c297d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 16:05:02 +0000

    Add package easy-media-utils/0.1.2/easy_media_utils-0.1.2-py3-none-any.whl

---
+++ packages/easy-media-utils/easy_media_utils-0.1.2-py3-none-any.whl/mp3_utils/tests/test_converters.py
@@ -0,0 +1,28 @@
+from mp3_utils.converters.audio_format_converter import AudioFormatConverter
+from mp3_utils.core.mp3_handler import MP3Handler
+
+
+def main(converter_type, file_path='../../testfiles/out/output.mp3') -> MP3Handler:
+    """
+    Convert MP4 each frame.
+    :param converter_type:
+    :param file_path:
+    """
+    Converter = {
+        "format": AudioFormatConverter,
+    }
+    converter = Converter.get(converter_type)(file_path)
+
+    return converter
+
+
+if __name__ == "__main__":
+    mp3_path = '../../testfiles/out/output.mp3'
+    wav_path = '../../testfiles/out/output.wav'
+    flac_path = '../../testfiles/out/output.flac'
+
+    converter_type = "format"
+    ans_path = flac_path
+
+    converter = main(converter_type, wav_path)
+    converter.process(ans_path, 'flac')
