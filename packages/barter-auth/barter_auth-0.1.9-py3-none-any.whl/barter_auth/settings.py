2e409bb26394d952641c4398cbcdd60ca5f07094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-05 14:04:30 +0000

    Add package barter-auth/0.1.9/barter_auth-0.1.9-py3-none-any.whl

---
+++ packages/barter-auth/barter_auth-0.1.9-py3-none-any.whl/barter_auth/settings.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+
+
+class TokenSettings(BaseSettings):
+    URL: str | None = None #'redis://localhost:6378/1'
+    HOST: str = '127.0.0.1'
+    PORT: int = 6379
+    PASSWORD: str | None = None
+    DB: int = 0
+    ACCESS_PREFIX: str = 'access'
+    REFRESH_PREFIX: str = 'refresh'
+    TOTP_PREFIX: str = 'totp'
+    PROFILE_PREFIX: str = 'profile'
+    TOKEN_STORAGE: str = 'headers'
+
+    class Config:
+        env_prefix = 'REDIS_AUTH_'
+
+
+app_settings = TokenSettings()
+
+__all__ = [
+    'app_settings',
+]
