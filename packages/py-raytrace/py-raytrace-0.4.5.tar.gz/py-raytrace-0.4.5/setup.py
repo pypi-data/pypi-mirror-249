20cdf5e612446ec176d87b47840ef1dbcfdd11c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:58 +0000

    Add package py-raytrace/0.4.5/py-raytrace-0.4.5.tar.gz

---
+++ packages/py-raytrace/py-raytrace-0.4.5.tar.gz/py-raytrace-0.4.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-raytrace',
+    version='0.4.5',
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    packages=find_packages(),  # Updated package name to use underscores
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'tqdm',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ray=py_raytrace.py_raytrace:main',
+            'raytrace=py_raytrace.py_raytrace:main',
+            'py-raytrace=py_raytrace.py_raytrace:main',
+        ],
+    },
+    author='Pranav A.',
+    author_email='prnv.school@gmail.com',
+    description='A simple ray tracing application',
+    license='MIT'
+)
