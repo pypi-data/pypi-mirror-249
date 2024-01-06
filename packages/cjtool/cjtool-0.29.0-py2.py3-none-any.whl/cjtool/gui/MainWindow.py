3e90a168a41dc9d65d1c5cffbb79ce055505c2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:04:05 +0000

    Add package cjtool/0.29.0/cjtool-0.29.0-py2.py3-none-any.whl

---
+++ packages/cjtool/cjtool-0.29.0-py2.py3-none-any.whl/cjtool/gui/MainWindow.py
@@ -0,0 +1,156 @@
+from debuger import FunctionData
+from gui.CallStackView import CallStackView, StandardItem
+from gui.SourceEdit import SourceEdit
+from gui.CommentEdit import CommentEdit
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QWidget, QMessageBox, \
+    QStatusBar, QFileDialog, QAction, QDockWidget
+from PyQt5.QtGui import QStandardItemModel
+from pathlib import Path
+from gui.Document import Document
+import sys
+
+
+def keystoint(x):
+    return {int(k): v for k, v in x.items()}
+
+
+def adjust_file_path(filename: str) -> str:
+    if Path(filename).is_file():
+        return filename
+
+    newpath = Path.cwd().joinpath(filename)
+    if Path(newpath).is_file():
+        return newpath
+
+    return None
+
+
+class MainWindow(QMainWindow):
+    beforeSave = pyqtSignal(FunctionData)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('CodeBook')
+        self.resize(1200, 900)
+
+        # You can't set a QLayout directly on the QMainWindow. You need to create a QWidget
+        # and set it as the central widget on the QMainWindow and assign the QLayout to that.
+        self.tree_view = CallStackView()
+        self.tree_view.setModel(QStandardItemModel())
+        self.tree_view.selectionModel().selectionChanged.connect(self.selectionChanged)
+        self.setCentralWidget(self.tree_view)
+        self.setContentsMargins(4, 0, 4, 0)
+
+        source_docker = self._addSourceDock()
+        comment_docker = self._addCommentDock()
+        self.resizeDocks([source_docker, comment_docker], [
+                         7, 3], Qt.Orientation.Vertical)
+        self._createMenuBar()
+        self.document: Document = None
+
+    def _addSourceDock(self):
+        source_edit = SourceEdit()
+        docker = QDockWidget('source', self)
+        docker.setWidget(source_edit)
+        docker.setTitleBarWidget(QWidget())
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, docker)
+        self.source_edit: SourceEdit = source_edit
+        return docker
+
+    def _addCommentDock(self):
+        comment_edit = CommentEdit()
+        docker = QDockWidget('comment', self)
+        docker.setWidget(comment_edit)
+        docker.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetClosable |
+                           QDockWidget.DockWidgetFeature.DockWidgetMovable)
+        self.addDockWidget(
+            Qt.DockWidgetArea.RightDockWidgetArea, docker)
+        self.comment_docker = docker
+        self.comment_edit = comment_edit
+        comment_edit.commentChanged.connect(self.tree_view.onCommentChanged)
+        return docker
+
+    # def _fillContent(self, rootNode) -> None:
+    #     filepath = ''
+    #     if (len(sys.argv) == 2):
+    #         filepath = adjust_file_path(sys.argv[1])
+
+    #     if filepath:
+    #         self._parse_file(rootNode, filepath)
+
+    def _createMenuBar(self) -> None:
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('&File')
+
+        openAct = QAction('&Open', self)
+        openAct.triggered.connect(self._open_file)
+        fileMenu.addAction(openAct)
+
+        saveAct = QAction('&Save', self)
+        saveAct.triggered.connect(self._save_file)
+        fileMenu.addAction(saveAct)
+
+        closeAct = QAction('&Close', self)
+        closeAct.triggered.connect(self._close_file)
+        fileMenu.addAction(closeAct)
+
+        viewMenu = menuBar.addMenu('&View')
+        toggleAction = self.comment_docker.toggleViewAction()
+        viewMenu.addAction(toggleAction)
+
+        helpMenu = menuBar.addMenu('&Help')
+        statusBar = QStatusBar()
+        self.setStatusBar(statusBar)
+        statusBar.showMessage('')
+
+    def _save_file(self) -> None:
+        self.document.save()
+
+    def _close_file(self) -> None:
+        if self.document.isDirty:
+            reply = QMessageBox.warning(self, 'File is modified but not saved',
+                                        'Yes to Save, No to Ignore', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+            if reply == QMessageBox.StandardButton.Yes:
+                self.document.save()
+
+        self.document.close()
+        self.document = None
+        self.tree_view.clear()
+        self.source_edit.clear()
+        self.comment_edit.clear()
+        self.setWindowTitle(f"CodeBook")
+
+    def _open_file(self) -> None:
+        if self.document:
+            self.document.close()
+
+        filename, _ = QFileDialog.getOpenFileName(
+            self, 'Open cst file', '', 'cst Files (*.cst)')
+        if filename:
+            self.setWindowTitle(f"CodeBook: {Path(filename).stem}")
+            rootNode = self.tree_view.model().invisibleRootItem()
+
+            self.document = Document(filename, rootNode)
+            self.document.open()
+            self.document.fill_tree()
+
+            self.tree_view.expandAll()
+            self.source_edit.setDocument(self.document)
+            self.comment_edit.setDocument(self.document)
+            self.tree_view.selectionModel().selectionChanged.connect(
+                self.document.onSelectionChanged)
+
+    def selectionChanged(self, selected, deselected) -> None:
+        if not selected.indexes():
+            return
+
+        selectedIndex = selected.indexes()[0]
+        item: StandardItem = selectedIndex.model().itemFromIndex(selectedIndex)
+        if not item.functionData:
+            return
+
+        # 确定函数名所在的行
+        filefullpath = item.functionData.fileName
+        self.statusBar().showMessage(
+            f"{filefullpath}({item.functionData.startLineNumber})")
---
+++ packages/cjtool/cjtool-0.29.0-py2.py3-none-any.whl/cjtool/gui/SourceEdit.py
@@ -0,0 +1,66 @@
+from PyQt5.QtGui import QFont, QFontMetrics, QColor
+from .Document import StandardItem, Document
+from PyQt5.Qsci import QsciScintilla, QsciLexerCPP
+
+
+class SourceEdit(QsciScintilla):
+
+    def __init__(self, parent=None):
+        super(SourceEdit, self).__init__(parent)
+
+        # Set the default font
+        font = QFont()
+        font.setFamily('Inconsolata')  # Courier
+        font.setFixedPitch(True)
+        font.setPointSize(10)
+        self.setFont(font)
+        self.setMarginsFont(font)
+        self.setUtf8(True)
+
+        # Margin 0 is used for line numbers
+        fontmetrics = QFontMetrics(font)
+        self.setMarginsFont(font)
+        self.setMarginWidth(0, fontmetrics.width("000") + 6)
+        self.setMarginLineNumbers(0, True)
+        self.setMarginsBackgroundColor(QColor("#cccccc"))
+
+        # Brace matching: enable for a brace immediately before or after
+        # the current position
+        #
+        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+
+        # Current line visible with special background color
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QColor("#ffe4e4"))
+
+        # Set CPP lexer
+        # Set style for Python comments (style number 1) to a fixed-width
+        # courier.
+        #
+        lexer = QsciLexerCPP()
+        lexer.setDefaultFont(font)
+        self.setLexer(lexer)
+
+        # Indentation
+        #
+        self.setIndentationsUseTabs(False)
+        self.setTabWidth(4)
+        self.setIndentationGuides(True)
+        self.setTabIndents(True)
+        self.setAutoIndent(True)
+
+        # Don't want to see the horizontal scrollbar at all
+        # Use raw message to Scintilla here (all messages are documented
+        # here: http://www.scintilla.org/ScintillaDoc.html)
+        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+
+        # not too small
+        self.setMinimumSize(600, 450)
+
+    def setDocument(self, doc: Document):
+        self.document: Document = doc
+        self.document.curItemChanged.connect(self.onCurItemChanged)
+
+    def onCurItemChanged(self, item: StandardItem) -> None:
+        content = self.document.get_source(item.functionData)
+        self.setText(content)
