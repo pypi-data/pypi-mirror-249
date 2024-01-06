a6c611164b93fae549eae449936d467969a0c33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:05:03 +0000

    Add package jupyai/0.1.0/jupyai-0.1.0-py3-none-any.whl

---
+++ packages/jupyai/jupyai-0.1.0-py3-none-any.whl/jupyai/tests/test_handlers.py
@@ -0,0 +1,28 @@
+import json
+
+
+import pytest
+from tornado.httpclient import HTTPClientError
+
+
+async def test_autocomplete(jp_fetch):
+    with pytest.raises(HTTPClientError) as e:
+        await jp_fetch(
+            "jupyai",
+            "autocomplete",
+            method="POST",
+            body=json.dumps(
+                {
+                    "cell": {
+                        "source": "print('hello')",
+                        "id": "cellid",
+                    },
+                    "sources": [
+                        {"source": "print('hello')", "id": "cellid"},
+                        {"source": "x = 1", "id": "anotherid"},
+                    ],
+                }
+            ),
+        )
+
+    assert e.value.code == 401
