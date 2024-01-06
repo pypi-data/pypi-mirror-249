aca1c14a96ae530ef9476bc2604701a8388bd140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:04:39 +0000

    Add package rinch-sql/1.0.2/rinch_sql-1.0.2.tar.gz

---
+++ packages/rinch-sql/rinch_sql-1.0.2.tar.gz/rinch_sql-1.0.2/rinch_sql/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import __version__
+from .sql import Sql
+from .sql_static import SqlStatic
+from .mysql import Mysql
+from mysql.connector.errors import IntegrityError
---
+++ packages/rinch-sql/rinch_sql-1.0.2.tar.gz/rinch_sql-1.0.2/rinch_sql/__version__.py
@@ -0,0 +1,6 @@
+VERSION = (1, 0, 2)
+
+__version__ = ".".join(map(str, VERSION))
+
+if __name__ == "__main__":
+    print(f"v{__version__}")
