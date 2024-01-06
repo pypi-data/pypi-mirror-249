ed1b57df025c7d4ce8e679998aca784db23f5abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:06 +0000

    Add package asf-common-services/0.0.1/asf_common_services-0.0.1.tar.gz

---
+++ packages/asf-common-services/asf_common_services-0.0.1.tar.gz/asf_common_services-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='asf_common_services',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'protobuf',  # Add other dependencies here
+        # Other dependencies...
+    ],
+)
