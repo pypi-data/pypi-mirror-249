41bde6985cd25323646cf4635b2865ba170a0fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 14:04:15 +0000

    Add package vasp-pp/0.0.4/vasp-pp-0.0.4.tar.gz

---
+++ packages/vasp-pp/vasp-pp-0.0.4.tar.gz/vasp-pp-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = "0.0.4"
+DESCRIPTION = "Post-processes VASP outputs "
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="vasp-pp",
+    version=VERSION,
+    author="Brian Robinson",
+    author_email="b.p.robinson102@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy"],
+    keywords=["python", "VASP", "electronic structure", "post-processing"],
+    url="https://github.com/brobinson10296/vasp_pp_package",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
