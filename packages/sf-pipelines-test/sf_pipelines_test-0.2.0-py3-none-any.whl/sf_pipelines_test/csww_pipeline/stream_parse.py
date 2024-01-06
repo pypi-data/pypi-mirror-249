7ed07983fc1ee0c5233722a09711e75213832ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:59 +0000

    Add package sf-pipelines-test/0.2.0/sf_pipelines_test-0.2.0-py3-none-any.whl

---
+++ packages/sf-pipelines-test/sf_pipelines_test-0.2.0-py3-none-any.whl/sf_pipelines_test/csww_pipeline/stream_parse.py
@@ -0,0 +1,46 @@
+from sfdata_stream_parser.events import (
+    StartElement,
+    EndElement,
+    TextNode,
+    CommentNode,
+    ProcessingInstructionNode,
+)
+
+try:
+    from lxml import etree
+except ImportError:
+    pass
+
+
+def dom_parse(source, filename, **kwargs):
+    """
+    Equivalent of the xml parse included in the sfdata_stream_parser package, but uses the ET DOM
+    and allows direct DOM manipulation.
+
+    :param source: File to be parsed
+    :param filename: The name of the file
+    :return: Stream of events
+    """
+    parser = etree.iterparse(source, events=("start", "end", "comment", "pi"), **kwargs)
+    for action, elem in parser:
+        if action == "start":
+            yield StartElement(
+                tag=elem.tag, attrib=elem.attrib, node=elem, filename=filename
+            )
+            yield TextNode(cell=elem.text, filename=filename, text=None)
+        elif action == "end":
+            yield EndElement(tag=elem.tag, node=elem, filename=filename)
+            if elem.tail:
+                yield TextNode(cell=elem.tail, filename=filename, text=None)
+        elif action == "comment":
+            yield CommentNode(cell=elem.text, node=elem, filename=filename, text=None)
+        elif action == "pi":
+            yield ProcessingInstructionNode(
+                name=elem.target,
+                cell=elem.text,
+                node=elem,
+                filename=filename,
+                text=None,
+            )
+        else:
+            raise ValueError(f"Unknown event: {action}")
