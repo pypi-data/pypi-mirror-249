4849028be67ef4bd09a9d53742e72599cd324284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:14 +0000

    Add package xuance/1.0.9/xuance-1.0.9.tar.gz

---
+++ packages/xuance/xuance-1.0.9.tar.gz/xuance-1.0.9/xuance/torch/runners/runner_magent.py
@@ -0,0 +1,7 @@
+from .runner_pettingzoo import Pettingzoo_Runner
+
+
+class MAgent_Runner(Pettingzoo_Runner):
+    def __init__(self, args):
+        super(MAgent_Runner, self).__init__(args)
+        self.fps = 50
