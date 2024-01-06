05404e2faba32deb6868ba199e54f39f8a31efee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:33 +0000

    Add package randovania/7.2.0/randovania-7.2.0-py3-none-any.whl

---
+++ packages/randovania/randovania-7.2.0-py3-none-any.whl/randovania/games/prime1/gui/preset_settings/prime_goal_tab.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from PySide6 import QtCore
+
+from randovania.games.prime1.layout.artifact_mode import LayoutArtifactMode
+from randovania.gui.generated.preset_prime_goal_ui import Ui_PresetPrimeGoal
+from randovania.gui.preset_settings.preset_tab import PresetTab
+
+if TYPE_CHECKING:
+    from randovania.game_description.game_description import GameDescription
+    from randovania.gui.lib.window_manager import WindowManager
+    from randovania.interface_common.preset_editor import PresetEditor
+    from randovania.layout.preset import Preset
+
+
+class PresetPrimeGoal(PresetTab, Ui_PresetPrimeGoal):
+    def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
+        super().__init__(editor, game_description, window_manager)
+        self.setupUi(self)
+
+        self.goal_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.slider.valueChanged.connect(self._on_slider_changed)
+
+    @classmethod
+    def tab_title(cls) -> str:
+        return "Goal"
+
+    @classmethod
+    def uses_patches_tab(cls) -> bool:
+        return False
+
+    def _update_editor(self):
+        with self._editor as editor:
+            editor.set_configuration_field("artifact_target", LayoutArtifactMode(self.slider.value()))
+
+    def _on_slider_changed(self):
+        self.slider_label.setText(str(self.slider.value()))
+        self._update_editor()
+
+    def on_preset_changed(self, preset: Preset):
+        artifacts = preset.configuration.artifact_target
+        self.slider.setValue(artifacts.num_artifacts)
