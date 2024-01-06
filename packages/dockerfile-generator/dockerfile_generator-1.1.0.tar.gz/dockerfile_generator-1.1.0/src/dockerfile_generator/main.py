a3aaed6fc494f8b02542e4b01b1a882258a69ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:04:53 +0000

    Add package dockerfile-generator/1.1.0/dockerfile_generator-1.1.0.tar.gz

---
+++ packages/dockerfile-generator/dockerfile_generator-1.1.0.tar.gz/dockerfile_generator-1.1.0/src/dockerfile_generator/main.py
@@ -0,0 +1,147 @@
+import sys
+import argparse
+import pathlib
+import types
+from typing import Iterable
+import itertools
+
+import jinja2
+
+
+CONFIGS_DIR = pathlib.Path(__file__).parent / 'configs'
+TEMPLATES_DIR = pathlib.Path(__file__).parent / 'templates'
+DEFAULT_TEMPLATE = TEMPLATES_DIR / 'Dockerfile.jinja'
+
+
+# Make a read only 'dictionary'
+BUILT_IN_CONFIGS = types.MappingProxyType(
+                      {'alpine' : 'alpine.json',
+                       'debian' : 'debian.json',
+                       'ubuntu' : 'ubuntu.json',
+                       'ubuntu_gcc' : 'ubuntu_gcc.json',
+                      }
+                     )
+
+
+def _get_json_importer():
+    import json
+    return json.load
+
+def _get_toml_importer():
+    import tomllib
+    return tomllib.load
+
+# Dots need to be included.
+CONFIG_IMPORTERS = types.MappingProxyType(
+                      {'.json' : _get_json_importer,
+                       '.toml' : _get_toml_importer,
+                      }
+                     )
+
+
+def rendered_Dockerfile(
+    config: str,
+    params: Iterable[str],
+    encoding: str = 'utf8',
+    template: str | pathlib.Path = DEFAULT_TEMPLATE,
+    ):
+    
+    # For compatibility with jinja2-cli's -d option, the Params are 
+    # provided as a space separated string 
+    ws_separated_params = ' '.join(params)
+
+    template = pathlib.Path(template)
+
+    if config in BUILT_IN_CONFIGS:
+        path = CONFIGS_DIR / BUILT_IN_CONFIGS[config]
+    else:
+        
+        error_msg = ''
+
+        for ext in itertools.chain([''], CONFIG_IMPORTERS.keys()):
+            path = pathlib.Path(config + ext)
+            if path.exists() and path.is_file():
+                break
+        else:
+
+            if not path.exists():
+                error_msg = f'No file found: {config=}. '
+            elif not path.is_file():
+                error_msg = f'{config=} is not file (is it a directory?). ' 
+                
+            error_msg = (f'{error_msg} config must be a file '
+                         f'or in {", ".join(BUILT_IN_CONFIGS)} '
+                        )
+            raise FileNotFoundError(error_msg)
+
+    ext = path.suffix
+    if ext.lower() not in CONFIG_IMPORTERS:
+        raise NotImplementedError(
+            f'Unsupported file extension: {ext} for {config=}. '
+            f'Supported extensions: {", ".join(CONFIG_IMPORTERS)} (case insensitive)'
+            ) 
+
+    importer = CONFIG_IMPORTERS[ext]()
+
+    with path.open('rt', encoding=encoding) as f:
+        config_dict = importer(f)['config']
+
+    env = jinja2.Environment(
+        loader = jinja2.FileSystemLoader(template.parent),
+        extensions = ['jinja2.ext.do',  # So statements need not return values.
+                      'jinja2.ext.loopcontrols'], # For continue and break
+        )
+
+    template_obj = env.get_template(template.name)
+
+    return template_obj.render(config = config_dict, params = ws_separated_params)
+
+
+def main(args = sys.argv[1:]):
+    
+    parser = argparse.ArgumentParser(description=('Parse the config defining '
+                                                  'the Dockerfile type, and '
+                                                  'the params to render the '
+                                                  'particular Dockerfile with')
+                                    )
+    
+    parser.add_argument(
+        'config',
+        help = ('The configuration for the general type of Dockerfile chosen, '
+                'e.g. for an Alpine Linux image, or a Ubuntu image with gcc '
+                'must be the path of a config file, or one of the key words '
+                f'for a built in config: {", ".join(BUILT_IN_CONFIGS)} '
+               ),
+        type=str,
+        default = 'alpine'
+        )    
+
+    parser.add_argument(
+        '--template',
+        help = ('The Dockerfile template chosen, corresponding to the  '
+                'configuration in config.  '
+                f'Default: {DEFAULT_TEMPLATE} '
+               ),
+        type=str,
+        default = DEFAULT_TEMPLATE
+        )
+    
+    parser.add_argument(
+        'params',
+        nargs = '*',
+        help = ('The params to render the Dockerfile with.  If config=ubuntu_gcc, '
+                'then for example params could be the packages to be installed: '
+                '"make", "cmake", "gcc", "g++" '
+               ),
+        type=str
+        )
+
+    namespace = parser.parse_args(args)
+
+    print(rendered_Dockerfile(**vars(namespace)))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/dockerfile-generator/dockerfile_generator-1.1.0.tar.gz/dockerfile_generator-1.1.0/tests/test_case_parameters.py
@@ -0,0 +1,150 @@
+
+import os
+import pathlib
+import json
+import random
+from typing import Iterator
+
+
+
+ENFORCE_ALL_VERSION_PINNING_WARNINGS = os.getenv('TEST_DOCKERFILE_GENERATOR_ENFORCE_ALL_VERSION_PINNING_WARNINGS', "False") == "True"
+
+VERSION_PINNING_RULES = {'DL3008', 'DL3013', 'DL3016', 'DL3018', 'DL3028',}
+#                         Apt-get   Pip       Npm       Apk       Gem
+# E.g: https://github.com/hadolint/hadolint/wiki/DL3008
+
+RULES_TO_ALWAYS_IGNORE = {'DL3059',}
+
+
+def _contains_constraint(name):
+    return any(c in name for c in ('<','>','='))
+
+
+
+def _rendered_names_generator_factory(config):
+    pkgs_that_could_be_unaliased = []
+    alias_dicts = []
+
+    for command_name in config.get('commands_order'):
+        command = config.get('commands', {}).get(command_name, {})
+        supported_parameters = command.get('supported_parameters', []) 
+        if isinstance(supported_parameters, dict):
+            alias_dicts.append(supported_parameters)
+        else:
+            # assert isinstance(supported_parameters, list)
+            pkgs_that_could_be_unaliased.extend(supported_parameters)
+
+    def _rendered_names(pkgs):
+        for pkg in pkgs:
+
+            recognised = False
+            
+            if pkg in pkgs_that_could_be_unaliased:
+                yield pkg
+                recognised = True
+            
+            for alias_dict in alias_dicts:
+                if pkg in alias_dict:
+                    yield alias_dict[pkg]
+                recognised = True
+
+            if (not recognised and 
+                'unused_parameters_command' in config and
+                config['unused_parameters_command'] in config['commands_order']):
+                yield pkg
+    
+    return _rendered_names
+
+
+
+def _all_versioned(pkgs, config):
+
+    _rendered_names_gen = _rendered_names_generator_factory(config)
+    
+    return all(_contains_constraint(name)
+               for name in _rendered_names_gen(pkgs)
+              )   
+
+
+
+def rules_to_ignore(pkgs, config):
+    if ENFORCE_ALL_VERSION_PINNING_WARNINGS or _all_versioned(pkgs, config):
+        return RULES_TO_ALWAYS_IGNORE
+    
+    return RULES_TO_ALWAYS_IGNORE | VERSION_PINNING_RULES
+
+
+def _all_configs_paths_and_params() -> Iterator[tuple[dict, pathlib.Path, list[str]]]:
+
+    for path in pathlib.Path('.').glob('**/*.json'):
+
+        with open(path, 'rt') as f:
+            config = json.load(f).get('config', None)
+
+        if config is None:
+            continue
+
+        params = [param
+                  for command in config.get('commands', {}).values()
+                  for param in command.get('supported_parameters', []) 
+                 ]
+
+        params.extend(config.get('build_stages', {}))
+
+        yield config, path, params
+
+
+def _generate_test_data(a = 2, b = None, n = 4) -> Iterator[tuple[pathlib.Path, str, set[str]]]:
+    """ Yields different test data depending on an env variable,
+        and depending on whatever json config files 
+        are in the repo.  And ends with n random test cases per config.  
+        Set the bool ENFORCE_ALL_VERSION_PINNING_WARNINGS 
+        to True to require all
+        configs to pass hadolint's version pinning rules.
+    """
+
+    for config, path, params in _all_configs_paths_and_params():
+
+
+        # Test generating Dockerfile with no params
+        yield path, '', RULES_TO_ALWAYS_IGNORE
+
+        if not params: 
+            continue
+
+        # Test generating Dockerfile with all the params
+        yield path, ' '.join(params), rules_to_ignore(params, config)
+
+
+        # Test generating Dockerfile with each param on its own
+        for param in params:
+            yield path, param, rules_to_ignore([param], config)
+
+
+        max_sample_size = len(params) - 1 if b is None else b
+
+
+        # Test generating Dockerfile with n random selections of between a and b params
+        for _ in range(n):
+            try:
+                k = random.randint(a, max_sample_size)
+                sample = random.sample(params, k)
+            except ValueError as e:
+                print(f'{a=}, {max_sample_size=}, {k=}, {path=}, {b=}')
+                raise e
+            yield path, ' '.join(sample), rules_to_ignore(sample, config)
+
+
+def _only_all_param_tests(except_params = [('rc', ('alpine')),])  -> Iterator[tuple[pathlib.Path, str, set[str]]]:  
+                                            # rc does compile on alpine.  The particular subtemplate 
+                                            # currently used compiles all of Plan9, which 
+                                            # takes almost an hour
+
+    for config, path, params in _all_configs_paths_and_params():
+        for param_to_remove, distros in except_params:
+            if param_to_remove in params and any(distro in str(path) 
+                                                 for distro in distros
+                                                ):
+                params.remove(param_to_remove)
+        yield path, ' '.join(params), rules_to_ignore(params, config)
+
