a61ac6d13f4ef798971ac35bcd5a8ca465cbf4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:45 +0000

    Add package odk-mailer/0.1.2/odk_mailer-0.1.2-py3-none-any.whl

---
+++ packages/odk-mailer/odk_mailer-0.1.2-py3-none-any.whl/odk_mailer/lib/__init__.py
@@ -0,0 +1,1 @@
+from . import db, prompts, utils, log, smtp, globals
---
+++ packages/odk-mailer/odk_mailer-0.1.2-py3-none-any.whl/odk_mailer/lib/smtp.py
@@ -0,0 +1,28 @@
+from odk_mailer.lib import globals
+from email.message import EmailMessage
+import smtplib
+
+def send_mail(subject, sender, recipient, message, type='plain'):
+
+    config = globals.odk_mailer_config
+
+    # smtp_host = 'smtp.freesmtpservers.com'
+    # smtp_port = 25
+    # smtp_username = ''
+    # smtp_password = ''
+
+    email = EmailMessage()
+    email['Subject'] = subject
+    email['From'] = sender
+    email['To'] = recipient
+    email.set_content(message, subtype=type)
+
+    try:
+        smtp = smtplib.SMTP(timeout=5)
+        smtp.set_debuglevel(2)
+        smtp.connect(config.smtp_host, config.smtp_port)
+        smtp.send_message(email)
+        smtp.quit()
+        print("Successfully sent email to " + email["To"])
+    except Exception as error:
+        print(error)
