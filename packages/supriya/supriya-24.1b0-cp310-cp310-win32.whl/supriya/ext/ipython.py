db2dc7c41fcd3e70091f86a8629c53c1c95a3390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:05:04 +0000

    Add package supriya/24.1b0/supriya-24.1b0-cp310-cp310-win32.whl

---
+++ packages/supriya/supriya-24.1b0-cp310-cp310-win32.whl/supriya/ext/ipython.py
@@ -0,0 +1,21 @@
+from IPython.display import Audio, display  # type: ignore
+
+from supriya.ext import websafe_audio
+from supriya.io import Player
+
+
+def load_ipython_extension(ipython):
+    ipython.extension_manager.load_extension("uqbar.ext.ipython")
+    patch_player()
+
+
+def patch_player():
+    def render(self):
+        output_path = self.renderable.__render__(**self.render_kwargs)
+        return websafe_audio(output_path)
+
+    def open_output_path(self, output_path):
+        display(Audio(filename=str(output_path)))
+
+    Player.open_output_path = open_output_path
+    Player.render = render
