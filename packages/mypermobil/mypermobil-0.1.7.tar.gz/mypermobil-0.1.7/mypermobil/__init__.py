9dd5e0494ac5d2ebe811e8947fc04d49e93c7830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:48 +0000

    Add package mypermobil/0.1.7/mypermobil-0.1.7.tar.gz

---
+++ packages/mypermobil/mypermobil-0.1.7.tar.gz/mypermobil-0.1.7/mypermobil/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.7"
+
+from mypermobil.mypermobil import (
+    MyPermobil,
+    create_session,
+    MyPermobilException,
+    MyPermobilAPIException,
+    MyPermobilEulaException,
+    MyPermobilClientException,
+    MyPermobilConnectionException,
+)
+from mypermobil.const import *
---
+++ packages/mypermobil/mypermobil-0.1.7.tar.gz/mypermobil-0.1.7/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="mypermobil",
+    version="0.1.7",
+    description="A Python wrapper for the MyPermobil API",
+    url="https://github.com/IsakNyberg/MyPermobil-API",
+    author="Isak Nyberg",
+    author_email="isak@nyberg.dev",
+    license="MIT",
+    packages=["mypermobil"],
+    install_requires=["aiohttp", "aiocache"],
+    test_requires=["pytest", "aiounittest", "aiocache"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
