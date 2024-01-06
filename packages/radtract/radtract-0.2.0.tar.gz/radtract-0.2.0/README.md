31466f16368e41391b77720e8d0e574a0508816b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:03 +0000

    Add package radtract/0.2.0/radtract-0.2.0.tar.gz

---
+++ packages/radtract/radtract-0.2.0.tar.gz/radtract-0.2.0/README.md
@@ -0,0 +1,58 @@
+<!--
+Copyright © 2023 German Cancer Research Center (DKFZ), Division of Medical Image Computing
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+# Radiomic Tractometry (RadTract)
+
+Copyright © German Cancer Research Center (DKFZ), [Division of Medical Image Computing (MIC)](https://www.dkfz.de/en/mic/index.php). Please make sure that your usage of this code is in compliance with the code [license](https://github.com/MIC-DKFZ/radtract/blob/master/LICENSE).
+
+If you use RadTract, please cite our paper: `P. Neher, D. Hirjak, K. Maier-Hein. "Radiomic tractometry: a rich and tract-specific class of imaging biomarkers for neuroscience and medical applications", May 23, 2023. doi: https://doi.org/10.1101/2021.05.23.21257610`
+
+
+## Overview
+
+Python package for radiomic tractometry (RadTract).
+RadTract enables the extraction of comprehensive and highly informative microstructural feature sets along individual white matter tracts suitable for subject-level predictions, where classic tractometry is limited to a vector of mean values per tract. 
+
+For details about the approach, please refer to our paper [Radiomic tractometry: a rich and tract-specific class of imaging biomarkers for neuroscience and medical applications](https://doi.org/10.21203/rs.3.rs-2950610/v1).
+An overview of the method is shown in Figure 1.
+
+![](resources/radtract_overview.png)_Figure 1: Illustration of the complete RadTract process. The points of a statically resampled tract (a) can be seen as samples of partly overlapping classes that are not linearly separable. We are aiming at finding the hyperplanes, superimposed as white lines on the tract in (a), that optimally separate the classes with the smallest amount of errors. This task can be solved using large-margin classifiers such as SVMs. This enables us to create parcellations directly in voxel-space (b) that do not suffer from projection-induced misassignments, as is the case in the centerline-based approach (d). For visualization purposes, the tract parcellation in voxel-space is projected back on the original streamlines (e). The proposed tract parcellation in voxel-space (b) is used to calculate 1106 features per parcel, visualized in (c). In the case of the CST example used in this figure, this results in 18,802 features for the complete tract. The calculated feature classes and used image filters are listed in (f). Figure from [Radiomic tractometry: a rich and tract-specific class of imaging biomarkers for neuroscience and medical applications](https://doi.org/10.21203/rs.3.rs-2950610/v1)._
+
+## Installation
+
+### Requirements
+
+- No specific hardware requirements. A state-of-the-art desktop computer should be sufficient.
+- Tested on Ubuntu 22.04 but should run on other systems as well.
+- Tested with Python 3.8 and higher
+- Numpy should be installed prior to the RadTract setup, all other dependencies will be installed automatically. 
+- It is recommended to use a virtual environment for the installation. 
+
+See `.gitlab-ci.yml` for the currently tested configurations.
+
+### Installation
+
+Installation via anaconda is not supported currently!
+
+1. virtual environment
+   - Create a virtual environment: `python -m venv myvenv`
+   - Activate the virtual environment: `source myvenv/bin/activate`
+2. Installation
+   - Install from source: navigate to the root directory of RadTract and run `pip install .`
+   - Install from PyPI: run `pip install radtract`
+
+Installation should complete within a few seconds.
+
+## Examples
+
+A complete pipeline example can be found in [example.ipynb](https://github.com/MIC-DKFZ/radtract/blob/main/example.ipynb). 
+
+Further examples can be found in the RadTract test script `tests\test_radtract.py`. Test data is included in `tests\test_data`.
+
+
+### Expected runtimes
+
+RadTract parcellation and feature calculation should complete within a couple of minutes on a standard desktop computer.
---
+++ packages/radtract/radtract-0.2.0.tar.gz/radtract-0.2.0/tests/test_radtract.py
@@ -0,0 +1,92 @@
+# Copyright © 2023 German Cancer Research Center (DKFZ), Division of Medical Image Computing
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+import pandas as pd
+from radtract import parcellation, features, tractdensity
+import nibabel as nib
+import os
+
+
+def load_data():
+    data_folder = os.path.dirname(__file__) + '/test_data/'
+    beginnings = nib.load(data_folder + 'test_tract_b.nii.gz')
+    envelope = nib.load(data_folder + 'test_tract_envelope.nii.gz')
+    centerline_parcellation = nib.load(data_folder + 'centerline_parcellation.nii.gz')
+    hyperplane_parcellation = nib.load(data_folder + 'hyperplane_parcellation.nii.gz')
+    sl_file = nib.streamlines.load(data_folder + 'test_tract.trk')
+    streamlines = sl_file.streamlines
+    return streamlines, beginnings, envelope, centerline_parcellation, hyperplane_parcellation
+
+
+def get_results_path():
+    p = os.path.dirname(__file__) + '/test_results/'
+    os.makedirs(p, exist_ok=True)
+    return p
+
+
+def test_envelope():
+    streamlines, beginnings, envelope, _, _ = load_data()
+
+    new_envelope = tractdensity.tract_envelope(streamlines, reference_image=beginnings, out_image_filename=get_results_path() + 'test_tract_envelope.nii.gz')
+
+    assert np.equal(new_envelope.get_fdata(), envelope.get_fdata()).all(), 'envelope test 1 failed'
+    new_envelope = nib.load(get_results_path() + 'test_tract_envelope.nii.gz')
+    assert np.equal(new_envelope.get_fdata(), envelope.get_fdata()).all(), 'envelope test 2 failed'
+
+
+def test_centerline_parcellation():
+    streamlines, beginnings, envelope, centerline_parcellation, _ = load_data()
+    new_parcellation, _, _, _ = parcellation.parcellate_tract(streamlines=streamlines, parcellation_type='centerline', binary_envelope=envelope, num_parcels=17, start_region=beginnings, out_parcellation_filename=get_results_path() + 'test_tract_parcellation-centerline.nii.gz')
+
+    assert np.equal(new_parcellation.get_fdata(), centerline_parcellation.get_fdata()).all(), 'centerline parcellation test 1 failed'
+    new_parcellation = nib.load(get_results_path() + 'test_tract_parcellation-centerline.nii.gz')
+    assert np.equal(new_parcellation.get_fdata(), centerline_parcellation.get_fdata()).all(), 'centerline parcellation test 2 failed'
+
+
+
+def test_hyperplane_parcellation():
+    streamlines, beginnings, envelope, _, hyperplane_parcellation = load_data()
+    new_parcellation, _, _, _ = parcellation.parcellate_tract(streamlines=streamlines, parcellation_type='hyperplane', binary_envelope=envelope, num_parcels=17, start_region=beginnings, out_parcellation_filename=get_results_path() + 'test_tract_parcellation-hyperplane.nii.gz')
+
+    assert np.equal(new_parcellation.get_fdata(), hyperplane_parcellation.get_fdata()).all(), 'hyperplane parcellation test 1 failed'
+    new_parcellation = nib.load(get_results_path() + 'test_tract_parcellation-hyperplane.nii.gz')
+    assert np.equal(new_parcellation.get_fdata(), hyperplane_parcellation.get_fdata()).all(), 'hyperplane parcellation test 2 failed'
+
+
+def test_num_parcel_esimation():
+    streamlines, beginnings, _, _, _ = load_data()
+
+    num = parcellation.estimate_num_parcels(streamlines=streamlines,
+                                            reference_image=beginnings,
+                                            num_voxels=5)
+    assert num == 18, 'num parcel estimation test 1 failed'
+
+
+def test_hyperplane_features():
+    data_folder = os.path.dirname(__file__) + '/test_data/'
+    features_df = pd.read_pickle(data_folder + 'hyperplane_features.pkl')
+    pyrad_extractor = features.PyradiomicsExtractor(num_parcels=17)
+    new_features = pyrad_extractor.calc_features(parcellation_file_name=data_folder + 'hyperplane_parcellation.nii.gz',
+                                                 parameter_map_file_name=data_folder + 'test_map.nii.gz'
+                                                )
+    # remove path from 'map' and 'parcellation' columns
+    new_features['map'] = new_features['map'].str.split('/').str[-1]
+    new_features['parcellation'] = new_features['parcellation'].str.split('/').str[-1]
+    new_features.to_pickle(get_results_path() + 'hyperplane_features.pkl')
+    pd.testing.assert_frame_equal(new_features, features_df, check_dtype=False)
+
+
+def test_centerline_features():
+    data_folder = os.path.dirname(__file__) + '/test_data/'
+    features_df = pd.read_pickle(data_folder + 'centerline_features.pkl')
+    pyrad_extractor = features.PyradiomicsExtractor(num_parcels=17)
+    new_features = pyrad_extractor.calc_features(parcellation_file_name=data_folder + 'centerline_parcellation.nii.gz',
+                                                 parameter_map_file_name=data_folder + 'test_map.nii.gz'
+                                                )
+    # remove path from 'map' and 'parcellation' columns
+    new_features['map'] = new_features['map'].str.split('/').str[-1]
+    new_features['parcellation'] = new_features['parcellation'].str.split('/').str[-1]
+    new_features.to_pickle(get_results_path() + 'centerline_features.pkl')
+    pd.testing.assert_frame_equal(new_features, features_df, check_dtype=False)
