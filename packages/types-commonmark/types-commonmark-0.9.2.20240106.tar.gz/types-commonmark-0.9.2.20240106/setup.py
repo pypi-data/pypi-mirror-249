64d2abe430f6fa9d4daf26a95f6a912afd77adba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:38 +0000

    Add package types-commonmark/0.9.2.20240106/types-commonmark-0.9.2.20240106.tar.gz

---
+++ packages/types-commonmark/types-commonmark-0.9.2.20240106.tar.gz/types-commonmark-0.9.2.20240106/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+name = "types-commonmark"
+description = "Typing stubs for commonmark"
+long_description = '''
+## Typing stubs for commonmark
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`commonmark`](https://github.com/rtfd/commonmark.py) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`commonmark`.
+
+This version of `types-commonmark` aims to provide accurate annotations
+for `commonmark==0.9.*`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/commonmark. All fixes for
+types and metadata should be contributed there.
+
+This stub package is marked as [partial](https://peps.python.org/pep-0561/#partial-stub-packages).
+If you find that annotations are missing, feel free to contribute and help complete them.
+
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `1de5830a2703936a96a126248227d5c7db883674` and was tested
+with mypy 1.8.0, pyright 1.1.342, and
+pytype 2023.12.18.
+'''.lstrip()
+
+setup(name=name,
+      version="0.9.2.20240106",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/commonmark.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['commonmark-stubs'],
+      package_data={'commonmark-stubs': ['__init__.pyi', 'blocks.pyi', 'cmark.pyi', 'common.pyi', 'dump.pyi', 'entitytrans.pyi', 'inlines.pyi', 'main.pyi', 'node.pyi', 'normalize_reference.pyi', 'render/__init__.pyi', 'render/html.pyi', 'render/renderer.pyi', 'render/rst.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0 license",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
