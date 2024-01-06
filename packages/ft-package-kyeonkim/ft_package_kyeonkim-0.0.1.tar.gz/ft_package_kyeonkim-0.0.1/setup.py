de48fbe87ca3a9596c5880e5eae9f690ccde7d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 14:04:35 +0000

    Add package ft-package-kyeonkim/0.0.1/ft_package_kyeonkim-0.0.1.tar.gz

---
+++ packages/ft-package-kyeonkim/ft_package_kyeonkim-0.0.1.tar.gz/ft_package_kyeonkim-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package_kyeonkim',
+    version='0.0.1',
+    description='A sample test package',
+    author='kyeonkim',
+    author_email='kyeonkim@student.42seoul.kr',
+    # url='',
+    # install_requires=[],
+    # packages=find_packages(exclude=[]),
+    keywords=['kyeonkim', 'ft_package'],
+    # python_requires='>=3.6',
+    # package_data={},
+    # zip_safe=False,
+    # classifiers=[],
+)
