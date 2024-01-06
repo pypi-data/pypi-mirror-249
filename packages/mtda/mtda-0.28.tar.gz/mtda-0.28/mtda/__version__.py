d41d5f71a89ad2f849b3c123f6e3529269e729ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:04:57 +0000

    Add package mtda/0.28/mtda-0.28.tar.gz

---
+++ packages/mtda/mtda-0.28.tar.gz/mtda-0.28/mtda/__version__.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------------
+# MTDA version
+# ---------------------------------------------------------------------------
+#
+# This software is a part of MTDA.
+# Copyright (C) 2023 Siemens Digital Industries Software
+#
+# ---------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT
+# ---------------------------------------------------------------------------
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (C) 2023 Siemens Digital Industries Software'
+__version__ = '0.28'
