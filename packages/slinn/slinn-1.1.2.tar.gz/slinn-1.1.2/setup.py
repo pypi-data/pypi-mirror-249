eccf36c75d2fe4c457da7888044b440b23bff57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:21 +0000

    Add package slinn/1.1.2/slinn-1.1.2.tar.gz

---
+++ packages/slinn/slinn-1.1.2.tar.gz/slinn-1.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(name='slinn',
+      version='1.1.2',
+      description='HTTPS and HTTP server framework',
+      packages=['slinn', 'slinn.default'],
+      data_files=[('slinn', ['slinn/default/project.json'])],
+      author='Mark Radin',
+      author_email='mrybs2@gmail.com',
+      url='https://slinn.miotp.ru/',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      python_requires='>=3.11',
+      zip_safe=True)
