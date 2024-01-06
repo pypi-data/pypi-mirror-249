e3f66b224a2c21aa0cee8bd9bf87668caa607784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:18 +0000

    Add package pauytinka-vsoh/1.0/pauytinka_vsoh-1.0.tar.gz

---
+++ packages/pauytinka-vsoh/pauytinka_vsoh-1.0.tar.gz/pauytinka_vsoh-1.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='pauytinka_vsoh',
+    version='1.0',
+    packages=['pauytinka_vsoh'],
+    install_requires=[
+        'requests',
+    ],
+)
