952a9429201448f9631ddd7465f538987a9871a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:01 +0000

    Add package fastask/0.2.7/fastask-0.2.7.tar.gz

---
+++ packages/fastask/fastask-0.2.7.tar.gz/fastask-0.2.7/setup.py
@@ -0,0 +1,27 @@
+# file: setup.py
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fastask',
+    packages=['fastask'],
+    version='0.2.7',
+    py_modules=['fastask.ask'],
+    include_package_data=True,
+    package_data={'fastask': ['Modelfile']},
+    install_requires=[
+        'openai',
+        'configparser',
+        'inquirer',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ask = fastask.ask:main',
+        ],
+    },
+    description='A fast and easy tool for asking questions via command line',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
