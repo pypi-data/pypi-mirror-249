b0b572ec6c8e74260ffa9c27ec754ff92f0c71c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:56 +0000

    Add package messages-local/0.0.35/messages_local-0.0.35-py3-none-any.whl

---
+++ packages/messages-local/messages_local-0.0.35-py3-none-any.whl/messages_local/MessagesLocalConstants.py
@@ -0,0 +1,15 @@
+from logger_local.LoggerComponentEnum import LoggerComponentEnum
+
+MESSAGE_ACTION_ID = 15
+MESSAGE_LOCAL_PYTHON_COMPONENT_ID = 259
+MESSAGE_LOCAL_PYTHON_COMPONENT_COMPONENT_NAME = 'messages-local-python-package'
+DEVELOPER_EMAIL = 'akiva.s@circ.zone'
+
+logger_object_message = {
+    'component_id': MESSAGE_LOCAL_PYTHON_COMPONENT_ID,
+    'component_name': MESSAGE_LOCAL_PYTHON_COMPONENT_COMPONENT_NAME,
+    'component_category': LoggerComponentEnum.ComponentCategory.Code.value,
+    'developer_email': DEVELOPER_EMAIL
+}
+
+MESSAGES_API_TYPE_ID = 5
