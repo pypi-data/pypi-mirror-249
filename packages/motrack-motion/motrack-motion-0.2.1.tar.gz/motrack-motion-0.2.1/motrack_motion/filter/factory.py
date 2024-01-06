e88b86c8029480f5f248b90c14c9b7437b41aef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:51 +0000

    Add package motrack-motion/0.2.1/motrack-motion-0.2.1.tar.gz

---
+++ packages/motrack-motion/motrack-motion-0.2.1.tar.gz/motrack-motion-0.2.1/motrack_motion/filter/factory.py
@@ -0,0 +1,43 @@
+"""
+Filter factory.
+"""
+from torch import nn
+
+from motrack_motion.datasets import transforms
+from motrack_motion.filter.end_to_end_filter import BufferedE2EFilter
+from motrack_motion.filter.transfilter import TransFilter
+from motrack_motion.filter.base import StateModelFilter
+
+FILTER_CATALOG = {
+    'end_to_end': BufferedE2EFilter,
+    'transfilter': TransFilter
+}
+
+
+def filter_factory(
+    name: str,
+    params: dict,
+    model: nn.Module,
+    transform: transforms.InvertibleTransformWithVariance,
+) -> StateModelFilter:
+    """
+    Creates filter by given name.
+
+    Args:
+        name: Dataset name
+        params: Filter parameters
+        model: Filter core model
+        transform: Filter preprocess - postprocess
+
+    Returns:
+        Initialized filter
+    """
+    name = name.lower()
+
+    cls = FILTER_CATALOG[name]
+    return cls(
+        name=name,
+        params=params,
+        model=model,
+        transform=transform
+    )
