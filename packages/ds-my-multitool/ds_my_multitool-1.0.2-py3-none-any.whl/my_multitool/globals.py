c3d9cdb9a38268f34f3ba8aff5ac1cfd98c02759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 14:04:13 +0000

    Add package ds-my-multitool/1.0.2/ds_my_multitool-1.0.2-py3-none-any.whl

---
+++ packages/ds-my-multitool/ds_my_multitool-1.0.2-py3-none-any.whl/my_multitool/globals.py
@@ -0,0 +1,23 @@
+"""Globals for the package.
+
+Contains the global objects for the package.
+"""
+
+from my_data.my_data import MyData
+from .config import ConfigManager
+
+config = ConfigManager()
+
+
+def get_global_data_object() -> MyData:
+    """Get a configured MyData object.
+
+    Returns a MyData object with the correct configuration for the active
+    context.
+
+    Returns:
+        A MyData object.
+    """
+    data = MyData()
+    data.configure(db_connection_str=config.active_context.db_string)
+    return data
