4c4ec384a9c3355c45295f54705711360a2a53cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:38 +0000

    Add package odoo14-addon-ssi-stock/14.0.2.11.0/odoo14_addon_ssi_stock-14.0.2.11.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-stock/odoo14_addon_ssi_stock-14.0.2.11.0-py3-none-any.whl/odoo/addons/ssi_stock/models/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    product_category,
+    picking_type_category,
+    stock_picking,
+    stock_picking_type,
+    location_type,
+    stock_location,
+    stock_move,
+    stock_move_line,
+    stock_rule,
+    mixin_picking_type_m2o_configurator,
+    mixin_picking_type_category_m2o_configurator,
+)
