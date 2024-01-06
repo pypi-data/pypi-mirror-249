824deb816b033d82391e7d589cefb0ad5388ba07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:29 +0000

    Add package edc-pdf-reports/0.3.18/edc-pdf-reports-0.3.18.tar.gz

---
+++ packages/edc-pdf-reports/edc-pdf-reports-0.3.18.tar.gz/edc-pdf-reports-0.3.18/runtests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_constants.constants import IGNORE
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_pdf_reports"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    EDC_AUTH_SKIP_SITE_AUTHS=True,
+    EDC_AUTH_SKIP_AUTH_UPDATER=True,
+    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    failures = DiscoverRunner(failfast=True).run_tests([f"{app_name}.tests"])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
