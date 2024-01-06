c552461ba42e908d639abc5a10aecf7dd424fcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:05:04 +0000

    Add package dynamic-valuation/1/dynamic_valuation-1.tar.gz

---
+++ packages/dynamic-valuation/dynamic_valuation-1.tar.gz/dynamic_valuation-1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dynamic_valuation",
+    version="1",
+    author="Eric Larson",
+    author_email="ericl3@illinois.edu",
+    description="Find present value of a benefit stream subjectto dynamics.",
+    packages=find_packages(),
+
+)
