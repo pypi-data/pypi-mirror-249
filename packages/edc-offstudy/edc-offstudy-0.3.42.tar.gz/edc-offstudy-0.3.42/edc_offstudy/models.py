4bfd00879a977d293e4907bc2d8e68e90417a424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:08 +0000

    Add package edc-offstudy/0.3.42/edc-offstudy-0.3.42.tar.gz

---
+++ packages/edc-offstudy/edc-offstudy-0.3.42.tar.gz/edc-offstudy-0.3.42/edc_offstudy/models.py
@@ -0,0 +1,30 @@
+from edc_action_item.models import ActionNoManagersModelMixin
+from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
+from edc_identifier.managers import SubjectIdentifierManager
+from edc_model.models import BaseUuidModel, HistoricalRecords
+from edc_sites.managers import CurrentSiteManager
+from edc_sites.model_mixins import SiteModelMixin
+
+from .constants import END_OF_STUDY_ACTION
+from .model_mixins import OffstudyModelMixin
+
+
+class SubjectOffstudy(
+    RequiresConsentFieldsModelMixin,
+    OffstudyModelMixin,
+    SiteModelMixin,
+    ActionNoManagersModelMixin,
+    BaseUuidModel,
+):
+    action_name = END_OF_STUDY_ACTION
+
+    objects = SubjectIdentifierManager()
+
+    on_site = CurrentSiteManager()
+
+    history = HistoricalRecords()
+
+    class Meta(BaseUuidModel.Meta):
+        verbose_name = "Subject Offstudy"
+        verbose_name_plural = "Subject Offstudy"
+        indexes = ActionNoManagersModelMixin.Meta.indexes + BaseUuidModel.Meta.indexes
