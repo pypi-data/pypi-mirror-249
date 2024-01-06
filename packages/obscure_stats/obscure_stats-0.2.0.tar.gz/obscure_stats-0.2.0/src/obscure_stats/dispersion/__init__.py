2b7aa0c14b4fa5dec5dca287f6f8db99e12eab54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:55 +0000

    Add package obscure_stats/0.2.0/obscure_stats-0.2.0.tar.gz

---
+++ packages/obscure_stats/obscure_stats-0.2.0.tar.gz/obscure_stats-0.2.0/src/obscure_stats/dispersion/__init__.py
@@ -0,0 +1,29 @@
+"""Dispersion module."""
+
+from .dispersion import (
+    coefficient_of_lvariation,
+    coefficient_of_range,
+    coefficient_of_variation,
+    dispersion_ratio,
+    lloyds_index,
+    morisita_index,
+    quartile_coefficient_of_dispersion,
+    robust_coefficient_of_variation,
+    shamos_estimator,
+    standard_quantile_absolute_deviation,
+    studentized_range,
+)
+
+__all__ = [
+    "coefficient_of_lvariation",
+    "coefficient_of_range",
+    "coefficient_of_variation",
+    "dispersion_ratio",
+    "lloyds_index",
+    "morisita_index",
+    "quartile_coefficient_of_dispersion",
+    "robust_coefficient_of_variation",
+    "shamos_estimator",
+    "standard_quantile_absolute_deviation",
+    "studentized_range",
+]
