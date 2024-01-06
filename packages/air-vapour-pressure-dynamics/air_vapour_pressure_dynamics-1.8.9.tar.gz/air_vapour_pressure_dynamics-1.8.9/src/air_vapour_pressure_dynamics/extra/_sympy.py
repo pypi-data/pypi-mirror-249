c14f899307dba8349c7c9450de4f156f312c00ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:26 +0000

    Add package air-vapour-pressure-dynamics/1.8.9/air_vapour_pressure_dynamics-1.8.9.tar.gz

---
+++ packages/air-vapour-pressure-dynamics/air_vapour_pressure_dynamics-1.8.9.tar.gz/air_vapour_pressure_dynamics-1.8.9/src/air_vapour_pressure_dynamics/extra/_sympy.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# ====== IMPORTS ===============
+
+from basic_decorators import argument_check
+
+from ..initialize import SETTINGS, sp
+
+# ======= CONSTANTS ============
+
+sympySymbol = sp.Symbol if SETTINGS.SYMPY_DETECTED else None
+
+# ======= CLASSES ============
+
+if SETTINGS.SYMPY_DETECTED :
+    class UnitSympyExpression(sp.UnevaluatedExpr):
+
+        @argument_check(object, sp.Expr, str)
+        def __new__(self, value, unit=None):
+            return sp.UnevaluatedExpr.__new__(self, value)
+        
+        @argument_check(object, sp.Expr, str)
+        def __init__(self, value, unit=None):
+            self.unit = unit
+            super()
+else:
+    class UnitSympyExpression():
+        pass
+
+class SympySimbol():
+    """sympy Simbol: sympy.Simbol"""
+    pass
+class SympyExpression():
+    """sympy Expression: sympy.Expr"""
+    pass
+
+# ======= FUNCTIONS ===========
+
+@argument_check(sp.Expr)
+def log_sympy(value: sp.Expr) -> sp.Expr :
+    return sp.log(value)
+
+def isSympyExpr(value: object) -> bool:
+    return SETTINGS.SYMPY_DETECTED and isinstance(value, sp.Expr)
+
+if __name__ == '__main__':
+    
+    pass
