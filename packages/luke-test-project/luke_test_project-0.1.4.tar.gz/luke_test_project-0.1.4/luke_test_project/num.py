166cdc3df2f08512f8d55915c55a1fb8ce523e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:18 +0000

    Add package luke-test-project/0.1.4/luke_test_project-0.1.4.tar.gz

---
+++ packages/luke-test-project/luke_test_project-0.1.4.tar.gz/luke_test_project-0.1.4/luke_test_project/num.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Num:
+	def __init__(self):
+		self.x = 0
+		self.ones = np.ones(5)
+
+	def add(self):
+		self.x += 1
+
+	def sub(self):
+		self.x -= 1
+
+	def print(self):
+		print(self.x)
+		print(self.ones)
