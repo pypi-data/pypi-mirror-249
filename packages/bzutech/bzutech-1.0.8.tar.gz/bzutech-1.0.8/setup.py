6faf478e6246f42c6d5f99c7959ea937f5319930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:04:54 +0000

    Add package bzutech/1.0.8/bzutech-1.0.8.tar.gz

---
+++ packages/bzutech/bzutech-1.0.8.tar.gz/bzutech-1.0.8/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="bzutech",
+    version="1.0.8",
+    author="enioo22",
+    description="library to bzutech services",
+    packages=["bzutech"],
+)
