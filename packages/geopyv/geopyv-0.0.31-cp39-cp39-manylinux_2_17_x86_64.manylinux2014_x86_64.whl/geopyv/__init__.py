6c0dece699eddf8bd59fc8f4943d0bf21d0498f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:04:18 +0000

    Add package geopyv/0.0.31/geopyv-0.0.31-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/geopyv/geopyv-0.0.31-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/geopyv/__init__.py
@@ -0,0 +1,26 @@
+import geopyv.check
+import geopyv.calibration
+import geopyv.log
+import geopyv.geometry
+import geopyv.gui
+import geopyv.image
+import geopyv._image_extensions
+import geopyv.io
+import geopyv.field
+import geopyv.mesh
+import geopyv.object
+import geopyv.particle
+import geopyv.plots
+import geopyv.sequence
+import geopyv.speckle
+import geopyv.subset
+import geopyv._subset_extensions
+import geopyv.templates
+import geopyv.validation
+import logging
+
+# Initialise log at default settings.
+level = logging.INFO
+geopyv.log.initialise(level)
+log = logging.getLogger(__name__)
+log.debug("Initialised geopyv log.")
