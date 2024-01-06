85a55fbf3a2352b2ee1c783b45138d2e9ad8783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 06:03:42 +0000

    Add package pyetbd/0.1.4/pyetbd-0.1.4.tar.gz

---
+++ packages/pyetbd/pyetbd-0.1.4.tar.gz/pyetbd-0.1.4/setup.py
@@ -0,0 +1,24 @@
+# This file is used for the package creation when uploading to PyPi
+
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="pyetbd",
+    version="0.1.4",
+    author="Ryan Higginbotham",
+    author_email="ryanhigginbotham@ufl.edu",
+    description="McDowell's (2004) ETBD implemented in Python",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numba==0.57.1",
+        "numpy==1.24.4",
+        "pandas==2.1.0",
+        "openpyxl==3.0.10",
+    ],
+)
