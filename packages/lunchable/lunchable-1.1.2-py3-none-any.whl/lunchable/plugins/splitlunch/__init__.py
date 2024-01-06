ec6983a70d12e51b129f830952f73b3429db1132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:57 +0000

    Add package lunchable/1.1.2/lunchable-1.1.2-py3-none-any.whl

---
+++ packages/lunchable/lunchable-1.1.2-py3-none-any.whl/lunchable/plugins/splitlunch/__init__.py
@@ -0,0 +1,9 @@
+"""
+Splitwise Plugin for Lunchmoney
+"""
+
+from .exceptions import SplitLunchError
+from .lunchmoney_splitwise import SplitLunch
+from .models import SplitLunchExpense
+
+__all__ = ["SplitLunch", "SplitLunchError", "SplitLunchExpense"]
