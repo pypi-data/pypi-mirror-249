c5a3e24f26d57b840c6793167d9d05af9780d26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:37 +0000

    Add package nexor/2024.1.5.67316/nexor-2024.1.5.67316-py3-none-any.whl

---
+++ packages/nexor/nexor-2024.1.5.67316-py3-none-any.whl/nexor/cli.py
@@ -0,0 +1,27 @@
+import argparse
+import logging
+from typing import Sequence, Mapping, Callable
+
+import gather
+from gather import commands as commandslib
+
+LOGGER = logging.getLogger(__name__)
+
+_SUBCOMMANDS = gather.Collector()
+
+command = commandslib.make_command_register(_SUBCOMMANDS)
+
+
+def main(*, argv: Sequence[str], env: Mapping[str, str], run: Callable) -> None:
+    def error(args):
+        parser.print_help()
+        raise SystemExit(1)
+
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(__gather_command__=error)
+    commandslib.run_maybe_dry(
+        parser=commandslib.set_parser(parser=parser, collected=_SUBCOMMANDS.collect()),
+        argv=argv,
+        env=env,
+        sp_run=run,
+    )
