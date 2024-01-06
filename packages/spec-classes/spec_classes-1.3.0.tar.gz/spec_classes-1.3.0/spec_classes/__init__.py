45e38f7730e366fe53d3ed5a595f6aa3a1c84688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:49 +0000

    Add package spec-classes/1.3.0/spec_classes-1.3.0.tar.gz

---
+++ packages/spec-classes/spec_classes-1.3.0.tar.gz/spec_classes-1.3.0/spec_classes/__init__.py
@@ -0,0 +1,22 @@
+from ._version import __version__, __version_tuple__
+from .errors import FrozenInstanceError
+from .spec_class import spec_class
+from .types import Alias, DeprecatedAlias, Attr, AttrProxy, MISSING, spec_property
+
+__author__ = "Matthew Wardrop"
+__author_email__ = "mpwardrop@gmail.com"
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "__author__",
+    "__author_email__",
+    "spec_class",
+    "FrozenInstanceError",
+    "MISSING",
+    "Alias",
+    "DeprecatedAlias",
+    "Attr",
+    "AttrProxy",
+    "spec_property",
+]
---
+++ packages/spec-classes/spec_classes-1.3.0.tar.gz/spec_classes-1.3.0/spec_classes/types/__init__.py
@@ -0,0 +1,21 @@
+from .alias import Alias, DeprecatedAlias
+from .attr import Attr
+from .attr_proxy import AttrProxy
+from .keyed import KeyedList, KeyedSet
+from .missing import MISSING
+from .spec_property import spec_property
+from .validated import ValidatedType, bounded, validated
+
+__all__ = (
+    "Alias",
+    "DeprecatedAlias",
+    "Attr",
+    "AttrProxy",
+    "KeyedList",
+    "KeyedSet",
+    "MISSING",
+    "ValidatedType",
+    "bounded",
+    "spec_property",
+    "validated",
+)
