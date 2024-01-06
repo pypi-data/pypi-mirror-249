d24d8aa32585e8d0424c915d2e4a5d352632c74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:50 +0000

    Add package w-dao/0.1/w-dao-0.1.tar.gz

---
+++ packages/w-dao/w-dao-0.1.tar.gz/w-dao-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w-dao",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="Will Ooi",
+    author_email="willooi@qq.com",
+    description="A useless db connection tool based on psycopg2",
+    license="MIT",
+    keywords="w-dao",
+    url="https://github.com/mkitpro/w-dao.git"
+)
