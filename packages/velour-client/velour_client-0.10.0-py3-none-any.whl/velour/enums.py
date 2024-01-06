19ce1ef00edd40f9bcedfc63d75b78bcde513517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:05:11 +0000

    Add package velour-client/0.10.0/velour_client-0.10.0-py3-none-any.whl

---
+++ packages/velour-client/velour_client-0.10.0-py3-none-any.whl/velour/enums.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class JobStatus(str, Enum):
+    NONE = "none"
+    PENDING = "pending"
+    CREATING = "creating"
+    PROCESSING = "processing"
+    DELETING = "deleting"
+    FAILED = "failed"
+    DONE = "done"
+
+
+class DataType(Enum):
+    IMAGE = "image"
+    TABULAR = "tabular"
+
+    @classmethod
+    def invert(cls, value: str):
+        for member in cls:
+            if member.value == value:
+                return member
+        raise ValueError(f"the value {value} is not in enum {cls.__name__}.")
+
+
+class AnnotationType(str, Enum):
+    NONE = "none"
+    JSON = "json"
+    BOX = "box"
+    POLYGON = "polygon"
+    MULTIPOLYGON = "multipolygon"
+    RASTER = "raster"
+
+
+class TaskType(str, Enum):
+    CLASSIFICATION = "classification"
+    DETECTION = "object-detection"
+    SEGMENTATION = "semantic-segmentation"
