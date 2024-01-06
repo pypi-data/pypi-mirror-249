1a0dc088c57590d44b05a6110b6e7c0be5bc21e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:06:02 +0000

    Add package pyfuelprices/2.1.1/pyfuelprices-2.1.1-py3-none-any.whl

---
+++ packages/pyfuelprices/pyfuelprices-2.1.1-py3-none-any.whl/pyfuelprices/sources/mapping.py
@@ -0,0 +1,23 @@
+"""Sources mapping file."""
+
+from .australia.fuelwatch import FuelWatchSource
+from .germany.tankerkoenig import TankerKoenigSource
+from .uk.map import SOURCE_MAP as UK_SOURCE_MAP
+from .usa.gasbuddy import GasBuddyUSASource
+from .netherlands.map import SOURCE_MAP as NL_SOURCE_MAP
+
+SOURCE_MAP = {
+    **UK_SOURCE_MAP,
+    **NL_SOURCE_MAP,
+    "gasbuddy": GasBuddyUSASource,
+    "tankerkoenig": TankerKoenigSource,
+    "fuelwatch": FuelWatchSource}
+
+COUNTRY_MAP = {
+    "BE": ["directlease"],
+    "DE": ["tankerkoenig"],
+    "NL": [k for k in NL_SOURCE_MAP],
+    "GB": [k for k in UK_SOURCE_MAP],
+    "US": ["gasbuddy"],
+    "AU": ["fuelwatch"]
+}
