7266480f97491a372103eff39d2c52c2893c8bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:20 +0000

    Add package mafiagg/1.0.1/mafiagg-1.0.1-py3-none-any.whl

---
+++ packages/mafiagg/mafiagg-1.0.1-py3-none-any.whl/mafiagg/helper/decorators.py
@@ -0,0 +1,46 @@
+from difflib import SequenceMatcher
+from functools import wraps
+from typing import Dict
+
+
+def get_similar_score(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+# ignore messages from itself
+def ignore_bot_message(func):
+    @wraps(func)
+    def wrapper(self, payload: Dict):
+        if payload["type"] == "chat":
+            if "from" in payload:
+                if payload["from"]["userId"] == self.botUser.id:
+                    return
+        res = func(self, payload)
+        return res
+
+    return wrapper
+
+
+# owner only commands
+def isOwnerOnly(func):
+    @wraps(func)
+    def wrapper(self, payload: Dict):
+        if payload["type"] == "chat":
+            if "from" in payload:
+                if payload["from"]["userId"] not in self.ALLOWED:
+                    return
+        res = func(self, payload)
+        return res
+
+    return wrapper
+
+
+# register a command
+def register_command(v):
+    def _(f):
+        if not hasattr(f, "_commandName"):
+            f._commandName = v
+            f.isCommand = True
+        return f
+
+    return _
