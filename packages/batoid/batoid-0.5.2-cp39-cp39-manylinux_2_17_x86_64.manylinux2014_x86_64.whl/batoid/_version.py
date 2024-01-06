de78c3c98b9a96d3420e068f9b1eb14fbecadb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 14:04:48 +0000

    Add package batoid/0.5.2/batoid-0.5.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/batoid/batoid-0.5.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/batoid/_version.py
@@ -0,0 +1,7 @@
+#https://stackoverflow.com/a/7071358/7530778
+
+__version__ = "0.5.2"
+tmp = __version__
+if "rc" in tmp:  # pragma: no cover
+    tmp = tmp[:tmp.find("rc")]
+__version_info__ = tuple(map(int, tmp.split('.')))
