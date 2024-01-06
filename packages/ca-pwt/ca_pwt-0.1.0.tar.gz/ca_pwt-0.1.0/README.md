caa1a46f05d2462114d46dc9a1956d198318164b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:42 +0000

    Add package ca-pwt/0.1.0/ca_pwt-0.1.0.tar.gz

---
+++ packages/ca-pwt/ca_pwt-0.1.0.tar.gz/ca_pwt-0.1.0/README.md
@@ -0,0 +1,205 @@
+# Conditional Access PowerToys (CA-PowerToys)
+
+[![PyPI - Version](https://img.shields.io/pypi/v/ca-pwt.svg)](https://pypi.org/project/ca-pwt)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/ca-pwt.svg)](https://pypi.org/project/ca-pwt)
+[![GitHub - Lint](https://github.com/0x6f677548/zerotrust-ca-powertoys/actions/workflows/lint.yml/badge.svg)](https://github.com/0x6f677548/zerotrust-ca-powertoys/actions/workflows/lint.yml)
+[![GitHub - Test](https://github.com/0x6f677548/zerotrust-ca-powertoys/actions/workflows/test.yml/badge.svg)](https://github.com/0x6f677548/zerotrust-ca-powertoys/actions/workflows/test.yml)
+
+CA-PowerToys is a set of tools to help you manage Conditional Access policies in your tenant. It is a command line tool that can be used to export and import Conditional Access policies and associated groups, facilitating the editing of the policies in a human readable format. This is particular useful if you are implementing a Policy-as-Code approach, eventually using a Git repository to store your policies and a CI/CD pipeline (like Azure DevOps) to import them into your tenant.
+
+## Why ?
+There are several tools to manage Conditional Access policies, such as Graph PowerShell, Microsoft Graph API, Azure AD PowerShell and even M365DSC. Unfortunately, none of these tools can be used to export Conditional Access policies in a **format that can be human readable and editable**, and then **import them back to another tenant**. This is where CA-PowerToys can help you, with several commands that can be chained to export, clean up, replace guids with attributes, and import Conditional Access policies and groups. 
+
+## Capabilities
+
+CA-PowerToys can be used to:
+- **Get an access token** to be used in subsequent commands or to be used in other tools, such as Graph PowerShell, using a desired client_id (useful if Graph PowerShell or other tools are blocked in the target tenant)
+- **Export/Import Conditional Access policies** to/from a file
+- **Export groups** that are used in Conditional Access policies to a file
+- **Clean up Conditional Access policies and Groups files**, removing attributes that are read-only or not allowed in the import process
+- **Replace guids with attributes in Conditional Access policies (and vice-versa)**, making it "human readable" and editable. For example, replace the `id` attribute with the `displayName` attribute in a list of excluded groups in a Conditional Access policy
+- **Throttle the number of requests** to the Graph API, to avoid hitting the rate limits
+
+## Zero Trust Sample Policies
+A set of sample policies can be found in the [Zero Trust Conditional Access Policies](https://github.com/0x6f677548/zerotrust-ca-policies) repository. These policies are based on the samples available at https://github.com/microsoft/ConditionalAccessforZeroTrustResources and the [recommended guidelines](https://docs.microsoft.com/en-us/azure/architecture/guide/security/conditional-access-zero-trust?msclkid=d1768a34ceda11ec9b6c8f244f8d05bd) and can be used as a starting point to implement a Zero Trust strategy in your organization.
+
+## Installation
+CA-PowerToys is a command line tool that can be used in Windows, Linux, and MacOS. It is written in Python and can be used as a module or as a standalone tool, as long as you have Python >3.7 installed (or Docker).
+
+### pip
+To install it, you can use pip:
+```console
+> pip install ca-pwt
+> ca-pwt --help
+```
+
+### From source code
+You can also install it from the source code:
+```console
+> git clone https://github.com/0x6f677548/zerotrust-ca-powertoys.git
+> cd ca-powertoys
+> pip install .
+> ca-pwt --help
+```
+### Docker
+Alternatively, you can use the Docker image:
+```console
+> docker pull ghcr.io/0x6f677548/zerotrust-ca-powertoys:latest
+> docker run -it --rm ghcr.io/0x6f677548/zerotrust-ca-powertoys --help
+```
+
+## Usage
+All available commands and options can be seen by using the `--help` option.
+```console
+> ca-pwt --help
+```
+
+To get help on a specific command, use the `--help` option with the command name. For example, to get help on the `export-policies` command, use the following command:
+```console
+> ca-pwt export-policies --help
+```
+
+### Usage Examples
+
+#### Obtaining an access token 
+
+There are several ways to obtain an access token. The easiest is to use `acquire-token` in chain with other commands and login interactively. In the example below, the token obtained will be injected in the subsequent command to export the policies. 
+
+```console
+> ca-pwt acquire-token export-policies --output_file policies.json
+```
+```
+Exporting conditional access policies...
+Obtaining policies from tenant...
+Writing policies to file policies.json...
+```
+##### Store it in a variable to be used in subsequent commands
+You can also obtain an access token and store it in a variable to be used in subsequent commands. In this case, you need to instruct the command to output the token using the `--output_token` option. 
+
+(pwsh)
+```console
+> $token = (ca-pwt acquire-token --output_token)
+> ca-pwt --access_token $token export-policies --output_file policies.json
+```
+(bash)
+```console
+> token = $(ca-pwt acquire-token --output_token)
+> ca-pwt --access_token $token export-policies --output_file policies.json
+```
+```
+Exporting conditional access policies...
+Obtaining policies from tenant...
+Writing policies to file policies.json...
+```
+If you are in an endpoint where you can't use the interactive login, you can use `acquire-token` command with `--client_id` and `--client_secret` options, or, alternatively, using device flow, through the `--device_code` option. 
+```console
+> $token = (ca-pwt acquire-token --device_code --output_token)
+To sign in, use a web browser to open the page https://microsoft.com/devicelogin and enter the code 123456789 to authenticate.
+```
+
+##### Using the tool in a CI/CD pipeline
+If you are using the tool in a CI/CD pipeline, CA-PowerToys supports authentication through a service principal, using the `--client_id` and `--client_secret` options. In this case, you need to instruct the command to output the token using the `--output_token` option. You should also use the `--tenant_id` option to specify the tenant where the service principal was created.
+```console
+> $token = (ca-pwt acquire-token --client_id $client_id --client_secret $client_secret --tenant_id $tenant_id --output_token)
+> ca-pwt --access_token $token import-policies --input_file policies.json --duplicate_action overwrite
+```
+
+
+#### Exporting policies
+    
+```console
+> ca-pwt --access_token $token export-policies --output_file policies.json
+```
+```
+Exporting conditional access policies...
+Obtaining policies from tenant...
+Writing policies to file policies.json...
+```
+
+You can also define oData filters to export only a subset of the policies. In the example below, we export only the policies that have the word "Global" in the DisplayName. 
+
+```console
+> ca-pwt --access_token $token export-policies --output_file policies.json --filter "startswith(displayName,'Global')"
+```
+```
+Exporting conditional access policies...
+Obtaining policies from tenant...
+Writing policies to file policies.json...
+```
+
+#### Exporting all Policies and associated Groups
+
+```console
+> ca-pwt --access_token $token export-policies --output_file policies.json export-policy-groups --output_file groups.json
+```
+```
+Exporting ca policies...
+Output file: policies.json
+Exporting groups found in CA policies...
+Input file: policies.json; Output file: groups.json; Lookup cache file: None
+```
+
+#### Exporting all Policies and associated Groups, replace guids with human-readable attributes in the policies file and make the file ready for import
+
+```console
+> ca-pwt --access_token $token export-policies --output_file policies.json cleanup-policies replace-guids-with-attrs export-policy-groups --output_file groups.json cleanup-groups
+```
+```
+Exporting ca policies...
+Output file: policies.json
+Cleaning up CA policies for import...
+Input file: policies.json; Output file: policies.json
+Replacing guids with attributes in CA policies...
+Input file: policies.json; Output file: policies.json
+Exporting groups found in CA policies...
+Input file: policies.json; Output file: groups.json
+Cleaning up groups for import...
+Input file: groups.json; Output file: groups.json
+```
+#### Importing groups and policies
+
+```console
+> ca-pwt --access_token $token import-groups --input_file groups.json import-policies --input_file .\policies-humanreadable.json
+```
+```
+Importing groups...
+Input file: groups.json
+Successfully created groups:
+97d90185-6aef-4b84-9051-ed92c3b023a1: CA-Test-Group
+Importing CA policies...
+Input file: policies-humanreadable.json; Lookup cache file:
+Successfully created policies:
+896d7a7f-8300-4537-8d39-287b25f7259c: CA-Test-Policy
+```
+
+#### Using CA-PowerToys to export policies and import them using Graph PowerShell
+
+You can use CA-PowerToys to export policies in a compatible format with Graph PowerShell. This is useful if you want to export policies from one tenant and import them into another tenant and use Graph PowerShell to import them (you can also use CA-PowerToys to import them, using the `import-policies` command).
+
+This example chains the `export-policies` and `cleanup-policies` commands to export the policies and clean them up for import. The output is stored in the policies.json file. Then we read the policies from the file and import them using the New-MgIdentityConditionalAccessPolicy command. 
+
+```console
+> ca-pwt --access_token $token export-policies --output_file policies.json cleanup-policies
+```
+```
+Exporting conditional access policies...
+Obtaining policies from tenant...
+Writing policies to file policies.json...
+Cleaning up conditional access policies for import...
+Reading policies from file policies.json...
+Cleaning up policies...
+Writing policies to file policies.json...
+```
+```powershell
+>  $policy = Get-Content -Path .\policies.json | ConvertFrom-Json -AsHashtable
+>  $policy.count
+2
+>  New-MgIdentityConditionalAccessPolicy -BodyParameter $policy[0]
+```
+```
+
+Id                                   CreatedDateTime     Description DisplayName
+--                                   ---------------     ----------- -----------
+7b385aec-569a-470a-980b-8624bfa6332c 14/10/2023 07:52:54             CA001-Global-BaseProtection-All…
+```
+Note: In the above example, a token was previously obtained using the `acquire-token` and stored in the `$token` variable.
---
+++ packages/ca-pwt/ca_pwt-0.1.0.tar.gz/ca_pwt-0.1.0/src/ca_pwt/helpers/graph_api.py
@@ -0,0 +1,271 @@
+import requests
+import logging
+import time
+from abc import ABC, abstractmethod
+from enum import StrEnum
+from typing import Any, Callable
+from requests.models import Response
+from ca_pwt.helpers.utils import assert_condition
+
+_REQUEST_TIMEOUT = 500
+_THROTTLING_STATUS_CODE = 429
+_THROTTLING_RETRY_AFTER_HEADER = "Retry-After"
+_THROTTLING_RETRY_AFTER_DEFAULT = 10
+_THROTTLING_MAX_RETRIES = 5
+
+_HTTP_NOT_FOUND = 404
+
+
+class DuplicateActionEnum(StrEnum):
+    IGNORE = "ignore"
+    OVERWRITE = "overwrite"
+    DUPLICATE = "duplicate"
+    FAIL = "fail"
+
+
+class APIResponse:
+    """A class to represent an API response"""
+
+    _logger = logging.getLogger(__name__)
+
+    def __init__(self, request_response: requests.Response, expected_status_code: int = 200):
+        """Creates an API_Response object
+        - request_response: the response from the API request
+        - expected_status_code: the expected status code for the request
+        if the status code of the response matches the expected status code,
+        the success property will be set to True
+        """
+        self.status_code = request_response.status_code
+        self.response: requests.Response | str | dict[str, Any] = request_response
+        self.expected_status_code = expected_status_code
+        self.success = self.status_code == self.expected_status_code
+        if self._logger.isEnabledFor(logging.DEBUG):
+            self._logger.debug(f"Status code: {self.status_code}")
+            self._logger.debug(f"Response: {self.response.text}")
+
+    def json(self):
+        """Returns the JSON representation of the response"""
+        # check if the self.response has a json() method. If so, use it
+        if hasattr(self.response, "json") and callable(self.response.json):
+            text = self.response.text
+            # check if the response is JSON
+            if text.startswith("{") and text.endswith("}"):
+                return self.response.json()
+            else:
+                return text
+        else:
+            return self.response
+
+    def assert_success(self, error_message: str = ""):
+        """Asserts that the request was successful
+        - error_message: the error message to display if the request was not successful
+        """
+        assert_condition(
+            self.success, f"{error_message}: Request failed with status code {self.status_code}; {self.json()}"
+        )
+
+    def __str__(self) -> str:
+        """Returns a string representation of the object"""
+        response_text = self.response.text if hasattr(self.response, "text") else self.response
+        return f"APIResponse: status_code={self.status_code}, success={self.success}, response={response_text}"
+
+
+class EntityAPI(ABC):
+    """An abstract class to represent an entity in the Microsoft Graph API"""
+
+    _logger = logging.getLogger(__name__)
+
+    def __init__(self, access_token: str):
+        """Creates an EntityAPI object
+        - access_token: the access token to use for requests to the API
+        """
+        self.entity_url = f"https://graph.microsoft.com/v1.0/{self._get_entity_path()}"
+        self.access_token = access_token
+        self.request_headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
+
+    @abstractmethod
+    def _get_entity_path(self) -> str:
+        """Returns the path to the entity in the Microsoft Graph API"""
+        pass
+
+    def _request_with_throttling_control(self, func: Callable[..., Response], *args, **kwargs) -> Response:
+        """Sends a request to the API with throttling control"""
+        # set the number of retries to 0
+        retries = 0
+        # while we have not exceeded the maximum number of retries
+        while retries < _THROTTLING_MAX_RETRIES:
+            # send the request
+            response = func(*args, **kwargs)
+            # if the request failed with a throttling error
+            if response.status_code == _THROTTLING_STATUS_CODE:
+                # get the retry after header
+                retry_after_header = response.headers.get(_THROTTLING_RETRY_AFTER_HEADER)
+                if retry_after_header is not None:
+                    retry_after = int(retry_after_header)
+                else:
+                    retry_after = _THROTTLING_RETRY_AFTER_DEFAULT
+                # log the throttling error
+                self._logger.warning(
+                    f"Throttling error: {response.status_code}  {response.text}. "
+                    f"Retrying in {retry_after} seconds..."
+                )
+                # wait for the specified number of seconds
+                time.sleep(retry_after)
+                # increment the number of retries
+                retries += 1
+            else:
+                break
+        return response
+
+    def _request_get(self, url: str) -> APIResponse:
+        """Sends a GET request to the API"""
+        self._logger.debug(f"GET {url}")
+        return APIResponse(
+            self._request_with_throttling_control(
+                requests.get, url, headers=self.request_headers, timeout=_REQUEST_TIMEOUT
+            ),
+            expected_status_code=200,
+        )
+
+    def _request_post(self, url: str, entity: dict) -> APIResponse:
+        """Sends a POST request to the API"""
+        self._logger.debug(f"POST {url}")
+        return APIResponse(
+            self._request_with_throttling_control(
+                requests.post, url, headers=self.request_headers, json=entity, timeout=_REQUEST_TIMEOUT
+            ),
+            expected_status_code=201,
+        )
+
+    def _request_delete(self, url: str) -> APIResponse:
+        """Sends a DELETE request to the API"""
+        self._logger.debug(f"DELETE {url}")
+        return APIResponse(
+            self._request_with_throttling_control(
+                requests.delete, url, headers=self.request_headers, timeout=_REQUEST_TIMEOUT
+            ),
+            expected_status_code=204,
+        )
+
+    def _request_patch(self, url: str, entity: dict) -> APIResponse:
+        """Sends a PATCH request to the API"""
+        self._logger.debug(f"PATCH {url}")
+        return APIResponse(
+            self._request_with_throttling_control(
+                requests.patch, url, headers=self.request_headers, json=entity, timeout=_REQUEST_TIMEOUT
+            ),
+            expected_status_code=204,
+        )
+
+    def get_all(
+        self,
+        odata_filter: str | None = None,
+        odata_top: int | None = None,
+    ) -> APIResponse:
+        """Returns all entities in the API"""
+        url = f"{self.entity_url}?"
+
+        if odata_filter:
+            url += f"$filter={odata_filter}&"
+
+        if odata_top:
+            url += f"$top={odata_top}&"
+
+        # remove the last character if it is a & or ?
+        # this is here for future use if we add more query parameters
+        if url[-1] in ["&", "?"]:
+            url = url[:-1]
+
+        return self._request_get(url)
+
+    def get_by_id(self, entity_id: str) -> APIResponse:
+        """Returns an entity by its ID
+        Entity is returned as a JSON object in the response (response.json())"""
+        assert_condition(entity_id, "entity_id cannot be None")
+        url = f"{self.entity_url}/{entity_id}"
+        return self._request_get(url)
+
+    def get_by_display_name(self, display_name: str) -> APIResponse:
+        """Gets the top entity found with the given display name
+        Returns an API_Response object and the entity is in the json property of the API_Response object
+        """
+        return self.get_top_entity(f"displayName eq '{display_name}'")
+
+    def get_top_entity(self, odata_filter: str, *, use_top: bool = True) -> APIResponse:
+        """Gets the top entity found with the given filter
+        Returns an API_Response object and the entity is in the json property of the API_Response object
+        If use_top is True, the $top query parameter is used to get only the top entity, otherwise all entities are
+        returned and the first one is returned
+        """
+
+        assert_condition(odata_filter, "odata_filter cannot be None")
+        response = self.get_all(odata_filter=odata_filter, odata_top=1 if use_top else None)
+        # if the request was successful, transform the response to a dict
+        if response.success:
+            # move the value property to the response property
+            value = response.json()["value"]
+            # check if we have results
+            if value == []:
+                response.success = False
+                response.status_code = 404
+                response.response = "No results found"
+            else:
+                response.response = value[0]
+        return response
+
+    def create(self, entity: dict) -> APIResponse:
+        """Creates an entity"""
+        assert_condition(entity, "entity cannot be None")
+        return self._request_post(self.entity_url, entity)
+
+    def create_checking_duplicates(
+        self, entity: dict, odata_filter: str, duplicate_action: DuplicateActionEnum = DuplicateActionEnum.IGNORE
+    ) -> APIResponse:
+        """Creates an entity checking for duplicates first and taking the specified action if a duplicate is found
+        A duplicate is determined by the odata_filter parameter, getting the top entity with the specified filter"""
+        assert_condition(entity, "entity cannot be None")
+        assert_condition(odata_filter, "odata_filter cannot be None")
+
+        # if duplicate_action is not duplicate, check if the entity already exists
+        if duplicate_action != DuplicateActionEnum.DUPLICATE:
+            existing_entity = self.get_top_entity(odata_filter)
+            if existing_entity.success:
+                if duplicate_action == DuplicateActionEnum.IGNORE:
+                    self._logger.warning(
+                        f"Entity {self._get_entity_path()} with filter {odata_filter} already exists. Skipping..."
+                    )
+                    return existing_entity
+                elif duplicate_action == DuplicateActionEnum.OVERWRITE:
+                    existing_entity_id = existing_entity.json()["id"]
+                    self._logger.warning(
+                        f"Overwriting entity {self._get_entity_path()} with id {existing_entity_id}..."
+                    )
+                    response = self.update(existing_entity_id, entity)
+                    response.assert_success()
+                    # response should be a "204 No Content" or "200 OK" response
+                    # we need to return the existing_entity_id in the response body
+                    response.response = {"id": existing_entity_id}
+                    return response
+                elif duplicate_action == DuplicateActionEnum.FAIL:
+                    msg = f"Entity {self._get_entity_path()} with filter {odata_filter} already exists."
+                    raise ValueError(msg)
+                else:
+                    msg = f"Invalid duplicate_action: {duplicate_action}"
+                    raise ValueError(msg)
+        return self.create(entity)
+
+    def delete(self, entity_id: str) -> APIResponse:
+        """Deletes an entity by its ID"""
+        assert_condition(entity_id, "entity_id cannot be None")
+        url = f"{self.entity_url}/{entity_id}"
+        return self._request_delete(url)
+
+    def update(self, entity_id: str, entity: dict) -> APIResponse:
+        """Updates an entity by its ID"""
+        assert_condition(entity_id, "entity_id cannot be None")
+        assert_condition(entity, "entity cannot be None")
+        url = f"{self.entity_url}/{entity_id}"
+        return self._request_patch(url, entity)
