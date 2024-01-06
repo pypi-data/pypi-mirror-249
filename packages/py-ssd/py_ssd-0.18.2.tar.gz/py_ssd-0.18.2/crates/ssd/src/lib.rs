9aeba6fbd0b17b613cc9447a48ddff2f729b9a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:04:25 +0000

    Add package py-ssd/0.18.2/py_ssd-0.18.2.tar.gz

---
+++ packages/py-ssd/py_ssd-0.18.2.tar.gz/py_ssd-0.18.2/crates/ssd/src/lib.rs
@@ -0,0 +1,57 @@
+mod ast;
+mod generators;
+#[cfg(feature = "_bin")]
+mod options;
+mod parser;
+mod helper;
+
+pub use helper::update_types;
+
+pub use parser::{
+    parse, parse_file, parse_file_raw, parse_file_with_namespace, parse_raw, ParseError,
+};
+pub use ssd_data::{
+    Attribute, DataType, Dependency, Enum, EnumValue, Event, Function, Import, Namespace,
+    OrderedMap, Parameter, Service, SsdModule, TypeName,
+};
+
+#[cfg(feature = "_web")]
+pub use generators::rhai::generate_web;
+
+#[cfg(feature = "_python")]
+mod python {
+    use std::path::Path;
+
+    use pyo3::exceptions::PyException;
+    use pyo3::prelude::*;
+    use pyo3::Python;
+
+    use ssd_data::Namespace;
+    use ssd_data::SsdModule;
+
+    #[pyfunction]
+    pub fn parse(content: &str, namespace: &str) -> PyResult<SsdModule> {
+        crate::parse(content, Namespace::new(namespace))
+            .map_err(|e| PyException::new_err(e.to_string()))
+    }
+
+    #[pyfunction]
+    pub fn parse_file(base: &str, path: &str) -> PyResult<SsdModule> {
+        crate::parse_file(&Path::new(base), &Path::new(path))
+            .map_err(|e| PyException::new_err(e.to_string()))
+    }
+
+    #[pyfunction]
+    pub fn parse_file_with_namespace(path: &str, namespace: &str) -> PyResult<SsdModule> {
+        crate::parse_file_with_namespace(Path::new(path), Namespace::new(namespace))
+            .map_err(|e| PyException::new_err(e.to_string()))
+    }
+
+    #[pymodule]
+    fn py_ssd(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+        m.add_function(wrap_pyfunction!(parse, m)?)?;
+        m.add_function(wrap_pyfunction!(parse_file, m)?)?;
+        m.add_function(wrap_pyfunction!(parse_file_with_namespace, m)?)?;
+        Ok(())
+    }
+}
