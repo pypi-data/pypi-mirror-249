8969359d156cac19d26de296c46697fc81365d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:05:22 +0000

    Add package genjax/0.0.1/genjax-0.0.1-py3-none-any.whl

---
+++ packages/genjax/genjax-0.0.1-py3-none-any.whl/genjax/core/datatypes.py
@@ -0,0 +1,80 @@
+# Copyright 2023 MIT Probabilistic Computing Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from genjax._src.core.datatypes.generative import (
+    AllSelection,
+    ChoiceMap,
+    ChoiceValue,
+    DisjointUnionChoiceMap,
+    EmptyChoice,
+    GenerativeFunction,
+    HierarchicalChoiceMap,
+    HierarchicalSelection,
+    JAXGenerativeFunction,
+    Mask,
+    NoneSelection,
+    Selection,
+    Trace,
+    all_select,
+    choice_map,
+    choice_value,
+    disjoint_union_choice_map,
+    empty_choice,
+    mask,
+    none_select,
+    select,
+)
+from genjax._src.core.datatypes.hashable_dict import HashableDict, hashable_dict
+from genjax._src.core.datatypes.trie import Trie, trie
+from genjax._src.core.pytree.closure import DynamicClosure, dynamic_closure
+from genjax._src.core.pytree.const import PytreeConst, const
+from genjax._src.core.pytree.pytree import Pytree
+
+__all__ = [
+    # Hashable dictionary type.
+    "HashableDict",
+    "hashable_dict",
+    # Trie type.
+    "Trie",
+    "trie",
+    # Generative datatypes.
+    "ChoiceMap",
+    "EmptyChoice",
+    "empty_choice",
+    "ChoiceValue",
+    "choice_value",
+    "HierarchicalChoiceMap",
+    "choice_map",
+    "DisjointUnionChoiceMap",
+    "disjoint_union_choice_map",
+    "Trace",
+    "Selection",
+    "AllSelection",
+    "all_select",
+    "NoneSelection",
+    "none_select",
+    "HierarchicalSelection",
+    "select",
+    "GenerativeFunction",
+    "JAXGenerativeFunction",
+    # Masking.
+    "Mask",
+    "mask",
+    # Pytree meta.
+    "Pytree",
+    "PytreeConst",
+    "const",
+    "DynamicClosure",
+    "dynamic_closure",
+]
