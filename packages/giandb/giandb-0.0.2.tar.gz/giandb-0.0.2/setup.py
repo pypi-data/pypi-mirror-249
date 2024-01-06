678984fe9bacedaa1f81fc19c2039b2be54687f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 04:04:05 +0000

    Add package giandb/0.0.2/giandb-0.0.2.tar.gz

---
+++ packages/giandb/giandb-0.0.2.tar.gz/giandb-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='giandb',
+    version='0.0.2',  # Cambia la versión a la nueva
+    license='MIT',
+    description="paquete gian de insertar data dba",
+    author="Gianacarlos Cardenas Galarza",
+    install_requires=['numpy'],
+    author_email="alex10estadistica@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/CardenasGalarza/ESPEROOK",
+    entry_points={
+        'console_scripts': [
+            'dbgian = mimodulo:main',  # Reemplaza 'mimodulo' con el nombre real de tu módulo y 'main' con la función principal si la tienes
+        ],
+    },
+)
