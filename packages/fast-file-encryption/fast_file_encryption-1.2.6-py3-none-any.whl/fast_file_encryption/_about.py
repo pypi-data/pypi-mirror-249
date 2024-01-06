b81c296b84b5e807976ec4e8ec08d423d48b5679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 05:05:19 +0000

    Add package fast-file-encryption/1.2.6/fast_file_encryption-1.2.6-py3-none-any.whl

---
+++ packages/fast-file-encryption/fast_file_encryption-1.2.6-py3-none-any.whl/fast_file_encryption/_about.py
@@ -0,0 +1,14 @@
+#  Copyright © 2022-2024 Tobias Erbsland https://erbsland.dev/ and EducateIT GmbH https://educateit.ch/
+#  According to the copyright terms specified in the file "COPYRIGHT.md".
+#  SPDX-License-Identifier: Apache-2.0
+
+
+__all__ = [
+    'VERSION',
+    'AUTHOR',
+    'COPYRIGHT',
+]
+
+VERSION = '1.2.6'
+AUTHOR = 'Tobias Erbsland'
+COPYRIGHT = f'2021-2024, Tobias Erbsland and EducateIT GmbH'
