8f57fd121dc5fba4ad7603235bc19f917831fd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:32 +0000

    Add package types-PyScreeze/0.1.30.20240106/types-PyScreeze-0.1.30.20240106.tar.gz

---
+++ packages/types-PyScreeze/types-PyScreeze-0.1.30.20240106.tar.gz/types-PyScreeze-0.1.30.20240106/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+name = "types-PyScreeze"
+description = "Typing stubs for PyScreeze"
+long_description = '''
+## Typing stubs for PyScreeze
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`PyScreeze`](https://github.com/asweigart/pyscreeze) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`PyScreeze`.
+
+This version of `types-PyScreeze` aims to provide accurate annotations
+for `PyScreeze==0.1.30`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/PyScreeze. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `1de5830a2703936a96a126248227d5c7db883674` and was tested
+with mypy 1.8.0, pyright 1.1.342, and
+pytype 2023.12.18.
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.30.20240106",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/PyScreeze.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-Pillow'],
+      packages=['pyscreeze-stubs'],
+      package_data={'pyscreeze-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
