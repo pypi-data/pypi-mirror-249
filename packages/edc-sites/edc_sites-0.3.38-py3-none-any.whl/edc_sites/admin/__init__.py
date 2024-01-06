1af22a46270688a2987c2a9300cb8876f782f0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:31 +0000

    Add package edc-sites/0.3.38/edc_sites-0.3.38-py3-none-any.whl

---
+++ packages/edc-sites/edc_sites-0.3.38-py3-none-any.whl/edc_sites/admin/__init__.py
@@ -0,0 +1,2 @@
+from .site_admin import SiteAdmin
+from .site_model_admin_mixin import SiteModelAdminMixin
---
+++ packages/edc-sites/edc_sites-0.3.38-py3-none-any.whl/edc_sites/admin/site_admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.sites.models import Site
+from edc_model_admin.mixins import TemplatesModelAdminMixin
+
+from ..admin_site import edc_sites_admin
+
+admin.site.unregister(Site)
+
+
+@admin.register(Site, site=edc_sites_admin)
+class SiteAdmin(TemplatesModelAdminMixin, admin.ModelAdmin):
+    pass
