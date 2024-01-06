9934f9fec5d1a98fcec669fb3dd0cef074797d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:04:40 +0000

    Add package pyNFFT2/1.4.2/pyNFFT2-1.4.2.tar.gz

---
+++ packages/pyNFFT2/pyNFFT2-1.4.2.tar.gz/pyNFFT2-1.4.2/pynfft/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '1.4.2'
+version = '1.4.2'
+full_version = '1.4.2'
+git_revision = 'eda64b553dc82d9ac0fae6a18c0991972aa3cea6'
+release = True
+
+if not release:
+    version = full_version
