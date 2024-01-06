fa14ebb698d10b03dd60a1332205607a28ec7c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:05:36 +0000

    Add package python-tgpt/0.1.4/python-tgpt-0.1.4.tar.gz

---
+++ packages/python-tgpt/python-tgpt-0.1.4.tar.gz/python-tgpt-0.1.4/tgpt/__init__.py
@@ -0,0 +1,11 @@
+from .tgpt import TGPT
+from .imager import Imager
+from .utils import Optimizers
+from .utils import Conversation
+from .utils import AwesomePrompts
+
+__version__ = "0.1.4"
+__author__ = "Smartwa"
+__repo__ = "https://github.com/Simatwa/python-tgpt"
+
+all = ["TGPT", "Imager"]
