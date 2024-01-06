909bdb98f268e825ba6eadffa97449e1a72f5ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:05:50 +0000

    Add package bb-openpyxl/0.2.2/bb_openpyxl-0.2.2-py3-none-any.whl

---
+++ packages/bb-openpyxl/bb_openpyxl-0.2.2-py3-none-any.whl/bb_openpyxl/tests/insert_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@author: hubo
+@project: bb-py
+@file: insert_test.py
+@time: 2023/12/22 14:15
+@desc:
+"""
+from openpyxl import Workbook, load_workbook
+from .. import patch_all
+patch_all()
+
+
+def test_insert_cols():
+    wb = load_workbook('bb_openpyxl/tests/test.xlsx')
+    ws = wb.worksheets[0]
+    # ws.insert_rows(6)
+    ws.insert_cols(2, amount=2)
+    wb.save('bb_openpyxl/tests/out.xlsx')
+
+
+def test_insert_rows():
+    wb = load_workbook('bb_openpyxl/tests/test.xlsx')
+    ws = wb.worksheets[0]
+    # ws.insert_rows(6)
+    # ws.insert_rows(4)
+    ws.insert_rows(4)
+    wb.save('bb_openpyxl/tests/out.xlsx')
+
+
+
