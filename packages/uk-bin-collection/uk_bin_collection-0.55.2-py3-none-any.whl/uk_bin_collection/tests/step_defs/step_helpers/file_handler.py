33ba7b03361b9a4daf24a7883d6ac78109742ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 03:06:11 +0000

    Add package uk-bin-collection/0.55.2/uk_bin_collection-0.55.2-py3-none-any.whl

---
+++ packages/uk-bin-collection/uk_bin_collection-0.55.2-py3-none-any.whl/uk_bin_collection/tests/step_defs/step_helpers/file_handler.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import os
+from jsonschema import validate, ValidationError
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+
+def load_inputs_file(file_name):
+    cwd = os.getcwd()
+    with open(os.path.join(cwd, "uk_bin_collection", "tests", file_name)) as f:
+        data = json.load(f)
+        logging.info(f"{file_name} Input file loaded")
+    return data
+
+
+def load_schema_file(file_name):
+    cwd = os.getcwd()
+    with open(os.path.join(cwd, "uk_bin_collection", "tests", file_name)) as f:
+        data = json.load(f)
+        logging.info(f"{file_name} Schema file loaded")
+    return data
+
+
+def validate_json(json_str):
+    try:
+        json.loads(json_str)
+    except ValueError as err:
+        logging.info(f"The following error occured {err}")
+        return False
+    return True
+
+
+def validate_json_schema(json_str, schema):
+    json_data = json.loads(json_str)
+    try:
+        validate(instance=json_data, schema=schema)
+    except ValidationError as err:
+        logging.info(f"The following error occured {err}")
+        logging.info(f"Data: {json_str}")
+        logging.info(f"Schema: {schema}")
+        return False
+    return True
