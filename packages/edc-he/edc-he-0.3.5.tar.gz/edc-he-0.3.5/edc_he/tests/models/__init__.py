a84990a0e7e956d818988ec4a664c7f4d88c183b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-06 15:04:57 +0000

    Add package edc-he/0.3.5/edc-he-0.3.5.tar.gz

---
+++ packages/edc-he/edc-he-0.3.5.tar.gz/edc-he-0.3.5/edc_he/tests/models/__init__.py
@@ -0,0 +1,6 @@
+from .assets import HealthEconomicsAssets
+from .health_economics import HealthEconomics
+from .household_head import HealthEconomicsHouseholdHead
+from .income import HealthEconomicsIncome
+from .patient import HealthEconomicsPatient
+from .property import HealthEconomicsProperty
