76eb1b200a8b0860a44e2060deb889d2555bef32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:09 +0000

    Add package lyyprocess/0.11/lyyprocess-0.11.tar.gz

---
+++ packages/lyyprocess/lyyprocess-0.11.tar.gz/lyyprocess-0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lyyprocess',
+    version='0.11',
+    author='lyy',
+    author_email='',
+    description='lyy process tools for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        'psutil',
+    ],
+)
