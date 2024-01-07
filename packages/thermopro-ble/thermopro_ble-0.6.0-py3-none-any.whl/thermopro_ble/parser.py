716faf74274c443dada8620f17a42cfe6d1ac982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:04:58 +0000

    Add package thermopro-ble/0.6.0/thermopro_ble-0.6.0-py3-none-any.whl

---
+++ packages/thermopro-ble/thermopro_ble-0.6.0-py3-none-any.whl/thermopro_ble/parser.py
@@ -0,0 +1,74 @@
+"""Parser for ThermoPro BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/thermopro.py
+
+MIT License applies.
+"""
+from __future__ import annotations
+
+import logging
+from struct import Struct
+
+from bluetooth_data_tools import short_address
+from bluetooth_sensor_state_data import BluetoothData
+from home_assistant_bluetooth import BluetoothServiceInfo
+from sensor_state_data import SensorLibrary
+
+_LOGGER = logging.getLogger(__name__)
+
+
+BATTERY_VALUE_TO_LEVEL = {
+    0: 1,
+    1: 50,
+    2: 100,
+}
+
+UNPACK = Struct("<hB").unpack
+
+
+class ThermoProBluetoothDeviceData(BluetoothData):
+    """Date update for ThermoPro Bluetooth devices."""
+
+    def _start_update(self, service_info: BluetoothServiceInfo) -> None:
+        """Update from BLE advertisement data."""
+        _LOGGER.debug("Parsing thermopro BLE advertisement data: %s", service_info)
+        if not service_info.name.startswith(("TP35", "TP39")):
+            return
+        if not service_info.manufacturer_data:
+            return
+        if len(list(service_info.manufacturer_data.values())[0]) < 4:
+            return
+        self.set_device_type(service_info.name.split(" ")[0])
+        self.set_title(f"{service_info.name} {short_address(service_info.address)}")
+        self.set_device_name(service_info.name)
+        self.set_precision(2)
+        self.set_device_manufacturer("ThermoPro")
+        changed_manufacturer_data = self.changed_manufacturer_data(service_info)
+
+        if not changed_manufacturer_data or len(changed_manufacturer_data) > 1:
+            # If len(changed_manufacturer_data) > 1 it means we switched
+            # ble adapters so we do not know which data is the latest
+            # and we need to wait for the next update.
+            return
+
+        last_id = list(changed_manufacturer_data)[-1]
+        data = (
+            int(last_id).to_bytes(2, byteorder="little")
+            + changed_manufacturer_data[last_id]
+        )
+
+        if len(data) < 6:
+            return
+
+        (temp, humi) = UNPACK(data[1:4])
+
+        # TP357S seems to be in 6, TP397 and TP393 in 4
+        battery_byte = data[6] if len(data) == 7 else data[4]
+        if battery_byte in BATTERY_VALUE_TO_LEVEL:
+            self.update_predefined_sensor(
+                SensorLibrary.BATTERY__PERCENTAGE,
+                BATTERY_VALUE_TO_LEVEL[battery_byte],
+            )
+        self.update_predefined_sensor(SensorLibrary.TEMPERATURE__CELSIUS, temp / 10)
+        self.update_predefined_sensor(SensorLibrary.HUMIDITY__PERCENTAGE, humi)
