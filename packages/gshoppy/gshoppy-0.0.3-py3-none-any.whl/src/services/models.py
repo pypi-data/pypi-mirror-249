65ebc2d537dc2b719f6ca7a18aff056084a66e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:04:52 +0000

    Add package gshoppy/0.0.3/gshoppy-0.0.3-py3-none-any.whl

---
+++ packages/gshoppy/gshoppy-0.0.3-py3-none-any.whl/src/services/models.py
@@ -0,0 +1,17 @@
+"""
+models.py contains the models for the application.
+"""
+from enum import Enum
+from typing import Final
+
+import src.constants as constants
+
+
+class ProductAvailability(str, Enum):
+    """
+    The product availability.
+    """
+
+    IN_STOCK: Final[str] = constants.AVAILABILITY_IN_STOCK
+    OUT_OF_STOCK: Final[str] = constants.AVAILABILITY_OUT_OF_STOCK
+    PREORDER: Final[str] = constants.AVAILABILITY_PREORDER
