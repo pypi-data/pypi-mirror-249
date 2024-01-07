250946fd55eb048e256ac282c33fa3d6b4f6a0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:52 +0000

    Add package inm-distributions/0.1/inm_distributions-0.1.tar.gz

---
+++ packages/inm-distributions/inm_distributions-0.1.tar.gz/inm_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='inm_distributions',
+      version='0.1',
+      description='This package provides classes for various statistical distributions, including Gaussian and Binomial distributions. It includes methods for calculating mean, standard deviation, probability density function, and more.',
+      packages=['inm_distributions'],
+      author = 'Isaac Ndirangu Muturi',
+      author_email = 'ndirangumuturi749@gmail.com',
+      zip_safe=False)
