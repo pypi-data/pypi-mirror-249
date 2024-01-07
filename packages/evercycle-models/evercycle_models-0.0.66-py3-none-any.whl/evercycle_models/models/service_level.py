1969cc9d781aac4ca8043ba7dd9ad3bddfcdb81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:13 +0000

    Add package evercycle-models/0.0.66/evercycle_models-0.0.66-py3-none-any.whl

---
+++ packages/evercycle-models/evercycle_models-0.0.66-py3-none-any.whl/evercycle_models/models/service_level.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .carrier_type import CarrierType
+
+
+class ServiceLevel(models.Model):
+    name = models.CharField(max_length=100)
+    f_id = models.CharField(max_length=100)
+    carrier_type = models.ForeignKey(CarrierType, models.DO_NOTHING)
