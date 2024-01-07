52525dd5933f6ce87afe82118ee362e1ad65cad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:04:12 +0000

    Add package example-package-qi2017-20240106/0.0.1/example_package_qi2017_20240106-0.0.1-py3-none-any.whl

---
+++ packages/example-package-qi2017-20240106/example_package_qi2017_20240106-0.0.1-py3-none-any.whl/example_package/example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File  : example.py
+@Author: crawlSpider
+@Address: https://weixin.sogou.com/weixin?type=1&s_from=input&query=%E7%BD%91%E8%99%ABspider&ie=utf8&_sug_=n&_sug_type_=
+@Date  : 2021/7/24 上午12:57
+@Desc  : 
+"""
+
+
+def add_one(number):
+    return number + 1.
+
+
+if __name__ == '__main__':
+    pass
