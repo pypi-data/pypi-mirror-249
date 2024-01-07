0536814de5722c0841516003efb4b18cbcd748ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:05:15 +0000

    Add package buff163-unofficial-api/0.1.3/buff163-unofficial-api-0.1.3.tar.gz

---
+++ packages/buff163-unofficial-api/buff163-unofficial-api-0.1.3.tar.gz/buff163-unofficial-api-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="buff163-unofficial-api",
+    version="0.1.3",
+    author="Mark Zhdan",
+    author_email="markzhdan@gmail.com",
+    description="An unofficial API wrapper for Buff163, a CS skin marketplace.",
+    url="https://github.com/markzhdan/buff163-unofficial-api",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
