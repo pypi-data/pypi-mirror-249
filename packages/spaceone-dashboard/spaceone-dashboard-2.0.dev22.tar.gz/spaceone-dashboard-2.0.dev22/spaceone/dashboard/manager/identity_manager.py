193765ae5203496bcbb89a17333d0784b5d78905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:05 +0000

    Add package spaceone-dashboard/2.0.dev22/spaceone-dashboard-2.0.dev22.tar.gz

---
+++ packages/spaceone-dashboard/spaceone-dashboard-2.0.dev22.tar.gz/spaceone-dashboard-2.0.dev22/spaceone/dashboard/manager/identity_manager.py
@@ -0,0 +1,21 @@
+from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
+
+
+class IdentityManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_conn: SpaceConnector = self.locator.get_connector(
+            "SpaceConnector", service="identity"
+        )
+
+    def check_workspace(self, workspace_id: str, domain_id: str) -> None:
+        system_token = self.transaction.get_meta("token")
+        return self.identity_conn.dispatch(
+            "Workspace.check",
+            {"workspace_id": workspace_id, "domain_id": domain_id},
+            token=system_token,
+        )
+
+    def get_project(self, project_id: str) -> dict:
+        return self.identity_conn.dispatch("Project.get", {"project_id": project_id})
