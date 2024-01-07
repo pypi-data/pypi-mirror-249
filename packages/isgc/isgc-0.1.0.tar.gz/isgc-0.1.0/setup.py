eb0fa9b61185070ce37bc141de9cb9b2ae2710aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 15:04:28 +0000

    Add package isgc/0.1.0/isgc-0.1.0.tar.gz

---
+++ packages/isgc/isgc-0.1.0.tar.gz/isgc-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isgc',
+    version='0.1.0',
+    author='Ahsan Tariq',
+    author_email='ahsantariq0724@gmail.com',
+    description='A SGPA and CGPA Calculator for Islamia University Bahawalpur Graduation Students under supervision of Malik Shahzad, a Senior Developer. Also thankful for departmental support from M. Bux Alvi Sahb and Engr. Mubashir Hussain. Also very thankful to Sundas Tariq and Laiba Saleem for contributing to this project and making it usable for all IUB Graduation Students.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ahsantariq7/isgc',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    keywords=['GPA calculator education','Islamia University Bahawalpur', 'Ahsan Tariq','Ahsan Tariq 0724'],
+    install_requires=[
+        'math',
+        'matplotlib',
+        'seaborn',
+        'tabulate',
+    ],
+    entry_points={
+        'console_scripts': [
+            'calculate_gpa=isgc.gpa_calculator.calculator:calculate_gpa',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/ahsantariq7/isgc/issues',
+        'Source': 'https://github.com/ahsantariq7/isgc',
+    },
+)
