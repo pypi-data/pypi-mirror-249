c94d254a855c48bea4f0cf2e8efc3e4ff936072c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:30 +0000

    Add package wheke/0.0.1a11/wheke-0.0.1a11-py3-none-any.whl

---
+++ packages/wheke/wheke-0.0.1a11-py3-none-any.whl/wheke/__init__.py
@@ -0,0 +1,10 @@
+from wheke.core import Wheke
+from wheke.pod import Pod
+from wheke.service import aget_service, get_service
+
+__all__ = [
+    "Pod",
+    "Wheke",
+    "aget_service",
+    "get_service",
+]
