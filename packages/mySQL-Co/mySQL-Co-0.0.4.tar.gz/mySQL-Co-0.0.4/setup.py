a315ec67cb90350dc2d4ba744f381854f2251464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:04:37 +0000

    Add package mySQL-Co/0.0.4/mySQL-Co-0.0.4.tar.gz

---
+++ packages/mySQL-Co/mySQL-Co-0.0.4.tar.gz/mySQL-Co-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mySQL-Co',
+    version='0.0.4',
+    author='Kasra-Khaksar',
+    author_email='ksra13khaksar@gmail.com',
+    description='This is mysql saver , partition , set primary key and update with dataframe',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
