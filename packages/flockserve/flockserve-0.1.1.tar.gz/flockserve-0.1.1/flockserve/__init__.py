26db2a41fc3bb4e364072af36fb729b3ad3fec3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:12 +0000

    Add package flockserve/0.1.1/flockserve-0.1.1.tar.gz

---
+++ packages/flockserve/flockserve-0.1.1.tar.gz/flockserve-0.1.1/flockserve/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for flockserve."""
+
+__author__ = """ Antti Puurula, Anil Gurbuz"""
+__email__ = 'antti@jdoodle.com, anl_grbz@outlook.com'
+__version__ = '0.1.0'
+
+from flockserve.config import *
+from flockserve.run import *
+from flockserve.worker_handler import *
+from flockserve.worker_manager import *
+from flockserve.flockserver import *
+from flockserve.flockserve_app import *
