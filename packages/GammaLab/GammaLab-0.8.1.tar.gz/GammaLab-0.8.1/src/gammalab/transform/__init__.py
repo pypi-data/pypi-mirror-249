b196f28e4c3897517abb1c17820a716f95452483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:49 +0000

    Add package GammaLab/0.8.1/GammaLab-0.8.1.tar.gz

---
+++ packages/GammaLab/GammaLab-0.8.1.tar.gz/GammaLab-0.8.1/src/gammalab/transform/__init__.py
@@ -0,0 +1,5 @@
+from .simple import (
+                    DownSampleMaxed, Normalize, 
+                    Float2Int16, Int162Float
+                    ) 
+from .calibration import Scale, Interpolate, SecondOrder
