c2b2eec601bdc68bb53e0027da253360b6e2d676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:05:08 +0000

    Add package BCEmbedding/0.1.1/BCEmbedding-0.1.1-py3-none-any.whl

---
+++ packages/BCEmbedding/BCEmbedding-0.1.1-py3-none-any.whl/BCEmbedding/models/__init__.py
@@ -0,0 +1,9 @@
+'''
+@Description: 
+@Author: shenlei
+@Date: 2023-11-29 12:34:35
+@LastEditTime: 2024-01-04 17:19:07
+@LastEditors: shenlei
+'''
+from .embedding import EmbeddingModel
+from .reranker import RerankerModel
