d84f71b34ce3b1dcb3fdd45c4c29f7f540703532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:13 +0000

    Add package evercycle-models/0.0.66/evercycle_models-0.0.66.tar.gz

---
+++ packages/evercycle-models/evercycle_models-0.0.66.tar.gz/evercycle_models-0.0.66/evercycle_models/models/carrier_type.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class CarrierType(models.Model):
+    name = models.CharField(max_length=50)
+    f_id = models.CharField(max_length=100)
+    account_type_id = models.CharField(max_length=50)
+    account_type = models.CharField(max_length=50)
