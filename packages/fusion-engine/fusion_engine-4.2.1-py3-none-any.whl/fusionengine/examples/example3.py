93f0fd87de1ec531fcf2c5f6b19259f5b34818d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:05:14 +0000

    Add package fusion-engine/4.2.1/fusion_engine-4.2.1-py3-none-any.whl

---
+++ packages/fusion-engine/fusion_engine-4.2.1-py3-none-any.whl/fusionengine/examples/example3.py
@@ -0,0 +1,29 @@
+import fusionengine as fusion
+
+window = fusion.Window("Example: 3", 800, 600)
+
+player = fusion.Entity(window, 50, 50, 50, 50)
+
+
+speed = 10
+
+
+@window.loop
+def loop():
+    fusion.set_background_color(window, fusion.WHITE)
+
+    player.load_rect(fusion.AQUA)
+
+    if fusion.Key(fusion.KEY_UP).key_down():
+        player.y = int(player.y - speed)
+
+    elif fusion.Key(fusion.KEY_DOWN).key_down():
+        player.y = int(player.y + speed)
+
+    elif fusion.Key(fusion.KEY_RIGHT).key_down():
+        player.x = int(player.x + speed)
+
+    elif fusion.Key(fusion.KEY_LEFT).key_down():
+        player.x = int(player.x - speed)
+
+    player.draw_rect()
