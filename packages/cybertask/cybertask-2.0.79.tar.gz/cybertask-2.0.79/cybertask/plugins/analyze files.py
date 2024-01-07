b2d14cc2b2d4c755a7ca339b33dce27b4d5164d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:16 +0000

    Add package cybertask/2.0.79/cybertask-2.0.79.tar.gz

---
+++ packages/cybertask/cybertask-2.0.79.tar.gz/cybertask-2.0.79/cybertask/plugins/analyze files.py
@@ -0,0 +1,56 @@
+"""
+LetMeDoIt AI Plugin - analyze files
+
+analyze files with integrated "AutoGen Retriever"
+
+[FUNCTION_CALL]
+"""
+
+
+from cybertask import config
+import os
+from cybertask.autoretriever import AutoGenRetriever
+
+def analyze_files(function_args):
+    query = function_args.get("query") # required
+    files = function_args.get("files") # required
+    if os.path.exists(files):
+        if os.path.isfile(files) and SharedUtil.is_valid_image_file(files):
+            # call function "analyze image" instead if it is an image
+            function_args = {
+                "query": query,
+                "files": [files],
+            }
+            config.print3("Running function: 'analyze_images'")
+            return config.chatGPTApiAvailableFunctions["analyze_images"](function_args)
+        config.stopSpinning()
+        config.print2("AutoGen Retriever launched!")
+        last_message = AutoGenRetriever().getResponse(files, query)
+        config.currentMessages += last_message
+        config.print2("AutoGen Retriever closed!")
+        return ""
+
+    return "[INVALID]"
+
+functionSignature = {
+    "name": "analyze_files",
+    "description": "retrieve information from files",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "Detailed queries about the given files",
+            },
+            "files": {
+                "type": "string",
+                "description": """Return a directory or non-image file path. Return an empty string '' if it is not given.""",
+            },
+        },
+        "required": ["query", "files"],
+    },
+}
+
+config.pluginsWithFunctionCall.append("analyze_files")
+config.chatGPTApiFunctionSignatures.append(functionSignature)
+config.chatGPTApiAvailableFunctions["analyze_files"] = analyze_files
