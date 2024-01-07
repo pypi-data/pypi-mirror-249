c62fe60222451ac4710ff93289020131c2477afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:05:12 +0000

    Add package brunch/0.1.1/brunch-0.1.1.tar.gz

---
+++ packages/brunch/brunch-0.1.1.tar.gz/brunch-0.1.1/setup.py
@@ -0,0 +1,54 @@
+
+'''
+	pip install --upgrade setuptools
+
+	(rm -rf dist && python3 -m build --sdist && twine upload dist/*)
+'''
+#
+#	https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+#
+#	https://github.com/pypa/sampleproject/blob/db5806e0a3204034c51b1c00dde7d5eb3fa2532e/setup.py
+#
+from setuptools import setup, find_packages
+
+version = "0.1.1"
+name = 'brunch'
+install_requires = [
+	'homeostasis',
+
+	'shares',
+	'ships',
+	'flask',
+
+	'jsonschema',
+	'rich'
+]
+
+def scan_description ():
+	try:
+		with open ('structures/modules/brunch/brunch.s.HTML') as f:
+			return f.read ()
+
+	except Exception as E:
+		pass;
+		
+	return '';
+
+
+setup (
+    version = version,
+
+	name = name,
+    install_requires = install_requires,	
+	
+	package_dir = { 
+		'brunch': 'structures/modules/brunch'
+	},
+	scripts = [ 
+		'restaurant/scripts/brunch' 
+	],
+	
+	license = "CL",
+	long_description = scan_description (),
+	long_description_content_type = "text/plain",
+)
