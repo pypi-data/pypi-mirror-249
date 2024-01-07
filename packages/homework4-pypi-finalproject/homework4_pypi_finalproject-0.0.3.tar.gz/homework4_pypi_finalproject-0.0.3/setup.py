01a2f7052d7117b3ad7e028bb26caca972d8afee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:08 +0000

    Add package homework4-pypi-finalproject/0.0.3/homework4_pypi_finalproject-0.0.3.tar.gz

---
+++ packages/homework4-pypi-finalproject/homework4_pypi_finalproject-0.0.3.tar.gz/homework4_pypi_finalproject-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="homework4_pypi_finalproject",
+    version="0.0.3",
+    packages=find_packages(),
+    
+    long_description=description,
+    long_description_content_type= "text/markdown",
+)
