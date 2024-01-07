445de54864a51b6850ff0d21728e189fa55411bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:13 +0000

    Add package geoclip/1.0.13/geoclip-1.0.13.tar.gz

---
+++ packages/geoclip/geoclip-1.0.13.tar.gz/geoclip-1.0.13/geoclip/model/image_encoder.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from transformers import CLIPModel, AutoProcessor
+from transformers.utils import logging as transformers_logging
+
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        super(ImageEncoder, self).__init__()
+        self.CLIP = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+
+        transformers_logging.disable_default_handler()
+        transformers_logging.enable_progress_bar()
+        
+        self.image_processor = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14")
+        self.mlp = nn.Sequential(nn.Linear(768, 768),
+                                 nn.ReLU(),
+                                 nn.Linear(768, 512))
+
+        # Freeze the CLIP model
+        for param in self.CLIP.parameters():
+            param.requires_grad = False
+
+    def preprocess_image(self, image):
+        x = self.image_processor(images=image, return_tensors="pt")["pixel_values"]
+        return x
+
+    def forward(self, x):
+        x = self.CLIP.get_image_features(pixel_values=x)
+        x = self.mlp(x)
+        return x
