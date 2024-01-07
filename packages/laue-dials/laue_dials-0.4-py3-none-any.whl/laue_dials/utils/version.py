eed74be8575b765424e99f70e792c6b4b9b94371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:04:52 +0000

    Add package laue-dials/0.4/laue_dials-0.4-py3-none-any.whl

---
+++ packages/laue-dials/laue_dials-0.4-py3-none-any.whl/laue_dials/utils/version.py
@@ -0,0 +1,29 @@
+# Get package versioning information
+import os
+from importlib.metadata import version
+
+
+def laue_version():
+    """
+    Print the versions of DIALS and laue-dials packages.
+
+    This function retrieves the versions of the DIALS and laue-dials packages and prints
+    them to the terminal.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    try:
+        width = os.get_terminal_size().columns
+    except OSError:
+        width = 65
+    dials_version = version("dials")
+    laue_dials_version = version("laue-dials")
+
+    print("-" * width)
+    print(f"DIALS version " + dials_version + ".")
+    print(f"laue-dials version " + laue_dials_version + ".")
+    print("-" * width)
