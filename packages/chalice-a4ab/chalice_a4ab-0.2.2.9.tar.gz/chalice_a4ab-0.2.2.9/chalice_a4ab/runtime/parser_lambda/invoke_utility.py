bfa29c2b3b86dfcd96b442951bbeaf0c97538def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:06:36 +0000

    Add package chalice-a4ab/0.2.2.9/chalice_a4ab-0.2.2.9.tar.gz

---
+++ packages/chalice-a4ab/chalice_a4ab-0.2.2.9.tar.gz/chalice_a4ab-0.2.2.9/chalice_a4ab/runtime/parser_lambda/invoke_utility.py
@@ -0,0 +1,72 @@
+from chalice_a4ab.model_types import ModelTypes
+from chalice_a4ab.runtime.models.parser_lambda import (
+    ParserLambdaResponseModel,
+    PromptType,
+)
+from chalice_a4ab.runtime.parser_lambda.agents_parser import (
+    AgentsParserFunction,
+    is_agents_parser_instance,
+)
+from chalice_a4ab.runtime.parser_lambda.amazon_example.pre_processing import (
+    lambda_handler as pre_processing_lambda_handler,
+)
+from chalice_a4ab.runtime.parser_lambda.amazon_example.orchestration import (
+    lambda_handler as orchestration_lambda_handler,
+)
+from chalice_a4ab.runtime.parser_lambda.amazon_example.v2_1.orchestration import (
+    lambda_handler as orchestration_lambda_handler_v2_1,
+)
+from chalice_a4ab.runtime.parser_lambda.exceptions import create_response_from_expception
+from chalice_a4ab.runtime.pydantic_tool.utility import PydanticUtility as u
+
+
+def _invoke_agents_with_function(event, context, parser_function, handler) -> dict:
+    """
+    Invoke agents parser function(Common process)
+    """
+    parsed = parser_function(event, context)
+    parsed_model = u(ParserLambdaResponseModel).parse_obj(parsed)
+    handled_parsed_model = handler(event, parsed_model)
+    if isinstance(handled_parsed_model, ParserLambdaResponseModel):
+        # Return response from pydantic model
+        return u(handled_parsed_model).dict(by_alias=True, exclude_none=True)
+    # Return response from handler
+    return handled_parsed_model
+
+
+def is_handle_event_agents_parser(target: AgentsParserFunction, event: dict, context: dict) -> bool:
+    if is_agents_parser_instance(target) is False:
+        # call from mixin class
+        return False
+    for handler in target._parser_function_handlers:
+        if handler.is_handle_event(event, context):
+            return True
+    return False
+
+
+def invoke_agents_parser(target: AgentsParserFunction, event: dict, context: dict):
+    """
+    Invoke agents parser function
+    """
+    if is_agents_parser_instance(target) is False:
+        # call from mixin class
+        return False
+    failed_event = {}
+    try:
+        for handler in target._parser_function_handlers:
+            if handler.is_handle_event(event, context):
+                if handler._prompt_type == PromptType.PRE_PROCESSING:
+                    # Prompt Type : Pre Processing
+                    return _invoke_agents_with_function(event, context, pre_processing_lambda_handler, handler)
+                if handler._prompt_type == PromptType.ORCHESTRATION:
+                    if handler._foundation_model == ModelTypes.CLAUDE_V2_1:
+                        # Model Version : Claude V2.1
+                        # And, Prompt Type : Orchestration
+                        return _invoke_agents_with_function(event, context, orchestration_lambda_handler_v2_1, handler)
+                    else:
+                        # Model Version : Claude V2
+                        # And, Prompt Type : Orchestration
+                        return _invoke_agents_with_function(event, context, orchestration_lambda_handler, handler)
+    except Exception as e:
+        failed_event = create_response_from_expception(event, e)
+    return failed_event
