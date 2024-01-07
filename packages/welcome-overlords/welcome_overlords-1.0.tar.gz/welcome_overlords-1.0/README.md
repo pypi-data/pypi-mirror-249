c471371aeaaaf7c7394ad2f41895e29ba906c1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:05:13 +0000

    Add package welcome-overlords/1.0/welcome_overlords-1.0.tar.gz

---
+++ packages/welcome-overlords/welcome_overlords-1.0.tar.gz/welcome_overlords-1.0/README.md
@@ -0,0 +1,13 @@
+# Welcome, Overlords!
+
+Welcome your new *function name* overlords by using this module.
+
+This module introduces the `@Overlord` (not overload with an 'a'!)
+annotation. It only welcomes your new overloards and then returns
+the function/class/object it has been applied to.
+
+This module is not only funny because of the associations with the
+OOP concept of overloading and the famous The Simpsons meme, but
+can also be considered essential for basically any project, as
+this annotation can be quite useful for certain debugging
+situations.
---
+++ packages/welcome-overlords/welcome_overlords-1.0.tar.gz/welcome_overlords-1.0/src/welcome_overlords/__init__.py
@@ -0,0 +1,20 @@
+"""
+    welcome_overlords.py
+    -----------
+
+    Welcome your new {function name} overlords by using this module.
+
+    This module introduces the @Overlord (not overload with an 'a'!)
+    annotation. It only welcomes your new overloards and then returns
+    the function/class/object it has been applied to.
+
+    This module is not only funny because of the associations with the
+    OOP concept of overloading and the famous The Simpsons meme, but
+    can also be considered essential for basically any project, as
+    this annotation can be quite useful for certain debugging
+    situations.
+"""
+
+def Overlord(obj):
+    print(f"I, for one, welcome our new {obj.__name__} overlords.")
+    return obj
