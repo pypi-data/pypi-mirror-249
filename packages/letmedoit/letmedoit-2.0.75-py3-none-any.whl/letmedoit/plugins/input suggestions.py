11a02cf299676ab814bf1c19459babb0b22e4bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:21 +0000

    Add package letmedoit/2.0.75/letmedoit-2.0.75-py3-none-any.whl

---
+++ packages/letmedoit/letmedoit-2.0.75-py3-none-any.whl/letmedoit/plugins/input suggestions.py
@@ -0,0 +1,40 @@
+"""
+LetMeDoIt AI Plugin - input suggestions
+
+add input suggestions
+"""
+
+from letmedoit import config
+import sys, os
+
+config.inputSuggestions += [
+        "[CHAT] ",
+        "CHAT] ",
+        "[CHAT_chatgpt] ",
+        "CHAT_chatgpt] ",
+        "[CHAT_geminipro] ",
+        "CHAT_geminipro] ",
+        "[CHAT_palm2] ",
+        "CHAT_palm2] ",
+        "[CHAT_codey] ",
+        "CHAT_codey] ",
+        "[NO_FUNCTION_CALL] ",
+        "NO_FUNCTION_CALL] ",
+        f"!{config.open} ",
+        config.open,
+        f"!{sys.executable} ",
+        sys.executable,
+        "open ",
+        "read ",
+        "search ",
+        "analyze ",
+        "tell me about ",
+        "Craft a prompt for ChatGPT that outlines the necessary steps it should take to complete the following task at hand:\n[NO_FUNCTION_CALL]\n",
+        f"Improve the following content according to {config.improvedWritingSytle}:\n[NO_FUNCTION_CALL]\n",
+        "Before you start, please ask me any questions you have about this so I can give you more context. Be extremely comprehensive.",
+    ]
+
+config.inputSuggestions.append("""Translate Content. Assist me by acting as a translator. Once I have provided you with the content, you should inquire about the language I need it translated into. After I inform you of the desired language, proceed with the translation.
+[NO_FUNCTION_CALL]
+Please translate the content below:
+""")
