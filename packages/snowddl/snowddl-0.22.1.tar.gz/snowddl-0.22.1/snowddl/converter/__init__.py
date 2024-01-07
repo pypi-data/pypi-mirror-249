42873be84c057b9b8fcfee62907a17fcc16c8b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:04:57 +0000

    Add package snowddl/0.22.1/snowddl-0.22.1.tar.gz

---
+++ packages/snowddl/snowddl-0.22.1.tar.gz/snowddl-0.22.1/snowddl/converter/__init__.py
@@ -0,0 +1,17 @@
+from ._yaml import SnowDDLDumper, YamlFoldedStr, YamlLiteralStr
+from .abc_converter import AbstractConverter, ConvertResult
+from .abc_schema_object_converter import AbstractSchemaObjectConverter
+from .database import DatabaseConverter
+from .schema import SchemaConverter
+from .sequence import SequenceConverter
+from .table import TableConverter
+from .view import ViewConverter
+
+
+default_converter_sequence = [
+    DatabaseConverter,
+    SchemaConverter,
+    SequenceConverter,
+    TableConverter,
+    ViewConverter,
+]
---
+++ packages/snowddl/snowddl-0.22.1.tar.gz/snowddl-0.22.1/snowddl/parser/sequence.py
@@ -0,0 +1,36 @@
+from snowddl.blueprint import SequenceBlueprint, SchemaObjectIdent
+from snowddl.parser.abc_parser import AbstractParser, ParsedFile
+
+
+# fmt: off
+sequence_json_schema = {
+    "type": "object",
+    "properties": {
+        "start": {
+            "type": "integer"
+        },
+        "interval": {
+            "type": "integer"
+        },
+        "comment": {
+            "type": "string"
+        }
+    },
+    "additionalProperties": False
+}
+# fmt: on
+
+
+class SequenceParser(AbstractParser):
+    def load_blueprints(self):
+        self.parse_schema_object_files("sequence", sequence_json_schema, self.process_sequence)
+
+    def process_sequence(self, f: ParsedFile):
+        bp = SequenceBlueprint(
+            full_name=SchemaObjectIdent(self.env_prefix, f.database, f.schema, f.name),
+            start=f.params.get("start", 1),
+            interval=f.params.get("interval", 1),
+            comment=f.params.get("comment"),
+        )
+
+        self.config.add_blueprint(bp)
