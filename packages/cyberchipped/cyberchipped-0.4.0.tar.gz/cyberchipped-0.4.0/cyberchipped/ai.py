26c31943e8a74e3232ec4ae688d84f4f0d700224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:05:05 +0000

    Add package cyberchipped/0.4.0/cyberchipped-0.4.0.tar.gz

---
+++ packages/cyberchipped/cyberchipped-0.4.0.tar.gz/cyberchipped-0.4.0/cyberchipped/ai.py
@@ -0,0 +1,4 @@
+from cyberchipped.components.ai_function import ai_fn as fn
+from cyberchipped.components.ai_model import ai_model as model
+
+__all__ = ["fn", "model"]
---
+++ packages/cyberchipped/cyberchipped-0.4.0.tar.gz/cyberchipped-0.4.0/cyberchipped/components/__init__.py
@@ -0,0 +1,11 @@
+from .ai_function import ai_fn, AIFunction
+from .ai_model import ai_model
+from .prompt import prompt_fn, PromptFunction
+
+__all__ = [
+    "ai_fn",
+    "ai_model",
+    "prompt_fn",
+    "AIFunction",
+    "PromptFunction",
+]
