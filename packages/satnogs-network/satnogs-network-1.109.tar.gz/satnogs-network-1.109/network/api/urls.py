c05ad758865f57d3f202d55c0d6771b4243cf555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:24 +0000

    Add package satnogs-network/1.109/satnogs-network-1.109.tar.gz

---
+++ packages/satnogs-network/satnogs-network-1.109.tar.gz/satnogs-network-1.109/network/api/urls.py
@@ -0,0 +1,15 @@
+"""SatNOGS Network django rest framework API url routings"""
+from django.urls import path
+from rest_framework import routers
+
+from network.api import views
+
+ROUTER = routers.DefaultRouter()
+
+ROUTER.register(r'jobs', views.JobView, basename='jobs')
+ROUTER.register(r'observations', views.ObservationView, basename='observations')
+ROUTER.register(r'stations', views.StationView, basename='stations')
+ROUTER.register(r'configuration', views.StationConfigurationView, basename='configuration')
+ROUTER.register(r'transmitters', views.TransmitterView, basename='transmitters')
+
+API_URLPATTERNS = ROUTER.urls + [path('station/register', views.station_register_view)]
