9975b8a6589d9d58b97428644dc643f39b2b6d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:49 +0000

    Add package GammaLab/0.8.0/GammaLab-0.8.0.tar.gz

---
+++ packages/GammaLab/GammaLab-0.8.0.tar.gz/GammaLab-0.8.0/src/gammalab/analysis/count.py
@@ -0,0 +1,78 @@
+import sys
+import pickle
+import numpy
+
+from ..service import ReceivingService, ThreadService, SourceService
+from ..wire import PulseWire, CountWire
+      
+class Count(ThreadService, ReceivingService, SourceService):
+    input_wire_class=PulseWire
+    output_wire_class=CountWire
+    def __init__(self, outfile=None, runtime=None, interval=1., silent=False):
+        super().__init__()
+        self.outfile=outfile
+        self.runtime=runtime
+        self.tmax=2*interval
+        self.tmin=0
+        self.interval=interval
+        self.total_count=0
+        self.total_time=0
+        self.recent_pulse_times=[] # all pulse times from last interval
+        self.silent=silent
+    
+    @property
+    def nbins(self):
+        return int((self.tmax-self.tmin)/self.interval)
+        
+    def start_process(self):
+        self.cps, self.tbins=numpy.histogram([], bins=self.nbins, 
+            range=(self.tmin,self.tmax))
+        super().start_process() 
+    
+    def process(self, data):      
+
+        self.total_time=data["total_time"]
+        self.total_count+=len(data["pulses"])
+
+        if self.tmax<self.total_time:
+            self.tmax=self.tmax*2
+        if self.runtime and self.tmax>self.runtime:
+            self.tmax=self.runtime
+
+        _cps=self.cps
+
+        pulse_times=[d[0] for d in data["pulses"]]
+        self.recent_pulse_times.extend(pulse_times)
+        self.recent_pulse_times=[t for t in self.recent_pulse_times if t>self.total_time-self.interval]
+        pulse_times=numpy.array(pulse_times)
+
+        counts, self.tbins=numpy.histogram(pulse_times, bins=self.nbins, 
+            range=(self.tmin,self.tmax))
+
+        self.cps=counts/self.interval
+        self.cps[0:len(_cps)]+=_cps
+
+        avgcps=self.total_count/(self.total_time)
+        if self.total_time>0:
+          cps=len(self.recent_pulse_times)/min(self.total_time, self.interval)
+        else:
+          cps=0
+
+        message=f"time: {self.total_time:.2f}, cps: {cps:.0f}, average: {avgcps:5.2f}, total: {self.total_count}"
+        if not self.silent:
+            self.print_message(message, end="\r")
+        
+        return self.outdata
+    
+    @property            
+    def outdata(self):
+        return dict(count_per_sec=self.cps, time_bins=self.tbins, total_time=self.total_time, interval=self.interval)
+      
+                
+    def cleanup(self):
+        if self.outfile is not None:
+            outfile=self.outfile+".pkl"
+            with open(outfile,"wb") as f:
+                pickle.dump(self.outdata,f)
+            self.print_message(f"Data written to {outfile}")
+        super().cleanup()
---
+++ packages/GammaLab/GammaLab-0.8.0.tar.gz/GammaLab-0.8.0/src/gammalab/service.py
@@ -0,0 +1,177 @@
+from . import all_services, shared_output
+
+import multiprocessing
+import ctypes
+import time
+
+class ServiceError(Exception):
+    pass
+
+class Service:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        all_services.add(self)
+        self._output=shared_output
+    
+    def start(self):
+        raise Exception("not implemented for %s"%str(self))
+    def stop(self):
+        raise Exception("not implemented for %s"%str(self))
+    def close(self):
+        raise Exception("not implemented for %s"%str(self))
+    def plugs_into(self, other):
+        raise ServiceError("service %s has no outputs"%str(self))
+    def connect_to(self, other):
+        raise ServiceError("service %s has no inputs"%str(self))
+    def print_message(self, message, end=None):
+        message=f'\33[2K[{self.__class__.__name__}] '+message
+        message=dict(message=message,end=end)
+        self._output.put(message)
+
+class SourceService(Service):
+    output_wire_class=None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.output_wires=[]
+        if self.output_wire_class is None:
+            raise Exception(f"SourceService {self.__class__.__name__} does not specify output_wire_class")
+
+    def output_protocol(self, wire):
+        assert isinstance(wire, self.output_wire_class)
+
+    def connect(self, wire):
+        self.output_protocol(wire)
+        self.output_wires.append(wire)
+
+    def send_output(self,data):
+        for w in self.output_wires:
+            try:
+                w.put_nowait(data)
+            except:
+                self.print_message("%s buffer full"%str(self))
+
+    def plugs_into(self, other):
+        try:
+          other.connect_input(self)
+        except ServiceError as ex:
+          raise ex      
+        except AssertionError as ex:
+          self.print_message(str(ex))
+          raise Exception(f"Wiring fault! Trying to connect incompatible services {self.__class__.__name__} and {other.__class__.__name__}" )
+        except AttributeError as ex:
+          self.print_message(str(ex))
+          raise Exception("Wiring fault! Wires connected out of order?")
+        
+class ReceivingService(Service):
+    input_wire_class=None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.input_wire_class is None:
+            raise Exception(f"ReceivingService {self.__class__.__name__} does not specify input_wire_class")
+    def connect_input(self, service):
+        self.input_wire=self.input_wire_class()
+        service.connect(self.input_wire)
+    def receive_input(self, block=True):
+        if not hasattr(self,"input_wire"):
+            self.print_message("Unconnected input")
+            return None
+        try:
+            return self.input_wire.get(block)
+        except multiprocessing.queues.Empty:
+            return None
+        except Exception as ex:
+            self.print_message("receive_input error:")
+            self.print_message(str(ex))
+            return None
+
+# multiple input/outputs will be implemented by connecting to/from special attributes:
+# s1.output1.plugs_into(s2)
+
+class ThreadService(Service):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__stopped=multiprocessing.Value(ctypes.c_bool)
+        self.__done=multiprocessing.Value(ctypes.c_bool)
+        self.stopped=True
+        self.done=False
+        self.thread=multiprocessing.Process(target=self._start_process, 
+                                            name=self.__class__.__name__)
+
+    def start(self):
+        self.stopped=False
+        if not self.thread.is_alive():
+            self.thread.start()
+
+    def _start_process(self):
+        try:
+          self.start_process()
+        except Exception as ex:
+          message="Service process exception:"
+          self.print_message(message)
+          self.print_message(str(ex))
+          if hasattr(self,"send_output"):
+              self.send_output(None)
+        self.cleanup()
+
+
+    def start_process(self):
+        self._process()
+
+    def _process(self):
+        while not self.stopped:
+            if hasattr(self,"receive_input"):
+                data=self.receive_input()
+                if data is None:
+                    self.stopped=True
+            else:
+                data=None
+
+            # if stopped reveive input, but do not process, nor output
+
+            if not self.stopped:
+                data=self.process(data)
+
+            if hasattr(self,"send_output"):
+                if (not self.stopped and 
+                    data is not None):  
+                    self.send_output(data)
+            
+        if hasattr(self,"send_output"):
+            self.send_output(None)
+                    
+        self.stopped=True
+        
+    def cleanup(self):
+        self.stopped=True
+        self.done=True
+
+    def stop(self):
+        self.stopped=True
+
+    def close(self):
+        self.stop()
+
+        while not self.done:
+            time.sleep(0.1)
+
+        self.thread.terminate()
+        self.thread.join()
+        self.thread.close()
+
+    @property
+    def done(self):
+        return self.__done.value
+
+    @done.setter
+    def done(self, value):
+        self.__done.value=value
+
+    @property
+    def stopped(self):
+        return self.__stopped.value
+
+    @stopped.setter
+    def stopped(self, value):
+        self.__stopped.value=value
