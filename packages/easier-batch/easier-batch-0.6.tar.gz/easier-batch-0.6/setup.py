967977fe996dfb3267d8f91e9fdc96a3ce169a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:54 +0000

    Add package easier-batch/0.6/easier-batch-0.6.tar.gz

---
+++ packages/easier-batch/easier-batch-0.6.tar.gz/easier-batch-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+import easier_batch
+setup(
+    name='easier-batch',
+    version='0.6',
+    packages=[],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
