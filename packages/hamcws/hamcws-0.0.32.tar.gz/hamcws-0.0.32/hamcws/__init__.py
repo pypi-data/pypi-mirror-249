9498259bfbf9a54fed87561eca4c07d3d82cf369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 15:04:17 +0000

    Add package hamcws/0.0.32/hamcws-0.0.32.tar.gz

---
+++ packages/hamcws/hamcws-0.0.32.tar.gz/hamcws-0.0.32/hamcws/__init__.py
@@ -0,0 +1,14 @@
+from .hamcws import (
+    get_mcws_connection,
+    MediaServer,
+    MediaServerConnection,
+    CannotConnectError,
+    InvalidAuthError,
+    MediaServerInfo,
+    PlaybackInfo,
+    Zone,
+    PlaybackState,
+    MediaType,
+    MediaSubType,
+    KeyCommand
+)
