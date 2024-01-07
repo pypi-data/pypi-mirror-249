29421081f781a8cc2a5d5b7f95e357adbd217afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 14:05:53 +0000

    Add package forager-service/0.1.1/forager_service-0.1.1-py3-none-any.whl

---
+++ packages/forager-service/forager_service-0.1.1-py3-none-any.whl/forager_service/services.py
@@ -0,0 +1,24 @@
+"""Services for Forager project."""
+from __future__ import annotations
+
+from forager_service.validators import special_validators, validators
+
+
+def create_and_validate_params(operation_type: str, **kwargs) -> dict:
+    """
+    Add params from keyword arguments.
+
+    :param operation_type: str Name of request operation.
+    :param kwargs: dict Key word arguments for particular operation.
+    :return: dict Params for request.
+    """
+    special_validators.validate_arguments(operation_type, kwargs)
+    param_dict: dict = {}
+    for key, element in kwargs.items():
+        if element is not None:
+            for validator in validators.get(key):
+                validator(key, element)
+            param_dict[key] = element
+    for validation_handler in validators.get('required_arguments'):
+        validation_handler(operation_type, param_dict)
+    return param_dict
