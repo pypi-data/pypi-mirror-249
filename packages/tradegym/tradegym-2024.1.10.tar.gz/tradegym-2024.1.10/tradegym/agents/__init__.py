e0c5622bef6789284698bdc844e404a8c0ec1f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:49 +0000

    Add package tradegym/2024.1.10/tradegym-2024.1.10.tar.gz

---
+++ packages/tradegym/tradegym-2024.1.10.tar.gz/tradegym-2024.1.10/tradegym/agents/__init__.py
@@ -0,0 +1,13 @@
+from .baseline import (
+    BaseAgent, 
+    BuyAndHoldAgent, 
+    RandomAgent, 
+    EqualWeighting, 
+    baseline_agents
+    )
+from .sb3 import A2C, PPO, DDPG, TD3, SAC, sb3_agents
+
+all_agents = dict(
+    **baseline_agents,
+    **sb3_agents, 
+    )
