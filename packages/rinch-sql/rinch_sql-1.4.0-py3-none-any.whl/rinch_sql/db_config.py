3a2bc6163faf3feada13927c449420bf8f8eb377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 15:04:15 +0000

    Add package rinch-sql/1.4.0/rinch_sql-1.4.0-py3-none-any.whl

---
+++ packages/rinch-sql/rinch_sql-1.4.0-py3-none-any.whl/rinch_sql/db_config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field, replace
+from mysql.connector.pooling import MySQLConnectionPool
+from functools import cache
+
+
+@dataclass(frozen=True)
+class DbConfig:
+    host: str
+    port: int
+    user: str
+    password: str
+    pool_size: int = 1
+    database: str = None
+    autocommit: bool = field(init=False, default=True)
+    # autocommit: bool = True  # 强制行为
+
+    @cache
+    def pool(self) -> MySQLConnectionPool:
+        return MySQLConnectionPool(**self.__dict__)
+
+    def replace(self, /, **changes):
+        return replace(self, **changes)
