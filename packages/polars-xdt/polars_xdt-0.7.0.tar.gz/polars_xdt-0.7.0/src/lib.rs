d4885383ffaad874dd7fa10206655fec4a68879d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:12 +0000

    Add package polars-xdt/0.7.0/polars_xdt-0.7.0.tar.gz

---
+++ packages/polars-xdt/polars_xdt-0.7.0.tar.gz/polars_xdt-0.7.0/src/lib.rs
@@ -0,0 +1,22 @@
+mod business_days;
+mod expressions;
+mod format_localized;
+mod is_workday;
+mod sub;
+mod timezone;
+
+use pyo3::types::PyModule;
+use pyo3::{pymodule, PyResult, Python};
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc;
+
+#[pymodule]
+fn _internal(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
