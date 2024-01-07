617abe13d507c8ced0d687daa49913290a49722c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:28 +0000

    Add package pySTTM/0.1.1/pySTTM-0.1.1.tar.gz

---
+++ packages/pySTTM/pySTTM-0.1.1.tar.gz/pySTTM-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pySTTM',
+    packages = ['pySTTM'],
+    version = '0.1.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'First Version',
+    author = 'Mohammad Akbari',
+    author_email = 'akbari.ma@gmail.com',
+    url = 'https://github.com/DSInCenter/pySTTM/tree/0.1.1',
+    download_url = 'https://github.com/DSInCenter/pySTTM/archive/refs/tags/0.1.1.tar.gz',
+    keywords = ['0.1.1'],
+    license='(c) DSInCenter License',
+    classifiers = ['License :: Other/Proprietary License'],
+)
