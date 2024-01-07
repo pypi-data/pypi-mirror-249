880ebf4c2ff51b57d83077375d731325c6204922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 15:04:26 +0000

    Add package atcodercli/0.0.3/atcodercli-0.0.3-py3-none-any.whl

---
+++ packages/atcodercli/atcodercli-0.0.3-py3-none-any.whl/atcodercli/__main__.py
@@ -0,0 +1,37 @@
+import rich, argparse
+from .login import handle as handleLogin
+from .loginstat import handle as handleLoginStat
+from .fetchresult import handle as handleFetchResult
+from .fetchpage import handle as handleFetchPage
+
+def dispatch_args():
+    parser = argparse.ArgumentParser(
+        prog = "atcli",
+        description = "A lightweight, fast and beautiful command line interface for Atcoder.jp",
+        epilog = '''
+            default config path: ~/.config/atcli/config.yaml
+            default login session path: ~/.config/atcli/session.yaml
+            program source: https://github.com/rickyxrc/atcli
+        ''',
+    )
+
+    OPERATIONS = {
+        "login" : handleLogin,
+        "loginstat" : handleLoginStat,
+        "fetchresult" : handleFetchResult,
+        "fetchpage" : handleFetchPage
+    }
+    parser.add_argument(
+        "operation",
+        type = str,
+        choices = OPERATIONS.keys()
+    )
+    arg = parser.parse_args()
+
+    console = rich.console.Console()
+
+    OPERATIONS[arg.operation](console)
+
+if __name__ == '__main__':
+    dispatch_args()
+
