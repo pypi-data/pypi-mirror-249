b7c1ad64cd70e62646f33827eaeda2b6765865b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:15 +0000

    Add package edc-phq9/0.3.0/edc-phq9-0.3.0.tar.gz

---
+++ packages/edc-phq9/edc-phq9-0.3.0.tar.gz/edc-phq9-0.3.0/runtests.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_phq9"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    template_dirs=[os.path.join(base_dir, app_name, "tests", "templates")],
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    SUBJECT_VISIT_MISSED_MODEL="edc_appointment.subjectvisitmissed",
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "simple_history",
+        "multisite",
+        "django_crypto_fields.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_export.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_crf.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_phq9.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    failures = DiscoverRunner(failfast=True).run_tests([f"{app_name}.tests"])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
