349934143835b96edd41b0488567d68449ce8b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:11 +0000

    Add package spaceone-inventory/2.0.dev66/spaceone_inventory-2.0.dev66-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-2.0.dev66-py3-none-any.whl/spaceone/inventory/model/note_model.py
@@ -0,0 +1,41 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class Note(MongoModel):
+    note_id = StringField(max_length=40, generate_id="note", unique=True)
+    note = StringField()
+    created_by = StringField(max_length=40)
+    record_id = StringField(max_length=40)
+    cloud_service_id = StringField(max_length=40)
+    project_id = StringField(max_length=40)
+    workspace_id = StringField(max_length=40)
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+
+    meta = {
+        "updatable_fields": ["note"],
+        "minimal_fields": [
+            "note_id",
+            "note",
+            "created_by",
+            "record_id",
+            "cloud_service_id",
+        ],
+        "change_query_keys": {"user_projects": "project_id"},
+        "ordering": ["-created_at"],
+        "indexes": [
+            "record_id",
+            "cloud_service_id",
+            "created_by",
+            {
+                "fields": ["domain_id", "workspace_id", "project_id"],
+                "name": "COMPOUND_INDEX_FOR_SEARCH",
+            },
+            {
+                "fields": ["domain_id", "note_id"],
+                "name": "COMPOUND_INDEX_FOR_GET",
+            },
+        ],
+    }
