4b5ac7bc0deca208e5e137715a755837ae9bb46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 04:04:31 +0000

    Add package typing-validation/1.2.1/typing-validation-1.2.1.tar.gz

---
+++ packages/typing-validation/typing-validation-1.2.1.tar.gz/typing-validation-1.2.1/docs/api-toc.rst
@@ -0,0 +1,8 @@
+.. toctree::
+    :maxdepth: 2
+    :caption: API Documentation
+
+    api/typing_validation
+    api/typing_validation.descriptor
+    api/typing_validation.validation
+    api/typing_validation.validation_failure
---
+++ packages/typing-validation/typing-validation-1.2.1.tar.gz/typing-validation-1.2.1/test/test_01_can_validate.py
@@ -0,0 +1,35 @@
+# pylint: disable = missing-docstring
+
+import typing
+
+import pytest
+
+from typing_validation.validation import (can_validate, validation_aliases, _pseudotypes_dict)
+from .test_00_validate import _test_cases, _union_cases, _literal_cases, _alias_cases, _validation_aliases, _typed_dict_cases
+
+@pytest.mark.parametrize("val, ts", _test_cases)
+def test_valid_cases(val: typing.Any, ts: typing.List[typing.Any]) -> None:
+    ts = ts+[_pseudotypes_dict[t] for t in ts if t in _pseudotypes_dict]
+    for t in ts:
+        assert can_validate(t), f"Should be able to validate {t}"
+        assert can_validate(typing.Optional[t]), f"Should be able to validate {typing.Optional[t]}"
+    assert can_validate(typing.Any), f"Should be able to validate {typing.Any}"
+
+@pytest.mark.parametrize("val, ts", _union_cases+_literal_cases)
+def test_other_cases(val: typing.Any, ts: typing.List[typing.Any]) -> None:
+    for t in ts:
+        assert can_validate(t), f"Should be able to validate {t}"
+        assert can_validate(typing.Optional[t]), f"Should be able to validate {typing.Optional[t]}"
+
+@pytest.mark.parametrize("val, ts", _alias_cases)
+def test_alias_cases(val: typing.Any, ts: typing.List[typing.Any]) -> None:
+    for t in ts:
+        with validation_aliases(**_validation_aliases):
+            assert can_validate(t), f"Should be able to validate {t}"
+            assert can_validate(typing.Optional[t]), f"Should be able to validate {typing.Optional[t]}"
+
+@pytest.mark.parametrize("t, vals", _typed_dict_cases.items())
+def test_typed_dict_cases(t: typing.Any, vals: typing.List[typing.Any]) -> None:
+    with validation_aliases(**_validation_aliases):
+        assert can_validate(t), f"Should be able to validate {t}"
+        assert can_validate(typing.Optional[t]), f"Should be able to validate {typing.Optional[t]}"
