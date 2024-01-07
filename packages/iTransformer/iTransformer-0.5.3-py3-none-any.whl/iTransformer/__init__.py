af6793cb4d8346341e890ed6951ce5926f9a2534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 03:05:19 +0000

    Add package iTransformer/0.5.3/iTransformer-0.5.3-py3-none-any.whl

---
+++ packages/iTransformer/iTransformer-0.5.3-py3-none-any.whl/iTransformer/__init__.py
@@ -0,0 +1,10 @@
+from iTransformer.iTransformer import (
+    iTransformer,
+    RevIN
+)
+
+from iTransformer.iTransformer2D import iTransformer2D
+
+from iTransformer.iTransformerFFT import iTransformerFFT
+
+from iTransformer.iTransformerNormConditioned import iTransformerNormConditioned
