1504cb2f5f3b725b39f7b426a8af5795372ac50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 15:04:14 +0000

    Add package genpro/0.1.1/genpro-0.1.1.tar.gz

---
+++ packages/genpro/genpro-0.1.1.tar.gz/genpro-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genpro',
+    version='0.1.1',
+    author="Rajika Keminda",
+    author_email="rkeminda0@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click', 'python-dotenv', 'rich', 'openai'
+    ],
+    keywords=["cli", "AI", "python", "project manager"],
+    entry_points={
+        'console_scripts': [
+            'gp = genpro.commands:cli',
+            'gen = genpro.commands:cli'
+        ],
+    },
+)
