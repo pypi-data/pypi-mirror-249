563a1c39ee17c401cabf23ca82af7bf8d609bc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-07 05:04:19 +0000

    Add package cybertask/2.0.79/cybertask-2.0.79-py3-none-any.whl

---
+++ packages/cybertask/cybertask-2.0.79-py3-none-any.whl/cybertask/systemtray.py
@@ -0,0 +1,127 @@
+import os, sys, platform, subprocess
+from PySide6.QtWidgets import QSystemTrayIcon, QMenu, QApplication
+from PySide6.QtGui import QIcon, QAction, QGuiApplication
+from pathlib import Path
+from functools import partial
+
+
+letMeDoItFile = os.path.realpath(__file__)
+letMeDoItAIFolder = os.path.dirname(letMeDoItFile)
+with open(os.path.join(letMeDoItAIFolder, "package_name.txt"), "r", encoding="utf-8") as fileObj:
+    package = fileObj.read()
+apps = {
+    "myhand": "MyHand",
+    "letmedoit": "LetMeDoIt",
+    "taskwiz": "TaskWiz",
+    "cybertask": "CyberTask",
+}
+iconFile = os.path.join(letMeDoItAIFolder, "icons", f"{apps[package]}.png")
+thisOS = platform.system()
+
+
+class SystemTrayIcon(QSystemTrayIcon):
+    def __init__(self, icon, parent=None):
+        super().__init__(icon, parent)
+
+        self.menu = QMenu(parent)
+
+        for i in (
+            package,
+            "chatgpt",
+            "geminipro",
+            "geminiprovision",
+            "palm2",
+            "codey",
+            "autoassist",
+            "autoretriever",
+            "automath",
+            "autoteachable",
+            "autobuilder",
+            "etextedit",
+        ):
+            action = QAction(i, self)
+            action.triggered.connect(partial(self.runLetMeDoItCommand, i))
+            self.menu.addAction(action)
+
+        self.menu.addSeparator()
+
+        exitAction = QAction("Exit", self)
+        exitAction.triggered.connect(self.exit)
+        self.menu.addAction(exitAction)
+
+        self.setContextMenu(self.menu)
+
+    def exit(self):
+        self.setVisible(False)
+        QGuiApplication.instance().quit()
+
+    def runLetMeDoItCommand(self, command):
+        def createShortcutFile(filePath, content):
+            with open(filePath, "w", encoding="utf-8") as fileObj:
+                fileObj.write(content)
+
+        def isPackageInstalled(package):
+            whichCommand = "where.exe" if platform.system() == "Windows" else "which"
+            try:
+                isInstalled, *_ = subprocess.Popen("{0} {1}".format(whichCommand, package), shell=True, stdout=subprocess.PIPE).communicate()
+                return True if isInstalled else False
+            except:
+                return False
+
+        shortcut_dir = os.path.join(letMeDoItAIFolder, "shortcuts")
+        Path(shortcut_dir).mkdir(parents=True, exist_ok=True)
+
+        # The following line does not work on Windows
+        commandPath = os.path.join(os.path.dirname(sys.executable), command)
+
+        if thisOS == "Windows":
+            opencommand = "start"
+            filePath = os.path.join(shortcut_dir, f"{command}.bat")
+            if not os.path.isfile(filePath):
+                filenames = {
+                    package: "main.py",
+                    "etextedit": "eTextEdit.py",
+                }
+                systemTrayFile = os.path.join(letMeDoItAIFolder, filenames.get(command, f"{command}.py"))
+                content = f'''powershell.exe -NoExit -Command "{sys.executable} '{systemTrayFile}'"'''
+                createShortcutFile(filePath, content)
+        elif thisOS == "Darwin":
+            opencommand = "open"
+            filePath = os.path.join(shortcut_dir, f"{command}.command")
+            if not os.path.isfile(filePath):
+                content = f"""#!/bin/bash
+cd {letMeDoItAIFolder}
+{commandPath}"""
+                createShortcutFile(filePath, content)
+                os.chmod(filePath, 0o755)
+        elif thisOS == "Linux":
+            opencommand = ""
+            for i in ("dex", "exo-open", "gio launch", "xdg-open"):
+                if isPackageInstalled(i):
+                    opencommand = i
+            # Remarks:
+            # 'exo-open' comes with 'exo-utils'
+            # 'gio' comes with 'glib2'
+            if opencommand:
+                filePath = os.path.join(shortcut_dir, f"{command}.desktop")
+                if not os.path.isfile(filePath):
+                    content = f"""#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Path={letMeDoItAIFolder}
+Exec={commandPath}
+Icon={iconFile}
+Name={command}"""
+                    createShortcutFile(filePath, content)
+        os.system(f"{opencommand} {filePath}")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    icon = QIcon(iconFile)
+    trayIcon = SystemTrayIcon(icon)
+    trayIcon.show()
+
+    sys.exit(app.exec())
