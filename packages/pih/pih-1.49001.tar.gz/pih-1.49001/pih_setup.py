13fdd24c4e4b93642e63d4b9c68932301ccb533f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:31 +0000

    Add package pih/1.49001/pih-1.49001.tar.gz

---
+++ packages/pih/pih-1.49001.tar.gz/pih-1.49001/pih_setup.py
@@ -0,0 +1,92 @@
+import shutil
+import os
+import importlib.util
+import sys
+from setuptools import setup
+
+pih_is_exists = importlib.util.find_spec("pih") is not None
+if not pih_is_exists:
+    sys.path.append("//pih/facade")
+from pih import PIH
+
+# for facade
+# setuptools
+# prettytable
+# colorama
+# protobuf
+# grpcio (six)
+# pyperclip
+# win32com -> pywin32 (!!!)
+
+# MC
+# pywhatkit
+
+# for orion
+# myssql
+
+# for log
+# telegram_send
+
+# for dc2
+# docs:
+# mailmerge (pip install docx-mailmerge)
+# xlsxwriter
+# xlrd
+# python-barcode
+# Pillow
+# ad:
+# pyad
+# pywin32 (!!!)
+# wmi
+# transliterate
+
+# for data storage
+# pysos
+# lmdbm
+
+# for printer (dc2)
+# pysnmp
+
+# for polibase
+# cx_Oracle
+
+# for mobile helper
+# paramiko
+
+#########################################################################################################
+
+folder = "//pih/facade/dist/pih"
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as error:
+        print("Failed to delete %s. Reason: %s" % (file_path, error))
+
+# This call to setup() does all the work
+setup(
+    name=PIH.NAME,
+    version=PIH.VERSION.value,
+    description="PIH library",
+    long_description_content_type="text/markdown",
+    url="https://pacifichosp.com/",
+    author="Nikita Karachentsev",
+    author_email="it@pacifichosp.com",
+    license="MIT",
+    classifiers=[],
+    packages=[PIH.NAME],
+    include_package_data=True,
+    install_requires=[
+        "prettytable",
+        "colorama",
+        "grpcio",
+        "protobuf",
+        "requests",
+        "transliterate",
+        "psutil",
+        "setuptools",
+    ],
+)
