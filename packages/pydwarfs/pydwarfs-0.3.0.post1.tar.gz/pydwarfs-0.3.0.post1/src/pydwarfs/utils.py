14324e67ab60c3fcddc57ed0c9c4463ef66db708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:06:02 +0000

    Add package pydwarfs/0.3.0.post1/pydwarfs-0.3.0.post1.tar.gz

---
+++ packages/pydwarfs/pydwarfs-0.3.0.post1.tar.gz/pydwarfs-0.3.0.post1/src/pydwarfs/utils.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+import errno
+import os
+from typing import Any
+from typing import Callable
+
+import attrs
+
+try:
+    from typing import Self
+except ImportError:
+    from typing_extensions import Self
+
+__all__ = ['AttrFieldValidatorFactory']
+
+
+class AttrFieldValidatorFactory:
+    @staticmethod
+    def executableField(basename: str | tuple[str, ...]) -> Callable[[Any, attrs.Attribute, Any], None]:
+        def validator(__: Any, ___: attrs.Attribute, value: Any) -> None:
+            if not os.path.exists(value):
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), value)
+            elif os.path.isdir(value):
+                raise IsADirectoryError(errno.EISDIR, os.strerror(errno.EISDIR), value)
+            elif not os.access(value, os.X_OK):
+                raise PermissionError(errno.EPERM, os.strerror(errno.EPERM), value)
+
+            actual_basename = os.path.basename(value).lower()
+            if isinstance(basename, tuple):
+                basenames: list[str] = [s.lower() for s in basename]
+                if actual_basename not in basenames:
+                    raise ValueError(f'Executable name must be one of the following: {basenames!r} (got {actual_basename!r})')
+            elif actual_basename != basename.lower():
+                raise ValueError(f'Executable name must be {basename!r} (got {actual_basename!r}')
+
+        return validator
