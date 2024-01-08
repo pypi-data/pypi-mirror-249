e31f9c591191b6d1586ce75bff7bb3be5126acdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:05:05 +0000

    Add package Statoscop/1.2.0/Statoscop-1.2.0.tar.gz

---
+++ packages/Statoscop/Statoscop-1.2.0.tar.gz/Statoscop-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Statoscop",
+    version="1.2.0",
+    author="Finallo",
+    author_email="finallo360@gmail.com",
+    description="A package to have data of League, TFT and Valorant.",
+    packages=find_packages(),
+    keywords='example project',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
