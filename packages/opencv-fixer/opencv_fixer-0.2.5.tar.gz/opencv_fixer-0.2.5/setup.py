48c69bd647c32f3f76b851796ead03a351be2336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:26 +0000

    Add package opencv-fixer/0.2.5/opencv_fixer-0.2.5.tar.gz

---
+++ packages/opencv-fixer/opencv_fixer-0.2.5.tar.gz/opencv_fixer-0.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from opencv_fixer import AutoFix
+
+class AutoFixCommand(install):
+    def run(self):
+        install.run(self)
+        AutoFix()
+
+setup(
+    name='opencv_fixer',
+    version='0.2.5',
+    license='Apache 2.0',
+    author="soulteary",
+    author_email="soulteary@gmail.com",
+    packages=find_packages(),
+    description='Fix OpenCV Issue for numpy, diffusers, etc.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/soulteary/opencv-fixer',
+    install_requires=[],
+    cmdclass={
+        'install': AutoFixCommand,
+    },
+)
