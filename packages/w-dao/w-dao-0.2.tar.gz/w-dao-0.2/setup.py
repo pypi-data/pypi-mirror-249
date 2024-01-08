a84f665a62477565f8350831f1b4ce0420bb6f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:45 +0000

    Add package w-dao/0.2/w-dao-0.2.tar.gz

---
+++ packages/w-dao/w-dao-0.2.tar.gz/w-dao-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w-dao",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="wooi",
+    author_email="willooi@qq.com",
+    description="A useless mode for stand output",
+    license="MIT",
+    keywords="w-dao",
+    url="https://github.com/mkitpro/w-dao.git"
+)
