5538b7153ce10e0faf544e7bbb5dff3490cbd1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 05:04:52 +0000

    Add package pyenigma/0.4.2/pyenigma-0.4.2.tar.gz

---
+++ packages/pyenigma/pyenigma-0.4.2.tar.gz/pyenigma-0.4.2/pyenigma/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+__author__ = "Christophe Goessen, Cedric Bonhomme"
+__version__ = "0.5.0"
+__date__ = "$Date: 2010/01/29 $"
+__revision__ = "$Date: 2024/01/07 $"
+__copyright__ = "Copyright (c) Christophe Goessen, Cedric Bonhomme"
+__license__ = "GPLv3"
