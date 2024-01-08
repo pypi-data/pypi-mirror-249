aabd62ee0a76af605f7a780b29d2d2c5e084a417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:02 +0000

    Add package demo-tracify/1.3.0/demo-tracify-1.3.0.tar.gz

---
+++ packages/demo-tracify/demo-tracify-1.3.0.tar.gz/demo-tracify-1.3.0/tracify/tests/test_channels_factory.py
@@ -0,0 +1,12 @@
+import pytest
+
+from tracify.channels.channels_factory import ChannelTransformer
+
+
+class TestChannelTransformer:
+    def test_get_channel_invalid_name(self):
+        # Mock the channel instance
+        with pytest.raises(ValueError) as exc:
+            ChannelTransformer().get_channel("INVALID")
+        # Assert that the correct exception is raised
+        assert str(exc.value) == "Invalid channel name: INVALID"
