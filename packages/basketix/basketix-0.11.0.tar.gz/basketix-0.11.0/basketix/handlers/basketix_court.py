22dbea18cc7d854e6bff2acd7abb4409ab869fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:33 +0000

    Add package basketix/0.11.0/basketix-0.11.0.tar.gz

---
+++ packages/basketix/basketix-0.11.0.tar.gz/basketix-0.11.0/basketix/handlers/basketix_court.py
@@ -0,0 +1,48 @@
+"""Basketball court handler"""
+
+from ..models import BasketballCourtSettings, BasketixId, SeasonId
+from ..tables import BasketixSeasonsTable
+
+
+class BasketixCourtHandler:
+
+    BLACK = "#000000"
+    DEFAULT_COURT_COLOR = "#e6be91"
+    DEFAULT_LINE_WIDTH = 3
+
+    def __init__(self, environment: str) -> None:
+        self.basketix_seasons_table = BasketixSeasonsTable(environment)
+
+
+    def get(self, season_id: SeasonId, basketix_id: BasketixId) -> BasketballCourtSettings:
+        season = self.basketix_seasons_table.get(season_id)
+
+        if "courtSettings" in season["basketixs"][basketix_id]:
+            return season["basketixs"][basketix_id]["courtSettings"]
+
+        return self.default()
+
+    def upsert(self, season_id: SeasonId, basketix_id: BasketixId, court_settings: BasketballCourtSettings) -> BasketballCourtSettings:
+        """Update or insert court setting of a user"""
+        season = self.basketix_seasons_table.get(season_id)
+
+        court = self.default()
+        if "courtSettings" in season["basketixs"][basketix_id]:
+            court: BasketballCourtSettings = season["basketixs"][basketix_id]["courtSettings"]
+
+        updated_court: BasketballCourtSettings = {**court, **court_settings} # type: ignore
+        season["basketixs"][basketix_id]["courtSettings"] = updated_court
+        self.basketix_seasons_table.update([season])
+
+        return updated_court
+
+    def default(self) -> BasketballCourtSettings:
+        return {
+            "background_color": self.DEFAULT_COURT_COLOR,
+            "court_color": self.DEFAULT_COURT_COLOR,
+            "lines_color": self.BLACK,
+            "lines_width": self.DEFAULT_LINE_WIDTH,
+            "paint_color": self.DEFAULT_COURT_COLOR,
+            "side_paint_color": self.DEFAULT_COURT_COLOR,
+            "three_points_color": self.DEFAULT_COURT_COLOR,
+        }
---
+++ packages/basketix/basketix-0.11.0.tar.gz/basketix-0.11.0/basketix/models/basketball_court_settings.py
@@ -0,0 +1,10 @@
+from typing import TypedDict
+
+class BasketballCourtSettings(TypedDict):
+    background_color: str
+    court_color: str
+    paint_color: str
+    three_points_color: str
+    lines_color: str
+    side_paint_color: str
+    lines_width: int
