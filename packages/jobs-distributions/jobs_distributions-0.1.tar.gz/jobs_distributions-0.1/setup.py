486e53d02dba2e73f6c47b20c481621ac539e18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:26 +0000

    Add package jobs-distributions/0.1/jobs_distributions-0.1.tar.gz

---
+++ packages/jobs-distributions/jobs_distributions-0.1.tar.gz/jobs_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jobs_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jobs_distributions'],
+      zip_safe=False)
