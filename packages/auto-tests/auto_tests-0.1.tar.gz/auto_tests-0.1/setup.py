dede6a8a86b838bd0498beff302269972fa34af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:05:43 +0000

    Add package auto-tests/0.1/auto_tests-0.1.tar.gz

---
+++ packages/auto-tests/auto_tests-0.1.tar.gz/auto_tests-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto_tests',
+    version='0.1',
+    description="package for fast testing console code",
+    packages=find_packages(),
+    url="https://github.com/pavelglazunov/-LIB-autoTests",
+
+)
