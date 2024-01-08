21fc62f3b525ccb3eca9a2e82e8c56fabd2dfd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:20 +0000

    Add package dronebuddylib/2.0.14/dronebuddylib-2.0.14.tar.gz

---
+++ packages/dronebuddylib/dronebuddylib-2.0.14.tar.gz/dronebuddylib-2.0.14/dronebuddylib/models/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "2.0.14"
+
+from .engine_configurations import EngineConfigurations
+from .enums import Configurations, FaceRecognitionAlgorithm, TextRecognitionAlgorithm
---
+++ packages/dronebuddylib/dronebuddylib-2.0.14.tar.gz/dronebuddylib-2.0.14/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name='dronebuddylib',
+    version='2.0.14',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'dronebuddylib': ['resources/*'],
+    },
+    zip_safe=False,
+    setup_requires=[
+        'setuptools>=50.3.0',
+    ],
+    install_requires=[requirements],
+    extras_require={
+        "FACE_RECOGNITION": ['face-recognition'],
+        "OBJECT_DETECTION": ['mediapipe', 'tensorflow_hub', 'opencv-python'],
+    },
+    python_requires='>=3.9',
+    description='Everything to control and customize Tello',
+    author='Malsha de Zoysa',
+    author_email='malsha@ahlab.org',
+    license='MIT',
+)
