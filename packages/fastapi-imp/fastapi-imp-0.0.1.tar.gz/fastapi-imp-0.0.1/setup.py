91ee01b8d2b845d8866df09c7e220c8cd6680c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:13 +0000

    Add package fastapi-imp/0.0.1/fastapi-imp-0.0.1.tar.gz

---
+++ packages/fastapi-imp/fastapi-imp-0.0.1.tar.gz/fastapi-imp-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastapi-imp",
+    version="0.0.1",
+    author="eatmoreapple",
+    description="class base view for fastapi",
+    url="https://github.com/eatmoreapple/imp",
+    packages=find_packages(),
+    author_email="eatmoreorange@gmail.com",
+    install_requires=[
+        'fastapi',
+    ]
+)
