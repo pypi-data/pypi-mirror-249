df05fd705f4a673b88489c4e7ecf9b864a8453b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:03 +0000

    Add package demo-tracify/1.1.0/demo-tracify-1.1.0.tar.gz

---
+++ packages/demo-tracify/demo-tracify-1.1.0.tar.gz/demo-tracify-1.1.0/tracify/tests/channels/test_app_settings.py
@@ -0,0 +1,36 @@
+import pytest
+from django.core.exceptions import ImproperlyConfigured
+
+from tracify import app_settings
+
+
+class TestTracifyChannelsConfiguration:
+    def test_missing_configuration(self, tracify_channels_configuration):
+        """
+        Test case to verify that an ImproperlyConfigured exception is raised when the configuration is missing.
+        """
+        app_settings.TRACIFY_CHANNELS_CONFIGURATION = {}
+        with pytest.raises(ImproperlyConfigured):
+            raise ImproperlyConfigured("TRACIFY_CHANNELS_CONFIGURATION is missing")
+
+    def test_valid_configuration(self, tracify_channels_configuration):
+        """
+        Test case to verify the validity of the configuration when it is present and valid.
+        """
+        app_settings.TRACIFY_CHANNELS_CONFIGURATION = tracify_channels_configuration
+        assert (
+            app_settings.TRACIFY_CHANNELS_CONFIGURATION["CHANNEL_ONE"]["WEBHOOK_URL"]
+            == "fake channel one webhook url"
+        )
+        assert (
+            app_settings.TRACIFY_CHANNELS_CONFIGURATION["CHANNEL_TWO"]["WEBHOOK_URL"]
+            == "fake channel two webhook url"
+        )
+
+    def test_invalid_configuration(self):
+        """
+        Test case to verify that an ImproperlyConfigured exception is raised when the configuration is invalid.
+        """
+        app_settings.TRACIFY_CHANNELS_CONFIGURATION = "invalid_configuration"
+        with pytest.raises(ImproperlyConfigured):
+            raise ImproperlyConfigured("TRACIFY_CHANNELS_CONFIGURATION is invalid")
