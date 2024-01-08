3c668178eaf656f177ed10459ecfb42e645c39c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:55 +0000

    Add package duit/0.1.11/duit-0.1.11-py3-none-any.whl

---
+++ packages/duit/duit-0.1.11-py3-none-any.whl/duit/model/DataField.py
@@ -0,0 +1,163 @@
+from typing import TypeVar, Generic, Any, Optional, Callable, Iterable
+
+from duit.event.Event import Event
+from duit.settings import SETTING_ANNOTATION_ATTRIBUTE_NAME
+
+T = TypeVar("T")
+
+
+class DataField(Generic[T]):
+    """
+    A generic data field that can hold a value of type T and provides event handling functionality.
+    """
+
+    def __init__(self, value: T):
+        """
+        Initialize a DataField with the given value.
+
+        Args:
+            value (T): The initial value of the DataField.
+        """
+        self._value: T = value
+        self.publish_enabled: bool = True
+        self.on_changed: Event[T] = Event[T]()
+
+        # Add serialization attribute by default
+        from duit.settings.Setting import Setting
+        self.__setattr__(SETTING_ANNOTATION_ATTRIBUTE_NAME, Setting())
+
+    @property
+    def value(self) -> T:
+        """
+        Get the current value of the DataField.
+
+        Returns:
+            T: The current value.
+        """
+        return self._value
+
+    @value.setter
+    def value(self, new_value: T) -> None:
+        """
+        Set the value of the DataField and trigger the 'on_changed' event if the value changes.
+
+        Args:
+            new_value (T): The new value to set.
+        """
+        old_value = self._value
+        self._value = new_value
+
+        if self.publish_enabled and not self._is_equal(self._value, old_value):
+            self.fire()
+
+    def set_silent(self, value: T) -> None:
+        """
+        Set the value of the DataField without triggering the 'on_changed' event.
+
+        Args:
+            value (T): The new value to set.
+        """
+        old_publish_value = self.publish_enabled
+        self.publish_enabled = False
+        self.value = value
+        self.publish_enabled = old_publish_value
+
+    def fire(self):
+        """
+        Trigger the 'on_changed' event with the current value.
+        """
+        self.on_changed(self._value)
+
+    def fire_latest(self):
+        """
+        Trigger the 'on_changed' event with the current value, invoking only the latest listener.
+        """
+        self.on_changed.invoke_latest(self._value)
+
+    def bind_to(self, model: "DataField[T]") -> None:
+        """
+        Bind this DataField to another DataField, propagating changes from this to the other.
+
+        Args:
+            model (DataField[T]): The target DataField to bind to.
+        """
+
+        def on_change(*args: Any):
+            old_publish_value = self.publish_enabled
+            self.publish_enabled = False
+            model.value = self._value
+            self.publish_enabled = old_publish_value
+
+        self.on_changed.append(on_change)
+
+    def bind_bidirectional(self, model: "DataField[T]") -> None:
+        """
+        Bind this DataField bidirectionally to another DataField.
+
+        Args:
+            model (DataField[T]): The target DataField to bind to bidirectionally.
+        """
+        self.bind_to(model)
+        model.bind_to(self)
+
+    def bind_to_attribute(self, obj: Any, field_name: Any,
+                          converter: Optional[Callable[[T], Any]] = None,
+                          fire_latest: bool = False) -> None:
+        """
+        Bind this DataField to an attribute of an object.
+
+        Args:
+            obj (Any): The target object.
+            field_name (Any): The name of the attribute to bind to.
+            converter (Optional[Callable[[T], Any]]): A converter function to apply to the value.
+            fire_latest (bool): Whether to trigger the 'on_changed' event with the latest value.
+        """
+
+        def on_change(*args: Any):
+            if hasattr(obj, field_name):
+                output_value = self._value
+                if converter is not None:
+                    output_value = converter(output_value)
+
+                setattr(obj, field_name, output_value)
+
+        self.on_changed.append(on_change)
+
+        if fire_latest:
+            self.fire_latest()
+
+    @staticmethod
+    def _is_equal(value: T, new_value: T) -> bool:
+        """
+        Check if two values are equal, taking care of iterable comparisons.
+
+        Args:
+            value (T): The first value.
+            new_value (T): The second value.
+
+        Returns:
+            bool: True if the values are equal, False otherwise.
+        """
+        result = value == new_value
+
+        # fix numpy and list comparisons
+        if isinstance(result, Iterable):
+            return all(result)
+
+        return result
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}[{type(self._value).__name__}] ({self._value})"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, DataField):
+            return self._is_equal(self.value, other.value)
+        return False
+
+    def __getstate__(self):
+        d = dict(self.__dict__)
+        del d["on_changed"]
+        return d
---
+++ packages/duit/duit-0.1.11-py3-none-any.whl/duit/ui/tk/TkPropertyPanel.py
@@ -0,0 +1,105 @@
+import logging
+import tkinter as tk
+import typing
+from typing import Union, Optional
+
+import customtkinter as ctk
+
+from duit.collections.Stack import Stack
+from duit.ui.BasePropertyPanel import BasePropertyPanel
+from duit.ui.PropertyRegistry import UI_PROPERTY_REGISTRY
+from duit.ui.annotations import find_all_ui_annotations
+from duit.ui.annotations.container.EndSectionAnnotation import EndSectionAnnotation
+from duit.ui.annotations.container.StartSectionAnnotation import StartSectionAnnotation
+from duit.ui.tk.TkFieldProperty import TkFieldProperty
+
+
+class TkPropertyPanel(BasePropertyPanel, ctk.CTkScrollableFrame):
+    def __init__(self, master: Union[ctk.CTk, ctk.CTkFrame]):
+        """
+        Initializes a TkPropertyPanel.
+
+        Args:
+            master (Union[ctk.CTk, ctk.CTkFrame]): The parent widget.
+        """
+        BasePropertyPanel.__init__(self)
+        ctk.CTkScrollableFrame.__init__(self, master, corner_radius=0)
+
+        self.tabview: Optional[ctk.CTkTabview] = None
+        self.general_tab: Optional[ctk.CTkFrame] = None
+
+        self._init_tab_view()
+
+    def _init_tab_view(self):
+        self.tabview = ctk.CTkTabview(self)
+        self.tabview.pack(padx=5, pady=5, fill=tk.BOTH)
+
+        self.general_tab = self._add_tab("General")
+
+    def _add_tab(self, title: str) -> ctk.CTkFrame:
+        """
+        Add a tab to the tab view.
+
+        Args:
+            title (str): The title of the tab.
+
+        Returns:
+            ctk.CTkFrame: The added tab frame.
+        """
+        tab = self.tabview.add(title)
+        tab.grid_columnconfigure(1, weight=1)
+        return tab
+
+    def _clean_widgets(self):
+        """
+        Clear the widgets in the property panel.
+        """
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        self._init_tab_view()
+
+    def _create_panel(self):
+        """
+        Create the property panel with the widgets based on the annotations.
+        """
+        self._clean_widgets()
+
+        containers: Stack[ctk.CTkFrame] = Stack()
+        containers.push(self.general_tab)
+
+        annotations = find_all_ui_annotations(self.data_context)
+        row_counter = 0
+        for var_name, (model, anns) in annotations.items():
+            anns = sorted(anns)
+
+            for ann in anns:
+                ann_type = type(ann)
+
+                if isinstance(ann, StartSectionAnnotation):
+                    tab = self._add_tab(ann.name)
+                    containers.push(tab)
+                    continue
+
+                if isinstance(ann, EndSectionAnnotation):
+                    containers.pop()
+                    continue
+
+                if ann_type not in UI_PROPERTY_REGISTRY:
+                    logging.warning(f"Annotation not registered: {ann_type.__name__}")
+                    continue
+
+                # add property
+                property_field = typing.cast(TkFieldProperty, UI_PROPERTY_REGISTRY[ann_type](ann, model))
+                widgets = property_field.create_widgets(containers.peek())
+
+                for i, widget in enumerate(widgets):
+                    extra = {}
+                    if i == 1:
+                        extra = {
+                            "sticky": "we",
+                            "padx": 20,
+                        }
+
+                    widget.grid(row=row_counter, column=i, pady=4, **extra)
+                row_counter += 1
