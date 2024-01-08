4a0b654f856d7790187d2be13944cf0172917afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:27 +0000

    Add package unitorch/0.0.0.9/unitorch-0.0.0.9-py3-none-any.whl

---
+++ packages/unitorch/unitorch-0.0.0.9-py3-none-any.whl/unitorch/cli/models/peft/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+import unitorch.cli.models.peft.modeling_bloom
+import unitorch.cli.models.peft.modeling_llama
+from unitorch.cli.models.peft.modeling_bloom import (
+    BloomLoraForClassification,
+    BloomLoraForGeneration,
+)
+from unitorch.cli.models.peft.modeling_llama import (
+    LlamaLoraForClassification,
+    LlamaLoraForGeneration,
+)
