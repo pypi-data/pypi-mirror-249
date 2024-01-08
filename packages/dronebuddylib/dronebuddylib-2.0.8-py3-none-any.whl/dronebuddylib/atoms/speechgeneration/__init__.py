85267c4a26da0927c36b208c678dce159ae61285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:43 +0000

    Add package dronebuddylib/2.0.8/dronebuddylib-2.0.8-py3-none-any.whl

---
+++ packages/dronebuddylib/dronebuddylib-2.0.8-py3-none-any.whl/dronebuddylib/atoms/speechgeneration/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2.0.8"
+
+from .i_speech_generation import ISpeechGeneration
+from .speech_generation_engine import SpeechGenerationEngine
+from .tts_speech_generation_impl import TTSTextToSpeechGenerationImpl
