6815bc15b8c6d58b1f75eb2ddc6c9830f9d437da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 05:04:49 +0000

    Add package santatheking/0.1.0/santatheking-0.1.0.tar.gz

---
+++ packages/santatheking/santatheking-0.1.0.tar.gz/santatheking-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='santatheking',
+    version='0.1.0',
+    author='Kfir W',
+    author_email='kfirW@gmail.com',
+    description='An Christmes Tree, December 2023',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
