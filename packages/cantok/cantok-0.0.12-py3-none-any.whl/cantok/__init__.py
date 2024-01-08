6a95636970aa402cf07f4ac23644bb168f6c8027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:32 +0000

    Add package cantok/0.0.12/cantok-0.0.12-py3-none-any.whl

---
+++ packages/cantok/cantok-0.0.12-py3-none-any.whl/cantok/__init__.py
@@ -0,0 +1,10 @@
+from cantok.tokens.abstract_token import AbstractToken  # noqa: F401
+from cantok.tokens.simple_token import SimpleToken  # noqa: F401
+from cantok.tokens.condition_token import ConditionToken  # noqa: F401
+from cantok.tokens.counter_token import CounterToken  # noqa: F401
+from cantok.tokens.timeout_token import TimeoutToken
+
+from cantok.errors import CancellationError, ConditionCancellationError, CounterCancellationError, TimeoutCancellationError  # noqa: F401
+
+
+TimeOutToken = TimeoutToken
