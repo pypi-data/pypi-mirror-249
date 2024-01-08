364966e1a62a48989dc267ec4c0220fb1a5b0848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:37 +0000

    Add package w-dao/0.5/w_dao-0.5.tar.gz

---
+++ packages/w-dao/w_dao-0.5.tar.gz/w_dao-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="w_dao",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="wooi",
+    author_email="willooi@qq.com",
+    description="A useless database access model",
+    license="MIT",
+    keywords="w_dao",
+    url="https://github.com/mkitpro/w-dao.git"
+)
