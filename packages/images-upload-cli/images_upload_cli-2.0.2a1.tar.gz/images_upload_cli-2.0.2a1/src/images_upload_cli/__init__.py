7632ad4b0c292e3e220838a1ae0dd7a765710933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:46 +0000

    Add package images-upload-cli/2.0.2a1/images_upload_cli-2.0.2a1.tar.gz

---
+++ packages/images-upload-cli/images_upload_cli-2.0.2a1.tar.gz/images_upload_cli-2.0.2a1/src/images_upload_cli/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Public accessible objects of that module."""
+
+from images_upload_cli.upload import HOSTINGS, UPLOAD
+from images_upload_cli.util import get_default_font, get_font, make_thumbnail
