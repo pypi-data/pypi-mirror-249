c6344c59f1f863ffa4ce3d7dafb3767bfd43ae72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:27 +0000

    Add package sqllineage/1.5.0/sqllineage-1.5.0.tar.gz

---
+++ packages/sqllineage/sqllineage-1.5.0.tar.gz/sqllineage-1.5.0/tests/core/test_runner.py
@@ -0,0 +1,24 @@
+from sqllineage.runner import LineageRunner
+from sqllineage.utils.constant import LineageLevel
+
+
+def test_runner_dummy():
+    runner = LineageRunner(
+        """insert into tab2 select col1, col2, col3, col4, col5, col6 from tab1;
+insert into tab3 select * from tab2""",
+        verbose=True,
+    )
+    assert str(runner)
+    assert runner.to_cytoscape() is not None
+    assert runner.to_cytoscape(level=LineageLevel.COLUMN) is not None
+
+
+def test_statements_trim_comment():
+    comment = "------------------\n"
+    sql = "select * from dual;"
+    assert LineageRunner(comment + sql).statements()[0] == sql
+
+
+def test_silent_mode():
+    sql = "begin; select * from dual;"
+    LineageRunner(sql, dialect="greenplum", silent_mode=True)._eval()
