058502633dcd0895ef8f98ba470e4a9198f73975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:41 +0000

    Add package agentforge/0.1.38/agentforge-0.1.38.tar.gz

---
+++ packages/agentforge/agentforge-0.1.38.tar.gz/agentforge-0.1.38/src/agentforge/utils/function_utils.py
@@ -0,0 +1,19 @@
+from .functions.AgentUtils import AgentUtils
+from .functions.Printing import Printing
+from .functions.PromptHandling import PromptHandling
+from .functions.TaskHandling import TaskHandling
+from .functions.ToolUtils import ToolUtils
+from .functions.UserInferface import UserInterface
+
+
+class Functions:
+
+    def __init__(self):
+        self.agent_utils = AgentUtils()
+        self.printing = Printing()
+        self.prompt_handling = PromptHandling()
+        self.task_handling = TaskHandling()
+        self.tool_utils = ToolUtils()
+        self.user_interface = UserInterface()
+
+
