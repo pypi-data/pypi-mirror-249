d4042818fa06a41c33b462d4a51fe85b9cf0df8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:03 +0000

    Add package blueking-component-ieod/1.0.1/blueking-component-ieod-1.0.1.tar.gz

---
+++ packages/blueking-component-ieod/blueking-component-ieod-1.0.1.tar.gz/blueking-component-ieod-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='blueking-component-ieod', #package name
+      version='1.0.1',
+      description='Python Package By jordin',
+      author='jordin',
+      license='MIT',
+      zip_safe=False
+      )
