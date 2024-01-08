d17a25e1019390ad35f0f05dba548268bc8f2333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:10 +0000

    Add package mlops-ai/1.3.0/mlops-ai-1.3.0.tar.gz

---
+++ packages/mlops-ai/mlops-ai-1.3.0.tar.gz/mlops-ai-1.3.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent.parent
+
+# The text of the README file
+README = (HERE/"README.md").read_text()
+
+setup(
+   name="mlops-ai",
+   version="1.3.0",
+   description="Mlops-ai library for managing machine learning projects, experiments, iterations and datasets.",
+   long_description=README,
+   long_description_content_type="text/markdown",
+   URL="https://github.com/kajetsz/mlops/tree/main/library",
+   author="Kacper Pękalski, Kajetan Szal, Jędrzej Rybczyński",
+   author_email="kac.pekalski1@gmail.com",
+   license="Apache License 2.0",
+   classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+
+   ],
+   packages=find_packages(exclude=["tests*"]),
+   include_package_data=True,
+   install_requires=["requests==2.29.0", "scikit-learn==1.3.0", "torch==2.1.1", "json2html==1.3.0"],
+ )
