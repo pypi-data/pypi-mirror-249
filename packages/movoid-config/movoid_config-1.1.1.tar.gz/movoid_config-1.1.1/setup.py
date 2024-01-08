a9f7a9ba8b08d44c6615f3b2fcdbda08387ac871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:28 +0000

    Add package movoid-config/1.1.1/movoid_config-1.1.1.tar.gz

---
+++ packages/movoid-config/movoid_config-1.1.1.tar.gz/movoid_config-1.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='movoid_config',
+    version='1.1.1',
+    packages=['movoid_config'],
+    url='',
+    license='',
+    author='movoid',
+    author_email='bobrobotsun@163.com',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+)
