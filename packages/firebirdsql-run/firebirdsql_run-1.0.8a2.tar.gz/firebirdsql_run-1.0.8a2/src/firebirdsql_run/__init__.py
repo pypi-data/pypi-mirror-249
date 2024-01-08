1102a77197e23cfb3fd4bf3fa256868fefc2eaf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:05:00 +0000

    Add package firebirdsql-run/1.0.8a2/firebirdsql_run-1.0.8a2.tar.gz

---
+++ packages/firebirdsql-run/firebirdsql_run-1.0.8a2.tar.gz/firebirdsql_run-1.0.8a2/src/firebirdsql_run/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Public accessible objects of that module."""
+
+from firebirdsql_run.main import Connection, callproc, connect, connection, execute
+from firebirdsql_run.type import CompletedTransaction, Dataset, ExecuteError, FBTypes
