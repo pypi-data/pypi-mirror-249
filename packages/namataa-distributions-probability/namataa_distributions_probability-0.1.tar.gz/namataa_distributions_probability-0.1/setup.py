1f4ed02cd8e5095592d611a873a4cdcf0c79cf75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 05:04:48 +0000

    Add package namataa-distributions-probability/0.1/namataa_distributions_probability-0.1.tar.gz

---
+++ packages/namataa-distributions-probability/namataa_distributions_probability-0.1.tar.gz/namataa_distributions_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='namataa_distributions_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['namataa_distributions_probability'],
+      zip_safe=False)
