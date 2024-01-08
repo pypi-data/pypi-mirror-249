58698fdbb273856dc00b556a7f8b284073df17db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:18 +0000

    Add package rpi-remote/1.1.1/rpi-remote-1.1.1.tar.gz

---
+++ packages/rpi-remote/rpi-remote-1.1.1.tar.gz/rpi-remote-1.1.1/README.md
@@ -0,0 +1,47 @@
+![PyPI - Version](https://img.shields.io/pypi/v/rpi-remote?style=for-the-badge&logo=python&logoColor=yellow&link=https%3A%2F%2Fpypi.org%2Fproject%2Frpi-remote%2F)
+
+# Rpi Remote client
+
+## Installation
+
+### Prerequisites
+```
+sudo apt install build-essential libffi-dev gcc pkg-config python3-dev python3-pip libssl-dev cargo openssl
+```
+Install rust: https://www.rust-lang.org/tools/install
+
+### Install package
+```
+python3 -m pip install --upgrade rpi-remote --user
+```
+
+### Create service
+```
+echo "[Unit]
+Description=rpi_remote service
+After=multi-user.target
+Conflicts=getty@tty1.service
+[Service]
+User=${USER}
+Type=simple
+Environment="LC_ALL=C.UTF-8"
+Environment="LANG=C.UTF-8"
+ExecStart=${HOME}/.local/bin/rpi-remote
+Restart=on-failure
+RestartSec=3
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/rpi-remote.service
+```
+```
+sudo systemctl daemon-reload
+sudo systemctl enable rpi-remote.service
+sudo systemctl start rpi-remote.service
+```
+
+## Edit config
+Config file path: ```~/.config/rpi_remote/config.ini```
+
+## Check logs
+```
+journalctl -fu rpi-remote
+```
---
+++ packages/rpi-remote/rpi-remote-1.1.1.tar.gz/rpi-remote-1.1.1/rpi_remote/rpi_remote.py
@@ -0,0 +1,124 @@
+import time
+import os
+import json
+import configparser
+import statistics
+import logging.handlers
+import requests
+import psutil
+from requests.exceptions import ConnectionError # pylint: disable=redefined-builtin
+from .forward import ClientForwarder
+
+
+class RpiRemoteClient: # pylint: disable=too-few-public-methods
+
+    CONFIG_FOLDER_PATH = os.path.join(os.path.expanduser('~'), ".config", "rpi_remote")
+    CONFIG_PATH = os.path.join(CONFIG_FOLDER_PATH, "config.ini")
+    DEFAULT_CONFIG = {
+        "connection": {
+            "host_address": "http://localhost:8080",
+            "period_time_sec": "30",
+            "client_name": "test_client",
+            "disk_path": "/media/HDD",
+        }
+    }
+
+    def __init__(self):
+        self._logger = self._init_logger()
+        self._config = self._load_config()
+
+    def _init_logger(self):
+        syslog = logging.handlers.SysLogHandler(address="/dev/log")
+        syslog.setFormatter(logging.Formatter('rpi-remote %(name)s: %(levelname)s %(message)s'))
+        logger = logging.getLogger("rpi-remote")
+        logger.addHandler(syslog)
+        logger.setLevel(logging.INFO)
+        return logger
+
+    def _load_config(self):
+        config = configparser.ConfigParser()
+        if config.read(self.CONFIG_PATH):
+            return config
+        return self._create_config()
+
+    def _create_config(self):
+        if not os.path.exists(self.CONFIG_FOLDER_PATH):
+            os.makedirs(self.CONFIG_FOLDER_PATH)
+        config = configparser.ConfigParser()
+        for section, configs in self.DEFAULT_CONFIG.items():
+            config[section] = {}
+            for config_key, config_value in configs.items():
+                config[section][config_key] = config_value
+        with open(self.CONFIG_PATH, 'w', encoding='utf-8') as f:
+            config.write(f)
+        return config
+
+    def _get_order(self):
+        url = f"{self._config['connection']['host_address']}/rpi/api/order"
+        client_name = self._config['connection']['client_name']
+        request = requests.get(url, headers={'name': client_name}, timeout=5)
+        return request.json()
+
+    def _send_metrics(self):
+        metrics = self._collect_metrics()
+        url = f"{self._config['connection']['host_address']}/rpi/api/metric"
+        client_name = self._config['connection']['client_name']
+        data = {
+            'name': client_name,
+            'uptime': metrics['uptime_hous'],
+            'cpu_usage': metrics['cpu_percent'],
+            'memory_usage': metrics['mem_percent'],
+            'disk_usage': metrics['disk_usage'],
+            'temperature': metrics['temperature'],
+        }
+        headers = {'Content-type': 'application/json'}
+        resp = requests.put(url, data=json.dumps(data), headers=headers, timeout=5)
+        print(resp.status_code, resp.content)
+
+    def _collect_metrics(self):
+        uptime_sec = time.time() - psutil.boot_time()
+        disk_path = self._config['connection']['disk_path'] if \
+            os.path.exists(self._config['connection']['disk_path']) else '/'
+        return {
+            'uptime_hous': int(uptime_sec/3600),
+            'mem_percent': int(psutil.virtual_memory().percent),
+            'cpu_percent': self._get_cpu_avarage_load(),
+            'disk_usage': int(psutil.disk_usage(disk_path).percent),
+            'temperature': self._get_cpu_temperature(),
+        }
+
+    @staticmethod
+    def _get_cpu_temperature():
+        try:
+            sensor_data = psutil.sensors_temperatures()
+            return int([statistics.mean([i.current for i in value])
+                        for key, value in sensor_data.items() if key.startswith('cpu')][0])
+        except (IndexError, AttributeError):
+            return -1
+
+    @staticmethod
+    def _get_cpu_avarage_load():
+        try:
+            return int([x / psutil.cpu_count() * 100
+                        for x in psutil.getloadavg()][1])  # 5 min load average
+        except IndexError:
+            return -1
+
+    def run(self):
+        self._logger.info("Starting rpi-remote client...")
+        while True:
+            try:
+                if data := self._get_order():
+                    forwarder = ClientForwarder(**data, logger=self._logger)
+                    forwarder.start()
+                self._send_metrics()
+            except ConnectionError as e:
+                self._logger.warning("Cannot connect to host: '%s'", e.request.url)
+            except Exception as e: # pylint: disable=broad-except
+                self._logger.error(e)
+            finally:
+                time.sleep(int(self._config['connection']['period_time_sec']))
+
+def main():
+    client = RpiRemoteClient()
+    client.run()
