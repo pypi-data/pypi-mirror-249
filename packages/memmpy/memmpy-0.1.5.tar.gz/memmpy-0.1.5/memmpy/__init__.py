0ff0db3721458615baa92f4c5cf1d710635d92ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:30 +0000

    Add package memmpy/0.1.5/memmpy-0.1.5.tar.gz

---
+++ packages/memmpy/memmpy-0.1.5.tar.gz/memmpy-0.1.5/memmpy/__init__.py
@@ -0,0 +1,29 @@
+from ._labels import hash_64bit
+from ._loader import (
+    Batched,
+    FastShuffled,
+    SequenceDict,
+    Sliced,
+    Subindexed,
+    split,
+    unwrap_recursively,
+)
+from ._subset import compute_cut_batched
+from ._vector import WriteVector, WriteVectorDict, read_vector
+
+__all__ = [
+    "hash_64bit",
+    "Batched",
+    "FastShuffled",
+    "SequenceDict",
+    "Sliced",
+    "Subindexed",
+    "split",
+    "unwrap_recursively",
+    "compute_cut_batched",
+    "WriteVector",
+    "WriteVectorDict",
+    "read_vector",
+]
+
+__version__ = "0.1.5"
