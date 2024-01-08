6d686ef5620e6ac5bb01fcc356a0f0e57c292c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:42 +0000

    Add package qibosoq/0.1.1/qibosoq-0.1.1-py3-none-any.whl

---
+++ packages/qibosoq/qibosoq-0.1.1-py3-none-any.whl/qibosoq/programs/pulse_sequence.py
@@ -0,0 +1,22 @@
+"""Program used by qibosoq to execute sequences."""
+
+import logging
+
+from qick import AveragerProgram
+
+import qibosoq.configuration as qibosoq_cfg
+from qibosoq.programs.flux import FluxProgram
+
+logger = logging.getLogger(qibosoq_cfg.MAIN_LOGGER_NAME)
+
+
+class ExecutePulseSequence(FluxProgram, AveragerProgram):
+    """Class to execute arbitrary PulseSequences."""
+
+    def initialize(self):
+        """Declre nyquist zones for all the DACs and all the readout frequencies.
+
+        Function called by AveragerProgram.__init__.
+        """
+        self.declare_zones_and_ro(self.pulse_sequence)
+        self.sync_all(self.wait_initialize)
