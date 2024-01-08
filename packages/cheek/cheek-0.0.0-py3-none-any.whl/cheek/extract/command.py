be48961e46c438a762d652a444858aa69a7eb832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:47 +0000

    Add package cheek/0.0.0/cheek-0.0.0-py3-none-any.whl

---
+++ packages/cheek/cheek-0.0.0-py3-none-any.whl/cheek/extract/command.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+import bs4
+
+from .arg import Arg
+
+TEMPLATE = '''
+@register_command
+class {command_name}(Command):
+    """{doc}
+    """
+    # {args}
+    pass
+
+'''
+
+
+@dataclass
+class Command:
+    name: str
+    args: list[Arg]
+    doc: str
+
+    @classmethod
+    def from_spec(cls, spec: dict):
+        command_name = spec["id"]
+        args = [Arg.from_spec(param) for param in spec["params"]]
+        doc = spec["tip"]
+        return cls(command_name, args, doc.strip())
+
+    def class_string(self):
+        return TEMPLATE.format(command_name=self.name, doc=self.doc, args=self.args)
+
