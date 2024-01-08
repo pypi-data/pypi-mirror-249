4bb51f802eb71b40e0f316516066778147729405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:06:08 +0000

    Add package cutility/0.1.3/cutility-0.1.3.tar.gz

---
+++ packages/cutility/cutility-0.1.3.tar.gz/cutility-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="cutility",
+    version="0.1.3",
+    author="Sagar Sarkale",
+    author_email="sagarsarkale.work@gmail.com",
+    description="Common Utility functions for development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sagarshq/cutility",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "simplejson",
+        # "jsonl",
+        "pyyaml",
+        # Add other dependencies here if needed
+    ],
+)
