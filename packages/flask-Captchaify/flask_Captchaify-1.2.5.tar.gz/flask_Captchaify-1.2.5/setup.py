ff31e7404a718497903c77fcb5a3315a4e98bf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 03:04:23 +0000

    Add package flask-Captchaify/1.2.5/flask_Captchaify-1.2.5.tar.gz

---
+++ packages/flask-Captchaify/flask_Captchaify-1.2.5.tar.gz/flask_Captchaify-1.2.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_Captchaify',
+    version='1.2.5',
+    description='Protect against bots and DDoS attacks',
+    long_description='A DDoS defense system for flask applications, first sends users to a captcha page without a javascript script and creates a confirmation cookie/url arg after the captcha.',
+    author='TN3W',
+    author_email='tn3wA8xxfuVMs2@proton.me',
+    url='https://github.com/tn3w/flask_Captchaify',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={
+        'flask_Captchaify': ['data/*', 'templates/*']
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='flask, Python, Bot, Captcha, DDoS',
+    install_requires=[
+        'cryptography',
+        'Flask',
+        'googletrans==3.1.0a0',
+        'beautifulsoup4',
+        'ipaddress',
+        'Jinja2',
+        'Pillow',
+        'requests',
+        'bs4',
+        'captcha',
+    ],
+)
