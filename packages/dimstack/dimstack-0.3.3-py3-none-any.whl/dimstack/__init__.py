58bfd3e29b45fe687bdcea6509eeb478f8057aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:30 +0000

    Add package dimstack/0.3.3/dimstack-0.3.3-py3-none-any.whl

---
+++ packages/dimstack/dimstack-0.3.3-py3-none-any.whl/dimstack/__init__.py
@@ -0,0 +1,16 @@
+# from . import eval
+from . import dim
+from . import stats
+from . import display
+from . import tolerance
+from . import utils
+from . import dist
+from . import plot
+
+from .dim import Statistical, Basic, Stack, Spec
+from .dist import Normal, Uniform  # , Triangular, LogNormal, Weibull, Exponential, Gamma, Beta, Gumbel, Frechet
+from . import tolerance as tol
+
+__all__ = ["dim", "stats", "display", "tolerance", "utils", "dist", "plot"]
+
+# __version__ = "0.1.0"
