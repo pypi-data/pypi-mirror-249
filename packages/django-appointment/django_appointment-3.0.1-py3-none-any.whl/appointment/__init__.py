1c64cb03832fefecebcb6e323a7cb3cde7487968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:00 +0000

    Add package django-appointment/3.0.1/django_appointment-3.0.1-py3-none-any.whl

---
+++ packages/django-appointment/django_appointment-3.0.1-py3-none-any.whl/appointment/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Adams Pierre David"
+__author_email__ = "adamspd.developer@gmail.com"
+__author_website__ = "https://adamspierredavid.com/"
+__description__ = "Managing appointment scheduling with customizable slots, staff features, and conflict handling."
+__package_name__ = "django-appointment"
+__url__ = "https://github.com/adamspd/django-appointment"
+__version__ = "3.0.1"
+__test_version__ = False
