3727daf18f1d4e8d749dd8cffec0c9d68a44d899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:15 +0000

    Add package pyodps/0.11.5.post0/pyodps-0.11.5.post0-cp37-cp37m-win32.whl

---
+++ packages/pyodps/pyodps-0.11.5.post0-cp37-cp37m-win32.whl/odps/tunnel/pdio/pdwriter.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 1999-2022 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ..io.stream import get_compress_stream
+from ... import options
+from ...compat import six
+
+try:
+    from .pdwriter_c import BasePandasWriter
+except ImportError:
+    BasePandasWriter = None
+
+
+if BasePandasWriter:
+    class TunnelPandasWriter(BasePandasWriter):
+        def __init__(self, schema, request_callback, compress_option=None):
+            self._req_io = request_callback(options.chunk_size)
+            out = get_compress_stream(self._req_io, compress_option)
+            super(TunnelPandasWriter, self).__init__(schema, out)
+            self._req_io.open()
+
+        def write(self, data, columns=None, limit=-1, dim_offsets=None):
+            super(TunnelPandasWriter, self).write(
+                data, columns=columns, limit=limit, dim_offsets=dim_offsets
+            )
+
+        write.__doc__ = BasePandasWriter.write.__doc__
+
+        def close(self):
+            super(TunnelPandasWriter, self).close()
+            self._req_io.close()
