052ada1d70c464157826d1f50fa688215d64ec3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 04:04:37 +0000

    Add package oztimur-probability/0.2/oztimur_probability-0.2.tar.gz

---
+++ packages/oztimur-probability/oztimur_probability-0.2.tar.gz/oztimur_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='oztimur_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['oztimur_probability'],
+	  author = 'Irem Oztimur',
+	  author_email = 'iremoztimur@gmail.com',
+      zip_safe=False)
