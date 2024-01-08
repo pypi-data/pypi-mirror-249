9786ac8961dd0705b211fff254a78e0ed983d8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 15:04:16 +0000

    Add package my-minmax/1.0/my_minmax-1.0.tar.gz

---
+++ packages/my-minmax/my_minmax-1.0.tar.gz/my_minmax-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+name="my_minmax",
+version="1.0",
+description="edited by myself",
+author="lius",
+py_modules=["my_packages.getMax","my_packages.getMin"]
+)
