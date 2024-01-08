f683a0932f8842e3ad8e2a71989c3fc84cdb0d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-08 14:05:47 +0000

    Add package nbprint/0.1.3/nbprint-0.1.3.tar.gz

---
+++ packages/nbprint/nbprint-0.1.3.tar.gz/nbprint-0.1.3/js/build.mjs
@@ -0,0 +1,76 @@
+import {NodeModulesExternal} from "@finos/perspective-esbuild-plugin/external.js";
+import { build } from "@finos/perspective-esbuild-plugin/build.js";
+import { BuildCss } from "@prospective.co/procss/target/cjs/procss.js";
+import cpy from "cpy";
+import fs from "fs";
+import path_mod from "path";
+
+const BUILD = [
+  {
+    define: {
+      global: "window",
+    },
+    entryPoints: ["src/js/index.js"],
+    plugins: [NodeModulesExternal()],
+    format: "esm",
+    loader: {
+      ".css": "text",
+      ".html": "text",
+    },
+    outfile: "../nbprint/extension/index.js",
+  },
+  {
+    define: {
+      global: "window",
+    },
+    entryPoints: ["src/js/nbconvert.js"],
+    plugins: [],
+    format: "esm",
+    loader: {
+      ".css": "text",
+      ".html": "text",
+    },
+    outfile: "../nbprint/templates/nbprint/static/embedded.js",
+  },
+  {
+    define: {
+      global: "window",
+    },
+    entryPoints: ["src/js/nbconvert.js"],
+    plugins: [],
+    format: "esm",
+    loader: {
+      ".css": "text",
+      ".html": "text",
+    },
+    outfile: "../nbprint/voila/static/embedded.js",
+  },
+];
+
+function add(builder, path) {
+  builder.add(
+    path,
+    fs.readFileSync(path_mod.join("./src/less", path)).toString(),
+  );
+}
+
+async function compile_css() {
+  fs.mkdirSync("../nbprint/extension", { recursive: true });
+  fs.mkdirSync("../nbprint/templates/nbprint/static", { recursive: true });
+  const builder1 = new BuildCss("");
+  add(builder1, "./index.less");
+  fs.writeFileSync(
+    "../nbprint/extension/index.css",
+    builder1.compile().get("index.css"),
+  );
+}
+
+async function build_all() {
+  await compile_css();
+  await cpy("./src/css/*", "../nbprint/extension/");
+  await cpy("./src/css/*", "../nbprint/templates/nbprint/static/");
+  await cpy("./src/css/*", "../nbprint/voila/static/");
+  await Promise.all(BUILD.map(build)).catch(() => process.exit(1));
+}
+
+build_all();
---
+++ packages/nbprint/nbprint-0.1.3.tar.gz/nbprint-0.1.3/nbprint/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.3"
+
+from .config import *
+from .generate import generate
+from .utils import *
