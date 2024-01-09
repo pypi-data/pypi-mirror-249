70913d835c64e5ea005aaa544d502c08ca4441e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:05:12 +0000

    Add package herbarium/0.4.0/herbarium-0.4.0.tar.gz

---
+++ packages/herbarium/herbarium-0.4.0.tar.gz/herbarium-0.4.0/herbarium/__main__.py
@@ -0,0 +1,50 @@
+import typer
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from .registry import issue_services, presenters, stackers
+
+app = typer.Typer()
+
+issue_service = issue_services["Github"]()
+issue_presenter = presenters["Default"]()
+stacker = stackers["Graphite"]()
+
+
+@app.command()
+def new():
+    with Progress(
+        SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True
+    ) as progress:
+        progress.add_task("Getting issues assigned to you", start=True)
+        my_issues = issue_service.get_issues_assigned_to_me()
+    selected_issue = issue_presenter.select_issue_dialog(my_issues)
+    stacker.create_stack_from_trunk(selected_issue)
+
+
+@app.command()
+def next():  # noqa: A001 [Shadowing python built-in]
+    with Progress(
+        SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True
+    ) as progress:
+        progress.add_task("Getting issues assigned to you", start=True)
+        my_issues = issue_service.get_issues_assigned_to_me()
+
+    selected_issue = issue_presenter.select_issue_dialog(my_issues)
+    stacker.add_to_stack(selected_issue)
+
+
+@app.command()
+def submit(automerge: bool = False):
+    with Progress(
+        SpinnerColumn(), TextColumn("[progress.description]{task.description} "), transient=True
+    ) as progress:
+        progress.add_task("Submitting stack", start=True)
+        progress.console.print(stacker.submit_stack(automerge=automerge))
+        print(":rocket: [bold green]Stack submitted![/bold green]")
+
+    stacker.status()
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/herbarium/herbarium-0.4.0.tar.gz/herbarium-0.4.0/herbarium/stacker.py
@@ -0,0 +1,73 @@
+from typing import Protocol
+
+from .issue_service import Issue
+from .subprocess_utils import interactive_cmd
+
+
+def sanitise_issue_title(issue_title: str) -> str:
+    char2replacement = {
+        " ": "-",
+        ":": "/",
+        ",": "",
+        "'": "",
+        '"': "",
+        "(": "",
+        ")": "",
+        "[": "",
+        "": "",
+        "`": "",
+        ">": "",
+        "<": "",
+        "=": "",
+    }
+
+    for character, replacement in char2replacement.items():
+        issue_title = issue_title.replace(character, replacement)
+
+    issue_title = issue_title.replace("--", "-")
+    return issue_title
+
+
+class Stacker(Protocol):
+    def create_stack_from_trunk(self, issue: Issue):
+        ...
+
+    def add_to_stack(self, issue: Issue):
+        ...
+
+    def submit_stack(self, automerge: bool):
+        ...
+
+    def status(self):
+        ...
+
+
+class Graphite(Stacker):
+    def _sync(self):
+        interactive_cmd("gt sync --force --show-delete-progress")
+
+    def create_stack_from_trunk(self, issue: Issue):
+        self._sync()
+        interactive_cmd("git checkout main")
+        interactive_cmd("git pull")
+        self.add_to_stack(issue)
+
+    def add_to_stack(self, issue: Issue):
+        self._sync()
+        sanitised_title = sanitise_issue_title(issue.title)
+        branch_title = f"{issue.entity_id}-{sanitised_title}"
+        first_commit_str = f"'{issue.title}\n\nFixes #{issue.entity_id}'"
+        interactive_cmd(f"gt create {branch_title} --all -m {first_commit_str}")
+        interactive_cmd(f"git commit --allow-empty -m {first_commit_str}")
+
+    def submit_stack(self, automerge: bool):
+        self._sync()
+        submit_command = "gt submit --no-edit --publish"
+
+        if automerge:
+            submit_command += " --merge-when-ready"
+
+        interactive_cmd(submit_command)
+
+    def status(self):
+        interactive_cmd("gt log short")
