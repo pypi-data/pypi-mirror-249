385289381d5c6ed02b41d8127851563620fdebb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:05:16 +0000

    Add package kafka-broker-demoter/2.0.0/kafka_broker_demoter-2.0.0.tar.gz

---
+++ packages/kafka-broker-demoter/kafka_broker_demoter-2.0.0.tar.gz/kafka_broker_demoter-2.0.0/kafka_broker_demoter/exceptions.py
@@ -0,0 +1,26 @@
+class SetBrokerThrottleError(Exception):
+    pass
+
+
+class SetTopicThrottleError(Exception):
+    pass
+
+
+class RecordNotFoundError(Exception):
+    pass
+
+
+class BrokerStatusError(Exception):
+    pass
+
+
+class TriggerLeaderElectionError(Exception):
+    pass
+
+
+class ProduceRecordError(Exception):
+    pass
+
+
+class ChangeReplicaAssignmentError(Exception):
+    pass
