1e1cce922f4018091a748f4ba38eef12b86a09d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:05:03 +0000

    Add package test32/0.1/test32-0.1.tar.gz

---
+++ packages/test32/test32-0.1.tar.gz/test32-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test32",
+    version="0.1",
+    author="newdev12",
+    author_email="sargizbm@gmail.com",
+    description="A small example package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
