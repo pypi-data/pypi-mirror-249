daf3f086c3c5bedcd75f28741267fe44bf196bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:52 +0000

    Add package aiogram-uptodate/0.5.1/aiogram_uptodate-0.5.1-py3-none-any.whl

---
+++ packages/aiogram-uptodate/aiogram_uptodate-0.5.1-py3-none-any.whl/aiogram_uptodate/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from aiogram_uptodate.common import get_user_locale
+from aiogram_uptodate.simple_calendar import SimpleCalendar
+from aiogram_uptodate.dialog_calendar import DialogCalendar
+from aiogram_uptodate.schemas import (
+    SimpleCalendarCallback,
+    DialogCalendarCallback,
+    CalendarLabels,
+)
