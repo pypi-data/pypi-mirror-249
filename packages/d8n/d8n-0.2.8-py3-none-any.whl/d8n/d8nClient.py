8e388359e74cff24a72ab7b70535da8727ab453f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:18 +0000

    Add package d8n/0.2.8/d8n-0.2.8-py3-none-any.whl

---
+++ packages/d8n/d8n-0.2.8-py3-none-any.whl/d8n/d8nClient.py
@@ -0,0 +1,84 @@
+import http.client
+import mimetypes
+from codecs import encode
+import requests
+import json
+
+class d8nClient:
+    def __init__(self, api_key):
+        self.API_key = api_key
+        self._url = 'engisense.com'
+
+    def from_local_file(self, path, project_id = None):
+        conn = http.client.HTTPSConnection(self._url)
+        dataList = []
+
+        boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+        dataList.append(encode('--' + boundary))
+        dataList.append(encode('Content-Disposition: form-data; name=file; filename={0}'.format(path)))
+
+        fileType = mimetypes.guess_type(path)[0] or 'application/octet-stream'
+        dataList.append(encode('Content-Type: {}'.format(fileType)))
+        dataList.append(encode(''))
+    
+        with open(path, 'rb') as f:
+            dataList.append(f.read())
+
+        dataList.append(encode('--'+boundary+'--'))
+        dataList.append(encode(''))
+        body = b'\r\n'.join(dataList)
+        payload = body
+        headers = {
+            'API-KEY': self.API_key,
+            'Content-type': 'multipart/form-data; boundary={}'.format(boundary),
+        }
+        
+        if project_id is not None:
+            headers['projectId']=project_id
+            
+        conn.request("POST", "/api/analysis", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = (data.decode("utf-8"))
+        return json.loads(data)
+
+    def fetch_status(self, id):
+        url = "https://{0}/api/get_status?id={1}".format(self._url,id)
+
+        payload={}
+        files={}
+        headers = {
+            'API-KEY': self.API_key
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+        return json.loads(response.content.decode('utf-8'))
+
+
+    def get_completed(self, id):
+        url = "https://{0}/api/completed?id={1}".format(self._url,id)
+        payload={}
+        headers = {
+            'API-KEY': self.API_key
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        return json.loads(response.content.decode('utf-8'))
+    
+    def download_entry(self, id, format = 'yolo'):
+        url = "https://{0}/api/completed?id={1}".format(self._url,id)
+        
+        headers = {
+            'API-KEY': self.API_key,
+        }
+        
+        body = {
+            'only_id':id,
+            'format': format
+        }
+        
+        response = requests.request("GET", url, headers=headers, data=body)
+
+        return response.content
