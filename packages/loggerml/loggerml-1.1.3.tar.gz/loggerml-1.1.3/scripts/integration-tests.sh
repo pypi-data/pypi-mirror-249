682b753e1608a9d9f2aa42da9d1e2a82f5e53f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:05:11 +0000

    Add package loggerml/1.1.3/loggerml-1.1.3.tar.gz

---
+++ packages/loggerml/loggerml-1.1.3.tar.gz/loggerml-1.1.3/scripts/integration-tests.sh
@@ -0,0 +1,11 @@
+echo "*********** Integration tests ***********"
+echo "*** logger ***"
+python tests/integration/inte_logger.py
+echo "*** regex ***"
+python tests/integration/inte_regex.py
+echo "*** tqdm ***"
+python tests/integration/inte_tqdm.py
+echo "*** 2 loggers ***"
+python tests/integration/inte_two_loggers.py
+echo "*** multi logs ***"
+python tests/integration/inte_multi_logs.py
