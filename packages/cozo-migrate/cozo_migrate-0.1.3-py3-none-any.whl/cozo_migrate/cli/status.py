26d11443c688b435620c21b3375a3e7408c8aeb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 04:05:10 +0000

    Add package cozo-migrate/0.1.3/cozo_migrate-0.1.3-py3-none-any.whl

---
+++ packages/cozo-migrate/cozo_migrate-0.1.3-py3-none-any.whl/cozo_migrate/cli/status.py
@@ -0,0 +1,24 @@
+from rich import print
+import typer
+
+from ..queries import get_latest_migration
+from ..types import Migration
+from ..utils.console import fail
+
+from .main import app
+
+
+@app.command()
+def status(ctx: typer.Context):
+    """Display the current migration status."""
+
+    # Validate client
+    ctx.obj.check_client()
+
+    client = ctx.obj.client
+
+    last_migration: Migration = get_latest_migration(client)
+    if not last_migration:
+        fail("No migrations have been applied yet.")
+
+    print(last_migration.to_table())
