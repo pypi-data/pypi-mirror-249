3846e2bf75fc0af60ac54f21250c5e2e12e4a02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:59 +0000

    Add package dsw-storage/4.1.2/dsw-storage-4.1.2.tar.gz

---
+++ packages/dsw-storage/dsw-storage-4.1.2.tar.gz/dsw-storage-4.1.2/dsw/storage/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.1.2~ce4aacf',
+    built_at='2024-01-08 13:50:01Z',
+    sha='ce4aacf8b51912543720f49d8af2d89d1fb41fd2',
+    branch='HEAD',
+    tag='v4.1.2',
+)
