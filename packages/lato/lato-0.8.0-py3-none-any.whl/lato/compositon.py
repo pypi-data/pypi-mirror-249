86f38aa4c012b80cd9a69e14175be1142358f4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:19 +0000

    Add package lato/0.8.0/lato-0.8.0-py3-none-any.whl

---
+++ packages/lato/lato-0.8.0-py3-none-any.whl/lato/compositon.py
@@ -0,0 +1,26 @@
+from collections.abc import Callable
+from functools import partial, reduce
+from operator import add, or_
+from typing import Any, Optional
+
+from mergedeep import Strategy, merge
+
+additive_merge = partial(merge, strategy=Strategy.TYPESAFE_ADDITIVE)
+
+
+def compose(values: tuple[Any, ...], operator: Optional[Callable] = None):
+    first = values[0]
+    if len(values) == 1:
+        return first
+
+    if operator is not None:
+        operators = [operator]
+    else:
+        operators = [additive_merge, or_, add]
+
+    for op in operators:
+        try:
+            return reduce(op, values)
+        except TypeError:
+            pass
+    raise TypeError("Could not compose", values)
