a05f19092b62ca286cf9b9747ee443198b3bef9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:19 +0000

    Add package haystack-ai/2.0.0b4/haystack_ai-2.0.0b4-py3-none-any.whl

---
+++ packages/haystack-ai/haystack_ai-2.0.0b4-py3-none-any.whl/haystack/components/converters/markdown.py
@@ -0,0 +1,90 @@
+import logging
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Union
+
+from tqdm import tqdm
+
+from haystack import Document, component
+from haystack.dataclasses import ByteStream
+from haystack.lazy_imports import LazyImport
+from haystack.components.converters.utils import get_bytestream_from_source
+
+with LazyImport("Run 'pip install markdown-it-py mdit_plain'") as markdown_conversion_imports:
+    from markdown_it import MarkdownIt
+    from mdit_plain.renderer import RendererPlain
+
+
+logger = logging.getLogger(__name__)
+
+
+@component
+class MarkdownToDocument:
+    """
+    Converts a Markdown file into a text Document.
+
+    Usage example:
+    ```python
+    from haystack.components.converters.markdown import MarkdownToDocument
+
+    converter = MarkdownToDocument()
+    results = converter.run(sources=["sample.md"])
+    documents = results["documents"]
+    print(documents[0].content)
+    # 'This is a text from the markdown file.'
+    ```
+    """
+
+    def __init__(self, table_to_single_line: bool = False, progress_bar: bool = True):
+        """
+        :param table_to_single_line: Convert contents of the table into a single line. Defaults to False.
+        :param progress_bar: Show a progress bar for the conversion. Defaults to True.
+        """
+        markdown_conversion_imports.check()
+
+        self.table_to_single_line = table_to_single_line
+        self.progress_bar = progress_bar
+
+    @component.output_types(documents=List[Document])
+    def run(self, sources: List[Union[str, Path, ByteStream]], meta: Optional[List[Dict[str, Any]]] = None):
+        """
+        Reads text from a markdown file and executes optional preprocessing steps.
+
+        :param sources: A list of markdown data sources (file paths or binary objects)
+        :param meta: Optional list of metadata to attach to the Documents.
+          The length of the list must match the number of paths. Defaults to `None`.
+        :return: A dictionary containing a list of Document objects under the 'documents' key.
+        """
+        parser = MarkdownIt(renderer_cls=RendererPlain)
+        if self.table_to_single_line:
+            parser.enable("table")
+
+        documents = []
+
+        if meta is None:
+            meta = [{}] * len(sources)
+        elif len(sources) != len(meta):
+            raise ValueError("The length of the metadata list must match the number of sources.")
+
+        for source, metadata in tqdm(
+            zip(sources, meta),
+            total=len(sources),
+            desc="Converting markdown files to Documents",
+            disable=not self.progress_bar,
+        ):
+            try:
+                bytestream = get_bytestream_from_source(source)
+            except Exception as e:
+                logger.warning("Could not read %s. Skipping it. Error: %s", source, e)
+                continue
+            try:
+                file_content = bytestream.data.decode("utf-8")
+                text = parser.render(file_content)
+            except Exception as conversion_e:
+                logger.warning("Failed to extract text from %s. Skipping it. Error: %s", source, conversion_e)
+                continue
+
+            merged_metadata = {**bytestream.meta, **metadata}
+            document = Document(content=text, meta=merged_metadata)
+            documents.append(document)
+
+        return {"documents": documents}
---
+++ packages/haystack-ai/haystack_ai-2.0.0b4-py3-none-any.whl/haystack/components/converters/tika.py
@@ -0,0 +1,83 @@
+import logging
+from pathlib import Path
+from typing import List, Union, Dict, Any, Optional
+import io
+
+from haystack.lazy_imports import LazyImport
+from haystack import component, Document
+from haystack.dataclasses import ByteStream
+from haystack.components.converters.utils import get_bytestream_from_source
+
+
+with LazyImport("Run 'pip install tika'") as tika_import:
+    from tika import parser as tika_parser
+
+logger = logging.getLogger(__name__)
+
+
+@component
+class TikaDocumentConverter:
+    """
+    A component for converting files of different types (pdf, docx, html, etc.) to Documents.
+    This component uses [Apache Tika](https://tika.apache.org/) for parsing the files and, therefore,
+    requires a running Tika server.
+
+    The easiest way to run Tika is to use Docker: `docker run -d -p 127.0.0.1:9998:9998 apache/tika:latest`.
+    For more options on running Tika on Docker,
+    see the [documentation](https://github.com/apache/tika-docker/blob/main/README.md#usage).
+
+    Usage example:
+    ```python
+    from haystack.components.converters.tika import TikaDocumentConverter
+
+    converter = TikaDocumentConverter()
+    results = converter.run(sources=["sample.docx", "my_document.rtf", "archive.zip"])
+    documents = results["documents"]
+    print(documents[0].content)
+    # 'This is a text from the docx file.'
+    ```
+    """
+
+    def __init__(self, tika_url: str = "http://localhost:9998/tika"):
+        """
+        Create a TikaDocumentConverter component.
+
+        :param tika_url: URL of the Tika server. Default: `"http://localhost:9998/tika"`
+        """
+        tika_import.check()
+        self.tika_url = tika_url
+
+    @component.output_types(documents=List[Document])
+    def run(self, sources: List[Union[str, Path, ByteStream]], meta: Optional[List[Dict[str, Any]]] = None):
+        """
+        Convert files to Documents.
+
+        :param sources: List of file paths or ByteStream objects.
+        :param meta: Optional list of metadata to attach to the Documents.
+          The length of the list must match the number of sources. Defaults to `None`.
+        :return: A dictionary containing a list of Document objects under the 'documents' key.
+        """
+
+        documents = []
+
+        if meta is None:
+            meta = [{}] * len(sources)
+        elif len(sources) != len(meta):
+            raise ValueError("The length of the metadata list must match the number of sources.")
+
+        for source, metadata in zip(sources, meta):
+            try:
+                bytestream = get_bytestream_from_source(source)
+            except Exception as e:
+                logger.warning("Could not read %s. Skipping it. Error: %s", source, e)
+                continue
+            try:
+                text = tika_parser.from_buffer(io.BytesIO(bytestream.data), serverEndpoint=self.tika_url)["content"]
+            except Exception as conversion_e:
+                logger.warning("Failed to extract text from %s. Skipping it. Error: %s", source, conversion_e)
+                continue
+
+            merged_metadata = {**bytestream.meta, **metadata}
+            document = Document(content=text, meta=merged_metadata)
+            documents.append(document)
+        return {"documents": documents}
