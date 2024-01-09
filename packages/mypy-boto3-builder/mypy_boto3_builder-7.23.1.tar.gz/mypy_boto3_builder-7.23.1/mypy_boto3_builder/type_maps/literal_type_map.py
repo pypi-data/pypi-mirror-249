f757e088db7c050b591b5e3a867d4d36727c7d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:36 +0000

    Add package mypy-boto3-builder/7.23.1/mypy_boto3_builder-7.23.1.tar.gz

---
+++ packages/mypy-boto3-builder/mypy_boto3_builder-7.23.1.tar.gz/mypy_boto3_builder-7.23.1/mypy_boto3_builder/type_maps/literal_type_map.py
@@ -0,0 +1,28 @@
+"""
+String to type annotation map to replace overriden botocore literals.
+"""
+
+from mypy_boto3_builder.service_name import ServiceName
+
+LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {}
+
+
+def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> list[str] | None:
+    """
+    Get stub type for botocore literal.
+
+    Arguments:
+        service_name -- Service name.
+        literal_name -- Target Literal name.
+
+    Returns:
+        Literal children or None.
+    """
+    if service_name not in LITERAL_TYPE_MAP:
+        return None
+
+    service_literal_type_map = LITERAL_TYPE_MAP[service_name]
+    if literal_name not in service_literal_type_map:
+        return None
+
+    return service_literal_type_map[literal_name]
