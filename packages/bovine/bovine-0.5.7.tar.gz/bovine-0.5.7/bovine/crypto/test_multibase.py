66f3873f99e967060e61690e45a54170b8fcd6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:39 +0000

    Add package bovine/0.5.7/bovine-0.5.7.tar.gz

---
+++ packages/bovine/bovine-0.5.7.tar.gz/bovine-0.5.7/bovine/crypto/test_multibase.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 helge
+
+from .multibase import multibase_58btc_encode
+
+
+def test_multibase_58btc_encode():
+    message = "secret".encode("utf-8")
+    result = multibase_58btc_encode(message)
+
+    assert result == "zzTuS2beK"
