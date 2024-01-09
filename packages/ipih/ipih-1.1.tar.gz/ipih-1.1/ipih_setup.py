0d9a79739791c707443101e595eff80bf082f968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:56 +0000

    Add package ipih/1.1/ipih-1.1.tar.gz

---
+++ packages/ipih/ipih-1.1.tar.gz/ipih-1.1/ipih_setup.py
@@ -0,0 +1,32 @@
+import shutil
+import os
+from setuptools import setup
+
+NAME: str = "ipih"
+
+folder = "//pih/facade/dist/ipih"
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as error:
+        print("Failed to delete %s. Reason: %s" % (file_path, error))
+
+setup(
+    name=NAME,
+    version="1.01",
+    description="PIH import library",
+    long_description_content_type="text/markdown",
+    url="https://pacifichosp.com/",
+    author="Nikita Karachentsev",
+    author_email="it@pacifichosp.com",
+    license="MIT",
+    classifiers=[],
+    packages=[NAME],
+    include_package_data=True,
+    install_requires=[
+    ],
+)
