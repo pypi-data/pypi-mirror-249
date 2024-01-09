4e038d7001d57bd1a9d0c149abd6cc7a3ef04afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-09 03:04:16 +0000

    Add package mns-scheduler/1.0.0.4/mns_scheduler-1.0.0.4-py3-none-any.whl

---
+++ packages/mns-scheduler/mns_scheduler-1.0.0.4-py3-none-any.whl/mns_scheduler/ipo/auto_ipo_buy_api.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 17
+project_path = file_path[0:end]
+sys.path.append(project_path)
+
+
+def auto_ipo_buy():
+    return None
